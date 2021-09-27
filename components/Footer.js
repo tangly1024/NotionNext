5c46d7f7f0d8a5feded98659d0f56eccbd08f9d3
tangly1024 <tlyong1992@hotmail.com>
2021-09-27 17:40:06 +0800

    🐾‍ 修改Footer样式

--- components/Footer.js
+++ components/Footer.js
@@ -8,13 +8,41 @@ const Footer = ({ fullWidth = true }) => {
   const from = +BLOG.since
   return (
     <footer
-      className='p-5 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400 bottom-0'
+      className='p-5 flex-shrink-0 m-auto w-full text-gray-500 dark:text-gray-400'
     >
-      <SocialButton/>
-      <div className='text-sm'>
-        <span className='fa fa-shield leading-6'><a href='https://beian.miit.gov.cn/' className='ml-1'>闽ICP备20010331号</a></span>
-        <br/>
+      <div className='py-2'>
+        <span className='text-xl border-b-2'>菜单</span>
+        <ul className='py-5 leading-8'>
+          <li><a className='fa fa-info hover:underline' href='/article/about' id='about'><span
+            className='ml-2'>关于本站</span></a></li>
+          <li><a className='fa fa-rss hover:underline' href='/feed' target='_blank' id='feed'><span
+            className='ml-2'>RSS订阅</span></a></li>
+        </ul>
+      </div>
+
+      <div className='py-2'>
+        <span className='text-xl border-b-2'>联系我</span>
+        <div className='py-4'>
+          <SocialButton />
+        </div>
+      </div>
+
+      <hr />
+      <div className='text-sm py-10 text-gray-400'>
+        <span className='fa fa-shield leading-6'><a href='https://beian.miit.gov.cn/'
+                                                    className='ml-1'>闽ICP备20010331号</a></span>
+        <br />
         <span className='fa fa-copyright leading-6'> {from === y || !from ? y : `${from} - ${y}`} {BLOG.author} </span>
+        <br />
+        <span id='busuanzi_container_site_pv' className='hidden'>
+        <a href='https://www.cnzz.com/stat/website.php?web_id=1279970751' target='_blank'
+           id='busuanzi_container_site_pv'
+           className='fa fa-user' rel='noreferrer'> pv <span id='busuanzi_value_site_pv'></span></a>
+        </span>
+        <span id='busuanzi_container_site_uv' className='hidden'><span className='s'> | </span>
+        <a href='http://tongji.baidu.com/web/10000363165/overview/index?siteId=16809429' target='_blank'
+           className='fa fa-eye' rel='noreferrer'> uv <span id='busuanzi_value_site_uv'></span></a>
+        </span>
       </div>
     </footer>
   )
--- components/SocialButton.js
+++ components/SocialButton.js
@@ -2,9 +2,7 @@ import React from 'react'
 
 const SocialButton = () => {
   return <>
-    <div className='space-x-3 text-xl'>
-      <a className='fa fa-rss hover:underline' href='/feed' target='_blank' id='feed'/>
-      <a className='fa fa-info hover:underline mx-1' href='/article/about' id='about'/>
+    <div className='space-x-3 text-2xl'>
       <a className='fa fa-github' target='_blank' rel='noreferrer' title={'github'}
          href={'https://github.com/tangly1024'} />
       <a className='fa fa-twitter' target='_blank' rel='noreferrer' title={'twitter'}
@@ -12,16 +10,6 @@ const SocialButton = () => {
       <a className='fa fa-telegram' href={'https://t.me/tangly_1024'} title={'telegram'} />
       <a className='fa fa-weibo' target='_blank' rel='noreferrer' title={'weibo'}
          href={'http://weibo.com/tangly1024'} />
-      <span id='busuanzi_container_site_pv' className='hidden'><span className='s'> | </span>
-        <a href='https://www.cnzz.com/stat/website.php?web_id=1279970751' target='_blank'
-           id='busuanzi_container_site_pv'
-           className='fa fa-user' rel='noreferrer'> pv <span id='busuanzi_value_site_pv'></span></a>
-        </span>
-
-      <span id='busuanzi_container_site_uv' className='hidden'><span className='s'> | </span>
-        <a href='http://tongji.baidu.com/web/10000363165/overview/index?siteId=16809429' target='_blank'
-           className='fa fa-eye' rel='noreferrer'> uv <span id='busuanzi_value_site_uv'></span></a>
-        </span>
     </div>
   </>
 }
