9156095a79a991229a24d24a72bb8c19dce70608
tangly1024 <tlyong1992@hotmail.com>
2021-10-13 12:45:34 +0800

    微调

--- components/Container.js
+++ components/Container.js
@@ -1,11 +1,36 @@
 import PropTypes from 'prop-types'
+import React, { useEffect } from 'react'
+import CommonHead from '@/components/CommonHead'
+import TopNav from '@/components/TopNav'
 
-const Container = ({ children, layout, fullWidth, ...customMeta }) => {
+const Container = ({ children, layout, fullWidth, tags, meta, ...customMeta }) => {
+  let windowTop = 0
+  // 监听滚动
+  useEffect(() => {
+    function scrollTrigger () {
+      const scrollS = window.scrollY
+      const nav = document.querySelector('#sticky-nav')
+      const tagsBar = document.querySelector('#tags-bar')
+      console.log(windowTop, scrollS)
+      if (scrollS >= windowTop) {
+        nav && nav.classList.add('-mt-16')
+        tagsBar && tagsBar.classList.add('-mt-32')
+        windowTop = scrollS
+      } else {
+        nav && nav.classList.remove('-mt-16')
+        tagsBar && tagsBar.classList.remove('-mt-32')
+        windowTop = scrollS
+      }
+    }
+
+    window.addEventListener('scroll', scrollTrigger)
+  })
   return (
-    <div>
-      {/* 公共头 */}
+    <>
+      <CommonHead meta={meta} />
+      <TopNav tags={tags}/>
       {children}
-    </div>
+    </>
   )
 }
 
