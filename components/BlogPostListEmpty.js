8a700c32ebfaedc4c653fa8b349a1c495e46b537
tangly1024 <tlyong1992@hotmail.com>
2021-10-20 13:47:31 +0800

    文章空列表跳转处理

--- components/BlogPostListEmpty.js
+++ components/BlogPostListEmpty.js
@@ -1,12 +1,21 @@
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
 /**
  * 空白博客 列表
  * @returns {JSX.Element}
  * @constructor
  */
 const BlogPostListEmpty = () => {
+  const router = useRouter()
+  useEffect(() => {
+    setTimeout(() => {
+      router.push('/')
+    }, 3000)
+  })
   return <div className='w-full h-full flex justify-center mx-auto'>
       <div className='align-middle text-center my-auto'>
-        <p className='text-gray-500 dark:text-gray-300'>No posts found.</p>
+        <p className='text-gray-500 dark:text-gray-300'>没有文章了，3秒后返回首页</p>
       </div>
   </div>
 }
