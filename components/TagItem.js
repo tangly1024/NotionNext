dac545e08ee826476ede3de3361e67339f565d0a
tangly1024 <tlyong1992@hotmail.com>
2021-11-13 10:01:21 +0800

    bugfix： 文章底部标签

--- components/TagItem.js
+++ components/TagItem.js
@@ -4,7 +4,7 @@ const TagItem = ({ tag, count }) => (
   <Link href={`/tag/${encodeURIComponent(tag)}`}>
     <div className="cursor-pointer hover:shadow hover:border-gray-600 rounded-md dark:border-gray-500 border hover:scale-105 hover:bg-gray-500 bg-gray-100 hover:text-white duration-200 mr-1 p-2 leading-none text-sm
       dark:bg-gray-500 dark:text-gray-100 dark:hover:bg-black">
-     <span className='whitespace-nowrap'>  <i className='fa fa-tag mr-1 '/> {tag}({count})</span>
+     <span className='whitespace-nowrap'>  <i className='fa fa-tag mr-1 '/> {tag}{count && `(${count})`}</span>
     </div>
   </Link>
 )
