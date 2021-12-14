8148105b92e63a66630d0a56ab9b6d2c01d5551d
tangly1024 <tlyong1992@hotmail.com>
2021-12-14 12:06:04 +0800

    feature: 导航栏微调

--- components/TopNav.js
+++ components/TopNav.js
@@ -14,8 +14,8 @@ const TopNav = ({ tags, currentTag, post, posts, currentSearch, categories, curr
     <SideBarDrawer post={post} currentTag={currentTag} cRef={drawer} tags={tags} posts={posts} categories={categories} currentCategory={currentCategory}/>
 
     {/* 导航栏 */}
-    <div id='sticky-nav' className='fixed w-full top-0 z-20 transform duration-500 bg-white dark:bg-gray-700 border-b dark:border-gray-700'>
-      <div className='text-sm m-auto w-full flex flex-row justify-between items-center px-4 py-2 shadow-md'>
+    <div id='sticky-nav' className='fixed w-full top-0 z-20 transform duration-500 bg-white dark:bg-gray-700 opacity-90'>
+      <div className='text-sm m-auto w-full flex flex-row justify-between items-center px-4 py-2 shadow-xl '>
         {/* 左侧LOGO */}
         <div className='flex ml-12'>
           <div onClick={() => { drawer.current.handleSwitchSideDrawerVisible() }}
@@ -23,13 +23,9 @@ const TopNav = ({ tags, currentTag, post, posts, currentSearch, categories, curr
             <FontAwesomeIcon icon={faBars} className='hover:scale-125 transform duration-200'
             />
           </div>
-        </div>
-
-        {/* 中间搜索框 */}
-        <div className='w-96'>
           <Link href='/' passHref>
             <div className='cursor-pointer'>
-             <h1 className='text-lg font-serif dark:text-gray-200 whitespace-nowrap overflow-x-hidden'>{BLOG.title }</h1>
+             <h1 className='text-xl pb-0.5 hover:scale-105 duration-200 transform font-serif dark:text-gray-200 whitespace-nowrap overflow-x-hidden'>{BLOG.title }</h1>
             </div>
           </Link>
         </div>
