f8694456ef00b22516fc80e68dfcc64dcbcfacf3
tangly1024 <tlyong1992@hotmail.com>
2021-12-15 12:36:52 +0800

    feature: 本地编译优化

--- pages/article/[slug].js
+++ pages/article/[slug].js
@@ -152,10 +152,7 @@ const ArticleDetail = ({ post, blockMap, tags, prev, next, allPosts, categories 
 }
 
 export async function getStaticPaths () {
-  let posts = []
-  if (BLOG.isProd) {
-    posts = await getAllPosts({ from: 'slug - paths', includePage: true })
-  }
+  const posts = await getAllPosts({ from: 'slug - paths', includePage: true })
   return {
     paths: posts.map(row => `${BLOG.path}/article/${row.slug}`),
     fallback: true
