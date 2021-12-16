ae7edd80b6722921e666fea30c8cd40582b2dda7
tangly1024 <tlyong1992@hotmail.com>
2021-12-16 13:30:17 +0800

    bugfix:标签颜色

--- components/TagItem.js
+++ components/TagItem.js
@@ -12,13 +12,9 @@ const TagItem = ({ tag, selected }) => {
   return (
   <Link href={selected ? '/' : `/tag/${encodeURIComponent(tag.name)}`} passHref>
     <li
-      className={`notion-${tag.color}_background list-none cursor-pointer hover:bg-gray-300 rounded-md 
-      duration-200 mr-1 my-1 px-2 py-1 text-sm whitespace-nowrap
-                  dark:hover:bg-gray-800 dark:hover:text-white hover:text-black
-                 ${selected
-         ? ' text-white dark:text-white bg-black dark:hover:bg-gray-900 dark:bg-black'
-         : ' text-gray-600'}`}
-    >
+      className={`notion-${tag.color}_background list-none cursor-pointer rounded-md 
+      duration-200 mr-1 my-1 px-2 py-1 text-sm whitespace-nowrap 
+      text-gray-600 hover:bg-gray-200 dark:hover:bg-gray-800 dark:hover:text-white`}>
       <a>
         {selected && <FontAwesomeIcon icon={faTag} className='mr-1'/>} {`${tag.name} `} {tag.count ? `(${tag.count})` : ''}
       </a>
