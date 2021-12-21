94ecdc057cb03bd3c8cb77234b8853e9b8baeb05
tangly1024 <tlyong1992@hotmail.com>
2021-12-21 17:42:33 +0800

    footer链接

--- components/Footer.js
+++ components/Footer.js
@@ -2,6 +2,7 @@ import { faCopyright, faEye, faShieldAlt, faUsers } from '@fortawesome/free-soli
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import Link from 'next/link'
+import BLOG from '@/blog.config'
 
 const Footer = ({ title }) => {
   const d = new Date()
@@ -10,7 +11,7 @@ const Footer = ({ title }) => {
     <footer
       className='bg-gray-800 dark:bg-black dark:border-gray-900 border-t flex-shrink-0 justify-center text-center m-auto w-full text-gray-400 text-sm p-6'
     >
-      <FontAwesomeIcon icon={faCopyright} /> {` ${y}`} <span> <a href='https://tangly1024.com/article/about' className='underline font-bold text-gray-100'>tangly1024.com</a>. Powered by <a href='https://notion.so' className='underline font-bold text-gray-100'>Notion</a> & <a href='https://github.com/tangly1024/NotionNext' className='underline font-bold text-gray-100'>NotionNext</a>.</span>
+      <FontAwesomeIcon icon={faCopyright} /> {` ${y}`} <span> <a href={BLOG.link} className='underline font-bold text-gray-100'>{BLOG.author}</a>. Powered by <a href='https://notion.so' className='underline font-bold text-gray-100'>Notion</a> & <a href='https://github.com/tangly1024/NotionNext' className='underline font-bold text-gray-100'>NotionNext</a>.</span>
       <br />
       <FontAwesomeIcon icon={faShieldAlt} /> <a href='https://beian.miit.gov.cn/' className='ml-1 font-bold'>闽ICP备20010331号</a>
       <span > <Link href='/article/privacy-policy' ><a className='ml-1 mr-1 font-bold underline'>隐私政策</a></Link></span>
