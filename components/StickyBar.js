e430d55cf8fa144028d2f0fb457d0afbf0f4a062
tangly1024 <tlyong1992@hotmail.com>
2021-12-21 11:00:47 +0800

    feature: 微调

--- components/StickyBar.js
+++ components/StickyBar.js
@@ -9,7 +9,7 @@ const StickyBar = ({ children }) => {
   if (!children) return <></>
   return (
     <div id='sticky-bar' className='sticky flex-grow justify-center top-14 md:top-0 duration-500 z-10 pb-16'>
-      <div className='bg-white dark:bg-gray-800 dark:border-gray-600 px-5 mx-2 absolute rounded-none md:rounded-xl shadow-xl border w-full hidden-scroll'>
+      <div className='bg-white dark:bg-gray-800 dark:border-gray-600 px-5 absolute rounded-none md:rounded-xl shadow-xl border w-full hidden-scroll'>
           <div id='tag-container' className="md:pl-3 overflow-x-auto">
               { children }
           </div>
