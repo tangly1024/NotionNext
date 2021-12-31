ea4eba0d2f31c3d6cb6b776a800034d1c537f298
tangly1024 <tlyong1992@hotmail.com>
2021-12-31 15:04:46 +0800

    feature: 移动端显示标签

--- components/TagItemMini.js
+++ components/TagItemMini.js
@@ -8,8 +8,8 @@ const TagItemMini = ({ tag, selected = false }) => {
       mr-2 py-0.5 px-1 text-xs whitespace-nowrap dark:hover:text-white
        ${selected
       ? 'text-white dark:text-gray-300 bg-black dark:bg-black dark:hover:bg-gray-900'
-      : `text-gray-600 hover:shadow-xl dark:border-gray-400 notion-${tag.color}_background dark:bg-gray-600 dark:text-gray-300 `}` }>
-    <div className='font-light'>{selected && <FontAwesomeIcon icon={faTag} className='mr-1'/>} {tag.name + (tag.count ? `(${tag.count})` : '')} </div>
+      : `text-gray-600 hover:shadow-xl dark:border-gray-400 notion-${tag.color}_background dark:bg-gray-900`}` }>
+    <div className='font-light dark:text-gray-400'>{selected && <FontAwesomeIcon icon={faTag} className='mr-1'/>} {tag.name + (tag.count ? `(${tag.count})` : '')} </div>
     </a>
   </Link>
 }
