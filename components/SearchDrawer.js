167849950d20e8bca5450d282063ead977ba421b
tangly1024 <tlyong1992@hotmail.com>
2022-01-04 12:32:22 +0800

    搜索框动画

--- components/SearchDrawer.js
+++ components/SearchDrawer.js
@@ -21,7 +21,7 @@ const SearchDrawer = ({ cRef }) => {
   return (
     <div id='search-drawer-wrapper' ref={searchDrawer} className='hidden'>
       <div className='flex absolute px-5 w-full h-full left-0 top-14 z-50 justify-center'>
-          <div className='md:max-w-3xl w-full mx-auto'>
+          <div className='md:max-w-3xl w-full mx-auto animate__animated animate__faster animate__fadeIn'>
           <SearchInput cRef={searchInputRef} />
           </div>
       </div>
