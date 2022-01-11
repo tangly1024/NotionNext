d4ab0e7f4982bf75885a4b939d43a61a3029cd52
tangly1024 <tlyong1992@hotmail.com>
2022-01-11 22:49:40 +0800

    文章预览优化

--- pages/page/[page].js
+++ pages/page/[page].js
@@ -54,7 +54,7 @@ export async function getStaticProps ({ params: { page } }) {
 
   for (const i in postsToShow) {
     const post = postsToShow[i]
-    const blockMap = await getPostBlocks(post.id, 'slug')
+    const blockMap = await getPostBlocks(post.id, 'slug', 12)
     if (blockMap) {
       post.blockMap = blockMap
     }
