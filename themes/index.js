68fb3b2735f5027bc8b112ebe26317859d2ab0a3
tangly1024 <tlyong1992@hotmail.com>
2022-01-16 22:54:28 +0800

    切回NEXT主题

--- themes/index.js
+++ themes/index.js
@@ -2,6 +2,6 @@
  * 修改 from 后面的路径，实现主题切换
  */
 
-// export * from './NEXT' // 切换主题
-export * from './Fukasawa'
+export * from './NEXT' // 切换主题
+// export * from './Fukasawa'
 // export * from './Empty'
