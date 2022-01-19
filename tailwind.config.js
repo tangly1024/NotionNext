26fbe3b3b2cf125878566c73d50e347d4d32a4a7
tangly1024 <tlyong1992@hotmail.com>
2022-01-19 17:23:03 +0800

    google 思源宋体字体

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,28 +1,11 @@
 const BLOG = require('./blog.config')
-const { fontFamily } = require('tailwindcss/defaultTheme')
-const CJK = require('./lib/cjk')
-const fontSansCJK = !CJK()
-  ? []
-  : [`"Noto Sans CJK ${CJK()}"`, `"Noto Sans ${CJK()}"`]
-const fontSerifCJK = !CJK()
-  ? []
-  : [`"Noto Serif CJK ${CJK()}"`, `"Noto Serif ${CJK()}"`]
+const fontFamilies = require('./lib/font')
+
 module.exports = {
   purge: ['./pages/**/*.js', './components/**/*.js', './layouts/**/*.js', './themes/**/*.js'],
   darkMode: BLOG.APPEARANCE === 'class' ? 'media' : 'class', // or 'media' or 'class'
   theme: {
-    fontFamily: {
-      sans: ['"IBM Plex Sans"', ...fontFamily.sans, ...fontSansCJK],
-      serif: ['"Source Serif"', ...fontFamily.serif, ...fontSerifCJK],
-      noEmoji: [
-        '"IBM Plex Sans"',
-        'ui-sans-serif',
-        'system-ui',
-        '-apple-system',
-        'BlinkMacSystemFont',
-        'sans-serif'
-      ]
-    },
+    fontFamily: fontFamilies,
     extend: {
       colors: {
         day: {
