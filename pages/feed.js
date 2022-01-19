f1590f52e818a03132d4e9a94b6e7a710287d35b
tangly1024 <tlyong1992@hotmail.com>
2022-01-19 11:27:09 +0800

    bugfix: rss修复

--- pages/feed.js
+++ pages/feed.js
@@ -3,8 +3,8 @@ import { getGlobalNotionData } from '@/lib/notion/getNotionData'
 
 export async function getServerSideProps ({ res }) {
   res.setHeader('Content-Type', 'text/xml')
-  const { allPosts } = getGlobalNotionData({ from: 'rss' })
-  const xmlFeed = generateRss(allPosts.slice(0, 10))
+  const globalNotionData = await getGlobalNotionData({ from: 'rss' })
+  const xmlFeed = generateRss(globalNotionData?.allPosts?.slice(0, 10) || [])
   res.write(xmlFeed)
   res.end()
   return {
