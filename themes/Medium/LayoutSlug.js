952051a80f56ee906fde7dd7d293e88f66720859
tangly1024 <tlyong1992@hotmail.com>
2022-01-28 09:32:51 +0800

    文章详情页排班

--- themes/Medium/LayoutSlug.js
+++ themes/Medium/LayoutSlug.js
@@ -9,6 +9,10 @@ import 'prismjs/components/prism-typescript'
 import { Code, Collection, CollectionRow, Equation, NotionRenderer } from 'react-notion-x'
 import LayoutBase from './LayoutBase'
 import Comment from '@/components/Comment'
+import Image from 'next/image'
+import { useGlobal } from '@/lib/global'
+import formatDate from '@/lib/formatDate'
+import Link from 'next/link'
 
 const mapPageUrl = id => {
   return 'https://www.notion.so/' + id.replace(/-/g, '')
@@ -27,9 +31,25 @@ export const LayoutSlug = (props) => {
     post.content = Object.keys(post.blockMap.block)
     post.toc = getPageTableOfContents(post, post.blockMap)
   }
+  const { locale } = useGlobal()
+  const date = formatDate(post?.date?.start_date || post.createdTime, locale.LOCALE)
 
-  return <LayoutBase {...props} meta={meta}>
-    <h1 className='text-4xl mt-12'>{post?.title}</h1>
+  return <LayoutBase {...props} meta={meta} showInfoCard={false}>
+    <h1 className='text-4xl mt-12 font-sans'>{post?.title}</h1>
+    <Link href='/about' passHref>
+      <div className='flex py-3 items-center font-sans cursor-pointer'>
+        <Image
+          alt={BLOG.AUTHOR}
+          width={25}
+          height={25}
+          loading='lazy'
+          src='/avatar.jpg'
+          className='rounded-full'
+        />
+        <div className='mr-3 ml-1 text-green-500'>{BLOG.AUTHOR}</div>
+        <div className='text-gray-500'>{date}</div>
+      </div>
+    </Link>
     {/* Notion文章主体 */}
     <section id='notion-article' className='px-1 max-w-5xl'>
       {post.blockMap && (
