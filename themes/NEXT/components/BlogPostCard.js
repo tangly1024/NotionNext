9e3bcb75e58a29d05621976f1b9c7b8706b1e952
tangly1024 <tlyong1992@hotmail.com>
2022-02-28 17:46:31 +0800

    卡牌article-cover样式调整

--- themes/NEXT/components/BlogPostCard.js
+++ themes/NEXT/components/BlogPostCard.js
@@ -59,9 +59,9 @@ const BlogPostCard = ({ post, showSummary }) => {
           />
         </div> }
 
-        <div className='article-cover pointer-events-none text-right'>
+        <div className='text-right border-t pt-8 border-dashed'>
           <Link href={`${BLOG.PATH}/article/${post.slug}`}>
-              <a className='hover:bg-opacity-100 hover:scale-105 hover:underline pointer-events-auto transform duration-300 p-3 text-white bg-gray-800 cursor-pointer'>
+              <a className='hover:bg-opacity-100 hover:underline transform duration-300 p-3 text-white bg-gray-800 dark:bg-black cursor-pointer'>
                 {locale.COMMON.ARTICLE_DETAIL}
                 <i className='ml-1 fas fa-angle-right' /></a>
           </Link>
