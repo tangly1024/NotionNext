9a4f57d756a5c44fd72553dbaa40bf28ffb67986
tangly1024 <tlyong1992@hotmail.com>
2022-03-01 12:59:47 +0800

    FUKASAWA主题，分类标签调整

--- themes/Fukasawa/LayoutCategoryIndex.js
+++ themes/Fukasawa/LayoutCategoryIndex.js
@@ -17,11 +17,11 @@ export const LayoutCategoryIndex = (props) => {
         <i className='mr-4 fas fa-th' />{locale.COMMON.CATEGORY}:
       </div>
       <div id='category-list' className='duration-200 flex flex-wrap'>
-        {Object.keys(categories).map(category => {
-          return <Link key={category} href={`/category/${category}`} passHref>
+        {categories && categories.map(category => {
+          return <Link key={category.name} href={`/category/${category.name}`} passHref>
             <div
               className={'hover:text-black dark:hover:text-white dark:text-gray-300 dark:hover:bg-gray-600 px-5 cursor-pointer py-2 hover:bg-gray-100'}>
-              <i className='mr-4 fas fa-folder' />{category}({categories[category]})
+              <i className='mr-4 fas fa-folder' />{category.name}({category.count})
             </div>
           </Link>
         })}
