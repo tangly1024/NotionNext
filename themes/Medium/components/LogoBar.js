3ecc3679957bf705fa5303d1f4781e937dd70d90
tangly1024 <tlyong1992@hotmail.com>
2022-03-15 13:35:34 +0800

    Medium主题顶部导航栏微调

--- themes/Medium/components/LogoBar.js
+++ themes/Medium/components/LogoBar.js
@@ -2,11 +2,9 @@ import BLOG from '@/blog.config'
 import Link from 'next/link'
 
 export default function LogoBar () {
-  return <div id='top-wrapper' className='w-full max-w-5xl justify-center mx-auto font-sans'>
-      <div className='w-full'>
+  return <div id='top-wrapper' className='w-full flex items-center font-sans'>
         <Link href='/'>
           <a className='text-2xl dark:text-gray-200'>{BLOG.TITLE}</a>
         </Link>
-      </div>
   </div>
 }
