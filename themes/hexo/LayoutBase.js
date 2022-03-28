7ffb29982041c530a50015c355d1070f40961f27
tangly1024 <tlyong1992@hotmail.com>
2022-03-28 15:51:30 +0800

    hexo微调边距

--- themes/hexo/LayoutBase.js
+++ themes/hexo/LayoutBase.js
@@ -53,8 +53,8 @@ const LayoutBase = (props) => {
 
     <main id='wrapper' className='w-full py-8 min-h-screen'>
 
-      <div id='container-inner' className='pt-14 w-full mx-auto lg:flex justify-center md:space-x-4'>
-        <div className='flex-grow w-full max-w-4xl'>
+      <div id='container-inner' className='pt-14 w-full mx-auto lg:flex justify-center lg:space-x-4'>
+        <div className='flex-grow w-full lg:max-w-4xl'>
           {onLoading ? <LoadingCover/> : children}
         </div>
 
