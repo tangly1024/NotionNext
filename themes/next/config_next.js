1f8d9960b5ae2af9a0185e10e55f9f261b8522a0
Spac <101972491+spacding@users.noreply.github.com>
2022-04-02 00:44:15 +0800

    Update config_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -22,7 +22,7 @@ const CONFIG_NEXT = {
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
-  MENU_ABOUT: true, // 显示关于
+  MENU_ABOUT: false, // 显示关于
   MENU_SEARCH: true, // 显示搜索
 
   WIDGET_TO_TOP: true, // 是否显示回顶
