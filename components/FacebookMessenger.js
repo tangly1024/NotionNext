f6e936f12178a3cbb2d1c6e827993183773f4f77
txs <txshon@gmail.com>
2022-04-05 02:46:37 +0800

    Facebook Messenger trial

--- components/FacebookMessenger.js
+++ components/FacebookMessenger.js
@@ -1,13 +1,67 @@
 import BLOG from '@/blog.config'
-import React, { Component } from 'react'
-import { FacebookProvider, CustomChat } from 'react-facebook'
+import { useEffect } from 'react'
+import MessengerCustomerChat from 'react-messenger-customer-chat'
+const Messenger = () => (
+  <MessengerCustomerChat
+    pageId={BLOG.FACEBOOK_PAGE_ID}
+    appId={BLOG.FACEBOOK_APP_ID}
+  />
+)
+export default Messenger
+/**
+ *
+ */
+// export function init() {
+//   var chatbox = document.getElementById('fb-customer-chat')
+//   chatbox.setAttribute('page_id', BLOG.FACEBOOK_PAGE_ID) // TODO: move to args
+//   chatbox.setAttribute('attribution', 'biz_inbox')
 
-export default class Messenger extends Component {
-  render() {
-    return (
-      <FacebookProvider appId={BLOG.FACEBOOK_APP_ID} chatSupport>
-        <CustomChat pageId={BLOG.FACEBOOK_PAGE_ID} minimized={false} />
-      </FacebookProvider>
-    )
-  }
-}
+//   window.fbAsyncInit = function () {
+//     FB.init({
+//       xfbml: true,
+//       version: 'v11.0'
+//     })
+//   }
+//   ;(function (d, s, id) {
+//     var js,
+//       fjs = d.getElementsByTagName(s)[0]
+//     if (d.getElementById(id)) return
+//     js = d.createElement(s)
+//     js.id = id
+//     js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js'
+//     fjs.parentNode.insertBefore(js, fjs)
+//   })(document, 'script', 'facebook-jssdk')
+// }
+
+// /**
+//  *
+//  */
+// export function cleanup() {
+//   ;(function (d, id) {
+//     var target = d.getElementById(id)
+//     if (target) {
+//       target.parentNode.removeChild(target)
+//     }
+//   })(document, 'facebook-jssdk')
+
+//   delete window.FB
+// }
+
+// export default function Messenger() {
+//   useEffect(() => {
+//     console.log('Facebook1')
+//     init()
+
+//     return () => {
+//       cleanup()
+//     }
+//   }, [])
+
+//   return (
+//     <div>
+//       <div id="fb-root"></div>
+
+//       <div id="fb-customer-chat" className="fb-customerchat"></div>
+//     </div>
+//   )
+// }
