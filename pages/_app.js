357f7dc2461015a959636b25430f942ece36dfae
txs <txshon@gmail.com>
2022-04-06 04:37:09 +0800

    Add Facebook Customer Chat for the blogging system

--- pages/_app.js
+++ pages/_app.js
@@ -16,25 +16,32 @@ import dynamic from 'next/dynamic'
 import { GlobalContextProvider } from '@/lib/global'
 import { DebugPanel } from '@/components/DebugPanel'
 import { ThemeSwitch } from '@/components/ThemeSwitch'
+import Messenger from '@/components/FacebookMessenger'
 
 const Ackee = dynamic(() => import('@/components/Ackee'), { ssr: false })
 const Gtag = dynamic(() => import('@/components/Gtag'), { ssr: false })
 const Busuanzi = dynamic(() => import('@/components/Busuanzi'), { ssr: false })
-const GoogleAdsense = dynamic(() => import('@/components/GoogleAdsense'), { ssr: false })
+const GoogleAdsense = dynamic(() => import('@/components/GoogleAdsense'), {
+  ssr: false
+})
 
 const MyApp = ({ Component, pageProps }) => {
   return (
     <GlobalContextProvider>
-        {BLOG.THEME_SWITCH && <ThemeSwitch/>}
-        {BLOG.DEBUG && <DebugPanel/>}
-        {BLOG.ANALYTICS_ACKEE_TRACKER && <Ackee />}
-        {BLOG.ANALYTICS_GOOGLE_ID && <Gtag />}
-        {JSON.parse(BLOG.ANALYTICS_BUSUANZI_ENABLE) && <Busuanzi/>}
-        {BLOG.ADSENSE_GOOGLE_ID && <GoogleAdsense/>}
-        {/* FontawesomeCDN */}
-        <link href={BLOG.FONT_AWESOME_PATH} rel="stylesheet" referrerPolicy="no-referrer" />
-        <Component {...pageProps} />
-
+      {BLOG.THEME_SWITCH && <ThemeSwitch />}
+      {BLOG.DEBUG && <DebugPanel />}
+      {BLOG.ANALYTICS_ACKEE_TRACKER && <Ackee />}
+      {BLOG.ANALYTICS_GOOGLE_ID && <Gtag />}
+      {JSON.parse(BLOG.ANALYTICS_BUSUANZI_ENABLE) && <Busuanzi />}
+      {BLOG.ADSENSE_GOOGLE_ID && <GoogleAdsense />}
+      <Messenger />
+      {/* FontawesomeCDN */}
+      <link
+        href={BLOG.FONT_AWESOME_PATH}
+        rel="stylesheet"
+        referrerPolicy="no-referrer"
+      />
+      <Component {...pageProps} />
     </GlobalContextProvider>
   )
 }
