9839dd1aaa7f7da3d8c4a9232d58f767a8434102
Spac <101972491+spacding@users.noreply.github.com>
2022-04-07 18:55:39 +0800

    Update config_next.js

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -26,7 +26,7 @@ const CONFIG_NEXT = {
   MENU_SEARCH: true, // 显示搜索
 
   WIDGET_TO_TOP: true, // 是否显示回顶
-  WIDGET_TO_BOTTOM: false, // 显示回底
+  WIDGET_TO_BOTTOM: true, // 显示回底
   WIDGET_DARK_MODE: true, // 显示日间/夜间模式切换
   WIDGET_TOC: true, // 移动端显示悬浮目录
 
