c90c3c5256e4d2b387190de3af61a0ddea7049ed
MarcusPierce <MarcusPierce@pm.me>
2022-04-14 21:21:34 -0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -30,8 +30,8 @@ const BLOG = {
 
   POST_LIST_STYLE: 'page', // ['page','scroll] 文章列表样式:页码分页、单页滚动加载
   POST_LIST_PREVIEW: process.env.NEXT_PUBLIC_POST_PREVIEW || 'true', //  是否在列表加载文章预览
-  POST_PREVIEW_LINES: 6, // 预览博客行数
-  POST_RECOMMEND_COUNT: 6, // 推荐文章数量
+  POST_PREVIEW_LINES: 3, // 预览博客行数
+  POST_RECOMMEND_COUNT: 3, // 推荐文章数量
   POSTS_PER_PAGE: 6, // post counts per page
   POSTS_SORT_BY: 'notion', // 排序方式 'date'按时间,'notion'由notion控制
 
