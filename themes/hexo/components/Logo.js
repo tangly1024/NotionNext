d0edae963bc21aba8378e5ea58cd75bff905a40e
tangly1024 <tlyong1992@hotmail.com>
2022-04-26 22:20:02 +0800

    优化增量静态及404处理

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -1,12 +1,13 @@
+import BLOG from '@/blog.config'
 import Link from 'next/link'
 import React from 'react'
 
 const Logo = props => {
   const { siteInfo } = props
   return <Link href='/' passHref>
-        <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-          <div className='font-sans text-lg p-1.5 rounded bg-black text-white dark:border-white border-black border'> {siteInfo?.title}</div>
-        </div>
+    <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
+      <div className='font-sans text-lg p-1.5 rounded bg-black text-white dark:border-white border-black border'> {siteInfo?.title || BLOG.TITLE}</div>
+    </div>
   </Link>
 }
 export default Logo
