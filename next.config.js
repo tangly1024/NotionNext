ef1ea5edc76b29e7c418f6fb5b5d098fea13d732
tangly1024 <tlyong1992@hotmail.com>
2022-05-13 15:45:16 +0800

    next 图片压缩

--- next.config.js
+++ next.config.js
@@ -6,6 +6,8 @@ module.exports = withBundleAnalyzer({
     webpack5: true
   },
   images: {
+    // 图片压缩
+    formats: ['image/avif', 'image/webp'],
     // 允许next/image加载的图片 域名
     domains: [
       'gravatar.com',
