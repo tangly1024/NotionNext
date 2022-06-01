ece7f87d3299090e6068902c3cbc14dfb5488c2d
tlyong1992 <tlyong1992@hotmail.com>
2022-06-01 09:06:36 +0800

    bugfix：最近评论图片换行

--- themes/hexo/components/HexoRecentComments.js
+++ themes/hexo/components/HexoRecentComments.js
@@ -33,7 +33,7 @@ const HexoRecentComments = (props) => {
         {onLoading && <div>Loading...<i className='ml-2 fas fa-spinner animate-spin' /></div>}
         {!onLoading && comments && comments.length === 0 && <div>No Comments</div>}
         {!onLoading && comments && comments.length > 0 && comments.map((comment) => <div key={comment.objectId} className='pb-2 pl-1'>
-            <div className='dark:text-gray-200 text-sm waline-recent-content' dangerouslySetInnerHTML={{ __html: comment.comment }} />
+            <div className='dark:text-gray-200 text-sm waline-recent-content wl-content' dangerouslySetInnerHTML={{ __html: comment.comment }} />
             <div className='dark:text-gray-400 text-gray-400 font-sans text-sm text-right cursor-pointer hover:text-red-500 hover:underline pt-1 pr-2'><Link href={{ pathname: comment.url, hash: comment.objectId, query: { target: 'comment' } }}><a >-- {comment.nick}</a></Link></div>
         </div>)}
 
--- themes/next/components/NextRecentComments.js
+++ themes/next/components/NextRecentComments.js
@@ -25,7 +25,7 @@ const NextRecentComments = (props) => {
         {onLoading && <div>Loading...<i className='ml-2 fas fa-spinner animate-spin' /></div>}
         {!onLoading && comments && comments.length === 0 && <div>No Comments</div>}
         {!onLoading && comments && comments.length > 0 && comments.map((comment) => <div key={comment.objectId} className='pb-2'>
-            <div className='dark:text-gray-300 text-gray-600 text-xs waline-recent-content' dangerouslySetInnerHTML={{ __html: comment.comment }} />
+            <div className='dark:text-gray-300 text-gray-600 text-xs waline-recent-content wl-content' dangerouslySetInnerHTML={{ __html: comment.comment }} />
             <div className='dark:text-gray-400 text-gray-400 font-sans text-sm text-right cursor-pointer hover:text-red-500 hover:underline pt-1'><Link href={{ pathname: comment.url, hash: comment.objectId, query: { target: 'comment' } } }><a >-- {comment.nick}</a></Link></div>
         </div>)}
 
