cab76f1ad2e7dee3e0b76e156bf36997a40c9780
sunnydongbowen <46588976+sunnydongbowen@users.noreply.github.com>
2022-06-12 21:42:42 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -4,8 +4,8 @@ const BLOG = {
   BIO: '专于golang的开发者', // 作者简介
   LINK: 'https://bowen.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
-  NOTION_PAGE_ID:
-  process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  NOTION_PAGE_ID:'f315414a461a409d8598f29141a1e9b2'
+  process.env.NOTION_PAGE_ID || 'f315414a461a409d8598f29141a1e9b2', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
