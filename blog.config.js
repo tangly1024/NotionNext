bb6fe2993b3e46b819714e872cbdb3d4960c5945
Jeffrey Hung <17494876+Jeffreyhung@users.noreply.github.com>
2022-09-05 20:26:42 -0700

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,11 +1,11 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: 'tangly1024', // 作者
-  BIO: '一个普通的干饭人🍚', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
-  KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
-  NOTION_PAGE_ID:
-  process.env.NOTION_PAGE_ID || '02ab3b8678004aa69e9e415905ef32a5', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
+  AUTHOR: 'Jeffrey Hung', 
+  BIO: '', 
+  LINK: 'https://blog.jeffreyhung.com', 
+  KEYWORDS: 'Notion, blog', 
+  NOTION_PAGE_ID: 
+  process.env.NOTION_PAGE_ID || '957d8077dd79493682fdc9b51420af14', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
   DEBUG: process.env.NEXT_PUBLIC_DEBUG || false, // 是否显示调试按钮
 
