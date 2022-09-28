b814bf4c17f0a6a713607d8f5a027ecb53fc0667
rinxiaoxiao <33726100+rinxiaoxiao@users.noreply.github.com>
2022-09-28 13:23:06 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['凭何让彼此变一对雪人', '拥抱着等待决裂有声', '凝固那还未幻灭的风景'], // 首页大图标语文字
 
   // 菜单配置
   MENU_CATEGORY: true, // 显示分类
