430656f6b88ee8c473740ab82c7aae986823c49c
tangly <tlyong1992@hotmail.com>
2022-10-11 13:01:22 +0800

    支持配置Google字体地址

--- pages/_document.js
+++ pages/_document.js
@@ -16,7 +16,7 @@ class MyDocument extends Document {
           <link rel='icon' href='/favicon.ico' />
           <link rel='icon' href='/favicon.svg' type='image/svg+xml' />
           {/* 谷歌字体镜像 */}
-          <link href="https://fonts.loli.net/css2?family=Noto+Serif+SC&display=swap" rel="stylesheet"/>
+          <link href={`${BLOG.FONT_URL}`} rel="stylesheet"/>
           <CommonScript />
         </Head>
 
