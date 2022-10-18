0ecaadddd2f2706d477ba8cb67620e5bd6c46374
tangly <tlyong1992@hotmail.com>
2022-10-18 22:03:15 +0800

    调整首页导航高度

--- themes/hexo/components/NavButtonGroup.js
+++ themes/hexo/components/NavButtonGroup.js
@@ -10,10 +10,10 @@ import Link from 'next/link'
 const NavButtonGroup = (props) => {
   const { categories } = props
 
-  return <nav id='home-nav-button' className={'py-5 md:mt-24 mt-12 flex md:max-w-3xl md:space-x-12 md:flex justify-center'}>
+  return <nav id='home-nav-button' style={{ lineHeight: '100px' }} className={'md:h-52 md:mt-32 px-5 mt-12 flex flex-wrap m-0 md:max-w-5xl md:space-x-12 space-y-2 md:space-y-0 md:flex justify-center'}>
     {categories.map(category => {
       return <Link key={`${category.name}`} title={`${category.name}`} href={`/category/${category.name}`} passHref>
-            <a className='text-center w-full md:w-40 md:h-20 h-14 mr-4 mb-4 justify-center items-center flex border-white border-2 cursor-pointer rounded-lg font-serif hover:bg-white hover:text-black duration-200 font-bold hover:scale-110 transform'>{category.name}</a>
+            <a className='text-center w-full md:w-40 md:h-20 h-14 justify-center items-center flex border-white border-2 cursor-pointer rounded-lg font-serif hover:bg-white hover:text-black duration-200 font-bold hover:scale-110 transform'>{category.name}</a>
         </Link>
     })}
   </nav>
