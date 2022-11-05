a94931cac8c7dd5709b8c72a67cd904c01b8915a
tangly <tlyong1992@hotmail.com>
2022-11-05 15:26:58 +0800

    font-awesome 更新6.2版本

--- blog.config.js
+++ blog.config.js
@@ -31,8 +31,8 @@ const BLOG = {
   CUSTOM_FONT_SANS: ['LXGW WenKai Screen'], // 自定义无衬线字体
   CUSTOM_FONT_SERIF: ['LXGW WenKai Screen'], // 自定义衬线字体
 
-  // 图标字体
-  FONT_AWESOME_PATH: 'https://cdn.bootcdn.net/ajax/libs/font-awesome/5.15.4/css/all.min.css', // 图标库CDN ，国内推荐BootCDN，国外推荐 CloudFlare https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css
+  // 图标库CDN(可以直接改版本号）
+  FONT_AWESOME_PATH: 'https://cdn.bootcdn.net/ajax/libs/font-awesome/6.2.0/css/all.min.css',
 
   BACKGROUND_LIGHT: '#eeeeee', // use hex value, don't forget '#' e.g #fffefc
   BACKGROUND_DARK: '#000000', // use hex value, don't forget '#'
