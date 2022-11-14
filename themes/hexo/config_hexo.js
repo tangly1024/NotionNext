73cbc51798e57acc02815dcfdaeadf74146f120f
ddw20191222 <58885307+ddw20191222@users.noreply.github.com>
2022-11-15 00:04:14 +0800

    update config_hexo.js
    修改封面内容

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,8 +1,8 @@
 const CONFIG_HEXO = {
-  // HOME_BANNER_ENABLE: false,
-  // HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_ENABLE: false,
+  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
-  // HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
+  HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
 
   // 菜单配置
   MENU_CATEGORY: true, // 显示分类
@@ -19,7 +19,7 @@ const CONFIG_HEXO = {
   ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  WIDGET_ANALYTICS: false, // 显示统计卡
+  WIDGET_ANALYTICS: true, // 显示统计卡
   WIDGET_TO_TOP: true,
   WIDGET_TO_COMMENT: true, // 跳到评论区
   WIDGET_DARK_MODE: true, // 夜间模式
