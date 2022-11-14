e50966442a39829261d5e339d605d633b041beb8
ddw20191222 <58885307+ddw20191222@users.noreply.github.com>
2022-11-14 18:28:22 +0800

    20221122 modified
    修改作者内容和细节内容

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
   AUTHOR: 'dongdong', // 作者
-  BIO: '一个普通的学生', // 作者简介
+  BIO: '一个普通的求学者', // 作者简介
   LINK: 'dongliang1996.cn', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
