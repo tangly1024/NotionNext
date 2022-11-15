b3cd80ab9e12df2225e449d63e41bb81852692c5
徐岩 <74172512+Xuyan-cmd@users.noreply.github.com>
2022-11-15 14:45:03 +0800

    1

--- blog.config.js
+++ blog.config.js
@@ -64,7 +64,7 @@ const BLOG = {
   // 悬浮挂件
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
   WIDGET_PET_LINK:
-    'https://cdn.jsdelivr.net/npm/live2d-widget-model-shizuku/assets/shizuku.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
+    'https://cdn.jsdelivr.net/npm/live2d-widget-model-nipsilon/assets/nipsilon.model.json', // 挂件模型地址 @see https://github.com/xiazeyu/live2d-widget-models
   WIDGET_PET_SWITCH_THEME: true, // 点击宠物挂件切换博客主题
 
   // 评论互动 可同时开启多个支持 GISCUS CUSDIS UTTERRANCES GITALK
