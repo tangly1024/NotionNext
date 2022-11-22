19dd0aa2aaee369004b3bda75386b0e5194b9c81
xiongus <86041264+xiongus@users.noreply.github.com>
2022-11-22 17:33:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -2,7 +2,7 @@
 const BLOG = {
   AUTHOR: 'xiongus', // 作者
   BIO: '一个普通人', // 作者简介
-  LINK: 'https://xiongguisheng.com', // 网站地址
+  LINK: 'https://blog.xiongguisheng.com', // 网站地址
   KEYWORDS: 'Notion, Blog', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
   process.env.NOTION_PAGE_ID || '89eede3e1fcb4d5693fd8d7c11702555', // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
@@ -52,11 +52,11 @@ const BLOG = {
   PREVIEW_TAG_COUNT: 16, // 首页最多展示的标签数量，0为不限制
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: 'mail@tangly1024.com', // 邮箱
+  CONTACT_EMAIL: 'mail@xiongguisheng.com', // 邮箱
   CONTACT_WEIBO: '', // 你的微博个人主页
   CONTACT_TWITTER: '', // 你的twitter个人主页
-  CONTACT_GITHUB: 'https://github.com/tangly1024', // 你的github个人主页
-  CONTACT_TELEGRAM: 'https://t.me/tangly_1024', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_GITHUB: 'https://github.com/xiongus', // 你的github个人主页
+  CONTACT_TELEGRAM: 'https://t.me/xiongus', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
 
