c99b99a77e15f3a3402a1977b0464509400efae7
Raymond Hou <amonkey0917@gmail.com>
2022-11-23 20:37:40 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,7 +8,17 @@ module.exports = {
     fontFamily: fontFamilies,
     extend: {
       colors: {
-      
+        indigo: {
+          100: '#ccfbf1',
+          200: '#99f6e4',
+          300: '#5eead4',
+          400: '#2dd4bf',
+          500: '#14b8a6',
+          600: '#0d9488',
+          700: '#0f766e',
+          800: '#115e59',
+          900: '#134e4a'
+        },
         day: {
           DEFAULT: BLOG.BACKGROUND_LIGHT || '#ffffff'
         },
