6c1b93ca636dd9b10dee5976391377c82ce38584
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2022-12-09 13:25:08 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -32,7 +32,7 @@ const Footer = ({ title }) => {
         <a href="https://icp.gov.moe/?keyword=20225199" target="_blank">萌ICP备20225199号</a>
         <br />
         <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a>.</span>
-      </span>
+       </span>
     </footer>
   )
 }
