95ee015c2197c4cb68f66ba21647ddd42225f6d5
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2022-12-09 19:11:48 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -30,7 +30,7 @@ const Footer = ({ title }) => {
         <h1>{title}</h1>
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img style={{ width: '24px', height: '24px' }} src="https://icp.gov.moe/images/ico64.png">
-        <a href="https://icp.gov.moe/?keyword=20225199" target="_blank"> </img> 萌ICP备20225199号</a>
+        <a href="https://icp.gov.moe/?keyword=20225199" target="_blank" /> </img> 萌ICP备20225199号</a>
         <br />
         <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a>.</span>
        </span>
