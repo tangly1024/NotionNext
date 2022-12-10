c72f7581b5c45bb97d08d2b42b7b1d1874283698
violet-mio <caohai648@qq.com>
2022-12-10 17:22:29 +0800

    feat: 隐藏首页分类大图标按钮

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -2,7 +2,7 @@ const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
   HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
-  HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
+  HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
 
   // 菜单配置
   MENU_CATEGORY: true, // 显示分类
