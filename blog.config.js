437c9abbdadfaf49e8497234e4d04bb137fe508a
13977605323 <120115198+13977605323@users.noreply.github.com>
2022-12-10 12:20:18 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -10,9 +10,9 @@ const BLOG = {
   SINCE: 2021, // e.g if leave this empty, current year will be used.
   APPEARANCE: 'auto', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
-  AUTHOR: 'yyqx1024', // 作者
-  BIO: '一个普通的干饭人🍚', // 作者简介
-  LINK: 'https://tangly1024.com', // 网站地址
+  AUTHOR: '影院前线', // 作者
+  BIO: '喜欢电影的志同道合的朋友欢迎加入', // 作者简介
+  LINK: 'https://yyqx.xyz', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: '82422829@qq.com', // 邮箱
