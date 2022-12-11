cc7f75eed0775a18e14c19bd109211ddf625822d
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2022-12-11 14:30:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,9 +17,9 @@ const BLOG = {
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: 'mail@66619.eu.org', // 邮箱
   CONTACT_WEIBO: 'https://weibo.com/u/7458711630', // 你的微博个人主页
-  CONTACT_TWITTER: '', // 你的twitter个人主页
+  CONTACT_TWITTER: 'https://twitter.com/619Ethereal', // 你的twitter个人主页
   CONTACT_GITHUB: 'https://github.com/lifeafter619', // 你的github个人主页
-  CONTACT_TELEGRAM: '', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_TELEGRAM: 'https://t.me/ethereal619', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: '', // 你的linkedIn 首页
 
   // 自定义字体示例： 请先将 CUSTOM_FONT 改为 true， 并将 CUSTOM_FONT_URL 改为你的字体CSS地址，同时在 CUSTOM_FONT_SANS 与 CUSTOM_FONT_SERIF 中指定你的 fontfamily
