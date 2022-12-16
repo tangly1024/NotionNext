13c34f9a28c3c50609b7afe528f00db4b407e28e
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2022-12-16 18:23:15 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = ({ title }) => {
           <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br />
         <h1>{title}</h1>
-        <a href="https://ipw.cn/ipv6webcheck/?site=66619.eu.org" title="本站支持IPv6访问" target='_blank'><img style='display:inline-block;vertical-align:middle' alt="本站支持IPv6访问" src="https://static.ipw.cn/icon/ipv6-s1.svg"></a>
+        <a href="https://ipw.cn/ipv6webcheck/?site=66619.eu.org" title="本站支持IPv6访问" target='_blank'>本站支持IPv6访问</a>
         <a href="https://icp.gov.moe/?keyword=20225199" target="_blank">萌ICP备20225199号</a>
         <br />
         <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a>.</span>
