713214285fea7f745a440cbe6cc76bd4e901cc4b
nscmiaoxin <44441301+nscmiaoxin@users.noreply.github.com>
2022-12-17 17:04:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
   AUTHOR: '艾迪蓝波', // 作者
-  BIO: '艾迪蓝波=🆔 Nunber', // 作者简介
+  BIO: '艾迪蓝波=🆔 Number', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   NOTION_PAGE_ID:
