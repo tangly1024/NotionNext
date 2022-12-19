1918fc26ce399996e36fb2261acba65cfdb2df2d
zhangqiyuan <CaoXiaoqing2008@gmail.com>
2022-12-19 18:01:27 +0800

    Update Footer.js
    close dark mode button and delete title

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -16,17 +16,17 @@ const Footer = ({ title }) => {
     <footer
       className='dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 bg-white dark:text-gray-400'
     >
-      <DarkModeButton/>
+      <!-- DarkModeButton/ -->
       <span>
         <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={BLOG.LINK} className='underline font-bold '>{BLOG.AUTHOR}</a>.
-
+        { }
         {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></>}
 
         <span className='hidden busuanzi_container_site_pv'>
           <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
         <span className='pl-2 hidden busuanzi_container_site_uv'>
           <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
-        <h1>{title}</h1>
+        <!-- h1>{title}</h1 -->
         <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a>.</span>
       </span>
     </footer>
