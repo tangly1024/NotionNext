1fd754bbd6a8fc527fa9c022801a63ba046c599e
tanglyqq <107399839+tanglyqq@users.noreply.github.com>
2022-12-19 17:04:04 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,6 +1,6 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/zh/features/personality
 const BLOG = {
-  AUTHOR: '测试一下', // 作者
+  AUTHOR: '再测试一下', // 作者
   BIO: '测试一下🍚', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
