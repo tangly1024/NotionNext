317509387ebe1e559c624c5499cbbab104f28343
zhangqiyuan <CaoXiaoqing2008@gmail.com>
2022-12-19 18:43:40 +0800

    Update Footer.js
    delete dot

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -17,8 +17,8 @@ const Footer = ({ title }) => {
       className='dark:bg-gray-800 flex-shrink-0 justify-center text-center m-auto w-full leading-6 text-sm p-6 bg-white dark:text-gray-400'
     >
       <span>
-        <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={BLOG.LINK} className='underline font-bold '>{BLOG.AUTHOR}</a>.
-        &nbsp; { }
+        <i className='fas fa-copyright' /> {`${copyrightDate}`} <span className='mx-1 animate-pulse'><i className='fas fa-heart' /></span> <a href={BLOG.LINK} className='underline font-bold '>{BLOG.AUTHOR}</a>
+        &nbsp;
         {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></>}
 
         <span className='hidden busuanzi_container_site_pv'>
@@ -26,7 +26,7 @@ const Footer = ({ title }) => {
         <span className='pl-2 hidden busuanzi_container_site_uv'>
           <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br />
-        <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a>.</span>
+        <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a></span>
       </span>
     </footer>
   )
