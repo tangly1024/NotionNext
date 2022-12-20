dac11fb8b89d01c44318abebaa2063c412512931
zhangqiyuan <CaoXiaoqing2008@gmail.com>
2022-12-20 10:57:13 +0800

    Update Footer.js

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -22,11 +22,11 @@ const Footer = ({ title }) => {
         {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></>}
 
         <span className='hidden busuanzi_container_site_pv'>
-          <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span> </span>
+          <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'></span></span>
         <span className='pl-2 hidden busuanzi_container_site_uv'>
-          <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
+          <i className='fas fa-users' /><span className='px-1 busuanzi_value_site_uv'></span></span>
         <br />
-        <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a></span>
+        <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by <a href='https://github.com/tangly1024/NotionNext' className='underline '>NotionNext {BLOG.VERSION}</a></span>
       </span>
     </footer>
   )
