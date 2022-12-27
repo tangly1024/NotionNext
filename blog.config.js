13b41cc5d89a09d8e962cf76d48155ba1d88e07c
kou <80955577+tamakokou@users.noreply.github.com>
2022-12-27 22:59:27 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -10,8 +10,8 @@ const BLOG = {
   SINCE: 2021, // e.g if leave this empty, current year will be used.
   APPEARANCE: 'light', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
 
-  AUTHOR: 'tangly1024', // 作者
-  BIO: '一个普通的干饭人🍚', // 作者简介
+  AUTHOR: 'tamako', // 作者
+  BIO: '一二三四五六七', // 作者简介
   LINK: 'https://tangly1024.com', // 网站地址
   KEYWORDS: 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
