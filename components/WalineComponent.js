1ee6afd6a8c03f97cce5f36e191483e00f0dcfa6
Etherrreal <65111206+lifeafter619@users.noreply.github.com>
2022-12-28 08:02:08 +0800

    Update WalineComponent.js

--- components/WalineComponent.js
+++ components/WalineComponent.js
@@ -77,6 +77,7 @@ const WalineComponent = (props) => {
         emoji: [
           '//unpkg.com/@waline/emojis@1.1.0/weibo',
           '//unpkg.com/@waline/emojis@1.1.0/bilibili',
+        ],
       })
     }
 
