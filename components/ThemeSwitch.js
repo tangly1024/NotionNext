de5d1c258448d43c303eb7169d8df16af5eb5094
tangly1024 <mail@tangly1024.com>
2023-01-01 19:38:01 +0800

    切换主题按钮

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -19,10 +19,9 @@ export function ThemeSwitch() {
                 <div className="p-2 flex items-center">
                     <i className='fas fa-arrows cursor-move px-2' />
                     {/* <div className='uppercase font-sans whitespace-nowrap cursor-pointer ' onClick={switchTheme}> {theme}</div> */}
-                    <select onChange={onSelectChange} name="cars" className='text-white bg-black uppercase cursor-pointer'>
+                    <select value={theme} onChange={onSelectChange} name="cars" className='text-white bg-black uppercase cursor-pointer'>
                         {ALL_THEME.map(t => {
-                          const selected = t === theme
-                          return <option key={t} value={t} selected={selected}>{t}</option>
+                          return <option key={t} value={t}>{t}</option>
                         })}
                     </select>
                 </div>
