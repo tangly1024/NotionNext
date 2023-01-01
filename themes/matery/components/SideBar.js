2440a4591244c601efee14dc44bf2788834e4749
ykxkykx <ykxkykx@foxmail.com>
2023-01-01 14:45:00 +0800

    fix: 修复matery主题下,notion的description内容过长,导致侧边side隐藏时溢出的问题

--- themes/matery/components/SideBar.js
+++ themes/matery/components/SideBar.js
@@ -27,8 +27,8 @@ const SideBar = (props) => {
 
   return (
         <div id='side-bar' className=''>
-            <div className="h-48 w-full bg-indigo-700">
-                <div className='mx-5 pt-6'>
+            <div className="mh-48 w-full bg-indigo-700">
+                <div className='mx-5 pt-6 pb-2'>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img src={siteInfo?.icon} className='cursor-pointer rounded-full' width={80} alt={BLOG.AUTHOR} />
                     <div className='text-white text-xl my-1'>{siteInfo?.title}</div>
@@ -41,7 +41,7 @@ const SideBar = (props) => {
                     const selected = (router.pathname === link.to) || (router.asPath === link.to)
                     return <Link key={link.to} title={link.to} href={link.to} >
                             <a target={link.to.indexOf('http') === 0 ? '_blank' : '_self'}
-                               className={'py-2 px-5 duration-300 text-base justify-between hover:bg-gray-700 hover:text-white hover:shadow-lg cursor-pointer font-light flex flex-nowrap items-center ' +
+                              className={'py-2 px-5 duration-300 text-base justify-between hover:bg-gray-700 hover:text-white hover:shadow-lg cursor-pointer font-light flex flex-nowrap items-center ' +
                                 (selected ? 'bg-indigo-500 text-white ' : ' text-black dark:text-white ')} >
                                 <div className='my-auto items-center justify-between flex '>
                                     <i className={`${link.icon} w-4 ml-3 mr-6 text-center`} />
