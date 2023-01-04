7e238750da1f10cf83d3f3118b54c407847301cd
你好呀 <111191349+88lin@users.noreply.github.com>
2023-01-04 21:11:01 +0800

    Update cursor.js

--- static/cursor.js
+++ static/cursor.js
@@ -40,7 +40,7 @@ class Cursor {
 
         document.body.appendChild((this.scr = document.createElement("style")));
         // 这里改变鼠标指针的颜色 由svg生成
-        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='.5'/></svg>") 4 4, auto}`;
+        this.scr.innerHTML = `* {cursor: url("data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 8 8' width='8px' height='8px'><circle cx='4' cy='4' r='4' opacity='1.0' fill='rgb(57, 197, 187)'/></svg>") 4 4, auto}`;
     }
 
     refresh() {
