dd7e2a6727b04dac50f817d482c9f20bcc21acb6
你好呀 <111191349+88lin@users.noreply.github.com>
2023-01-04 22:11:38 +0800

    Update LayoutBase.js

--- themes/hexo/LayoutBase.js
+++ themes/hexo/LayoutBase.js
@@ -59,7 +59,7 @@ const LayoutBase = props => {
   }
 
   return (
-    <div className="custom-background dark:bg-black">
+    <div className="universe dark:bg-black">
       <CommonHead meta={meta} siteInfo={siteInfo}/>
 
       <TopNav {...props} />
