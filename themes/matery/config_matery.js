c8169f9bde4d5d8267675c45761778b59ee32d30
chenxlong3 <1226608021@qq.com>
2023-01-29 22:50:29 +0800

    matery banner greetings modification

--- themes/matery/config_matery.js
+++ themes/matery/config_matery.js
@@ -1,6 +1,6 @@
 const CONFIG_MATERY = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['Patiently hoping for a Degree', 'Year 1 PhD student in HKUST(GZ)', 'Approximation Algorithms on Graphs', 'Welcome to my Blog'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
 
