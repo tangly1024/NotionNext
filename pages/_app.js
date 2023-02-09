b2d66abb82a3ef87694ec0a6c5bf83f77465734d
你好呀 <111191349+88lin@users.noreply.github.com>
2023-02-09 16:35:21 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,20 +1,14 @@
 import BLOG from 'blog.config'
 import React from 'react'
 import dynamic from 'next/dynamic'
+
 import 'animate.css'
 import '@/styles/globals.css'
+
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import '@/styles/notion.css' //  重写部分样式
 
-
-// 代码样式  更多样式参考： https://github.com/PrismJS/prism-themes
-import 'prism-themes/themes/prism-holi-theme.css'
-// local webfont @see https://fontsource.org/
-import '@fontsource/noto-sans-sc'
-import '@fontsource/noto-serif-sc'
-// local fontawesome 6.2.1 @see https://www.npmjs.com/package/@fortawesome/fontawesome-free
-import '@fortawesome/fontawesome-free/css/all.min.css'
 import { GlobalContextProvider } from '@/lib/global'
 import { DebugPanel } from '@/components/DebugPanel'
 import { ThemeSwitch } from '@/components/ThemeSwitch'
@@ -59,14 +53,6 @@ const MyApp = ({ Component, pageProps }) => {
         {JSON.parse(BLOG.RIBBON) && <Ribbon />}
         <ExternalScript/>
     </>
-
-  // 默认Webfont:  请在font.js文件中检查font-family 新版改从npm本地导入；
-  const DEFAULT_FONTS_URL = [
-    // 'https://fonts.font.im/css2?family=Noto+Sans+SC&display=optional',
-    // 'https://fonts.font.im/css2?family=Noto+Serif+SC&display=optional'
-  ]
-  // 用户指定CUSTOM_FONT 则取CUSTOM_FONT_URL
-  const FONTS_URL = BLOG.CUSTOM_FONT ? BLOG.CUSTOM_FONT_URL : DEFAULT_FONTS_URL
 
   if (isBrowser()) {
     AOS.init()
