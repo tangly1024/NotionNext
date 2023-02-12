d0b15d750ef517f8a52c5f6e81d365c28ef3800c
你好呀 <111191349+88lin@users.noreply.github.com>
2023-02-12 22:30:20 +0800

    Update Sakura.js

--- components/Sakura.js
+++ components/Sakura.js
@@ -2,6 +2,7 @@ if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobi
     // 移动端不显示
 } else {
     // document.write('<canvas id="sakura" style="position:fixed;top:0;left:0;width:100%;height:100%;z-index:-2;pointer-events:none"></canvas>');
+}
 /* eslint-disable */
 import React from 'react'
 
