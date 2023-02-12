365f39d208a3a82fab9e515c4a7f353dd94926f6
你好呀 <111191349+88lin@users.noreply.github.com>
2023-02-12 22:23:48 +0800

    Update Sakura.js

--- components/Sakura.js
+++ components/Sakura.js
@@ -1,3 +1,6 @@
+if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+    // 移动端不显示
+}
 /* eslint-disable */
 import React from 'react'
 
