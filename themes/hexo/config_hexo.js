7e25e4dd1862a6b1f5533a64fb9f9f21b3f717da
派了个萌 <59004461+Pylogmon@users.noreply.github.com>
2023-02-12 09:45:23 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -5,9 +5,9 @@ const CONFIG_HEXO = {
   HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
 
   // 菜单配置
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true, // 列表显示文章封面
