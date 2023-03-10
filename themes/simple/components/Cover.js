7c0907fff8c94011a6ccd0d43407d89109feb9e5
tangly1024.com <tlyong1992@hotmail.com>
2023-03-10 17:22:27 +0800

    loading-cover

--- themes/simple/components/Cover.js
+++ themes/simple/components/Cover.js
@@ -7,7 +7,7 @@ import { useGlobal } from '@/lib/global'
  */
 export const Cover = (props) => {
   const { onLoading } = useGlobal()
-  return <div id='cover-loading' className={`${onLoading ? 'z-50  opacity-50 ' : '-z-10 opacity-0 pointer-events-none'} transition-all duration-200 bg-black fixed top-0 left-0 h-screen w-screen `}>
+  return <div id='cover-loading' className={`${onLoading ? 'z-50 opacity-20' : '-z-10 opacity-0'} pointer-events-none transition-all duration-300 bg-black fixed top-0 left-0 h-screen w-screen `}>
         <div className='w-screen h-screen flex justify-center items-center'>
             <i className="fa-solid fa-spinner text-2xl text-white animate-spin">  </i>
         </div>
