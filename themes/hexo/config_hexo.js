505c969b121660389a583a444a37a17bc0ee63f1
kaedeleo <106600661+kaedeleo1@users.noreply.github.com>
2023-03-17 04:26:05 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -12,10 +12,10 @@ const CONFIG_HEXO = {
   MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
-  POST_LIST_COVER: false, // 列表显示文章封面
-  POST_LIST_COVER_DEFAULT: false, // 封面为空时用站点背景做默认封面
+  POST_LIST_COVER: true, // 列表显示文章封面
+  POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   POST_LIST_SUMMARY: true, // 文章摘要
-  POST_LIST_PREVIEW: true, // 读取文章预览
+  POST_LIST_PREVIEW: false, // 读取文章预览
   POST_LIST_IMG_CROSSOVER: false, // 博客列表图片左右交错
 
   ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
