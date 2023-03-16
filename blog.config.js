1e39f538dcd52ac4ac07723e1a27eb3280704ac7
kaedeleo <106600661+kaedeleo1@users.noreply.github.com>
2023-03-16 15:06:28 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,8 +14,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '猩猩', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '热爱格斗游戏的中年油腻秃顶活肥宅', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xfianliang.xyz', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '格斗游戏,FGC,FightingGame', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
