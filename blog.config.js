ed0ea3e1c9461143bd42a12c456ddec8d736c8db
Yucheng Liu <yucheng.liu@protonmail.com>
2023-03-15 20:54:22 -0400

    giscus

--- blog.config.js
+++ blog.config.js
@@ -197,9 +197,10 @@ const BLOG = {
   COMMENT_GISCUS_REPO:
     process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO || 'yucliu/NotionNext', // 你的Github仓库名 e.g 'tangly1024/NotionNext'
   COMMENT_GISCUS_REPO_ID:
-    process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO_ID || 'yucliu/NotionNext', // 你的Github Repo ID e.g ( 設定完 giscus 即可看到 )
+    process.env.NEXT_PUBLIC_COMMENT_GISCUS_REPO_ID || 'R_kgDOJKDQFw', // 你的Github Repo ID e.g ( 設定完 giscus 即可看到 )
   COMMENT_GISCUS_CATEGORY_ID:
-    process.env.NEXT_PUBLIC_COMMENT_GISCUS_CATEGORY_ID || 'General', // 你的Github Discussions 內的 Category ID ( 設定完 giscus 即可看到 )
+    process.env.NEXT_PUBLIC_COMMENT_GISCUS_CATEGORY_ID ||
+    'DIC_kwDOJKDQF84CU53u', // 你的Github Discussions 內的 Category ID ( 設定完 giscus 即可看到 )
   COMMENT_GISCUS_MAPPING:
     process.env.NEXT_PUBLIC_COMMENT_GISCUS_MAPPING || 'pathname', // 你的Github Discussions 使用哪種方式來標定文章, 預設 'pathname'
   COMMENT_GISCUS_REACTIONS_ENABLED:
