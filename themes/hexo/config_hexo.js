8358b442224efe0329996215141cae84a760ac55
kaedeleo <106600661+kaedeleo1@users.noreply.github.com>
2023-03-17 04:23:32 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -8,12 +8,12 @@ const CONFIG_HEXO = {
   // 菜单配置
   MENU_INDEX: false, // 显示首页
   MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: false, // 显示标签
+  MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
-  POST_LIST_COVER: true, // 列表显示文章封面
-  POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
+  POST_LIST_COVER: false, // 列表显示文章封面
+  POST_LIST_COVER_DEFAULT: false, // 封面为空时用站点背景做默认封面
   POST_LIST_SUMMARY: true, // 文章摘要
   POST_LIST_PREVIEW: true, // 读取文章预览
   POST_LIST_IMG_CROSSOVER: false, // 博客列表图片左右交错
