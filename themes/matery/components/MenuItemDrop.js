8af16870f9bf1ba69e1381164aef132cfbdeb2d1
tangly1024 <mail@tangly1024.com>
2023-03-23 23:04:43 +0800

    微调

--- themes/matery/components/MenuItemDrop.js
+++ themes/matery/components/MenuItemDrop.js
@@ -27,7 +27,7 @@ export const MenuItemDrop = ({ link }) => {
         </>}
 
         {/* 子菜单 */}
-        {hasSubMenu && <ul style={{ 'backdrop-filter': 'blur(3px)' }} className={`${show ? 'visible opacity-100 top-12' : 'invisible opacity-0 top-20'} drop-shadow-md overflow-hidden rounded-md bg-white transition-all duration-300 z-20 absolute block  `}>
+        {hasSubMenu && <ul style={{ backdropFilter: 'blur(3px)' }} className={`${show ? 'visible opacity-100 top-12' : 'invisible opacity-0 top-20'} drop-shadow-md overflow-hidden rounded-md bg-white transition-all duration-300 z-20 absolute block  `}>
             {link.subMenus.map(sLink => {
               return <li key={sLink.id} className='cursor-pointer hover:bg-indigo-300 text-gray-900 hover:text-black tracking-widest transition-all duration-200 dark:border-gray-800  py-1 pr-6 pl-2'>
                     <Link href={sLink.to}>
