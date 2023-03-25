e1f6e874b22b255a885093262c0132f8585e4736
你好呀 <111191349+88lin@users.noreply.github.com>
2023-03-25 21:17:10 +0800

    Update BlogPostCard.js

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -22,9 +22,9 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
         data-aos-once="true"
         data-aos-anchor-placement="top-bottom"
         key={post.id}
-        className={`flex md:flex-row flex-col-reverse bg-[#191919ee] ${CONFIG_HEXO.POST_LIST_IMG_CROSSOVER ? 'even:md:flex-row-reverse' : ''}
+        className={`flex md:flex-row flex-col-reverse ${CONFIG_HEXO.POST_LIST_IMG_CROSSOVER ? 'even:md:flex-row-reverse' : ''}
         w-full justify-between overflow-hidden
-        border dark:border-black rounded-xl`}>
+        border dark:border-black rounded-xl bg-white drak:bg-[#191919ee]`}>
 
         {/* 文字内容 */}
         <BlogPostCardInfo index={index} post={post} showPageCover={showPageCover} showPreview={showPreview} showSummary={showSummary}/>
