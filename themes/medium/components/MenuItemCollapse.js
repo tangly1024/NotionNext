246ae4fa6df60159a3c45fbb669ffb71f5f39d6e
tangly1024 <mail@tangly1024.com>
2023-03-25 15:42:33 +0800

    css fix

--- themes/medium/components/MenuItemCollapse.js
+++ themes/medium/components/MenuItemCollapse.js
@@ -48,7 +48,7 @@ export const MenuItemCollapse = (props) => {
 
         {/* 折叠子菜单 */}
         {hasSubMenu && <Collapse isOpen={isOpen} onHeightChange={props.onHeightChange}>
-            {link.subMenus.map(sLink => {
+            {link?.subMenus?.map(sLink => {
               return <div key={sLink.id} className='
               not:last-child:border-b-0 border-b dark:border-gray-800 py-2 px-14 cursor-pointer hover:bg-gray-100 dark:text-gray-200
               font-extralight dark:bg-black text-left justify-start text-gray-600 bg-gray-50 dark:hover:bg-gray-900 tracking-widest transition-all duration-200'>
