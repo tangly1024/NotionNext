cdcf7b5f0b8cacd7a75ae949a2e5ceea5b2ed750
tangly1024 <mail@tangly1024.com>
2023-03-26 11:47:37 +0800

    simple search 自动focus

--- themes/simple/components/NavBar.js
+++ themes/simple/components/NavBar.js
@@ -52,7 +52,7 @@ export const NavBar = (props) => {
             <div id="nav-bar-inner" className="h-12 mx-auto max-w-9/10 justify-between items-center text-sm md:text-md md:justify-start">
                 {/* 左侧菜单 */}
                 <div className="h-full w-full float-left text-center md:text-left flex flex-wrap items-stretch md:justify-start md:items-start space-x-4">
-                    {showSearchInput && <input id="theme-simple-search" onKeyUp={onKeyUp} className='float-left w-full outline-none h-full px-4' aria-label="Submit search" type="search" name="s" autoComplete="off" placeholder="Type then hit enter to search..." />}
+                    {showSearchInput && <input autoFocus id="theme-simple-search" onKeyUp={onKeyUp} className='float-left w-full outline-none h-full px-4' aria-label="Submit search" type="search" name="s" autoComplete="off" placeholder="Type then hit enter to search..." />}
                     {!showSearchInput && (<MenuList {...props}/>)}
                 </div>
 
