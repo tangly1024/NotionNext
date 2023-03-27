28bd411edab4bf6a81fff87fc81eb502b13ffbbb
tangly1024.com <tlyong1992@hotmail.com>
2023-03-27 12:30:23 +0800

    修复next/example搜索结果内容太长

--- themes/example/components/BlogListPage.js
+++ themes/example/components/BlogListPage.js
@@ -42,7 +42,7 @@ export const BlogListPage = props => {
                         </p>
                             {/* 搜索结果 */}
                         {p.results && (
-                            <p className="mt-4 text-gray-700 dark:text-gray-300 text-sm font-light leading-7">
+                            <p className="p-4-lines mt-4 text-gray-700 dark:text-gray-300 text-sm font-light leading-7">
                             {p.results.map(r => (
                                 <span key={r}>{r}</span>
                             ))}
