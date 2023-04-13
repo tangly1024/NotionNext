90b98d688e5a5117122b1fbcf1b1906cecbb8206
longlife2049 <108611842+longlife2049@users.noreply.github.com>
2023-04-13 10:23:41 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,6 +1,6 @@
 const CONFIG_HEXO = {
   HOME_BANNER_ENABLE: true,
-  HOME_BANNER_GREETINGS: ['Hi，我相信有光', 'Hi，我相信有希望', 'Hi，我相信这个世界', '欢迎来到我的博客我是老胡🎉'], // 首页大图标语文字
+  HOME_BANNER_GREETINGS: ['热爱生活', '热爱彼此', '要相信相信的力量', '欢迎来到我的博客'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
