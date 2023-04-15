7be7a983c44922897a52559a9e4d41fc93e9bacf
cheng <15716317802@163.com>
2023-04-15 16:18:24 +0800

    图片来自微信公众平台 未经允许不可引用

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -24,6 +24,7 @@ const CommonHead = ({ meta, children }) => {
         content="width=device-width, initial-scale=1.0, user-scalable=no"
       />
       <meta name="robots" content="follow, index" />
+      <meta name="referrer" content="never"></meta>
       <meta charSet="UTF-8" />
       {BLOG.SEO_GOOGLE_SITE_VERIFICATION && (
         <meta
