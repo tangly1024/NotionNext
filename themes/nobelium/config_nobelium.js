cd611f538604c44fe4eea21a0ec636aa86cb24d8
析构理想 <wdqjj@qq.com>
2023-04-16 22:20:53 +0800

    Update config_nobelium.js

--- themes/nobelium/config_nobelium.js
+++ themes/nobelium/config_nobelium.js
@@ -1,8 +1,8 @@
 const CONFIG_NOBELIUM = {
 
   // 菜单配置
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
   MENU_RSS: false, // 显示订阅
