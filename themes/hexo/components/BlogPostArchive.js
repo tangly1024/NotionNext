59f7a0db0a116a2ea16678e0b9d0e4d46070aa7a
Vica <vica@sec.ht>
2023-04-18 13:00:28 +0800

    Archive样式调整

--- themes/hexo/components/BlogPostArchive.js
+++ themes/hexo/components/BlogPostArchive.js
@@ -26,14 +26,16 @@ const BlogPostArchive = ({ posts = [], archiveTitle }) => {
               key={post.id}
               className="border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-indigo-500 dark:hover:border-indigo-300 dark:border-indigo-400 transform duration-500"
             >
-                <span className="text-gray-400">{post.date?.start_date}</span>{' '}
-                &nbsp;&#45;
               <div id={post?.date?.start_date}>
+                <span className="text-gray-400">{post.date?.start_date}</span>{' '}
+                &nbsp;
                 <Link
                   href={`${BLOG.SUB_PATH}/${post.slug}`}
                   passHref
                   className="dark:text-gray-400  dark:hover:text-indigo-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600">
-                  {post.title}
+
+                   &#12304;{post.title}&#12305;
+
                 </Link>
               </div>
             </li>
