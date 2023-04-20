89e2be59462de22a5faf2ccc213e9a0c190284a9
ghstaytrue <ghstaytrue@users.noreply.github.com>
2023-04-20 14:13:10 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -105,7 +105,7 @@ const BLOG = {
   POST_LIST_STYLE: process.env.NEXT_PUBLIC_PPOST_LIST_STYLE || 'scroll', // ['page','scroll] 文章列表样式:页码分页、单页滚动加载
   POST_LIST_PREVIEW: process.env.NEXT_PUBLIC_POST_PREVIEW || 'false', //  是否在列表加载文章预览
   POST_PREVIEW_LINES: 12, // 预览博客行数
-  POST_RECOMMEND_COUNT: 3, // 推荐文章数量
+  POST_RECOMMEND_COUNT: 6, // 推荐文章数量
   POSTS_PER_PAGE: 6, // post counts per page
   POSTS_SORT_BY: process.env.NEXT_PUBLIC_POST_SORT_BY || 'notion', // 排序方式 'date'按时间,'notion'由notion控制
 
