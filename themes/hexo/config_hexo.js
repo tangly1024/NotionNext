22ada17695fa46d289469ad275f30af1d1a4d7aa
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-04-21 19:57:42 +0800

    Update config_hexo.js

--- themes/hexo/config_hexo.js
+++ themes/hexo/config_hexo.js
@@ -1,5 +1,5 @@
 const CONFIG_HEXO = {
-  HOME_BANNER_ENABLE: false,
+  HOME_BANNER_ENABLE: true,
   HOME_BANNER_GREETINGS: ['Hi，我是Matrix Core', '欢迎来到我的星球🚀'], // 首页大图标语文字
 
   HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
