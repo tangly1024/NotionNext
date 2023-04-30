b1b15f196f6ceb48c4cd4d1c1059c26417d07dc1
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-04-30 17:41:33 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,25 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+WIDGET = {
+  "CONFIG": {
+    "modules": "01234",
+    "background": "5",
+    "tmpColor": "FFFFFF",
+    "tmpSize": "16",
+    "cityColor": "FFFFFF",
+    "citySize": "16",
+    "aqiColor": "FFFFFF",
+    "aqiSize": "16",
+    "weatherIconSize": "24",
+    "alertIconSize": "18",
+    "padding": "10px 10px 10px 10px",
+    "shadow": "0",
+    "language": "auto",
+    "borderRadius": "5",
+    "fixed": "true",
+    "vertical": "top",
+    "horizontal": "left",
+    "left": "10",
+    "top": "10",
+    "key": "5d2f959c859c4bc5b4879522f3e67202"
+  }
+}
