2b8d37c3af188f5e1b198e85012abf646e735a47
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-04-30 18:00:56 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,29 +1,27 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-<div id="he-plugin-simple"></div>
-<script>
-WIDGET = {
-  "CONFIG": {
-    "modules": "01234",
-    "background": "5",
-    "tmpColor": "FFFFFF",
-    "tmpSize": "16",
-    "cityColor": "FFFFFF",
-    "citySize": "16",
-    "aqiColor": "FFFFFF",
-    "aqiSize": "16",
-    "weatherIconSize": "24",
-    "alertIconSize": "18",
-    "padding": "10px 10px 10px 10px",
-    "shadow": "0",
-    "language": "auto",
-    "borderRadius": "5",
-    "fixed": "true",
-    "vertical": "top",
-    "horizontal": "left",
-    "left": "10",
-    "top": "10",
-    "key": "5d2f959c859c4bc5b4879522f3e67202"
-  }
-}
-</script>
-<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>
+document.writeln("<div id=\"he-plugin-simple\"></div>");
+document.writeln("<script>");
+document.writeln("WIDGET = {");
+document.writeln("  \"CONFIG\": {");
+document.writeln("    \"modules\": \"01234\",");
+document.writeln("    \"background\": \"1\",");
+document.writeln("    \"tmpColor\": \"FFFFFF\",");
+document.writeln("    \"tmpSize\": \"16\",");
+document.writeln("    \"cityColor\": \"FFFFFF\",");
+document.writeln("    \"citySize\": \"16\",");
+document.writeln("    \"aqiColor\": \"FFFFFF\",");
+document.writeln("    \"aqiSize\": \"16\",");
+document.writeln("    \"weatherIconSize\": \"24\",");
+document.writeln("    \"alertIconSize\": \"18\",");
+document.writeln("    \"padding\": \"10px 10px 10px 10px\",");
+document.writeln("    \"shadow\": \"0\",");
+document.writeln("    \"language\": \"auto\",");
+document.writeln("    \"borderRadius\": \"5\",");
+document.writeln("    \"fixed\": \"false\",");
+document.writeln("    \"vertical\": \"top\",");
+document.writeln("    \"horizontal\": \"left\",");
+document.writeln("    \"key\": \"193a00908d2941b5af58d2697d724946\"");
+document.writeln("  }");
+document.writeln("}");
+document.writeln("</script>");
+document.writeln("<script src=\"https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0\"></script>");
