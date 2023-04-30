d0c4075b7bd59cbeb75100ec008acb4c05f11a82
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-04-30 17:48:38 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,4 +1,6 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+<div id="he-plugin-simple"></div>
+<script>
 WIDGET = {
   "CONFIG": {
     "modules": "01234",
@@ -23,3 +25,5 @@ WIDGET = {
     "key": "5d2f959c859c4bc5b4879522f3e67202"
   }
 }
+</script>
+<script src="https://widget.qweather.net/simple/static/js/he-simple-common.js?v=2.0"></script>
