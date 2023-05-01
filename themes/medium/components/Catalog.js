4083c2cd3a3969612e171002dfd897a2ecd03d76
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-05-01 11:42:55 +0800

    Update Catalog.js

--- themes/medium/components/Catalog.js
+++ themes/medium/components/Catalog.js
@@ -76,7 +76,7 @@ const Catalog = ({ toc }) => {
               notion-table-of-contents-item-indent-level-${tocItem.indentLevel} `}
             >
               <span style={{ display: 'inline-block', marginLeft: tocItem.indentLevel * 16 }}
-                className={`${activeSection === id && ' font-bold text-green-500 underline'}`}
+                className={`${activeSection === id && ' font-bold text-rose-500 underline'}`}
               >
                 {tocItem.text}
               </span>
