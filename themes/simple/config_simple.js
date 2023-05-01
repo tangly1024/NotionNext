678923a6ee41a96ff4cc446bd495a01234156228
zlt0917 <87749893+zlt0917@users.noreply.github.com>
2023-05-01 16:50:12 +0800

    Update config_simple.js

--- themes/simple/config_simple.js
+++ themes/simple/config_simple.js
@@ -1,7 +1,7 @@
 const CONFIG_SIMPLE = {
 
-  //LOGO_IMG: '/Logo.webp',
-  LOGO_IMG: 'getimg.gif',
+  LOGO_IMG: '/Logo.webp',
+  //LOGO_IMG: 'getimg.gif',
   
   TOP_BAR: true, // 显示顶栏
   TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
