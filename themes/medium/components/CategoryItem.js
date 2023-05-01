d231ca019831a8ccab61b9f74b33afd9fe25fc99
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-05-01 13:38:58 +0800

    Update CategoryItem.js

--- themes/medium/components/CategoryItem.js
+++ themes/medium/components/CategoryItem.js
@@ -6,8 +6,8 @@ export default function CategoryItem ({ selected, category, categoryCount }) {
       href={`/category/${category}`}
       passHref
       className={(selected
-        ? 'hover:text-white dark:hover:text-white bg-green-600 text-white '
-        : 'dark:text-green-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-green-600') +
+        ? 'hover:text-white dark:hover:text-white bg-rose-400 text-white '
+        : 'dark:text-sky-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-sky-200') +
       ' flex text-sm items-center duration-300 cursor-pointer py-1 font-light px-2 whitespace-nowrap'}>
 
       <div><i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category} {categoryCount && `(${categoryCount})`}
