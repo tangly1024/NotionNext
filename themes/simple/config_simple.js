c1e37699ef8140091aa2e42180961a8318ad1e76
712346867 <35997541+712346867@users.noreply.github.com>
2023-05-10 13:55:35 +0800

    Update config_simple.js

--- themes/simple/config_simple.js
+++ themes/simple/config_simple.js
@@ -11,6 +11,6 @@ const CONFIG_SIMPLE = {
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: true // 显示搜索
+  MENU_SEARCH: false // 显示搜索
 }
 export default CONFIG_SIMPLE
