1c0a32b7f7baeeb397c7022fc8d142c45eebc4c6
712346867 <35997541+712346867@users.noreply.github.com>
2023-05-10 11:13:00 +0800

    Update config_nobelium.js

--- themes/nobelium/config_nobelium.js
+++ themes/nobelium/config_nobelium.js
@@ -1,9 +1,9 @@
 const CONFIG_NOBELIUM = {
 
   // 菜单配置
-  MENU_CATEGORY: false, // 显示分类
+  MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: false, // 显示归档
+  MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
   MENU_RSS: false, // 显示订阅
 
