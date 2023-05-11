c2aadbec9582f536d5180f38bfe54d06210efdd5
guolong <guolong.deng@hand-china.com>
2023-05-11 15:17:47 +0800

    update: blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'guolong', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'No body is perfect, try to be nobody', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.deng.gl', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'deng.gl', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'guolong613@gmail.com', // 邮箱地址 例如mail@tangly1024.com
