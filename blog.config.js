d5789a36756548ea02071841feae90400acf92af
712346867 <35997541+712346867@users.noreply.github.com>
2023-05-13 00:06:49 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Sun', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '平静的努力，精致的执着🍚', // 作者 简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '平静的努力，精致的执着🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.ssuncz.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小太阳昌仔blog, 小太阳昌仔, 小太阳昌仔博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
