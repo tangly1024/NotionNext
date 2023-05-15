6aa67630edb990034c383ec5db81471cd8545e6f
ku <50163359+The-GO@users.noreply.github.com>
2023-05-16 07:11:46 +0800

    Update config_medium.js

--- themes/medium/config_medium.js
+++ themes/medium/config_medium.js
@@ -5,14 +5,14 @@ const CONFIG_MEDIUM = {
 
   POST_LIST_COVER: true, // 文章列表显示图片封面
   POST_LIST_PREVIEW: true, // 列表显示文章预览
-  POST_LIST_CATEGORY: true, // 列表显示文章分类
+  POST_LIST_CATEGORY: false, // 列表显示文章分类
   POST_LIST_TAG: true, // 列表显示文章标签
 
-  POST_DETAIL_CATEGORY: true, // 文章显示分类
+  POST_DETAIL_CATEGORY: false, // 文章显示分类
   POST_DETAIL_TAG: true, // 文章显示标签
 
   // 菜单
-  MENU_CATEGORY: true, // 显示分类
+  MENU_CATEGORY: false, // 显示分类
   MENU_TAG: true, // 显示标签
   MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
