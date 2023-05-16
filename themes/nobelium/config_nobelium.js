a60f1d2ff3687cb3a79743b26d4d9f2169f2d357
ku <50163359+The-GO@users.noreply.github.com>
2023-05-16 18:32:01 +0800

    Update config_nobelium.js

--- themes/nobelium/config_nobelium.js
+++ themes/nobelium/config_nobelium.js
@@ -2,7 +2,7 @@ const CONFIG_NOBELIUM = {
 
   // 菜单配置
   MENU_CATEGORY: false, // 显示分类
-  MENU_TAG: true, // 显示标签
+  MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
   MENU_RSS: false, // 显示订阅
