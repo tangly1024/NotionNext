57d0f8d85d85bb21308c8722577394a260b0bd73
ku <50163359+The-GO@users.noreply.github.com>
2023-05-18 20:02:34 +0800

    Update config_example.js

--- themes/example/config_example.js
+++ themes/example/config_example.js
@@ -3,7 +3,7 @@ const CONFIG_EXAMPLE = {
   MENU_CATEGORY: false, // 显示分类
   MENU_TAG: false, // 显示标签
   MENU_ARCHIVE: false, // 显示归档
-  MENU_SEARCH: false, // 显示搜索
+  MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true // 列表显示文章封面
 
