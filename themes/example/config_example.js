aeca2ee82c9b2d0c74221c8b1b4d71c78f72ba38
ku <50163359+The-GO@users.noreply.github.com>
2023-05-18 20:05:47 +0800

    Update config_example.js

--- themes/example/config_example.js
+++ themes/example/config_example.js
@@ -1,8 +1,8 @@
 const CONFIG_EXAMPLE = {
   // 菜单配置
-  MENU_CATEGORY: false, // 显示分类
-  MENU_TAG: false, // 显示标签
-  MENU_ARCHIVE: false, // 显示归档
+  MENU_CATEGORY: true, // 显示分类
+  MENU_TAG: true, // 显示标签
+  MENU_ARCHIVE: true, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true // 列表显示文章封面
