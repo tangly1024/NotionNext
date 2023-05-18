6ed8e94a66d7bdc93f67bb85ed9e6086b2d15b74
ku <50163359+The-GO@users.noreply.github.com>
2023-05-18 23:55:14 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -130,7 +130,7 @@ const BLOG = {
   SAKURA: process.env.NEXT_PUBLIC_SAKURA || true, // 开关
 
   // 漂浮线段特效
-  NEST: process.env.NEXT_PUBLIC_NEST || false, // 开关
+  NEST: process.env.NEXT_PUBLIC_NEST || true, // 开关
 
   // 动态彩带特效
   FLUTTERINGRIBBON: process.env.NEXT_PUBLIC_FLUTTERINGRIBBON || false, // 开关
