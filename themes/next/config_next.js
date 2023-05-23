7de9515fdb71520babc14f52bd036c42eca8feca
七三 <javen73@qq.com>
2023-05-23 16:18:58 +0800

    Update config_next.js
    1

--- themes/next/config_next.js
+++ themes/next/config_next.js
@@ -1,7 +1,7 @@
 const CONFIG_NEXT = {
 
   HOME_BANNER: false, // 首页是否显示大图及标语 [true,false]
-  HOME_BANNER_Strings: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  HOME_BANNER_Strings: ['晚天长，秋水苍，山腰落日，烟霞掩斜阳🌇', '烟霞问讯，⾵月相知🌙', '迷失的人迷失了，相逢的人再相逢👣'], // 首页大图标语文字
 
   NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
