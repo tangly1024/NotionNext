6bff4ccfffc05047c8449b599e44efd48e0b3f7b
happyzhangyyds <119606309+happyzhangyyds@users.noreply.github.com>
2023-05-27 14:30:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -179,7 +179,7 @@ const BLOG = {
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || true, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
-      process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'tecent', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
+      process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'tencent', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
   MUSIC_PLAYER_METING_ID:
       process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_ID || '8556483453', // 对应歌单的 id
   MUSIC_PLAYER_METING_LRC_TYPE:
