5ba286b4f64d7e807a88aa4e602b56681947f02f
Mortal <114851291+Ghlerrix@users.noreply.github.com>
2023-06-09 15:34:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ghlerrix', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '山高水长，怕什么来不及，慌什么到不了。', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ghlerrix.eu.org', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ghlerrix.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'ghlerrix@outlook.com', // 邮箱地址 例如mail@tangly1024.com
