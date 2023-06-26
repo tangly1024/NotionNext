e436af8b16e9ea498e74ef7620457b407fc9c25c
geeks7i <97093581+geeks7i@users.noreply.github.com>
2023-06-27 00:10:36 +0800

    Update config_simple.js

--- themes/simple/config_simple.js
+++ themes/simple/config_simple.js
@@ -3,7 +3,7 @@ const CONFIG_SIMPLE = {
   LOGO_IMG: '/Logo.webp',
   TOP_BAR: true, // 显示顶栏
   TOP_BAR_CONTENT: process.env.NEXT_PUBLIC_THEME_SIMPLE_TOP_TIPS || '',
-  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>编程爱好者<br/>/互联网从业者<br/>/知识分享博主</div>',
+  LOGO_DESCRIPTION: process.env.NEXT_PUBLIC_THEME_SIMPLE_LOGO_DESCRIPTION || '<div>云原生练习生<br/>网络保安<br/></div>',
 
   AUTHOR_LINK: process.env.NEXT_PUBLIC_AUTHOR_LINK || '#',
 
