1c8519c0d1341eb3191513efa1e2325e63412ef8
mimiqiao <mimiqiao@gmail.com>
2023-06-27 11:15:21 +0800

    Update blog.config.js
    RSS 修改头像

--- blog.config.js
+++ blog.config.js
@@ -19,8 +19,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Way to AGI', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'An AI learning wiki', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'http://waytoAGI.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, AI, AGI, Blog, wiki', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'http://blog.waytoAGI.com', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, AI, AGI, Blog, wiki, 知识库, AI知识', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
