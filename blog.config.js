5b8d450127b74ab09ef1213f5eddb0813396f117
zhengchao <21402416@qq.com>
2023-06-27 16:45:29 +0800

    345

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Duly-health', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'porter', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.dulyhealth.site/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.dulyhealth.site', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'health, blog', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
