1c7040f680c064b7df86de8d3c0c4730c93909fd
find-xposed-magisk <38677638+find-xposed-magisk@users.noreply.github.com>
2023-07-05 02:08:34 +0800

    更新 ArticleDetail.js

--- themes/next/components/ArticleDetail.js
+++ themes/next/components/ArticleDetail.js
@@ -64,8 +64,8 @@ export default function ArticleDetail(props) {
                                 <span className='mr-2'> | <i className='far fa-calendar-check mr-2' />{post.lastEditedTime} </span>
 
                                 <div className="hidden busuanzi_container_page_pv font-light mr-2">
-                                    <i className='mr-1 fas fa-eye' />
-                                    <span className="mr-2 busuanzi_value_page_pv" />
+								{/* <i className='mr-1 fas fa-eye' />
+								<span className="mr-2 busuanzi_value_page_pv" /> */}
                                 </div>
 
                             </>)}
