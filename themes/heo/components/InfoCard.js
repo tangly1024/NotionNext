87fad7015bf681f5599a1cfbf8012c897638d2d6
tangly1024 <mail@tangly1024.com>
2023-07-11 22:38:02 +0800

    heo-frameworkd

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -15,7 +15,7 @@ export function InfoCard(props) {
   return (
         <Card className={className}>
             <div
-                className='justify-center items-center flex py-6 dark:text-gray-100  transform duration-200 cursor-pointer'
+                className='justify-center items-center flex py-6 dark:text-gray-100 transform duration-200 cursor-pointer'
                 onClick={() => {
                   router.push('/')
                 }}
