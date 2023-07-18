44ed2e6606b54344f513370379d030076bc42f50
xiaojie555 <116651629+xiaojie555@users.noreply.github.com>
2023-07-18 16:45:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,20 +1,20 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-05-25', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: 'Hi,欢迎来到不二周的万事屋,这里收藏了一些优质的资源,如果你觉得不错,可以收藏本站哦🎉', url: 'https://buerzhou.top/' },
+     }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HERO_TITLE_1: '不二周的万事屋',
+  HERO_TITLE_2: '资源分享',
+  HERO_TITLE_3: 'buerzhou.top',
+  HERO_TITLE_4: '本站说明',
+  HERO_TITLE_5: '专享通道',
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
@@ -22,13 +22,12 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好！我是不二周',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
+    '🤝 互联网科技爱好',
+    '🏃 喜欢阅读跟运动',
     '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
   ],
 
   // 用户技能图标
