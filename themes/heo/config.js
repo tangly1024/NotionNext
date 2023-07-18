85ac36b6ac1dd7137f38405270e6824e1478f9fb
你好呀 <111191349+88lin@users.noreply.github.com>
2023-07-18 17:49:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,16 +5,16 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到故人博客', url: 'https://88lin.eu.org' },
+    { title: '点击使用ChatGPT', url: 'https://ai.88lin.eu.org' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
+  HERO_TITLE_1: '分享科技',
   HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
+  HERO_TITLE_3: '88LIN.EU.COM',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HERO_TITLE_5: '生活明朗 万物可爱',
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
