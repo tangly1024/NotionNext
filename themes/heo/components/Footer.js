7ddfe226cf000d3c920e30177fd945e7631e52c4
tangly1024.com <tlyong1992@hotmail.com>
2023-07-19 14:55:27 +0800

    备案

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -37,19 +37,23 @@ const Footer = ({ title }) => {
                     <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' /> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.
                 </div>
 
-                <div id='footer-bottom-right'>
-                    {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></>}
+                {BLOG.BEI_AN && (<div id='footer-bottom-center'>
+                    <a href="https://beian.miit.gov.cn/" className="text-black dark:text-gray-200 no-underline hover:underline ml-4">{BLOG.BEI_AN} </a>
+                </div>)}
 
-                    <span className='hidden busuanzi_container_site_pv'>
-                        <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
-                    <span className='pl-2 hidden busuanzi_container_site_uv'>
-                        <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
+            <div id='footer-bottom-right'>
+                {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></>}
 
-                    {/* <h1 className='text-xs text-light-400 dark:text-gray-400'>{title} | {BLOG.BIO}</h1> */}
+                <span className='hidden busuanzi_container_site_pv'>
+                    <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
+                <span className='pl-2 hidden busuanzi_container_site_uv'>
+                    <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
 
-                </div>
+                {/* <h1 className='text-xs text-light-400 dark:text-gray-400'>{title} | {BLOG.BIO}</h1> */}
+
             </div>
-        </footer>
+        </div>
+        </footer >
   )
 }
 
