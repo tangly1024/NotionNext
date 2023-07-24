d5c36b49f35eb2794a28cf0879817e2169f3d016
Charliefoo <cainefox@gmail.com>
2023-07-24 13:18:55 +0800

    Update config.js
    修改heo主题配置

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,26 +1,25 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2019-04-11', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '使用Notion搭建个人博客', url: 'https://www.chawfoo.com/article/blog1' },
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
+  HERO_TITLE_1: '分享设计',
   HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_3: 'chawfoo.com',
+  HERO_TITLE_4: 'AIGC初体验',
+  HERO_TITLE_5: '可能是一个新时代的开端',
+  HERO_TITLE_LINK: 'https://www.chawfoo.com/article/design3',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '技术分享', url: '/tag/技术分享' },
+  HERO_CATEGORY_2: { title: 'Design', url: '/tag/Design' },
+  HERO_CATEGORY_3: { title: '诗歌鉴赏', url: '/tag/诗歌鉴赏' },
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
@@ -32,7 +31,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://github.com/Chacat68', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
@@ -94,7 +93,7 @@ const CONFIG = {
     }
   ],
 
-  SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   SOCIAL_CARD_TITLE_1: '交流频道',
   SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   SOCIAL_CARD_TITLE_3: '点击加入社群',
