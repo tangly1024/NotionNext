73f5db948eb3da096d145880d760ff7998df4c72
Charliefoo <cainefox@gmail.com>
2023-07-25 17:20:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '查卫风', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Chawf', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '谦卑地去理解新世界，适应即将到来的变化', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.chawfoo.com/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ai, game', // 网站关键词 英文逗号隔开
