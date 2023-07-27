aa0568bf73745f4cfefe558b50298b73314ae394
Breadth <76241295+daitechlab@users.noreply.github.com>
2023-07-27 09:05:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,130 +1,128 @@
 const CONFIG = {
-  HOME_BANNER_ENABLE: true,
+  HOME_BANNER_ENABLE: true，
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2021-09-21'， // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  ]，
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '志同者'，
+  HERO_TITLE_2: '不以山海为远'，
+  HERO_TITLE_3: 'breadth.top'，
+  HERO_TITLE_4: '新版上线'，
+  HERO_TITLE_5: 'Obsidian 数字花园'，
+  HERO_TITLE_LINK: 'https://zuji.netlify.app'，
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '必看精选'， url: '/tag/必看精选' }，
+  HERO_CATEGORY_2: { title: '热门文章'， url: '/tag/热门文章' }，
+  HERO_CATEGORY_3: { title: '实用教程'， url: '/tag/实用教程' }，
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
+    '你好！我是'，
+    '🔍 分享与热心帮助'，
+    '🤝 专修交互与设计'，
+    '🏃 脚踏实地行动派'，
+    '🏠 智能家居小能手'，
+    '🤖️ 数码科技爱好者'，
     '🧱 团队小组发动机'
-  ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  ]，
+  INFO_CARD_URL: 'https://github.com/daitechlab/NotionNext'， // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_1: 'AfterEffect'，
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp'，
+      color_1: '#989bf8'，
+      title_2: 'Sketch'，
+      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp'，
       color_2: '#ffffff'
-    },
+    }，
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      title_1: 'Docker'，
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp'，
+      color_1: '#57b6e6'，
+      title_2: 'Photoshop'，
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp'，
       color_2: '#4082c3'
-    },
+    }，
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
-      color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_1: 'FinalCutPro'，
+      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp'，
+      color_1: '#ffffff'，
+      title_2: 'Python'，
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp'，
       color_2: '#ffffff'
-    },
+    }，
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+      title_1: 'Swift'，
+      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp'，
+      color_1: '#eb6840'，
+      title_2: 'Principle'，
+      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp'，
       color_2: '#8f55ba'
-    },
+    }，
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      title_1: 'illustrator'，
+      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp'，
+      color_1: '#f29e39'，
+      title_2: 'CSS3'，
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp'，
       color_2: '#2c51db'
-    },
+    }，
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      title_1: 'JS'，
+      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp'，
+      color_1: '#f7cb4f'，
+      title_2: 'HTML'，
+      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp'，
       color_2: '#e9572b'
-    },
+    }，
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      title_1: 'Git'，
+      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp'，
+      color_1: '#df5b40'，
+      title_2: 'Rhino'，
+      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp'，
       color_2: '#1f1f1f'
     }
-  ],
+  ]，
 
-  SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  SOCIAL_CARD_TITLE_1: '交流频道',
-  SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  SOCIAL_CARD_TITLE_3: '点击加入社群',
-  SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  SOCIAL_CARD: false， // 是否显示右侧，点击加入社群按钮
+  SOCIAL_CARD_TITLE_1: '交流频道'，
+  SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享'，
+  SOCIAL_CARD_TITLE_3: '点击加入社群'，
+  SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question'，
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
-  MENU_INDEX: true, // 显示首页
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: true, // 显示搜索
+  MENU_INDEX: true， // 显示首页
+  MENU_CATEGORY: true， // 显示分类
+  MENU_TAG: true， // 显示标签
+  MENU_ARCHIVE: true， // 显示归档
+  MENU_SEARCH: true， // 显示搜索
 
-  POST_LIST_COVER: true, // 列表显示文章封面
-  POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  POST_LIST_COVER: true， // 列表显示文章封面
+  POST_LIST_COVER_HOVER_ENLARGE: false， // 列表鼠标悬停放大
 
-  POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
-  POST_LIST_SUMMARY: true, // 文章摘要
-  POST_LIST_PREVIEW: false, // 读取文章预览
-  POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
+  POST_LIST_COVER_DEFAULT: true， // 封面为空时用站点背景做默认封面
+  POST_LIST_SUMMARY: true， // 文章摘要
+  POST_LIST_PREVIEW: false， // 读取文章预览
+  POST_LIST_IMG_CROSSOVER: true， // 博客列表图片左右交错
 
-  ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
-  ARTICLE_COPYRIGHT: true, // 显示文章版权声明
-  ARTICLE_RECOMMEND: true, // 文章关联推荐
+  ARTICLE_ADJACENT: true， // 显示上一篇下一篇文章推荐
+  ARTICLE_COPYRIGHT: true， // 显示文章版权声明
+  ARTICLE_RECOMMEND: true， // 文章关联推荐
 
-  WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  WIDGET_ANALYTICS: false, // 显示统计卡
-  WIDGET_TO_TOP: true,
-  WIDGET_TO_COMMENT: true, // 跳到评论区
-  WIDGET_DARK_MODE: true, // 夜间模式
+  WIDGET_LATEST_POSTS: true， // 显示最新文章卡
+  WIDGET_ANALYTICS: false， // 显示统计卡
+  WIDGET_TO_TOP: true，
+  WIDGET_TO_COMMENT: true， // 跳到评论区
+  WIDGET_DARK_MODE: true， // 夜间模式
   WIDGET_TOC: true // 移动端悬浮目录
 }
-export default CONFIG
+export 默认 CONFIG
