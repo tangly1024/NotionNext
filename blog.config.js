c010a462f517835aeb51a03a109be9b47d58bcab
12434565 <94098611+12434565@users.noreply.github.com>
2023-07-27 08:57:47 +0800

    Add files via upload
    把blog.config.js搞坏了，重新复制了一下，给最后的地方加了注释

--- blog.config.js
+++ blog.config.js
@@ -375,7 +375,7 @@ const BLOG = {
 
   //   ********挂件组件相关********
 // Chatbase
-CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || null, // 是否显示chatbase机器人 https://www.chatbase.co/
+//CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || null, // 是否显示chatbase机器人 https://www.chatbase.co/
   
 }
 
