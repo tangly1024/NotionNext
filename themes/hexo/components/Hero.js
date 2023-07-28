08218c484c9b820c3f756626994c81d319aae81a
tangly1024 <mail@tangly1024.com>
2023-07-28 16:23:49 +0800

    是否显示开始阅读按钮

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -68,7 +68,7 @@ const Hero = props => {
 
                 {/* 滚动按钮 */}
                 <div onClick={scrollToWrapper} className="z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white">
-                    <div className="opacity-70 animate-bounce text-xs">{locale.COMMON.START_READING}</div>
+                    <div className="opacity-70 animate-bounce text-xs">{CONFIG.SHOW_START_READING && locale.COMMON.START_READING}</div>
                     <i className='opacity-70 animate-bounce fas fa-angle-down' />
                 </div>
             </div>
--- themes/matery/config.js
+++ themes/matery/config.js
@@ -6,6 +6,9 @@ const CONFIG = {
   HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
 
+  // 是否显示开始阅读按钮
+  SHOW_START_READING: true,
+
   // 菜单配置
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
