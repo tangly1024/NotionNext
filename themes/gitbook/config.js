60253da7182bfd41549115977ff18f63680979dc
Dreamin <92585241+Dreamin121@users.noreply.github.com>
2023-07-28 16:38:27 +0800

    Update config.js

--- themes/gitbook/config.js
+++ themes/gitbook/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
 
-  INDEX_PAGE: 'about', // 文档首页显示的文章，请确此路径包含在您的notion数据库中
+  INDEX_PAGE: 'note', // 文档首页显示的文章，请确此路径包含在您的notion数据库中
 
   // 菜单
-  MENU_CATEGORY: true, // 显示分类
-  MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
-  MENU_SEARCH: true, // 显示搜索
+  MENU_CATEGORY: false, // 显示分类
+  MENU_TAG: false, // 显示标签
+  MENU_ARCHIVE: false, // 显示归档
+  MENU_SEARCH: false, // 显示搜索
 
   // Widget
   WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
