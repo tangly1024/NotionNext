e0412ebd69ce61fad220192ddc5849a87dcc9e35
lifeafter619 <65111206+lifeafter619@users.noreply.github.com>
2023-07-30 21:23:15 +0800

    heo

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,36 +6,37 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://66619.eu.org' },
-    { title: '【最近更新】关于Blind-Aihelper的一切', url: 'https://66619.eu.org/blind-helper' }
+    { title: '【最近更新】关于Blind-Aihelper的一切', url: '/blind-helper-everything' }
+    { title: '我好想看到小可爱给我的blog留个言，戳我直达~', url: '/message' }
   ],
 
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: '分享技术',
   HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_3: '619的blog',
+  HERO_TITLE_4: '最近更新',
+  HERO_TITLE_5: 'Blind-Helper的一切',
+  HERO_TITLE_LINK: 'https://66619.eu.org/blind-helper-everything',
 
   // 英雄区显示三个置顶分类
-  HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_1: { title: '学习思考', url: '/category/学习思考' },
+  HERO_CATEGORY_2: { title: '技术分享', url: '/category/技术分享' },
+  HERO_CATEGORY_3: { title: '班级相关', url: '/category/班级相关' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HERO_RECOMMEND_POST_TAG: '',
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   INFOCARD_GREETINGS: [
-    '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '你好！我是@619',
+    '🔍 热心分享与帮助',
+    '🤝 专修网络与安全',
     '🏃 脚踏实地行动派',
     '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  INFO_CARD_URL: 'https://github.com/lifeafter619', // 个人资料底部按钮链接
 
   // 用户技能图标
   GROUP_ICONS: [
@@ -98,10 +99,10 @@ const CONFIG = {
   ],
 
   SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  SOCIAL_CARD_TITLE_1: '交流频道',
-  SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  SOCIAL_CARD_TITLE_3: '点击加入社群',
-  SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  SOCIAL_CARD_TITLE_1: '想与我交流交流？',
+  SOCIAL_CARD_TITLE_2: '直接来本站留言版找我玩叭~',
+  SOCIAL_CARD_TITLE_3: '戳我直达☞',
+  SOCIAL_CARD_URL: 'https://66619.eu.org/message',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
