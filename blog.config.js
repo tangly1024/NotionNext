0ae88e975ad423a8beea33a78dee4cded1db70c7
tangly1024 <mail@tangly1024.com>
2023-07-31 22:17:10 +0800

    Ackee

--- blog.config.js
+++ blog.config.js
@@ -309,12 +309,10 @@ const BLOG = {
   ANALYTICS_CNZZ_ID: process.env.NEXT_PUBLIC_ANALYTICS_CNZZ_ID || '', // 只需要填写站长统计的id, [cnzz_id] -> https://s9.cnzz.com/z_stat.php?id=[cnzz_id]&web_id=[cnzz_id]
   ANALYTICS_GOOGLE_ID: process.env.NEXT_PUBLIC_ANALYTICS_GOOGLE_ID || '', // 谷歌Analytics的id e.g: G-XXXXXXXXXX
 
-  ANALYTICS_ACKEE_TRACKER:
-        process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_TRACKER || '', // e.g 'https://ackee.tangly1024.net/tracker.js'
-  ANALYTICS_ACKEE_DATA_SERVER:
-        process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DATA_SERVER || '', // e.g https://ackee.tangly1024.net , don't end with a slash
-  ANALYTICS_ACKEE_DOMAIN_ID:
-        process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DOMAIN_ID || '', // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
+  // ACKEE网站访客统计工具
+  ANALYTICS_ACKEE_TRACKER: process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_TRACKER || 'https://cdn.jsdelivr.net/npm/ackee-tracker@5.1.0/dist/ackee-tracker.min.js', // e.g 'https://ackee.tangly1024.net/tracker.js'
+  ANALYTICS_ACKEE_DATA_SERVER: process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DATA_SERVER || '', // e.g https://ackee.tangly1024.net , don't end with a slash
+  ANALYTICS_ACKEE_DOMAIN_ID: process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DOMAIN_ID || '', // e.g '0e2257a8-54d4-4847-91a1-0311ea48cc7b'
 
   SEO_GOOGLE_SITE_VERIFICATION:
         process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
--- components/Ackee.js
+++ components/Ackee.js
@@ -1,19 +1,28 @@
 // import { useRouter } from 'next/router'
-// import BLOG from '@/blog.config'
+import BLOG from '@/blog.config'
+import { loadExternalResource } from '@/lib/utils'
+import { useEffect } from 'react'
 
 const Ackee = () => {
-//   const router = useRouter()
-//   useAckee(
-//     router.asPath,
-//     {
-//       server: BLOG.ANALYTICS_ACKEE_DATA_SERVER,
-//       domainId: BLOG.ANALYTICS_ACKEE_DOMAIN_ID
-//     },
-//     {
-//       detailed: false,
-//       ignoreLocalhost: true
-//     }
-//   )
+  useEffect(() => {
+    loadExternalResource(BLOG.ANALYTICS_ACKEE_TRACKER, 'js').then(url => {
+      const ackeeTracker = window.ackeeTracker
+      console.log('ackeeTracker', ackeeTracker)
+    })
+  })
+
+  //   const router = useRouter()
+  //   useAckee(
+  //     router.asPath,
+  //     {
+  //       server: BLOG.ANALYTICS_ACKEE_DATA_SERVER,
+  //       domainId: BLOG.ANALYTICS_ACKEE_DOMAIN_ID
+  //     },
+  //     {
+  //       detailed: false,
+  //       ignoreLocalhost: true
+  //     }
+  //   )
   return null
 }
 
