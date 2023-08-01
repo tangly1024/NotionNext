fccd194c6b7841055f36e75a8bdd60e49985959a
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-01 07:25:16 +0000

    blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Hate_fish(zilingheimei)', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '管我叫什么，我有两个名字不行吗？', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '求小鱼干QAQ', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://zilingheimei.icu', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
