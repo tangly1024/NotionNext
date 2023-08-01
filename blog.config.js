63b09de646b9a20566e93498866a34a6bb78fe7b
geeks7i <97093581+geeks7i@users.noreply.github.com>
2023-08-01 20:56:05 +0800

    Update blog.config.js
    just for fun

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'geeks7i', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Keep moving', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'Just for fun', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.geeks7i.cool', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 's7i, geeks7i', '云原生'// 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
