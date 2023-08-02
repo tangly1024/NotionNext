c4c2122ad3d6e3846c0160dd0e671dee85780c20
tourse <821989627@qq.com>
2023-08-02 17:43:15 +0800

    Merge remote-tracking branch 'origin/main'

--- blog.config.js
+++ blog.config.js
@@ -13,14 +13,14 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [19, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '帮助忙碌的现代人快乐工作，品质生活',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '给忙碌的现代人对聪明工作，快乐生活的指南',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '立青｜Aimer', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '柒磊', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '我是一个搞钱区博主，全网30万粉丝。恭喜你发现了我分享搞钱经验的自留地。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.diranyun.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '搞钱,副业,创业,变现,生活,远程工作,自由职业,地理套利', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '搞钱,副业,创业,变现,生活,远程工作,自由职业,AIGC', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
