0680059366240dc0421134448c15dfb1895d1595
Peter Gam <51412849+MrPeterJin@users.noreply.github.com>
2023-08-03 13:59:06 +0800

    Change font size

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -77,7 +77,7 @@ function Banner(props) {
         {/* 遮罩 */}
         <div id='banner-cover' style={{ backdropFilter: 'blur(15px)' }} className={'rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#4259efdd] dark:bg-[#dca846] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'}>
             <div className='ml-12 -translate-x-32 group-hover:translate-x-0 duration-300 transition-all ease-in'>
-                <div className='text-7xl text-white font-extrabold'>Random Post</div>
+                <div className='text-4xl text-white font-extrabold'>Random Post</div>
                 <div className='-ml-3 text-gray-300'><ArrowSmallRight className={'w-24 h-24 stroke-2'} /></div>
             </div>
         </div>
