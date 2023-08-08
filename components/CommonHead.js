d9a683461be4588881513862c69ba669d178ef10
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 20:05:26 +0800

    Update CommonHead.js

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -17,7 +17,7 @@ const CommonHead = ({ meta, children }) => {
 
   return (
         <Head>
-            <title>{Hate_fish🐟的鱼缸}</title>
+            <title>{"Hate_fish🐟的鱼缸"}</title>
             <meta name="theme-color" content={BLOG.BACKGROUND_DARK} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0" />
             <meta name="robots" content="follow, index" />
