e6a0e6000dba2483297645a294b7b6bdf69806d2
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 19:45:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -88,7 +88,7 @@ const BLOG = {
 
   // 自定义外部脚本，外部样式
   CUSTOM_EXTERNAL_JS: ['https://cdn.jsdelivr.net/gh/fz6m/Private-web@1.2/js/custom/click.min.js'], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
+  CUSTOM_EXTERNAL_CSS: ['https://github.com/zilingheimei/NotionNext/blob/main/public/css/mousechange.css'], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
   LAYOUT_SIDEBAR_REVERSE: true,
