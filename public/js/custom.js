c20abfcd786f928a866e6532b382e1a72130c8bb
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 11:59:33 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,2 +1,8 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
 // pages/_app.js
+document.addEventListener('visibilitychange', function () {
+    if (document.visibilityState == 'hidden') {
+        normal_title = document.title;
+        document.title = 'QAQ别走好吗...';
+    } else document.title = normal_title;
+});
