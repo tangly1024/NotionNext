0f7b73d10bc58dd3327dc5468b4033f08d70e9be
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 11:56:46 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,4 @@
+// pages/_app.js
 import { useEffect } from 'react'
 
 import '@/styles/animate.css' // @see https://animate.style/
@@ -20,15 +21,44 @@ import ExternalScript from '@/components/ExternalScript'
 // 各种扩展插件 动画等
 const ExternalPlugins = dynamic(() => import('@/components/ExternalPlugins'))
 
+// 导入react-headroom模块
+import Headroom from 'react-headroom'
+
 const MyApp = ({ Component, pageProps }) => {
   useEffect(() => {
     AOS.init()
   }, [])
 
+  // 定义一个useEffect钩子函数
+  useEffect(() => {
+    // 定义一个监听网页可见性变化事件的函数
+    const handleVisibilityChange = () => {
+      if (document.hidden) {
+        // 如果网页不可见，修改网页标题为“我走了”
+        document.title = '我走了'
+      } else {
+        // 如果网页可见，修改网页标题为“我回来了”
+        document.title = '我回来了'
+        // 一秒后恢复原来的网页标题
+        setTimeout(() => {
+          document.title = BLOG.title
+        }, 1000)
+      }
+    }
+    // 给document对象添加visibilitychange事件的监听器
+    document.addEventListener('visibilitychange', handleVisibilityChange)
+    // 移除visibilitychange事件的监听器
+    return () => {
+      document.removeEventListener('visibilitychange', handleVisibilityChange)
+    }
+  }, [])
+
   return (
         <GlobalContextProvider {...pageProps}>
             <ExternalScript />
-            <Component {...pageProps} />
+            <Headroom>
+              <Component {...pageProps} />
+            </Headroom>
             <ExternalPlugins {...pageProps} />
         </GlobalContextProvider>
   )
