bfe6c2d0e499ac6f47a5af7d3468b9e34396ed54
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 16:35:16 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -8,3 +8,36 @@ document.addEventListener('visibilitychange', function () {
         document.title = 'Hate_fish🐟的鱼缸';
     }
 });
+var a_idx = 0;
+jQuery(document).ready(function($) {
+    $("body").click(function(e) {
+        var a = new Array
+        ("富强", "民主", "文明", "和谐", "自由", "平等", "公正", "法治", "爱国", "敬业", "诚信", "友善");
+        var $i = $("<span/>").text(a[a_idx]);
+        a_idx = (a_idx + 1) % a.length;
+        var x = e.pageX,
+        y = e.pageY;
+        $i.css({
+            "z-index": 5,
+            "top": y - 20,
+            "left": x,
+            "position": "absolute",
+            "font-weight": "bold",
+            "color": "#FF0000"
+        });
+        $("body").append($i);
+        $i.animate({
+            "top": y - 180,
+            "opacity": 0
+        },
+            3000,
+            function() {
+                $i.remove();
+            });
+    });
+    setTimeout('delay()', 2000);
+});
+
+function delay() {
+    $(".buryit").removeAttr("onclick");
+}
