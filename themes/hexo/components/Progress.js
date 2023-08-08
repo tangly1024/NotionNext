a43ff6a3ff8524971b047c57ce48cecc9676c479
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 17:08:00 +0800

    Update Progress.js

--- themes/hexo/components/Progress.js
+++ themes/hexo/components/Progress.js
@@ -30,7 +30,7 @@ const Progress = ({ targetRef, showPercent = true }) => {
   return (
     <div className="h-4 w-full shadow-2xl bg-gray-700 rounded-sm">
       <div
-        className="h-4 bg-indigo-600 duration-200 rounded-sm"
+        className="h-4 bg-pink-50 duration-200 rounded-sm"
         style={{ width: `${percent}%` }}
       >
         {showPercent && (
