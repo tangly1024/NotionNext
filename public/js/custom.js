d09c12327a3bffc601fa8c512212fa3773f3de5b
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 18:12:46 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -3,15 +3,15 @@
 var OriginTitile=document.title;
 var jiao;
 document.addEventListener('visibilitychange',function(){
-if(location.href != "https://你的域名/")(document.hidden){
-    document.title="(つェ⊂)誒呀，網站崩潰了";
-    clearTimeout(jiao);
-}
-else{
-    document.title='(*´∇｀*) 咦,又好了~ '+OriginTitile;
-    jiao=setTimeout(function(){
-        document.title=OriginTitile;
-    },4000);
+    if(location.href != "https://www.zilingheimei.icu/")(document.hidden){
+        document.title="(つェ⊂)誒呀，網站崩潰了";
+        clearTimeout(jiao);
+    }
+    else{
+        document.title='(*´∇｀*) 咦,又好了~ '+OriginTitile;
+        jiao=setTimeout(function(){
+            document.title=OriginTitile;
+        },4000);
     }
 });
 
