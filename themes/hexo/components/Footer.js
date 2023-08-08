6c1dbe876b6b88038bfb9389526c58a2511e0732
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-08 17:38:27 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -14,7 +14,7 @@ const Footer = ({ title }) => {
 
   return (
     <footer
-      className='relative z-10 dark:bg-black flex-shrink-0 bg-gray-100 justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
+      className='relative z-10 dark:bg-black flex-shrink-0 bg-neutral-100 justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
     >
       {/* <DarkModeButton/> */}
 
