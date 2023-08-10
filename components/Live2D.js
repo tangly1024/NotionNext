dc7d4654883a8a6d426d07365c8075d2dbe8224f
happywang2333 <141653396+happywang2333@users.noreply.github.com>
2023-08-10 14:04:44 +0800

    Update Live2D.js

--- components/Live2D.js
+++ components/Live2D.js
@@ -11,7 +11,7 @@ export default function Live2D() {
   useEffect(() => {
     if (showPet) {
       Promise.all([
-        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
+        loadExternalResource('https://cdn.jsdelivr.net/npm/live2d-widget-model-hijiki/assets/hijiki.model.jsonBash', 'js')
       ]).then((e) => {
         if (typeof window?.loadlive2d !== 'undefined') {
           // https://github.com/xiazeyu/live2d-widget-models
@@ -35,7 +35,7 @@ export default function Live2D() {
     return <></>
   }
 
-  return <canvas id="live2d" width="280" height="250" onClick={handleClick}
+  return <canvas id="live2d" width="250" height="350" onClick={handleClick}
         className="cursor-grab"
         onMouseDown={(e) => e.target.classList.add('cursor-grabbing')}
         onMouseUp={(e) => e.target.classList.remove('cursor-grabbing')}
