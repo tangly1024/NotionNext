ad319847aa97b187864c23a309aa4dea7b969441
HaoZhou3 <132916880+HaoZhou3@users.noreply.github.com>
2023-08-10 22:51:20 +0800

    Update Logo.js
    一言

--- themes/next/components/Logo.js
+++ themes/next/components/Logo.js
@@ -1,24 +1,30 @@
-import Link from 'next/link'
-import React from 'react'
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
 
 const Logo = props => {
-  const { siteInfo, className } = props
+  const { className } = props;
+  const [quote, setQuote] = useState('');
+
+  useEffect(() => {
+    fetch('https://v1.hitokoto.cn/')
+      .then(response => response.json())
+      .then(data => {
+        setQuote(data.hitokoto);
+      })
+      .catch(error => {
+        console.error('Error fetching quote:', error);
+      });
+  }, []);
+
   return (
-        <Link href='/' passHref legacyBehavior>
-            <div className={'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-800 space-y-3 font-bold ' + className}>
-                <div data-aos="fade-down"
-                    data-aos-duration="500"
-                    data-aos-once="true"
-                    data-aos-anchor-placement="top-bottom"
-                    className='font-serif text-xl text-white'> {siteInfo?.title}</div>
-                <div data-aos="fade-down"
-                    data-aos-duration="500"
-                    data-aos-delay="300"
-                    data-aos-once="true"
-                    data-aos-anchor-placement="top-bottom"
-                    className='text-sm text-gray-300 font-light text-center'> {siteInfo?.description}</div>
-            </div>
-        </Link>
-  )
-}
-export default Logo
+    <Link href='/' passHref legacyBehavior>
+      <div className={'flex flex-col justify-center items-center cursor-pointer bg-black dark:bg-gray-800 space-y-3 font-bold ' + className}>       
+        <div data-aos="fade-down" data-aos-duration="500" data-aos-once="true" data-aos-anchor-placement="top-bottom" className='text-sm text-gray-300 font-light text-center'>
+          {quote}
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default Logo;
