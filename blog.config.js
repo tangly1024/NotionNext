92b9ba1f0a775e385b961fa77289846a74d2d918
Vae_xiaoxin <73480014+ICer-gpt@users.noreply.github.com>
2023-08-11 05:19:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -168,7 +168,7 @@ const BLOG = {
   // 樱花飘落特效
   SAKURA: process.env.NEXT_PUBLIC_SAKURA || false, // 开关
   // 漂浮线段特效
-  NEST: process.env.NEXT_PUBLIC_NEST || true, // 开关
+  NEST: process.env.NEXT_PUBLIC_NEST || false, // 开关
   // 动态彩带特效
   FLUTTERINGRIBBON: process.env.NEXT_PUBLIC_FLUTTERINGRIBBON || false, // 开关
   // 静态彩带特效
