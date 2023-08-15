11160869ccf1eec183c62c1238f65769e6ac5256
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-15 19:41:01 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,10 +1,16 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
 // pages/_app.js
+var OriginTitile=document.title,jiao;
 document.addEventListener('visibilitychange', function () {
     if (document.visibilityState == 'hidden') {
-        document.title = 'QAQ别走好吗...';
+        document.title = '(つェ⊂)诶呀，网站崩溃了';
+        clearTimeout(jiao);
     } 
     else {
-        document.title = 'Hate_fish🐟的鱼缸';
+        document.title = '(*´∇｀*) 咦，又好了~ '+ OriginTitile;
+        jiao=setTimeout(function(){
+            document.title=OriginTitile;
+        },4000);
+        // document.title = 'Hate_fish🐟的鱼缸';
     }
 });
