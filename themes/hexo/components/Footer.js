49b4cb21ca19b38615c4b1b5b8cffcbe4524de2c
soeos <139830735+soeos@users.noreply.github.com>
2023-08-17 05:46:34 -0600

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -22,8 +22,7 @@ const Footer = ({ title }) => {
 
       {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
 
-        <img src="https://icp.gov.moe/favicon.ico" alt="https://icp.gov.moe" width="13" height="13"/>
-        <a href="https://icp.gov.moe/?keyword=20230968" target="_blank">萌ICP备20230968号</a>  
+     <a href="https://icp.gov.moe/?keyword=20230968" target="_blank">萌ICP备20230968号</a>  
   
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
