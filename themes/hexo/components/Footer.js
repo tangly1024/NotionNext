95fd57828cf0b16b02abbb45c50102b8307bc466
soeos <139830735+soeos@users.noreply.github.com>
2023-08-17 05:39:26 -0600

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -22,17 +22,8 @@ const Footer = ({ title }) => {
 
       {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
 
-  <img src="https://icp.gov.moe/favicon.ico" alt="https://icp.gov.moe" width="13" height="13"/>
+        <img src="https://icp.gov.moe/favicon.ico" alt="https://icp.gov.moe" width="13" height="13"/>
         <a href="https://icp.gov.moe/?keyword=20230968" target="_blank">萌ICP备20230968号</a>  
-
-
-        document.write("<b>本站已平稳运行</b><font color=red>");
-        var urodz= new Date("7/28/2021");
-        var now = new Date();    
-        var ile = now.getTime() - urodz.getTime();
-        var dni = Math.floor(ile / (1000 * 60 * 60 * 24));
-        document.write(dni);
-        document.write("</font>天！");
   
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
