8a7bd136736abee9186d8fe55b7c9325885eb2b6
soeos <139830735+soeos@users.noreply.github.com>
2023-08-17 05:53:25 -0600

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -12,6 +12,10 @@ const Footer = ({ title }) => {
     return currentYear
   })()
 
+  
+// 'https://beian.miit.gov.cn/'
+
+  
   return (
     <footer
       className='relative z-10 dark:bg-black flex-shrink-0 bg-hexo-light-gray justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm p-6'
@@ -20,9 +24,7 @@ const Footer = ({ title }) => {
 
       <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
 
-      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
-
-     <a href="https://icp.gov.moe/?keyword=20230968" target="_blank">萌ICP备20230968号</a>  
+      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href="https://icp.gov.moe/?keyword=20230968" target="_blank" className='mr-2'>萌ICP备20230968号{BLOG.BEI_AN}</a><br/></>}
   
       <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
