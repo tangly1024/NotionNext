4856948dddab73a2893143e95d4a86ce6c39af77
liash <77919953+lzk821@users.noreply.github.com>
2023-08-19 23:25:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'liash', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '❀缘聚缘散缘如水,背负万丈尘寰,只为一句,等待下一次相逢', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '❀缘聚缘散缘如水,背负万丈尘寰', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.liash.link/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'liash, 博客', // 网站关键词 英文逗号隔开
 
