6cbebe271709dabca1cc223af54d371bdc11eaf1
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-19 16:23:11 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -18,16 +18,28 @@ const Footer = ({ title }) => {
     >
       {/* <DarkModeButton/> */}
 
-      <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.<br/>
-
-      {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a><br/></>}
-
-      <span className='hidden busuanzi_container_site_pv'>
-            <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
-      <span className='pl-2 hidden busuanzi_container_site_uv'>
-        <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
+      {/* 将原来处在中间的东西改到第一个区域 */}
+      <div className='flex flex-col items-center'>
+        <i className='fas fa-copyright' /> {`${copyrightDate}`} <span><i className='mx-1 animate-pulse fas fa-heart'/> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.
         <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</h1>
         <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='dark:text-gray-300'>NotionNext {BLOG.VERSION}搭建</a>.</p></span><br/>
+      </div>
+
+      {/* 将页脚分为四个区域，使用flex布局 */}
+      <div className='flex flex-row justify-around'>
+        {/* 第二个区域，显示备案信息 */}
+        {BLOG.BEI_AN && <div><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></div>}
+
+        {/* 第三个区域，显示网站访问量 */}
+        <div className='busuanzi_container_site_pv'>
+            <i className='fas fa-eye'/><span className='px-1 busuanzi_value_site_pv'> </span>  
+        </div>
+
+        {/* 第四个区域，显示网站用户数 */}
+        <div className='pl-2 busuanzi_container_site_uv'>
+          <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> 
+        </div>
+      </div>
     </footer>
   )
 }
