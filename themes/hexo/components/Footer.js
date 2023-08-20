6edf8dc517e9308b6f783201acf800532aea8d89
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-20 08:58:03 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -52,13 +52,18 @@ const Footer = ({ title }) => {
           </div>
           {/* 显示版权信息和作者信息 */}
           <div>
-            <i className='fas fa-copyright' /> 
-            {`${copyrightDate}`} 
-            <i className='mx-1 animate-pulse fas fa-heart'/> 
-            <a href={BLOG.LINK} className='dark:text-gray-400 '>{BLOG.AUTHOR}</a>
-            <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</p>
-            <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>搭建.</p>
-            <br/>
+            <div>
+              <i className='fas fa-copyright' /> 
+              {`${copyrightDate}`} 
+              <i className='mx-1 animate-pulse fas fa-heart'/> 
+              <a href={BLOG.LINK} className='dark:text-gray-400 '>{BLOG.AUTHOR}</a>
+            </div>
+            <div>
+              <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</p>
+            </div>
+            <div>
+              <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>搭建.</p>
+            </div>
           </div>
         </div>
 
