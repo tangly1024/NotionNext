8c2f42a21321c6092208aa809f170b38ef1fa853
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-20 09:04:38 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -62,7 +62,7 @@ const Footer = ({ title }) => {
               <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</p>
             </div>
             <div>
-              <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>搭建.</p>
+              <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>搭建</p>
             </div>
           </div>
         </div>
@@ -103,7 +103,7 @@ const Footer = ({ title }) => {
             {/* 调用网站运行时间函数，并显示结果 */}
             <i className='fas fa-clock' />
             {runTime()}
-          <div>
+          </div>
         </div>
       </div>
     </footer>
