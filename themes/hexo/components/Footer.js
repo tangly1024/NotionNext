692ba14aca0dde62ed8703dbd6cd38a9836b26b5
hate fish <136240143+zilingheimei@users.noreply.github.com>
2023-08-20 09:02:21 +0800

    Update Footer.js

--- themes/hexo/components/Footer.js
+++ themes/hexo/components/Footer.js
@@ -62,7 +62,7 @@ const Footer = ({ title }) => {
               <p className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</p>
             </div>
             <div>
-              <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>搭建.</p>
+              <p className='text-xs pt-2 text-light-500 dark:text-gray-500'>基于<a href='https://github.com/tangly1024/NotionNext' className='underline dark:text-gray-300'>NotionNext {BLOG.VERSION}</a>搭建</p>
             </div>
           </div>
         </div>
@@ -93,14 +93,17 @@ const Footer = ({ title }) => {
         <div className='pl-2 busuanzi_container_site_uv'>
           {/* 使用h6格式标题，标题名为关于 */}
           <h6>关于</h6>
-          <i className='fas fa-users'/> 
-          <span className='px-1 busuanzi_value_site_uv'> </span> 
-          <i className='fas fa-eye'/>
-          <span className='px-1 busuanzi_value_site_pv'> </span>  <br/>
-          {/* 调用网站运行时间函数，并显示结果 */}
-          <i className='fas fa-clock' />
-          {runTime()}
-          <br/>
+          <div>
+            <i className='fas fa-users'/> 
+            <span className='px-1 busuanzi_value_site_uv'> </span> 
+            <i className='fas fa-eye'/>
+            <span className='px-1 busuanzi_value_site_pv'> </span>
+          </div>
+          <div>
+            {/* 调用网站运行时间函数，并显示结果 */}
+            <i className='fas fa-clock' />
+            {runTime()}
+          <div>
         </div>
       </div>
     </footer>
