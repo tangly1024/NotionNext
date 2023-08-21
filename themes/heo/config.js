61613813a073e93a9f7a697e78d76cfa0bdc221a
Jason Chen <jinghao_chen@msn.com>
2023-08-21 12:25:51 +0800

    更新主题信息

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,21 +1,20 @@
 const CONFIG = {
   HOME_BANNER_ENABLE: true,
 
-  SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  SITE_CREATE_TIME: '2023-08-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://alphachat.fun' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '研究、分享',
+  HERO_TITLE_2: '思维、认知',
+  HERO_TITLE_3: 'ALPHACHAT.FUN',
+  HERO_TITLE_4: 'AIGC研究',
+  HERO_TITLE_5: 'Midjourney & Stable Diffusion',
+  HERO_TITLE_LINK: 'https://alphachat.fun',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
