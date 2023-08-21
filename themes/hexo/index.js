b5687f82ca4a9cda52b80ad1f876e99385a3834d
Bob <nuagessir@icloud.com>
2023-08-21 15:18:28 +0800

    Update index.js
    添加广告

--- themes/hexo/index.js
+++ themes/hexo/index.js
@@ -89,6 +89,7 @@ const LayoutBase = props => {
 
                             {children}
                         </Transition>
+                        <AdSlot type='native' />
                     </div>
 
                     {/* 右侧栏 */}
