9018e1bd79d0bed1be17afd24adb691168016aa5
DalangXu <143288193+DalangXu@users.noreply.github.com>
2023-08-28 15:15:43 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,22 +5,22 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-  //  { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-  //  { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+//  { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
+//  { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HERO_TITLE_1: '分享编程',
-  HERO_TITLE_2: '与思维认知',
-  HERO_TITLE_3: 'TANGLY1024.COM',
-  HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_1: '与轮滑',
+  HERO_TITLE_2: '一起成长',
+  HERO_TITLE_3: 'dalang66.cn',
+  HERO_TITLE_4: '',
+  HERO_TITLE_5: '',
+  HERO_TITLE_LINK: 'https://www.dalang66.cn',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
   HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HERO_CATEGORY_3: { title: '', url: '' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,7 +40,7 @@ const CONFIG = {
 
   // 用户技能图标
   GROUP_ICONS: [
-    {
+/*    {
       title_1: 'AfterEffect',
       img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
       color_1: '#989bf8',
@@ -95,7 +95,7 @@ const CONFIG = {
       title_2: 'Rhino',
       img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
       color_2: '#1f1f1f'
-    }
+*/    }
   ],
 
   SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
