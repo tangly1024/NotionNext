d32d6313421a520e07b538c14fb55a980ce0623b
Seven <17610210221@163.com>
2023-09-05 16:06:20 +0800

    feat:隐藏右键切换主题按钮；隐藏归档按钮；修改滚动按钮样式

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   MENU_INDEX: true, // 显示首页
   MENU_CATEGORY: true, // 显示分类
   MENU_TAG: true, // 显示标签
-  MENU_ARCHIVE: true, // 显示归档
+  MENU_ARCHIVE: false, // 显示归档
   MENU_SEARCH: true, // 显示搜索
 
   POST_LIST_COVER: true, // 列表显示文章封面
--- themes/matery/components/Hero.js
+++ themes/matery/components/Hero.js
@@ -60,7 +60,7 @@ const Hero = props => {
                 </div>
                 {/* 滚动按钮 */}
                 <div onClick={() => { window.scrollTo({ top: wrapperTop, behavior: 'smooth' }) }}
-                    className="mt-12 border cursor-pointer w-40 text-center pt-4 pb-3 text-md text-white hover:bg-orange-600 duration-300 rounded-3xl z-40">
+                    className="mt-12 border cursor-pointer w-40 text-center pt-4 pb-3 text-md text-white hover:bg-gray-400/30 duration-300 rounded-3xl z-40">
                     <i className='animate-bounce fas fa-angle-double-down' /> <span>{CONFIG.SHOW_START_READING && locale.COMMON.START_READING}</span>
                 </div>
             </div>
