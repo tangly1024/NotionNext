67bb67c61d7c4ce120e81b3c8a0867a589bd862a
jingziccc <104838346+jingziccc@users.noreply.github.com>
2023-09-22 20:51:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'JingZiccc', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '同济大学大三软件学院在读菜菜🍚', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Jingziccc', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '同济大学大三软件学院在读🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'jingziccc.fun', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
