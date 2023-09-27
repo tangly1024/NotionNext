a9cb0ce275d554e0c32c53aa28d107b3e27b7c25
Jam <104814748+chris8611@users.noreply.github.com>
2023-09-27 05:14:03 -0500

    Update CommonHead.js

--- components/CommonHead.js
+++ components/CommonHead.js
@@ -18,6 +18,7 @@ const CommonHead = ({ meta, children }) => {
   return (
         <Head>
             <title>{title}</title>
+            <meta name="baidu-site-verification" content="codeva-J5CLwmJkLL" />
             <meta name="theme-color" content={BLOG.BACKGROUND_DARK} />
             <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=5.0, minimum-scale=1.0" />
             <meta name="robots" content="follow, index" />
