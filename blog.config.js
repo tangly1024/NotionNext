599b6fa15ed726d870e20c96ce4ac94eaf5c15ce
直播控 <bimulinsen@gmail.com>
2023-10-05 14:20:44 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -13,8 +13,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
-  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || 'Hi，比木です, 一个看直播、玩直播、现居日本的肥宅。博客分享内容创作的方法，涉及软件、硬件。
-🎉',
+  GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '比木一个看直播、玩直播、现居日本的肥宅。博客分享内容创作的方法，涉及软件、硬件🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
