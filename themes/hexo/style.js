10d6dcfaf0a0f01d3bfa234eb96df977a2d31d25
LooseLi <1329307562@qq.com>
2023-10-12 09:54:08 +0800

    fix(blog): 主题颜色

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -18,7 +18,7 @@ const Style = () => {
     #theme-hexo .menu-link {
         text-decoration: none;
         {/* background-image: linear-gradient(#928CEE, #928CEE); */}
-        background-image: linear-gradient(#9AC5F4, #9AC5F4);
+        background-image: linear-gradient(#AED2FF, #AED2FF);
         background-repeat: no-repeat;
         background-position: bottom center;
         background-size: 0 2px;
@@ -28,7 +28,7 @@ const Style = () => {
     #theme-hexo .menu-link:hover {
         background-size: 100% 2px;
         {/* color: #928CEE; */}
-        color: #9AC5F4;
+        color: #AED2FF;
     }
 
     /* 设置了从上到下的渐变黑色 */
