8004b49b857a1ea6faf0fa9c562ab12fb35e0224
凪 <143114165+CSfufu@users.noreply.github.com>
2023-10-14 17:53:24 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,7 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'fufu酱', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'ZJU’26 | CSer | ITP | 开源自我 | 数字游民 | Fintech | Web3.0', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || 'ZJU’26 | CSer | CDC | 开源自我 | 数字游民 | Fintech | Web3.0', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion-next-six-rouge.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
