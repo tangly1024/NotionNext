a80d7890dd39c18296cad340993420c443f1a35d
carwlyjoe <130526582+carwlyjoe@users.noreply.github.com>
2023-10-17 01:20:27 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  HOME_BANNER_ENABLE: true,
+  HOME_BANNER_ENABLE: false,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
