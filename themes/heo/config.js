fa5c7fe64492d3e36668d4479de4dd838a5a2df6
vsd269 <142389217+vsd269@users.noreply.github.com>
2023-10-17 23:56:31 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -57,12 +57,12 @@ const CONFIG = {
       color_2: '#4082c3'
     },
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'civitai',
+      img_1: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/202310172351889.webp',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
-      color_2: '#ffffff'
+      title_2: 'heygen',
+      img_2: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/202310172351906.webp',
+      color_2: '#161b32'
     },
     {
       title_1: 'Swift',
