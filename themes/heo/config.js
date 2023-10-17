61fb59b065c02f061e4607088e5459879f4c449a
vsd269 <142389217+vsd269@users.noreply.github.com>
2023-10-17 15:06:49 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.ai-hd.com/' },
+    { title: '访问文档中心获取更多帮助', url: 'https://www.ai-hd.com/' }
   ],
 
   // 英雄区(首页顶部大卡)
