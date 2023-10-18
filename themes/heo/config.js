8f364e9972e134a1db7ccc9072b054d870540d06
vsd269 <142389217+vsd269@users.noreply.github.com>
2023-10-18 14:11:32 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -43,7 +43,7 @@ const CONFIG = {
     {
       title_1: 'Topaz Photo AI',
       img_1: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/202310172313255.webp',
-      color_1: '#6869a9',
+      color_1: '#9ca2fb',
       title_2: 'Topaz Video AI',
       img_2: 'https://tupian1014-1251479570.cos.ap-shanghai.myqcloud.com/tupian/202310172318703.webp',
       color_2: '#9ca2fb'
