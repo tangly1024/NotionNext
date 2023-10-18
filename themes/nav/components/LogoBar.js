b61a72abb9563ce1c6f04f14a15315c8247d11cf
emengweb <emengweb@gmail.com>
2023-10-18 16:36:20 +0800

    remove  transition of logo

--- themes/nav/components/LogoBar.js
+++ themes/nav/components/LogoBar.js
@@ -15,7 +15,7 @@ export default function LogoBar(props) {
                 <Link href='/' className='md:w-48 grid justify-items-center text-md md:text-xl dark:text-gray-200'>
                     {/* eslint-disable-next-line @next/next/no-img-element */}
                     <img src={siteInfo?.icon?.replaceAll('width=400', 'width=280')}
-                        height='44px' alt={BLOG.AUTHOR + ' - ' + BLOG.NEXT_PUBLIC_BIO} className='md:block transition-all hover:scale-110 duration-150' placeholderSrc='' />
+                        height='44px' alt={BLOG.AUTHOR + ' - ' + BLOG.NEXT_PUBLIC_BIO} className='md:block' placeholderSrc='' />
                     {CONFIG.SHOW_TITLE_TEXT && siteInfo?.title}
                 </Link>
         </div>
