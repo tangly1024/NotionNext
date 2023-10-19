dbd002cc3c209ee03956d154bc865a5b12da5b66
xiaopeng Lu <sir.lu@live.cn>
2023-10-19 13:40:39 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -14,8 +14,8 @@ const CONFIG = {
   HERO_TITLE_2: '与思维认知',
   HERO_TITLE_3: 'TANGLY1024.COM',
   HERO_TITLE_4: '新版上线',
-  HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HERO_TITLE_LINK: 'https://tangly1024.com',
+  HERO_TITLE_5: '点击查看博主推荐内容',
+  HERO_TITLE_LINK: 'https://luxiaopeng.cn/tag/%E6%8E%A8%E8%8D%90',
 
   // 英雄区显示三个置顶分类
   HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
