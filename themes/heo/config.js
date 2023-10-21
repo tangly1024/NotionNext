a838d6d8ac9340793bce6b085ca39164a34ff0e3
carwlyjoe <130526582+carwlyjoe@users.noreply.github.com>
2023-10-21 20:53:03 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '超好用的ChatGPT客户端', url: 'https://crawlyjoe.top' },
   ],
 
   // 英雄区(首页顶部大卡)
