a423500f122b36327b255ff12804ff8ee0b327c0
QiaoMeng <hucige@gmail.com>
2023-11-10 19:04:32 +0800

    Update Footer.js

--- themes/matery/components/Footer.js
+++ themes/matery/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = ({ title }) => {
         <i className='fas fa-users'/> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
         <br/>
         <h1>{title}</h1>
-        <span className='text-xs '>Powered by <a href='' className='underline dark:text-gray-300'>QIAOMENG {BLOG.VERSION}</a>.</span></span><br/>
+        <span className='text-xs '>Powered by <a href='' className='underline dark:text-gray-300'>ZHIQIAO {BLOG.VERSION}</a>.</span></span><br/>
 
     </footer>
   )
