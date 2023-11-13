6881f5ef0b17debb8fb3a583826d22b71d8b7e32
MartinWave <131272323+MatrixWave-CY@users.noreply.github.com>
2023-11-13 23:49:17 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,26 +1,26 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-10-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://nex18.top' },
+    { title: '访问文档中心获取更多帮助', url: 'https://nex18.top' }
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '分享资源工具',
+  HEO_HERO_TITLE_2: '探索未来趋势',
+  HEO_HERO_TITLE_3: 'MatrixWave',
+  HEO_HERO_TITLE_4: 'MatrixWave',
+  HEO_HERO_TITLE_5: '吃饱睡好,知足常乐',
+  HEO_HERO_TITLE_LINK: 'https://nex18.top',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '资源分享', url: '/category/资源分享' },
+  HEO_HERO_CATEGORY_2: { title: '碎片杂文', url: '/category/碎片杂文' },
+  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/category/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -37,7 +37,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/MatrixWave-CY', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
