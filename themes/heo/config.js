d484d90041ec3b956242341b87aead91e8baca52
MartinWave <131272323+MatrixWave-CY@users.noreply.github.com>
2023-11-19 21:49:37 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,8 +19,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '资源分享', url: '/category/资源分享' },
-  HEO_HERO_CATEGORY_2: { title: '碎片杂文', url: '/category/碎片杂文' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/category/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/category/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '碎片杂文', url: '/category/碎片杂文' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -103,7 +103,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://pd.qq.com/s/1yf8pn4qi',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
