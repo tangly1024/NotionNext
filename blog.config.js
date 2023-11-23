c30199042cbe240861a66c8875451d5a6c8ea6f4
ggfeng369 <105947211+ggfeng369@users.noreply.github.com>
2023-11-23 22:03:51 +0100

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'Smart lock assistant', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Smart lock assistant', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://lockmatrix.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'smart Lock,Intelligens ajtózár,okos zár', // 网站关键词 英文逗号隔开
 
