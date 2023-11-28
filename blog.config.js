7988a6f53cef24b3b1acc0100700a3e73382e962
凪 <143114165+CSfufu@users.noreply.github.com>
2023-11-28 14:58:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -178,11 +178,11 @@ const BLOG = {
 
   //   ********挂件组件相关********
   // Chatbase 是否显示chatbase机器人 https://www.chatbase.co/
-  CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || gBPAkeQg7z4Ysu2NmwVlQ,
+  CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || "gBPAkeQg7z4Ysu2NmwVlQ",
   // WebwhizAI 机器人 @see https://github.com/webwhiz-ai/webwhiz
   WEB_WHIZ_ENABLED: process.env.NEXT_PUBLIC_WEB_WHIZ_ENABLED || true, // 是否显示
   WEB_WHIZ_BASE_URL: process.env.NEXT_PUBLIC_WEB_WHIZ_BASE_URL || 'https://api.webwhiz.ai', // 可以自建服务器
-  WEB_WHIZ_CHAT_BOT_ID: process.env.NEXT_PUBLIC_WEB_WHIZ_CHAT_BOT_ID || gBPAkeQg7z4Ysu2NmwVlQ, // 在后台获取ID
+  WEB_WHIZ_CHAT_BOT_ID: process.env.NEXT_PUBLIC_WEB_WHIZ_CHAT_BOT_ID || "gBPAkeQg7z4Ysu2NmwVlQ", // 在后台获取ID
 
   // 悬浮挂件
   WIDGET_PET: process.env.NEXT_PUBLIC_WIDGET_PET || true, // 是否显示宠物挂件
