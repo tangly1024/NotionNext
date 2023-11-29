8f53e6d790dcea6004b03b3f3d12614a6ec411c5
lei lin <lei.lin@narwal.com>
2023-11-29 15:55:39 +0800

    设置twikoo语言为0

--- components/Twikoo.js
+++ components/Twikoo.js
@@ -19,8 +19,8 @@ const Twikoo = ({ isDarkMode }) => {
     if (typeof twikoo !== 'undefined' && twikoo && typeof twikoo.init === 'function') {
       twikoo.init({
         envId: 'https://twikoo-preview-48rae22h9-leolin66s-projects.vercel.app', // 腾讯云环境填 envId；Vercel 环境填地址（https://xxx.vercel.app）
-        el: el // 容器元素
-        //lang: lang // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
+        el: el, // 容器元素
+        lang: 0 // 用于手动设定评论区语言，支持的语言列表 https://github.com/imaegoo/twikoo/blob/main/src/client/utils/i18n/index.js
         // region: 'ap-guangzhou', // 环境地域，默认为 ap-shanghai，腾讯云环境填 ap-shanghai 或 ap-guangzhou；Vercel 环境不填
         // path: location.pathname, // 用于区分不同文章的自定义 js 路径，如果您的文章路径不是 location.pathname，需传此参数
       })
