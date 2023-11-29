a7fd1375ca8d1e7dd7b76035d2b2dcbb4e928662
LSuperNova <LSuperNova@qq.com>
2023-11-29 10:20:34 +0800

    修改heo配置-tag

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '精选', url: '/tag/精选' },
   HEO_HERO_CATEGORY_2: { title: '热门', url: '/tag/热门' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '教程', url: '/tag/教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
