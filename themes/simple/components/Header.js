4d04e7de5d4550c0e9e21d3a513ef971bd562114
tangly1024.com <tlyong1992@hotmail.com>
2023-12-01 19:04:10 +0800

    theme-simple small fix seo

--- themes/simple/components/Header.js
+++ themes/simple/components/Header.js
@@ -20,7 +20,7 @@ export default function Header (props) {
                     {/* 可使用一张单图作为logo */}
                     <div className='flex space-x-6'>
                         <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer justify-center items-center flex'>
-                            <LazyImage src={avatar} className='rounded-full' width={120} height={120} alt={siteConfig('AUTHOR')} />
+                            <LazyImage priority={true} src={avatar} className='rounded-full' width={100} height={100} alt={siteConfig('AUTHOR')} />
                         </div>
 
                         <div className='flex-col flex justify-center'>
