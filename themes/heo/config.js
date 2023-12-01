afe3ab8706e637218e2417ac0fd428296b9d2faf
WINGSZD <152581213+WINGSZD@users.noreply.github.com>
2023-12-01 23:22:32 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -10,12 +10,8 @@ const CONFIG = {
   ],
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: 'WINGSZD.COM',
+  HEO_HERO_TITLE_LINK: '欢迎入场',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
