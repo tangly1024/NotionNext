d726881195e1e1096b74df944af9f0f07bffa867
Jason depp <37728137+Jasondeepny@users.noreply.github.com>
2023-12-01 15:36:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,21 +25,52 @@ const BLOG = {
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
 
   // 网站字体
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
-  FONT_URL: [// 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
-    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
-  ],
-  FONT_SANS: [// 无衬线字体 例如'LXGW WenKai'
-    'Bitter', '"PingFang SC"', '-apple-system', 'BlinkMacSystemFont', '"Hiragino Sans GB"',
-    '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Segoe UI"', '"Noto Sans SC"', 'HarmonyOS_Regular',
-    '"Microsoft YaHei"', '"Helvetica Neue"', 'Helvetica', '"Source Han Sans SC"',
-    'Arial', 'sans-serif', '"Apple Color Emoji"'],
-  FONT_SERIF: [// 衬线字体 例如'LXGW WenKai'
-    'Bitter', '"Noto Serif SC"', 'SimSun', '"Times New Roman"', 'Times', 'serif',
-    '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Apple Color Emoji"'],
-  FONT_AWESOME: '/css/all.min.css', // font-awesome 字体图标地址
+ // START ************网站字体*****************
+
+FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans', // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
+// 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
+FONT_URL: [
+  'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+  'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+  'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
+  'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
+],
+// 无衬线字体 例如'"LXGW WenKai"'
+FONT_SANS: [
+  '"LXGW WenKai"',
+  '"PingFang SC"',
+  '-apple-system',
+  'BlinkMacSystemFont',
+  '"Hiragino Sans GB"',
+  '"Microsoft YaHei"',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Segoe UI"',
+  '"Noto Sans SC"',
+  'HarmonyOS_Regular',
+  '"Helvetica Neue"',
+  'Helvetica',
+  '"Source Han Sans SC"',
+  'Arial',
+  'sans-serif',
+  '"Apple Color Emoji"'
+],
+// 衬线字体 例如'"LXGW WenKai"'
+FONT_SERIF: [
+  '"LXGW WenKai"',
+  'Bitter',
+  '"Noto Serif SC"',
+  'SimSun',
+  '"Times New Roman"',
+  'Times',
+  'serif',
+  '"Segoe UI Emoji"',
+  '"Segoe UI Symbol"',
+  '"Apple Color Emoji"'
+],
+FONT_AWESOME: process.env.NEXT_PUBLIC_FONT_AWESOME_PATH || 'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
+
+// END ************网站字体*****************
 
   // 自定义外部脚本，外部样式
   CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
