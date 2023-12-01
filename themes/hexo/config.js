5038b07cc5b465c6251d68dc1a586767a04f50e0
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2023-12-01 23:48:53 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -17,11 +17,11 @@ const CONFIG = {
   HEXO_MENU_SEARCH: true, // 显示搜索
 
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  HEXO_POST_LIST_COVER_HOVER_ENLARGE: true, // 列表鼠标悬停放大
 
   HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
-  HEXO_POST_LIST_PREVIEW: false, // 读取文章预览
+  HEXO_POST_LIST_PREVIEW: true, // 读取文章预览
   HEXO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEXO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
