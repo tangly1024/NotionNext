ce375d050a272043879a8da770eb2910cb8ee4c5
leolin <6leolin6@gmail.com>
2023-12-02 13:27:41 +0800

    modified:   themes/heo/config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-11-28', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://leolin66.top' },
-    { title: '访问文档中心获取更多帮助', url: '' }
+    { title: '欢迎交流😁😁😁', url: '' }
   ],
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'leolin66.top',
+  HEO_HERO_TITLE_3: 'LEOLIN66.top',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '',
   HEO_HERO_TITLE_LINK: 'https://leolin66.top',
@@ -23,7 +23,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
-  HEO_HERO_RECOMMEND_POST_TAG: '推荐',
+  HEO_HERO_RECOMMEND_POST_TAG: '',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
