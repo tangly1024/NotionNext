1d835ccbc838db2e9e01a49ae77fd684e87fa01c
Wjhtkj <142598326+Wjhtkj@users.noreply.github.com>
2023-12-10 17:17:54 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -241,13 +241,6 @@ const BLOG = {
       cover:
             'https://p1.music.126.net/CFKoBho0mSfptulYqsbgcg==/109951165508765863.jpg?param=130y130'
     }，
-    {
-      name: 'Do It（Orginal Max）',
-      artist: 'DJ文樂 / 鸢北',
-      url: 'https://music.163.com/song/media/outer/url?id=1892343290.mp3',
-      cover:
-            'http://p1.music.126.net/8uIL2PIJHvsdUeRADYa_Ug==/109951166561216191.jpg?param=130y130'
-    }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
