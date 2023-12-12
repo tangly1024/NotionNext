78891e332dc3d03b207bcf92f211e22872d4f759
niustark <niustark@qq.com>
2023-12-12 13:42:45 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -32,6 +32,7 @@ const BLOG = {
   CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
   CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || 'https://space.bilibili.com/1376800672/', // B站主页
   CONTACT_YOUTUBE: process.env.NEXT_PUBLIC_CONTACT_YOUTUBE || 'https://music.163.com/#/my/m/music/playlist?id=7479361785', // Youtube主页
+  CONTACT_TIKTOK: process.env.NEXT_PUBLIC_CONTACT_TIKTOK || 'https://v.douyin.com/i8jR3AFk/', // 抖音主页
 
   NOTION_HOST: process.env.NEXT_PUBLIC_NOTION_HOST || 'https://www.notion.so', // Notion域名，您可以选择用自己的域名进行反向代理，如果不懂得什么是反向代理，请勿修改此项
 
