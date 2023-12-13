c3d4d3be1d34ef0a890d6818edbd31063db8f46c
niustark <niustark@qq.com>
2023-12-13 20:10:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Niustark', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://niu7.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '铁牛爱分享, 铁牛博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '铁牛躺平, 铁牛博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'niustark@qq.com', // 邮箱地址 例如mail@tangly1024.com
