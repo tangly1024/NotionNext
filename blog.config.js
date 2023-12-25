977d9be43e8b908716ecfe7583c7118f9c5ea973
tinsir <tinsir@foxmail.com>
2023-12-25 13:47:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -114,7 +114,7 @@ const BLOG = {
   // 代码主题 @see https://github.com/PrismJS/prism-themes
   PRISM_THEME_PREFIX_PATH: process.env.NEXT_PUBLIC_PRISM_THEME_PREFIX_PATH || 'https://lib.baomitu.com/prism/latest/themes/prism-okaidia.css', // 代码块默认主题
   PRISM_THEME_SWITCH: process.env.NEXT_PUBLIC_PRISM_THEME_SWITCH || true, // 是否开启浅色/深色模式代码主题切换； 开启后将显示以下两个主题
-  PRISM_THEME_LIGHT_PATH: process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH || 'https://tinsir.com/prism-solarizedlight.css', // 浅色模式主题
+  PRISM_THEME_LIGHT_PATH: process.env.NEXT_PUBLIC_PRISM_THEME_LIGHT_PATH || 'https://lib.baomitu.com/prism/1.28.0/themes/prism-solarizedlight.min.css', // 浅色模式主题 https://tinsir.com/prism-solarizedlight.css
   PRISM_THEME_DARK_PATH: process.env.NEXT_PUBLIC_PRISM_THEME_DARK_PATH || 'https://lib.baomitu.com/prism/latest/themes/prism-okaidia.min.css', // 深色模式主题
 
   CODE_MAC_BAR: process.env.NEXT_PUBLIC_CODE_MAC_BAR || true, // 代码左上角显示mac的红黄绿图标
