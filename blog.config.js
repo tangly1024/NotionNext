21a42bfc013efd3b63eb47d9c93e855098dd7e30
tinsir <tinsir@foxmail.com>
2023-12-26 13:01:58 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024.
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介.
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tinsir.com', // 网站地址.
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tinsir.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '糖糕游记,糖糕遊記', // 网站关键词 英文逗号隔开.
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
