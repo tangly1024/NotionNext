e286d3bebc84782e8148a294f1ba4f14f9c576ab
窦月汐 <douyuexi@qq.com>
2023-12-27 13:53:03 +0800

    replace with matery's blogpostcard

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -1,47 +1,100 @@
-import Link from 'next/link'
-import CONFIG from '../config'
-import { BlogPostCardInfo } from './BlogPostCardInfo'
 import { siteConfig } from '@/lib/config'
+import Link from 'next/link'
+import TagItemMini from './TagItemMini'
+import CONFIG from '../config'
+import TwikooCommentCount from '@/components/TwikooCommentCount'
 import LazyImage from '@/components/LazyImage'
+import { formatDateFmt } from '@/lib/formatDate'
 // import Image from 'next/image'
 
 const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
-  const showPreview = CONFIG.POST_LIST_PREVIEW && post.blockMap
-  if (post && !post.pageCoverThumbnail && CONFIG.POST_LIST_COVER_DEFAULT) {
+  const showPreview = siteConfig('MATERY_POST_LIST_PREVIEW', null, CONFIG) && post.blockMap
+  // matery 主题默认强制显示图片
+  if (post && !post.pageCoverThumbnail) {
     post.pageCoverThumbnail = siteInfo?.pageCover
   }
-  const showPageCover = CONFIG.POST_LIST_COVER && post?.pageCoverThumbnail && !showPreview
-  //   const delay = (index % 2) * 200
-
+  const showPageCover = siteConfig('MATERY_POST_LIST_COVER', null, CONFIG) && post?.pageCoverThumbnail
+  const delay = (index % 3) * 300
   return (
+        <div
+            data-aos="zoom-in"
+            data-aos-duration="500"
+            data-aos-delay={delay}
+            data-aos-once="true"
+            data-aos-anchor-placement="top-bottom"
+            className="w-full mb-4 overflow-hidden shadow-md border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray">
 
-        <div className={`${CONFIG.POST_LIST_COVER_HOVER_ENLARGE ? ' hover:scale-110 transition-all duration-150' : ''}`} >
-            <div key={post.id}
-                data-aos="fade-up"
-                data-aos-easing="ease-in-out"
-                data-aos-duration="800"
-                data-aos-once="false"
-                data-aos-anchor-placement="top-bottom"
-                id='blog-post-card'
-                className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${CONFIG.POST_LIST_IMG_CROSSOVER && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
-                    overflow-hidden border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray`}>
+            {/* 固定高度 ，空白用图片拉升填充 */}
+            <div className="group flex flex-col h-80 justify-between">
 
-                {/* 文字内容 */}
-                <BlogPostCardInfo index={index} post={post} showPageCover={showPageCover} showPreview={showPreview} showSummary={showSummary} />
-
-                {/* 图片封面 */}
+                {/* 头部图片 填充卡片 */}
                 {showPageCover && (
-                    <div className="md:w-5/12 overflow-hidden">
-                        <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref legacyBehavior>
-                        <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500' />
-                        </Link>
-                    </div>
+                    <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref legacyBehavior>
+                        <div className="flex flex-grow w-full relative duration-200 = rounded-t-md cursor-pointer transform overflow-hidden">
+                            <LazyImage
+                                src={post?.pageCoverThumbnail}
+                                alt={post.title}
+                                className="h-full w-full group-hover:scale-125 group-hover:brightness-50 brightness-90 rounded-t-md transform object-cover duration-500"
+                            />
+                            <div className='absolute bottom-0 left-0 text-white p-6 text-2xl replace break-words w-full shadow-text'>{post.title}</div>
+                        </div>
+                    </Link>
                 )}
 
+                {/* 文字描述 */}
+                <div >
+                    {/* 描述 */}
+                    <div className="px-4 flex flex-col w-full  text-gray-700  dark:text-gray-300">
+
+                        {(!showPreview || showSummary) && post.summary && (
+                            <p style={{ overflow: 'hidden', textOverflow: 'ellipsis', display: '-webkit-box', WebkitLineClamp: '4', WebkitBoxOrient: 'vertical' }}
+                                className="replace my-2 text-sm font-light leading-7">
+                                {post.summary}
+                            </p>
+                        )}
+
+                        <div className='text-gray-800 justify-between flex my-2  dark:text-gray-300'>
+                            <div>
+                                <Link
+                                    href={`/archive#${formatDateFmt(post?.publishDate, 'yyyy-MM')}`}
+                                    passHref
+                                    className="font-light hover:underline cursor-pointer text-sm leading-4 mr-3">
+
+                                    <i className="far fa-clock mr-1" />
+                                    {post.date?.start_date || post.lastEditedDay}
+
+                                </Link>
+                                <TwikooCommentCount post={post} className='hover:underline cursor-pointer text-sm'/>
+                            </div>
+                            <Link
+                                href={`/category/${post.category}`}
+                                passHref
+                                className="cursor-pointer font-light text-sm hover:underline hover:text-indigo-700 dark:hover:text-indigo-400 transform">
+
+                                <i className="mr-1 far fa-folder" />
+                                {post.category}
+
+                            </Link>
+                        </div>
+                    </div>
+
+                    {post?.tagItems && post?.tagItems.length > 0 && (<>
+                        <hr />
+                        <div className="text-gray-400 justify-between flex px-5 py-3">
+                            <div className="md:flex-nowrap flex-wrap md:justify-start inline-block">
+                                <div>
+                                    {' '}
+                                    {post.tagItems.map(tag => (
+                                        <TagItemMini key={tag.name} tag={tag} />
+                                    ))}
+                                </div>
+                            </div>
+                        </div>
+                    </>)}
+                </div>
             </div>
 
         </div>
-
   )
 }
 
