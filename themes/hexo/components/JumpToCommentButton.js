7740f63b86173ef05bf37caef8d2152c43e572c2
tinsir <tinsir@foxmail.com>
2023-12-28 19:32:18 +0800

    Update JumpToCommentButton.js

--- themes/hexo/components/JumpToCommentButton.js
+++ themes/hexo/components/JumpToCommentButton.js
@@ -20,9 +20,9 @@ const JumpToCommentButton = () => {
     // if (commentElement) {
     // commentElement?.scrollIntoView({ behavior: 'smooth', block: 'start', inline: 'nearest' })
   }
-
+  // 调整跳转评论区按钮 现在看不懂 ， 调整为↓箭头  原始<i className='fas fa-comment text-xs' />
   return (<div className='flex space-x-1 items-center justify-center transform hover:scale-105 duration-200 w-7 h-7 text-center' onClick={navToComment} >
-    <i className='fas fa-comment text-xs' />
+    <i class='fa-solid fa-arrow-down' />
   </div>)
 }
 
