6a6bff20c69678fa8e6f1e178def28f7bb93df9d
leolin <6leolin6@gmail.com>
2023-12-31 14:40:36 +0800

    修改文章排序

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://leolin66.top' },
-    { title: '欢迎交流😁😁😁', url: '' }
+    { title: '多多交流😁😁😁', url: '' }
   ],
 
   // 英雄区(首页顶部大卡)
@@ -24,7 +24,7 @@ const CONFIG = {
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
-  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
+  HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: true, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
   HERO_RECOMMEND_COVER: 'https://pic.imgdb.cn/item/65674c6cc458853aef5b7b1b.png',// 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
