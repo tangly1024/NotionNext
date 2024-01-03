079cc0587ddfd7c07696eed27e30b921e7b401a6
tinsir <tinsir@foxmail.com>
2024-01-03 11:16:12 +0800

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,8 +1,1 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
-// 开始引入 Twikoo
-var script = document.createElement('script');
-script.src = 'https://cdn.bootcdn.net/ajax/libs/twikoo/1.6.29/twikoo.all.min.js';
-script.async = true;
-
-document.head.appendChild(script);
-// 引入 Twikoo 结束
