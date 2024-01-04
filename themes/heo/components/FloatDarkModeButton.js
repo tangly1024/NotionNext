683c10ba4fbf3f08357bfb1d060ce9fdb27ae9c4
Vam <120654757+VamRed@users.noreply.github.com>
2024-01-05 00:12:53 +0800

    Update FloatDarkModeButton.js

--- themes/heo/components/FloatDarkModeButton.js
+++ themes/heo/components/FloatDarkModeButton.js
@@ -26,7 +26,7 @@ export default function FloatDarkModeButton () {
       className={'justify-center items-center w-7 h-7 text-center transform hover:scale-105 duration-200'
       }
     >
-      <i id="darkModeButton" className={`${isDarkMode ? 'fa-sun' : 'fa-moon'} fas text-xs`}/>
+      <i id="darkModeButton" className={`${isDarkMode ? 'fa-solid fa-sun' : 'fa-solid fa-moon'} fas text-xs`}/>
     </div>
   )
 }
