9053b9aeab00c819b754dd2454c94207a029b333
Vam <120654757+VamRed@users.noreply.github.com>
2024-01-05 02:06:47 +0800

    Update NavButtonGroup.js

--- themes/heo/components/NavButtonGroup.js
+++ themes/heo/components/NavButtonGroup.js
@@ -20,7 +20,7 @@ const NavButtonGroup = (props) => {
             title={`${category.name}`}
             href={`/category/${category.name}`}
             passHref
-            className='text-center w-full sm:w-4/5 md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-white hover:text-black duration-200 font-bold hover:scale-105 transform'>
+            className='text-center w-full sm:w-4/5 md:mx-6 md:w-40 md:h-14 lg:h-20 h-14 justify-center items-center flex border-2 cursor-pointer rounded-lg glassmorphism hover:bg-[#1e1e1e] hover:text-black duration-200 font-bold hover:scale-105 transform'>
                 {category.name}
             </Link>
         )
