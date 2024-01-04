aa97ccbb33dbea1777cee9b04a11073cb4b33013
Vam <120654757+VamRed@users.noreply.github.com>
2024-01-05 00:28:24 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -19,7 +19,7 @@ export function InfoCard(props) {
   // 在文章详情页特殊处理
   const isSlugPage = router.pathname.indexOf('/[prefix]') === 0
   return (
-        <Card className='bg-[#4f65f0] dark:bg-yellow-600 text-white flex flex-col w-72 overflow-hidden relative'>
+        <Card className='bg-[#48D1CC] dark:bg-yellow-600 text-white flex flex-col w-72 overflow-hidden relative'>
             {/* 信息卡牌第一行 */}
             <div className='flex justify-between'>
                 {/* 问候语 */}
