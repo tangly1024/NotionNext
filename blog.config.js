d9f67ccfaa42f7c91dd9c62756c5f9dd0d034602
Vam <120654757+VamRed@users.noreply.github.com>
2024-01-05 01:29:20 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -181,7 +181,7 @@ const BLOG = {
   // 静态彩带特效
   RIBBON: process.env.NEXT_PUBLIC_RIBBON || false, // 开关
   // 星空雨特效 黑夜模式才会生效
-  STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || false, // 开关
+  STARRY_SKY: process.env.NEXT_PUBLIC_STARRY_SKY || true, // 开关
 
   //   ********挂件组件相关********
   // Chatbase 是否显示chatbase机器人 https://www.chatbase.co/
