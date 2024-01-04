8e8a329b6986256df57cb551e5c556450cbb3d21
Vam <120654757+VamRed@users.noreply.github.com>
2024-01-05 00:24:11 +0800

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,7 @@ module.exports = {
     extend: {
       colors: {
         day: {
-          DEFAULT: BLOG.BACKGROUND_LIGHT || '#ffffff'
+          DEFAULT: BLOG.BACKGROUND_LIGHT || '#000000'
         },
         night: {
           DEFAULT: BLOG.BACKGROUND_DARK || '#111827'
