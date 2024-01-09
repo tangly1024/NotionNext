e7a05374d651bbfccfe7297d6a2c71e56f67a54a
Axis <59349697+AxisZql@users.noreply.github.com>
2024-01-09 20:59:32 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,7 +1,7 @@
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 const BLOG = {
   // Important page_id！！！Duplicate Template from  https://www.notion.so/tanghh/02ab3b8678004aa69e9e415905ef32a5
-  NOTION_PAGE_ID: 'b78676201b634a07815da5c1b324009f',
+  NOTION_PAGE_ID:
 
     
     process.env.NOTION_PAGE_ID || 'b78676201b634a07815da5c1b324009f',
