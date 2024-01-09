41b25da4469fcdfbcdb83aa05b856c2f3bb6f679
Ayao <111835015+Ayao-AlwaysOnline@users.noreply.github.com>
2024-01-09 21:35:32 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -13,7 +13,7 @@ const CONFIG = {
   HEO_HERO_TITLE_2: 'can be faithful to truth',
   HEO_HERO_TITLE_3: 'Ayao.club',
   HEO_HERO_TITLE_4: 'recommend',
-  HEO_HERO_TITLE_5: '',
+  HEO_HERO_TITLE_5: 'recommend',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
