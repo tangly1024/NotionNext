13c489ea264a3c86c805ce4dc21ed096e71acd6e
victorliaocs <34571685+victorliaocs@users.noreply.github.com>
2024-01-11 15:53:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '廖曙光', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '现代数学启蒙博主', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://math1234567.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '现代数学启蒙，Modern Mathematics Enlightment', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '570634978@qq.com', // 邮箱地址 例如mail@tangly1024.com
