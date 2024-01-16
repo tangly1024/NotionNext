d707fee5301cafdd3b1103d54f8feb467ef2b894
Vam <me@vama.uk>
2024-01-16 14:39:46 +0800

    dd阿斯顿

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -51,12 +51,12 @@ const Hero = props => {
   }
 
   return (
-        <header id="header" style={{ zIndex: -100 }} className="w-full h-screen relative bg-black opacity-0">
+        <header id="header" style={{ zIndex: -100 }} className="w-full h-screen relative bg-black">
             <div className='z-101 text-white absolute bottom-12 flex flex-col h-full items-center justify-center w-full'>
                 {/* 站点标题 */}
-                <div className='z-101 font-black text-4xl md:text-5xl shadow-text opacity-100'>{siteConfig('TITLE')}</div>
+                <div className='z-101 font-black text-4xl md:text-5xl shadow-text'>{siteConfig('TITLE')}</div>
                 {/* 站点欢迎语 */}
-                <div className='z-103 mt-2 h-12 items-center text-center font-medium shadow-text text-lg opacity-100'>
+                <div className='z-103 mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
                     <span id='typed' />
                 </div>
 
@@ -64,14 +64,14 @@ const Hero = props => {
                 {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && <NavButtonGroup {...props} />}
 
                 {/* 滚动按钮 */}
-                <div onClick={scrollToWrapper} className="z-104 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-6 text-white opacity-100">
-                    <div className="opacity-70 animate-bounce text-xs opacity-100">{siteConfig('HEXO_SHOW_START_READING', null, CONFIG) && locale.COMMON.START_READING}</div>
+                <div onClick={scrollToWrapper} className="z-104 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-6 text-white">
+                    <div className="opacity-70 animate-bounce text-xs">{siteConfig('HEXO_SHOW_START_READING', null, CONFIG) && locale.COMMON.START_READING}</div>
                     <i className='opacity-70 animate-bounce fas fa-angle-down' />
                 </div>
             </div>
 
             <LazyImage id='header-cover' src={siteInfo?.pageCover}
-                className={`opacity-100 z-100 header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`} />
+                className={`z-100 header-cover w-full h-screen object-cover object-center ${siteConfig('HEXO_HOME_NAV_BACKGROUND_IMG_FIXED', null, CONFIG) ? 'fixed' : ''}`} />
 
         </header>
   )
