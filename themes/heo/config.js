2ff0511d0e3ff43a94e77994b9a69687bd135f9e
kforris <58551353+kforris@users.noreply.github.com>
2024-01-17 00:01:52 -0500

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -43,7 +43,7 @@ const CONFIG = {
   HEO_GROUP_ICONS: [
     {
       title_1: 'Slopes',
-      img_1: '/images/heo/slopes.webp',
+      img_1: '/images/heo/slopes.png',
       color_1: '#2B78D1',
       title_2: '滑呗',
       img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
