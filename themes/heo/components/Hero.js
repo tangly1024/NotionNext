3010b2f610a9176b724ba4f2c325d6f2e9453469
kforris <58551353+kforris@users.noreply.github.com>
2024-01-18 05:10:21 -0500

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -223,7 +223,7 @@ function TopGroup(props) {
     <div
       id="hero-right-wrapper"
       onMouseLeave={handleMouseLeave}
-      className="flex-1 relative w-full pr-10 md:pr-0"
+      className="flex-1 relative w-full pr-0 pr-10"
     >
       {/* 置顶推荐文章 */}
       <div
