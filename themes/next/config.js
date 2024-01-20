9f50e74f340824d0c3137683e08e41e1338543f1
hk6429 <87607648+hk6429@users.noreply.github.com>
2024-01-21 01:13:44 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
 
   NEXT_HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
-  NEXT_HOME_BANNER_STRINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  NEXT_HOME_BANNER_STRINGS: ['Hi，Hi，歡迎來到好聽書齋'], // 首页大图标语文字
 
   NEXT_NAV_TYPE: 'normal', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
