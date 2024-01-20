931debcaae305baf4d719128aa541f05ea4fd7f6
kforris <58551353+kforris@users.noreply.github.com>
2024-01-20 05:04:15 -0500

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -577,6 +577,12 @@ const LayoutTagIndex = props => {
     </LayoutBase>
   )
 }
+<link rel="stylesheet" href="https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css">
+<script>
+let tianliGPT_postSelector = '#notion-article';
+let tianliGPT_key = 'f50e6b018a094fbb0d7c';
+</script>
+<script src="https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.js"></script>
 
 export {
   CONFIG as THEME_CONFIG,
