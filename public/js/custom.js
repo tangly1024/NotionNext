76aefe352dc0ec29fd93906367e101c4b39edbdf
kforris <58551353+kforris@users.noreply.github.com>
2024-01-20 04:57:13 -0500

    Update custom.js

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,1 +1,7 @@
 // 这里编写自定义js脚本；将被静态引入到页面中
+<link rel="stylesheet" href="https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css">
+<script>
+let tianliGPT_postSelector = '#notion-article';
+let tianliGPT_key = 'f50e6b018a094fbb0d7c';
+</script>
+<script src="https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.js"></script>
