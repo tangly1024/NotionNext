b662a9d92a42ca5dcbd0a4a97ac482e80af7b59a
JOSUE VONRUEDEN <69669504+JeffeyShaw@users.noreply.github.com>
2024-01-21 21:01:20 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -16,7 +16,7 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享热爱',
-  HEO_HERO_TITLE_2: '拥抱新世界',
+  HEO_HERO_TITLE_2: '拥抱全世界',
   HEO_HERO_TITLE_3: 'kakarot.cc',
   HEO_HERO_TITLE_4: '新内容上线',
   HEO_HERO_TITLE_5: '不知道的事情要比知道的多得多',
