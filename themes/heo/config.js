da24bbc986b707bc2ae90286959e609765bd5a3f
niustark <niustark@qq.com>
2024-01-24 22:34:27 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-   { title: '欢迎来到我的博客', url: 'https://niu7.top' },
-    { title: '人穷就要多读书', url: 'https://zlibrary-hk.se/' },
+    { title: '欢迎来到我的博客', url: 'https://niu7.top' },
+    { title: '人穷就要多读书', url: '' },
    
    ],
 
