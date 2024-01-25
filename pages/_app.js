d9b04465fd7fe930816bb078c0fbc67ed9895988
waytoAGI <mimiqiao@gmail.com>
2024-01-25 18:49:53 +0800

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import '@/styles/globals.css'
 import '@/styles/nprogress.css'
 import '@/styles/utility-patterns.css'
 
+
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import '@/styles/notion.css' //  重写部分样式
@@ -12,14 +13,16 @@ import { GlobalContextProvider } from '@/lib/global'
 
 // 各种扩展插件 这个要阻塞引入
 import ExternalPlugins from '@/components/ExternalPlugins'
+import Header from '@/components/Header'
 
 const MyApp = ({ Component, pageProps }) => {
   return (
-        <GlobalContextProvider {...pageProps}>
-            <Component {...pageProps} />
-            {/* 全局插件 , 自定义样式、组件等在这里统一引入 */}
-            <ExternalPlugins {...pageProps} />
-        </GlobalContextProvider>
+        <GlobalContextProvider>
+          <Header currentNav="https://blog.waytoagi.com/" />
+          <Component {...pageProps} />
+          <ExternalPlugins {...pageProps} />
+          <ExternalScript />
+      </GlobalContextProvider>
   )
 }
 
