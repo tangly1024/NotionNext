145ebe51c7552651b99e70315eef46606e5ff91d
waytoAGI <mimiqiao@gmail.com>
2024-01-25 09:24:58 +0800

    Update blog.config.js 修改标题

--- blog.config.js
+++ blog.config.js
@@ -17,8 +17,8 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Way to AGI', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || 'An AI learning wiki', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '《通往 AGI 之路》知识库！这里旨在提供一个全面系统的 AI 学习路径，帮助您了解从 AI 常见名词到 AI 应用等各方面知识，更重要的是，希望引发您思考：「我可以用 AI 做什么，帮助自己更强大」', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'http://blog.waytoAGI.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, AI, AGI, Blog, wiki, 知识库, AI知识', // 网站关键词 英文逗号隔开
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
