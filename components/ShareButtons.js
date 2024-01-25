81e3ffc68e54ce29db5ba4a7818071c8ca2d64a5
waytoAGI <mimiqiao@gmail.com>
2024-01-25 17:58:27 +0800

    Add files via upload

--- components/ShareButtons.js
+++ components/ShareButtons.js
@@ -1,7 +1,8 @@
-import BLOG from '@/blog.config'
+import { siteConfig } from '@/lib/config'
 import { useGlobal } from '@/lib/global'
 import copy from 'copy-to-clipboard'
-import QRCode from 'qrcode.react'
+import dynamic from 'next/dynamic'
+import { useRouter } from 'next/router'
 import { useState } from 'react'
 
 import {
@@ -49,14 +50,22 @@ import {
   HatenaIcon
 } from 'react-share'
 
+const QrCode = dynamic(() => import('@/components/QrCode'), { ssr: false })
+
 /**
  * @author https://github.com/txs
  * @param {*} param0
  * @returns
  */
-const ShareButtons = ({ shareUrl, title, body, image }) => {
-  const services = BLOG.POSTS_SHARE_SERVICES.split(',')
-  const titleWithSiteInfo = title + ' | ' + BLOG.TITLE
+const ShareButtons = ({ post }) => {
+  const router = useRouter()
+  const shareUrl = siteConfig('LINK') + router.asPath
+  const title = post.title || siteConfig('TITLE')
+  const image = post.pageCover
+  const body = post?.title + ' | ' + title + ' ' + shareUrl + ' ' + post?.summary
+
+  const services = siteConfig('POSTS_SHARE_SERVICES').split(',')
+  const titleWithSiteInfo = title + ' | ' + siteConfig('TITLE')
   const { locale } = useGlobal()
   const [qrCodeShow, setQrCodeShow] = useState(false)
 
@@ -91,7 +100,7 @@ const ShareButtons = ({ shareUrl, title, body, image }) => {
                         <FacebookMessengerShareButton
                             key={singleService}
                             url={shareUrl}
-                            appId={BLOG.FACEBOOK_APP_ID}
+                            appId={siteConfig('FACEBOOK_APP_ID')}
                             className="mx-1"
                         >
                             <FacebookMessengerIcon size={32} round />
@@ -334,9 +343,9 @@ const ShareButtons = ({ shareUrl, title, body, image }) => {
               }
               if (singleService === 'qq') {
                 return <button key={singleService} className='cursor-pointer bg-blue-600 text-white rounded-full mx-1'>
-                        <div target='_blank' rel='noreferrer' href={`http://connect.qq.com/widget/shareqq/index.html?url=${shareUrl}&sharesource=qzone&title=${title}&desc=${body}`} >
+                        <a target='_blank' rel='noreferrer' aria-label="Share by QQ" href={`http://connect.qq.com/widget/shareqq/index.html?url=${shareUrl}&sharesource=qzone&title=${title}&desc=${body}`} >
                             <i className='fab fa-qq w-8' />
-                        </div>
+                        </a>
                     </button>
               }
               if (singleService === 'wechat') {
@@ -346,8 +355,8 @@ const ShareButtons = ({ shareUrl, title, body, image }) => {
                         </div>
                         <div className='absolute'>
                         <div id='pop' className={(qrCodeShow ? 'opacity-100 ' : ' invisible opacity-0') + ' z-40 absolute bottom-10 -left-10 bg-white shadow-xl transition-all duration-200 text-center'}>
-                                <div className='p-2 mt-1'>
-                                    <QRCode value={shareUrl} fgColor='#000000' />
+                                <div className='p-2 mt-1 w-28 h-28'>
+                                    { qrCodeShow && <QrCode value={shareUrl}/> }
                                 </div>
                                 <span className='text-black font-semibold p-1 rounded-t-lg text-sm mx-auto mb-1'>
                                     {locale.COMMON.SCAN_QR_CODE}
