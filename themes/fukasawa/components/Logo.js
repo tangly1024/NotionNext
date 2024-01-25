ba6235d0ad460fa62927422e3d71a85f98837cfc
waytoAGI <mimiqiao@gmail.com>
2024-01-25 20:07:51 +0800

    Update Logo.js

--- themes/fukasawa/components/Logo.js
+++ themes/fukasawa/components/Logo.js
@@ -5,7 +5,7 @@ const Logo = props => {
   return (
    <section className='flex'>
     <Link
-     href='/'
+     href='https://new.waytoagi.com/'
      className='hover:bg-black hover:text-white border-black border-2 duration-500 px-4 py-2 cursor-pointer dark:text-gray-300 dark:border-gray-300 font-black'>
      {siteConfig('TITLE')}
     </Link>
