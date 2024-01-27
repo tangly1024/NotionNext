49f43443fc047699a018fae6dfcf271bf25e577c
niustark <niustark@qq.com>
2024-01-27 16:23:13 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -42,7 +42,7 @@ export function InfoCard(props) {
                 <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
                     {/* 两个社交按钮 */}
                     <div className='bg-indigo-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
-                        <Link href={siteConfig('CONTACT_DOUYIN', null, CONFIG)} a target='_blank'><i className='fab fa-tiktok text-xl'/></Link>
+                        <Link href= '/' a target='_blank'><i className='fab fa-tiktok text-xl'/></Link>  // {siteConfig('CONTACT_DOUYIN', null, CONFIG)} 
                     </div>
                     <div className='bg-indigo-400 p-2 rounded-full w-10 items-center flex justify-center transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
                         <Link href={siteConfig('HEO_INFO_CARD_URL', null, CONFIG)} a target='_blank'><i className='fas fa-book text-xl' />
