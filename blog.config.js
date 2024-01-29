6a5d311dc14e4892cd185ba8210279ad5a3769b8
fangs <45865656+anxinweb@users.noreply.github.com>
2024-01-30 01:59:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Ah新', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'AHXIN｜我是Ah新呀', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '恭喜你在这里看到了一只新🐽', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://ahxin.cn', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ah新的博客,我是ah新呀,方岸新,方岸新的个人网站,方岸新的个人博客,阿新的个人博客,AHXIN,阿新的个人网站,ah新的个人网站,不知所措的新哥,ahxin', // 网站关键词 英文逗号隔开
