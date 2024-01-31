3a9da4ed6aff6979f7e955e418480e7ee334e5e3
tangly1024.com <tlyong1992@hotmail.com>
2024-01-31 16:23:48 +0800

    Gitbook导航微调

--- themes/gitbook/components/BlogPostCard.js
+++ themes/gitbook/components/BlogPostCard.js
@@ -6,13 +6,12 @@ const BlogPostCard = ({ post, className }) => {
   const router = useRouter()
   const currentSelected = router.asPath.split('?')[0] === '/' + post.slug
   return (
-        <div key={post.id} className={`${className} py-1 cursor-pointer px-2 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
+        <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref> <div key={post.id} className={`${className} py-1.5 cursor-pointer px-1.5 hover:bg-gray-50 rounded-md dark:hover:bg-gray-600  ${currentSelected ? 'bg-green-50 text-green-500' : ''}`}>
             <div className="flex flex-col w-full select-none">
-                <Link href={`${siteConfig('SUB_PATH', '')}/${post.slug}`} passHref>
-                    {post.title}
-                </Link>
+                {post.title}
             </div>
         </div>
+        </Link>
   )
 }
 
