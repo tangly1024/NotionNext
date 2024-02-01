005cfb0ad68878ac6fd31e705c5f9179ee7b879c
Leafbritain <130814915+Leafbritain@users.noreply.github.com>
2024-02-01 19:46:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS: process.env.NEXT_PUBLIC_GREETING_WORDS || '我们如海鸥之于波与波涛似的，遇见了，走近了, 海鸥飞去，波涛滚滚流开,我们也分别了。',
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '飞鸟集', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '心爱的，再会了。', // 作者简介
