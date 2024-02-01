8a9bb1fc9ffd3d807d0c906a8b398917e2865ac5
Leafbritain <130814915+Leafbritain@users.noreply.github.com>
2024-02-01 17:02:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,8 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'koimoyou', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '欢迎来到常春庭院
-    如今，已经在这里等待了漫长的时光。', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '欢迎来到常春庭院。如今，已经在这里等待了漫长的时光。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://koimoyou.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
