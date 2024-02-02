ab19a5a5d2b921976874095aa17c2e29396d7647
hw2001aq <149557994+hw2001aq@users.noreply.github.com>
2024-02-02 11:45:09 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -33,7 +33,7 @@ const Footer = ({ title }) => {
 
             <div id='footer-bottom-right'>
                 {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a></>}
-
+                {siteConfig('BEI_AN2') && <><i className='fas fa-shield-alt' /> <a href='https://icp.gov.moe/?keyword=20231188' className='mr-2'>{siteConfig('BEI_AN2')}</a></>}  
                 <span className='hidden busuanzi_container_site_pv'>
                     <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
                 <span className='pl-2 hidden busuanzi_container_site_uv'>
