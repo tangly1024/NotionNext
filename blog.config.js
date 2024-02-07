84f8125c63fb0e5f2f0a47f5d21816169ba9fa0d
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2024-02-07 21:59:57 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,14 +19,14 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '没用的小废鼠', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '真的很没用', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ymh0000123.github.io', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://xiaofeishu-boke.netlify.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
-  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/ymh00000123', // 你的github个人主页 例如 https://github.com/tangly1024
+  CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || 'https://github.com/ymh0000123', // 你的github个人主页 例如 https://github.com/tangly1024
   CONTACT_TELEGRAM: process.env.NEXT_PUBLIC_CONTACT_TELEGRAM || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
   CONTACT_LINKEDIN: process.env.NEXT_PUBLIC_CONTACT_LINKEDIN || '', // 你的linkedIn 首页
   CONTACT_INSTAGRAM: process.env.NEXT_PUBLIC_CONTACT_INSTAGRAM || '', // 您的instagram地址
