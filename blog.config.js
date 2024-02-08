a0df39fc0a57f4a437133ee98ec4d9486a0682b6
没用的小废鼠 <107793048+ymh0000123@users.noreply.github.com>
2024-02-08 19:22:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -95,7 +95,7 @@ const BLOG = {
   CUSTOM_RIGHT_CLICK_CONTEXT_MENU_THEME_SWITCH: process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU_THEME_SWITCH || true,
 
   // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: [''], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
+  CUSTOM_EXTERNAL_JS: ['https://ymh-resource-files.pages.dev/js/xn.js'], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
   CUSTOM_EXTERNAL_CSS: [''], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
 
   // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
