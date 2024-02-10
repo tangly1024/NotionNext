544a6418c8321dbad5b9cd1687213be0080c3268
shaxao <95196396+shaxao@users.noreply.github.com>
2024-02-10 12:59:41 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -33,12 +33,9 @@ export function InfoCard(props) {
                 {siteConfig('AUTHOR')}
             </h2>
 
-            {/* 公告栏 */}
-           <div>
-                <Announcement post={notice} style={{ color: 'white !important' }} />
-            </div>
+            {/* 公告栏  <div> <Announcement post={notice} style={{ color: 'white !important' }} /> </div> */}
 
-            <div className='flex justify-between'>
+            <div className='flex justify-between mt-4'>
                 <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
                     {/* 两个社交按钮 */}
                     <div className='bg-indigo-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
