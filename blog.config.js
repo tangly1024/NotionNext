2e57a826af110a641848c4ef527b77a6d8d0000a
Jason Wu <86462706+smthelping@users.noreply.github.com>
2024-02-18 22:24:48 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SMTfeeder', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Specialized in Design and Manufacturing SMT THT feeder', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://feeder.smthelp.link', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://fdr.smthelp.eu', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'SMT,THT,PCB,PCBA,EMS,Odd form, Wave soldering,PCB assembly', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
