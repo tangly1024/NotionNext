a8d9385a11c02a62501aff8efb7106c6760fc24e
xiaohao <jiahao.yin@qq.com>
2024-02-19 14:54:31 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -39,7 +39,7 @@ const CONFIG = {
     '🏃 脚踏实地行动派',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/tangly1024/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: '', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
