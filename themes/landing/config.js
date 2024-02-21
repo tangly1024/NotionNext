dbf4b00485d9ae707603dca346379636dbcf40e8
Jason Wu <86462706+smthelping@users.noreply.github.com>
2024-02-21 15:17:11 +0800

    Update config.js

--- themes/landing/config.js
+++ themes/landing/config.js
@@ -1,18 +1,18 @@
 const CONFIG = {
 
-  LANDING_HEADER_BUTTON_1_TITLE: 'Github开源',
-  LANDING_HEADER_BUTTON_1_URL: 'https://github.com/tangly1024/NotionNext',
+  LANDING_HEADER_BUTTON_1_TITLE: 'Professional SMT THT Machine',
+  LANDING_HEADER_BUTTON_1_URL: 'https://file.autoinsertion.com',
 
-  LANDING_HEADER_BUTTON_2_TITLE: '作者博客',
-  LANDING_HEADER_BUTTON_2_URL: 'https://blog.tangly1024.com/',
+  LANDING_HEADER_BUTTON_2_TITLE: 'Southern Machinery',
+  LANDING_HEADER_BUTTON_2_URL: 'https://www.smthelp.com/',
 
   // 首页大图英雄板块
-  LANDING_HERO_TITLE_1: 'NotionNext',
-  LANDING_HERO_P_1: '快速搭建独立站、轻松放大品牌价值！',
-  LANDING_HERO_BUTTON_1_TEXT: '开始体验',
-  LANDING_HERO_BUTTON_1_LINK: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next',
-  LANDING_HERO_BUTTON_2_TEXT: '了解更多',
-  LANDING_HERO_BUTTON_2_LINK: 'https://docs.tangly1024.com/about',
+  LANDING_HERO_TITLE_1: 'SMT THT Feeder',
+  LANDING_HERO_P_1: 'Specialized in Design and Manufacturing SMT THT Feeder for Smart EMS Factory',
+  LANDING_HERO_BUTTON_1_TEXT: 'Contact us: info@SMThelp.com',
+  LANDING_HERO_BUTTON_1_LINK: 'https://www.linkedin.com/company/smtmachine',
+  LANDING_HERO_BUTTON_2_TEXT: 'Learn More: SMThelp.com',
+  LANDING_HERO_BUTTON_2_LINK: 'https://www.youtube.com/c/Smthelping',
   LANDING_HERO_VIDEO_IMAGE: '/images/home.png',
   //   HERO_VIDEO_URL: '/videos/video.mp4',
   LANDING_HERO_VIDEO_IFRAME: '//player.bilibili.com/player.html?aid=913088616&bvid=BV1fM4y1L7Qi&cid=1187218697&page=1&&high_quality=1',
