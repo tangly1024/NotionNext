9310df0434f60d67bb84c0de651c8d717ba9d3df
tangly1024.com <tlyong1992@hotmail.com>
2024-02-23 14:11:25 +0800

    starter text

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -13,9 +13,9 @@ const CONFIG = {
   STARTER_HERO_TITLE_1: '开源且免费的基于 Notion 笔记的网站构建工具', // 英雄区文字
   STARTER_HERO_TITLE_2: '通过笔记无感知地建站、成倍放大您的价值', // 英雄区文字
   STARTER_HERO_TITLE_3: '使用最新技术构建', // 英雄区文字
-  STARTER_HERO_BUTTON_1_TEXT: 'Start Now', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
   STARTER_HERO_BUTTON_1_URL: 'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: 'Star on Github', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
