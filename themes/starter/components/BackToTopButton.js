d00701bcd7b42940697979fb28f26939717064fc
tangly1024.com <tlyong1992@hotmail.com>
2024-02-23 12:20:12 +0800

    4.3.0

--- themes/starter/components/BackToTopButton.js
+++ themes/starter/components/BackToTopButton.js
@@ -59,7 +59,7 @@ export const BackToTopButton = () => {
 
   return <>
         {/* <!-- ====== Back To Top Start --> */}
-        <a className="back-to-top fixed bottom-8 left-auto right-8 z-[999] hidden h-10 w-10 items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-dark">
+        <a className="back-to-top fixed bottom-16 left-auto right-8 z-[999] hidden h-10 w-10 items-center justify-center rounded-md bg-primary text-white shadow-md transition duration-300 ease-in-out hover:bg-dark">
             <span className="mt-[6px] h-3 w-3 rotate-45 border-l border-t border-white" ></span>
          </a>
     {/* <!-- ====== Back To Top End --> */}
