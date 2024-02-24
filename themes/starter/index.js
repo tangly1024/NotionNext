c983705829c869c5e274ed1a796a7c70b302d22d
tangly1024 <mail@tangly1024.com>
2024-02-24 20:52:42 +0800

    hexo 深色背景

--- themes/starter/index.js
+++ themes/starter/index.js
@@ -54,7 +54,7 @@ const LayoutBase = (props) => {
     loadWowJS()
   }, [])
 
-  return <div id='theme-starter' className={`${siteConfig('FONT_STYLE')} min-h-screen flex flex-col dark:bg-black scroll-smooth`}>
+  return <div id='theme-starter' className={`${siteConfig('FONT_STYLE')} min-h-screen flex flex-col dark:bg-[#212b36] scroll-smooth`}>
             <Style/>
             <NavBar {...props}/>
 
