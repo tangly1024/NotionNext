2749e92414fb473866856caf24dea7bc63bf07a5
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2024-02-27 00:00:43 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,13 +1,10 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-11-21', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
-  HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
-  ],
+  HEO_NOTICE_BAR: [],
 
   // 英雄区左右侧组件颠倒位置
   HEO_HERO_REVERSE: false,
@@ -15,12 +12,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '坚持梦想',
+  HEO_HERO_TITLE_2: '与我同行，大梦音乐',
+  HEO_HERO_TITLE_3: 'www.mskyer.com',
+  HEO_HERO_TITLE_4: '博客新版',
+  HEO_HERO_TITLE_5: '佛系小店——welcome to my shop',
+  HEO_HERO_TITLE_LINK: 'https://www.mskyer.com/shop',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
