e16e663602f317bc976f60f926ad2add30e86b8b
ZengGTmaster <150670393+ZengGTmaster@users.noreply.github.com>
2024-02-28 09:53:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,8 +22,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_2: { title: '会员下载', url: 'https://www.mskyer.com/download' },
+  HEO_HERO_CATEGORY_3: { title: '我的B站', url: 'https://space.bilibili.com/156820044?spm_id_from=333.1007.0.0' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
