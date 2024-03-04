8144e617f5c199bff51364a29a9d9545f769bfc9
Wata Appliances <157789171+WataAppliances@users.noreply.github.com>
2024-03-04 16:59:34 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -12,6 +12,9 @@ const CONFIG = {
   // 英雄区块导航
   STARTER_HERO_TITLE_1: 'Wata Home Appliances', // 英雄区文字
   STARTER_HERO_TITLE_2: 'Professional Supplier of Home Appliances', // 英雄区文字
+  STARTER_HERO_TITLE_3: ' ', // 英雄区文字
+  STARTER_HERO_BUTTON_1_TEXT: ' ', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: ' ', // 英雄区按钮
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
   // 特性区块
