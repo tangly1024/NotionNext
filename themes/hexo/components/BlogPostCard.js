95f8ae06a1d601ec3836ff57bf8e829edabc3d11
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2024-03-05 02:34:04 +0800

    Update BlogPostCard.js

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -34,7 +34,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
                 {showPageCover && (
                     <div className="md:w-5/12 overflow-hidden">
                         <Link href={url} passHref legacyBehavior>
-                        <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500' />
+                        <LazyImage priority={index === 1} src={post?.pageCoverThumbnail} className='h-60 w-full object-cover object-center group-hover:scale-110 duration-500' />
                         </Link>
                     </div>
                 )}
