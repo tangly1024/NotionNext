576e2b05b44e43a46c87f0b6598eb9795b133091
ohhyet <156559279+ohhyet@users.noreply.github.com>
2024-03-06 03:03:52 +0800

    Update ArticleInfo.js

--- themes/gitbook/components/ArticleInfo.js
+++ themes/gitbook/components/ArticleInfo.js
@@ -4,6 +4,6 @@ export default function ArticleInfo({ post }) {
   }
   return <div className="pt-10 pb-6 text-gray-400 text-sm border-b">
         <i className="fa-regular fa-clock mr-1" />
-        Last update:  { post.date?.start_date}
+        Last update:  {new Date().getFullYear()}
     </div>
 }
