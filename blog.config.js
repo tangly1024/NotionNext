4d85a23f50053ef266ba3c6f0ce29354141acadd
ohhyet <156559279+ohhyet@users.noreply.github.com>
2024-03-06 03:10:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,7 +19,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SIXCNC', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'wholesale price catalog', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'ttps://ohhyet.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://sixcnc.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ATC spindle,HMI,PLC', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
