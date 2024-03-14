8953f08be67712f9ec193cbbdfd353542b8e7efa
xiaohao <jiahao.yin@qq.com>
2024-03-14 16:39:00 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,7 +29,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  HERO_RECOMMEND_COVER: 'https://media.st.dl.eccdnx.com/steam/apps/1086940/header.jpg?t=1705604554', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://media.st.dl.eccdnx.com/steam/apps/1623730/header_tchinese.jpg?t=1707904340', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
