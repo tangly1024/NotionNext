8496f17dc8a6e5cc78f0fc1d00e38c3c98271318
maurice <mauricelau6@gmail.com>
2024-03-16 15:08:31 +0800

    316

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -37,10 +37,10 @@ export default function SideRight(props) {
 
             <InfoCard {...props} className='w-72' />
 
-            <div className='sticky top-20 space-y-4'>
+            <div className='.'>
 
                 {/* 文章页显示目录 */}
-                {post && post.toc && post.toc.length > 0 && (
+                {post && post.toc && (
                     <Card className='bg-white dark:bg-[#1e1e1e]'>
                         <Catalog toc={post.toc} />
                     </Card>
