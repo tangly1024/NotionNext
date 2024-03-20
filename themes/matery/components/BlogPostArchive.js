0bd6103113e3b80984c01809701657cd2c4cc14c
Enawawa <49240294+Enawawa@users.noreply.github.com>
2024-03-20 19:34:16 +0800

    Update BlogPostArchive.js

--- themes/matery/components/BlogPostArchive.js
+++ themes/matery/components/BlogPostArchive.js
@@ -27,13 +27,13 @@ const BlogPostArchive = ({ posts = [], archiveTitle }) => {
 
             return <li
               key={post.id}
-              className="border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-indigo-500 dark:hover:border-indigo-300 dark:border-indigo-400 transform duration-500"
+              className="border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-blue-500 dark:hover:border-blue-300 dark:border-blue-400 transform duration-500"
             >
               <div id={post?.publishDay}>
                 <span className="text-gray-400">{post.date?.start_date}</span>{' '}
                 &nbsp;
                 <Link href={url} passHref
-                  className="dark:text-gray-400  dark:hover:text-indigo-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600">
+                  className="dark:text-gray-400  dark:hover:text-blue-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600">
 
                   {post.title}
 
