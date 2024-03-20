0d774431e54eed9de3d60c441c9e995f3882e189
Enawawa <49240294+Enawawa@users.noreply.github.com>
2024-03-20 19:36:07 +0800

    Update index.js

--- themes/matery/index.js
+++ themes/matery/index.js
@@ -332,7 +332,7 @@ const LayoutCategoryIndex = props => {
                         {categoryOptions?.map(e => {
                           return (
                                 <Link key={e.name} href={`/category/${e.name}`} passHref legacyBehavior>
-                                    <div className='duration-300 text-md whitespace-nowrap dark:hover:text-white px-5 cursor-pointer py-2 hover:text-indigo-400' >
+                                    <div className='duration-300 text-md whitespace-nowrap dark:hover:text-white px-5 cursor-pointer py-2 hover:text-blue-400' >
                                         <i className={'mr-4 fas fa-folder'} />  {e.name}({e.count})
                                     </div>
                                 </Link>
