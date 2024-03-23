a3b1e0d57448ff6cb90691eceff86a354cbd5922
jiejie2023 <140963083+jiejie2023@users.noreply.github.com>
2024-03-23 14:24:36 +0800

    Update BlogPostCard.js

--- themes/movie/components/BlogPostCard.js
+++ themes/movie/components/BlogPostCard.js
@@ -14,7 +14,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
   const url = checkContainHttp(post.slug) ? sliceUrlFromHttp(post.slug) : `${siteConfig('SUB_PATH', '')}/${post.slug}`
 
   return (
-    <article data-wow-delay=".2s" className='wow fadeInUp w-full mb-4 cursor-pointer overflow-hidden shadow-movie dark:bg-hexo-black-gray md:mb-4'>
+    <article data-wow-delay=".2s" className='wow fadeInUp w-full mb-8 cursor-pointer overflow-hidden shadow-movie dark:bg-hexo-black-gray md:mb-12'>
       <Link href={url} passHref legacyBehavior>
         {/* 固定高度 ，空白用图片拉升填充 */}
         <div className='group flex flex-col aspect-[2.5/3] justify-between relative'>
