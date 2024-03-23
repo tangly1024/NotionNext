5339b42b6881ab06013911b008bf1fdd2d6476bf
ynkmsy <162958204+ynkmsy@users.noreply.github.com>
2024-03-23 21:12:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '心情如歌', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'SY Blog🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.sy66.eu.org', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.sy77.eu.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '数码, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
