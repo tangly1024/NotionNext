bcf9712eb0cfbd2f5fcd8d512d82c0b8e88963e5
ohhyet <156559279+ohhyet@users.noreply.github.com>
2024-03-23 19:35:05 +0800

    Update config.js

--- themes/gitbook/config.js
+++ themes/gitbook/config.js
@@ -2,13 +2,13 @@ const CONFIG = {
 
   GITBOOK_INDEX_PAGE: 'about', // 文档首页显示的文章，请确此路径包含在您的notion数据库中
 
-  GITBOOK_AUTO_SORT: process.env.NEXT_PUBLIC_GITBOOK_AUTO_SORT || true, // 是否自动按分类名 归组排序文章；自动归组可能会打乱您Notion中的文章顺序
+  GITBOOK_AUTO_SORT: process.env.NEXT_PUBLIC_GITBOOK_AUTO_SORT || flase, // 是否自动按分类名 归组排序文章；自动归组可能会打乱您Notion中的文章顺序
 
   // 菜单
-  GITBOOK_MENU_CATEGORY: true, // 显示分类
-  GITBOOK_BOOK_MENU_TAG: true, // 显示标签
-  GITBOOK_MENU_ARCHIVE: true, // 显示归档
-  GITBOOK_MENU_SEARCH: true, // 显示搜索
+  GITBOOK_MENU_CATEGORY: false, // 显示分类
+  GITBOOK_BOOK_MENU_TAG: false, // 显示标签
+  GITBOOK_MENU_ARCHIVE: false, // 显示归档
+  GITBOOK_MENU_SEARCH: false, // 显示搜索
 
   // Widget
   GITBOOK_WIDGET_REVOLVER_MAPS: process.env.NEXT_PUBLIC_WIDGET_REVOLVER_MAPS || 'false', // 地图插件
