954ce01c597ac3b528595a481e6436d085755617
ohhyet <156559279+ohhyet@users.noreply.github.com>
2024-03-25 17:55:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SIXCNC', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'SIX Catalog', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'wholesale price catalog', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://sixcnc.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ATC spindle,HMI,PLC', // 网站关键词 英文逗号隔开
