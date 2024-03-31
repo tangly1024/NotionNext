9600be41b2ae9e71f4c4accdf48c54d1ae30c0f8
kenchikuliu <44759890+kenchikuliu@users.noreply.github.com>
2024-04-01 01:26:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -19,8 +19,8 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '超越想象',
   HEO_HERO_TITLE_3: 'kenchikuliu.com',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '与AGI共同成长',
+  HEO_HERO_TITLE_LINK: 'https://www.linkedin.com/in/kenchikuliu/',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
