68a8cbd1ba9e12a91e76002011ee7697e467692f
kenchikuliu <44759890+kenchikuliu@users.noreply.github.com>
2024-04-01 12:56:16 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -108,7 +108,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_URL: 'https://www.linkedin.com/in/kenchikuliu/',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
