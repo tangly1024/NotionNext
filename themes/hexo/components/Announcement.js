9cfeb49bfe09a7e4aabbc9b3eb09bed1bbdfdeb7
12434565 <94098611+12434565@users.noreply.github.com>
2024-04-10 18:19:18 +0800

    Update Announcement.js

--- themes/hexo/components/Announcement.js
+++ themes/hexo/components/Announcement.js
@@ -9,8 +9,8 @@ const Announcement = ({ post, className }) => {
     return <div className={className}>
         <section id='announcement-wrapper' className="dark:text-gray-300 border dark:border-black rounded-xl lg:p-6 p-4 bg-white dark:bg-hexo-black-gray">
             <div><i className='mr-2 fas fa-bullhorn' />{locale.COMMON.ANNOUNCEMENT}</div>
-            {post && (<div id="announcement-content" />
-            <NotionPage post={post}>
+            {post && (<div id="announcement-content">
+            <NotionPage post={post}/>
         </div>)}
         </section>
     </div>
