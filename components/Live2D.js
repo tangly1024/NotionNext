d59cdb83499c66dc2fbaeba36dff01893248b562
rainnn-w <114235528+rainnn-w@users.noreply.github.com>
2024-04-12 19:01:11 +0800

    Add files via upload

--- components/Live2D.js
+++ components/Live2D.js
@@ -1,31 +1,22 @@
 /* eslint-disable no-undef */
-import { siteConfig } from '@/lib/config'
+import BLOG from '@/blog.config'
 import { useGlobal } from '@/lib/global'
-import { isMobile, loadExternalResource } from '@/lib/utils'
+import { loadExternalResource } from '@/lib/utils'
 import { useEffect } from 'react'
 
-/**
- * 网页动画
- * @returns
- */
 export default function Live2D() {
   const { theme, switchTheme } = useGlobal()
-  const showPet = JSON.parse(siteConfig('WIDGET_PET'))
-  const petLink = siteConfig('WIDGET_PET_LINK')
-  const petSwitchTheme = siteConfig('WIDGET_PET_SWITCH_THEME')
+  const showPet = JSON.parse(BLOG.WIDGET_PET)
 
   useEffect(() => {
-    if (showPet && !isMobile()) {
+    if (showPet) {
       Promise.all([
-        loadExternalResource(
-          'https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js',
-          'js'
-        )
-      ]).then(e => {
+        loadExternalResource('https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/live2d.min.js', 'js')
+      ]).then((e) => {
         if (typeof window?.loadlive2d !== 'undefined') {
           // https://github.com/xiazeyu/live2d-widget-models
           try {
-            loadlive2d('live2d', petLink)
+            loadlive2d('live2d', BLOG.WIDGET_PET_LINK)
           } catch (error) {
             console.error('读取PET模型', error)
           }
@@ -35,7 +26,7 @@ export default function Live2D() {
   }, [theme])
 
   function handleClick() {
-    if (petSwitchTheme) {
+    if (JSON.parse(BLOG.WIDGET_PET_SWITCH_THEME)) {
       switchTheme()
     }
   }
@@ -44,15 +35,9 @@ export default function Live2D() {
     return <></>
   }
 
-  return (
-    <canvas
-      id='live2d'
-      width='280'
-      height='250'
-      onClick={handleClick}
-      className='cursor-grab'
-      onMouseDown={e => e.target.classList.add('cursor-grabbing')}
-      onMouseUp={e => e.target.classList.remove('cursor-grabbing')}
+  return <canvas id="live2d" width="280" height="400" onClick={handleClick}
+        className="cursor-grab"
+        onMouseDown={(e) => e.target.classList.add('cursor-grabbing')}
+        onMouseUp={(e) => e.target.classList.remove('cursor-grabbing')}
     />
-  )
 }
