691c8d7794f00c423961d546915800fb632e8874
noirsue <110968741+noirsue@users.noreply.github.com>
2024-04-16 22:12:12 +0800

    Update style.js

--- themes/gitbook/style.js
+++ themes/gitbook/style.js
@@ -15,4 +15,35 @@ const Style = () => {
   `}</style>
 }
 
+// 视频聚合走马灯
+      .notion-carousel {
+        width: 100%; /* 根据需要调整 */
+        overflow: hidden;
+      }
+
+      .notion-carousel-wrapper .notion-carousel {
+        display: none;
+      }
+
+      .notion-carousel-wrapper .notion-carousel.active {
+        display: block;
+      }
+
+      .notion-carousel-route div {
+        cursor: pointer;
+        margin-bottom: 0.2rem;
+      }
+
+      .notion-carousel-route div:hover {
+        text-decoration: underline; 
+      }
+
+      .notion-carousel div {
+        height: auto !important;
+        aspect-ratio: 2/1 !important;
+      }
+    `}</style>
+  )
+}
+
 export { Style }
