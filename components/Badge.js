f7d1017477db065c90327fc205b23dccaa335364
Lyon Liu <ayon.liu@gmail.com>
2024-04-16 16:31:41 +0800

    change component Badge class to className

--- components/Badge.js
+++ components/Badge.js
@@ -4,8 +4,8 @@
 export default function Badge() {
   return <>
     {/* 红点 */}
-    <span class="absolute right-1 top-1 flex h-2 w-2">
-        <span class="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
-        <span class="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
+    <span className="absolute right-1 top-1 flex h-2 w-2">
+        <span className="animate-ping absolute inline-flex h-full w-full rounded-full bg-red-400 opacity-75"></span>
+        <span className="relative inline-flex rounded-full h-2 w-2 bg-red-500"></span>
     </span></>
 }
