b9644b7852899dc20d097a636745f44f1a2b2184
nanaDio <zhenzhong.fu@gmail.com>
2024-04-16 10:56:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -118,12 +118,12 @@ const CONFIG = {
   HEO_MENU_ARCHIVE: true, // 显示归档
   HEO_MENU_SEARCH: true, // 显示搜索
 
-  HEO_POST_LIST_COVER: false, // 列表显示文章封面
+  HEO_POST_LIST_COVER: true, // 列表显示文章封面
   HEO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
 
   HEO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEO_POST_LIST_SUMMARY: true, // 文章摘要
-  HEO_POST_LIST_PREVIEW: false, // 读取文章预览
+  HEO_POST_LIST_PREVIEW: true, // 读取文章预览
   HEO_POST_LIST_IMG_CROSSOVER: true, // 博客列表图片左右交错
 
   HEO_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
