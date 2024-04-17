55d52230cd343d871e5312ac58daad7ecba8986e
nfwl <96956500+nfwl@users.noreply.github.com>
2024-04-17 09:03:51 +0800

    Add files via upload

---
+++ public/Player.js
@@ -0,0 +1,7 @@
+var script = document.createElement("script");
+script.setAttribute("type","text/javascript");
+script.setAttribute("id","myhk");
+script.setAttribute("src","https://myhkw.cn/api/player/167490322175");
+script.setAttribute("key","167490322175");
+script.setAttribute("m","1");
+document.documentElement.appendChild(script);
