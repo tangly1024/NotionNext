4863f79930855856bdad32ff8cd6f9f134a6ed58
Lyon Liu <ayon.liu@gmail.com>
2024-04-19 10:21:57 +0800

    remove keywords

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -21,7 +21,7 @@ const GlobalHead = props => {
   const title = meta?.title || siteConfig('TITLE')
   const description = meta?.description || `${siteInfo?.description}`
   const type = meta?.type || 'website'
-  const keywords = meta?.tags || siteConfig('KEYWORDS')
+  // const keywords = meta?.tags || siteConfig('KEYWORDS')
   const lang = siteConfig('LANG').replace('-', '_') // Facebook OpenGraph 要 zh_CN 這樣的格式才抓得到語言
   const category = meta?.category || siteConfig('KEYWORDS') // section 主要是像是 category 這樣的分類，Facebook 用這個來抓連結的分類
 
