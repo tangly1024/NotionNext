62ddf12ec0cedacd6f3ded118d12d514bc179cd6
dear7575 <41235345+dear7575@users.noreply.github.com>
2024-04-20 00:38:52 +0800

    update

--- themes/next/components/Footer.js
+++ themes/next/components/Footer.js
@@ -24,8 +24,8 @@ const Footer = ({ title }) => {
                 <span className='pl-2 hidden busuanzi_container_site_uv'>
                     <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
                 <br />
-                <h1>{title}</h1>
-                <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://dear7575.cn' className='underline '>非你莫属</a>.</span>
+                {/* <h1>{title}</h1> */}
+                {/* <span className='text-xs font-serif  text-gray-500 dark:text-gray-300 '>Powered by  <a href='https://dear7575.cn' className='underline '>非你莫属</a>.</span> */}
             </span>
         </footer>
   )
