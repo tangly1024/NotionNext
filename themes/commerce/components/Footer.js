a908e46d92414106bc6812ae1630df8a8d134a19
dear7575 <dear7775@gmail.com>
2024-04-25 20:15:04 +0800

    add 备案号

--- themes/commerce/components/Footer.js
+++ themes/commerce/components/Footer.js
@@ -109,8 +109,9 @@ const Footer = (props) => {
                     <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{siteConfig('TITLE')} {siteConfig('BIO')}</h1>
                     <h2> {siteConfig('DESCRIPTION')}</h2>
                     {/* 可选备案信息 */}
-                    {siteConfig('GA_BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.mps.gov.cn/#/query/webSearch?code=44030002003110' className='mr-2'>{siteConfig('GA_BEI_AN')}</a></>}
-                    {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a></>}
+                    {siteConfig('GA_BEI_AN') && <><img style="object-fit:cover" src="/public/beian.png" /> <a href="https://beian.mps.gov.cn/#/query/webSearch?code=44030002003110"
+                      className="mr-2">{siteConfig('GA_BEI_AN')}</a></>}
+                  {siteConfig('BEI_AN') && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{siteConfig('BEI_AN')}</a></>}
                 </div>
             </div>
 
