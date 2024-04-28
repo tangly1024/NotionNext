f6bf40a3174fb79134e1f77edd4655ae941b60cd
闻瑞 <136212421+rui34@users.noreply.github.com>
2024-04-28 18:39:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '闻小瑞', // 您的昵称 例如 tangly1024 
-  BIO: process.env.NEXT_PUBLIC_BIO || '一位努力学习的普通人', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一位努力学习的干饭人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.wenrui.love/', // 网站地址         
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'wenrui,love,闻小瑞', // 网站关键词 英文逗号隔开 
 
@@ -430,7 +430,7 @@ const BLOG = {
   },
 
   // RSS订阅
-  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能   
+  ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || false, // 是否开启RSS订阅功能   
   MAILCHIMP_LIST_ID: process.env.MAILCHIMP_LIST_ID || null, // 开启mailichimp邮件订阅 客户列表ID ，具体使用方法参阅文档
   MAILCHIMP_API_KEY: process.env.MAILCHIMP_API_KEY || null, // 开启mailichimp邮件订阅 APIkey
 
