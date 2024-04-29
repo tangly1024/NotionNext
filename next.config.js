60a3650b60be1f922709fe850aedfa61c5f45505
tangly1024.com <tlyong1992@hotmail.com>
2024-04-29 14:41:17 +0800

    图片

--- next.config.js
+++ next.config.js
@@ -54,6 +54,22 @@ function scanSubdirectories(directory) {
 }
 
 const nextConfig = {
+  images: {
+    // 图片压缩
+    formats: ['image/avif', 'image/webp', 'image/png'],
+    // 允许next/image加载的图片 域名
+    domains: [
+      'gravatar.com',
+      'www.notion.so',
+      'avatars.githubusercontent.com',
+      'images.unsplash.com',
+      'source.unsplash.com',
+      'p1.qhimg.com',
+      'webmention.io',
+      'ko-fi.com'
+    ]
+  },
+
   // 默认将feed重定向至 /public/rss/feed.xml
   async redirects() {
     return [
