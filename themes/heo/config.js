a84049cd9cb4c24af5021ca0ebf88440f1aaabf0
lingyunsi <168530334+lingyunsi@users.noreply.github.com>
2024-05-01 13:00:41 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -98,7 +98,41 @@ const CONFIG = {
       title_2: 'Rhino',
       img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
       color_2: '#1f1f1f'
-    }
+    }HEO_GROUP_ICONS: [
+    {
+      title_1: 'Genshin-Impact',
+      img_1: '/images/heo/Genshin-Impact.webp',
+      color_1: '#ffffff',
+      title_2: 'Python',
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      color_2: '#ffffff'
+    },
+    {
+      title_1: 'Star-Rail',
+      img_1: '/images/heo/Star-Rail.webp',
+      color_1: '#ffffff',
+      title_2: 'git',
+      img_2: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
+      color_2: '#df5b40'
+    },
+    {
+      title_1: 'JS',
+      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
+      color_1: '#f7cb4f',
+      title_2: 'HTML',
+      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      color_2: '#e9572b'
+    },
+    {
+      title_1: 'Docker',
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
+      color_1: '#f29e39',
+      title_2: 'CSS3',
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      color_2: '#2c51db'
+    },
+  ],
+
   ],
 
   HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
