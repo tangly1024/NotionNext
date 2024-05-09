271d8cabb61b39e6576c5601f99392adee7e821d
DOCTOR5783 <161697619+DOCTOR5783@users.noreply.github.com>
2024-05-10 01:03:20 +0800

    Update Player.js

--- components/Player.js
+++ components/Player.js
@@ -74,7 +74,7 @@ const Player = () => {
           lrc-type={siteConfig('MUSIC_PLAYER_METING_LRC_TYPE')}
           api={siteConfig(
             'MUSIC_PLAYER_METING_API',
-            'https://api.i-meto.com/meting/api'
+        
           )}
           autoplay={autoPlay}
           order={siteConfig('MUSIC_PLAYER_ORDER')}
