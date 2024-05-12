451c14506e6e6f180309b50f592ffcfa3fa30e6d
NeoZed <tauruszero@gmail.com>
2024-05-12 20:26:56 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -4,7 +4,7 @@ const CONFIG = {
   MATERY_HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   MATERY_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
-  MATERY_HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
+  MATERY_HOME_NAV_BACKGROUND_IMG_FIXED: true, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
 
   // 是否显示开始阅读按钮
   MATERY_SHOW_START_READING: true,
