977da5985d9cb15460a1735815e0593071ca07e8
conservation <144693150+ccomnx@users.noreply.github.com>
2024-05-14 00:03:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,7 +25,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '守恒', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'AI小白，嵌入式爱好者，干饭人', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://techchenxiaoxu.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
