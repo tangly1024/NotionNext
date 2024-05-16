fecb4972888173d8614f7f0fc843e854a7f157ba
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-16 17:14:48 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -16,9 +16,9 @@ const CONFIG = {
     STARTER_HERO_BUTTON_1_TEXT: '我的博客', // 英雄区按钮
     STARTER_HERO_BUTTON_1_URL:
       'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-    STARTER_HERO_BUTTON_2_TEXT: '在哔哩哔哩上关注', // 英雄区按钮
+    STARTER_HERO_BUTTON_2_TEXT: '在BiliBili上关注', // 英雄区按钮
     STARTER_HERO_BUTTON_2_URL: 'https://space.bilibili.com/265656567', // 英雄区按钮
-    STARTER_HERO_BUTTON_2_ICON: '/images/starter/bilibili.png.', // 英雄区按钮2的图标，不需要则留空
+    STARTER_HERO_BUTTON_2_ICON: '/images/starter/bilibili.png', // 英雄区按钮2的图标，不需要则留空
   
     STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/followers.png', // 产品预览图 ，默认读取public目录下图片
   
