e0cfcc6ddf73a64558c4c1890177050fb1650cc5
NeoZed <tauruszero@gmail.com>
2024-05-16 22:56:52 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,8 +25,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '面试经验', url: '/tag/面试经验' },
-  HEO_HERO_CATEGORY_3: { title: '校友分享', url: '/tag/校友分享' },
+  HEO_HERO_CATEGORY_2: { title: '面试经验', url: '/category/面试经验' },
+  HEO_HERO_CATEGORY_3: { title: '校友分享', url: '/category/校友分享' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
