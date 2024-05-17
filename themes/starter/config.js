18cfea57a6a7f0d277310e9fefb2be7d33ead382
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-17 17:15:18 +0800

    thing

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
       'https://buxiantang.top/archive.html', // 英雄区按钮
     STARTER_HERO_BUTTON_2_TEXT: '在BiliBili上关注', // 英雄区按钮
     STARTER_HERO_BUTTON_2_URL: 'https://space.bilibili.com/265656567', // 英雄区按钮
-    STARTER_HERO_BUTTON_2_ICON: '/images/starter/bilibili.png', // 英雄区按钮2的图标，不需要则留空
+    STARTER_HERO_BUTTON_2_ICON: '/images/starter/bilibili.svg', // 英雄区按钮2的图标，不需要则留空
   
     STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/followers.png', // 产品预览图 ，默认读取public目录下图片
   
