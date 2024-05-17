5a1b15e3f34890838c8661dd0a508e26628c05a7
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-17 14:10:43 +0800

    Update MenuList.js

--- themes/starter/components/MenuList.js
+++ themes/starter/components/MenuList.js
@@ -1,15 +1,14 @@
-import { siteConfig } from '@/lib/config';
-import { useGlobal } from '@/lib/global';
-import { useEffect } from 'react';
-import CONFIG from '../config';
-import { MenuItem } from './MenuItem';
-
+import { siteConfig } from '@/lib/config'
+import { useGlobal } from '@/lib/global'
+import { useEffect } from 'react'
+import CONFIG from '../config'
+import { MenuItem } from './MenuItem'
 /**
  * 响应式 折叠菜单
  */
 export const MenuList = props => {
-  const { customNav, customMenu } = props;
-  const { locale } = useGlobal();
+  const { customNav, customMenu } = props
+  const { locale } = useGlobal()
 
   let links = [
     {
@@ -36,46 +35,50 @@ export const MenuList = props => {
       href: '/tag',
       show: siteConfig('HEO_MENU_TAG', null, CONFIG)
     }
-  ];
+  ]
 
   if (customNav) {
-    links = customNav.concat(links);
+    links = customNav.concat(links)
   }
 
   useEffect(() => {
     // ===== responsive navbar
-    const navbarToggler = document.querySelector('#navbarToggler');
-    const navbarCollapse = document.querySelector('#navbarCollapse');
+    const navbarToggler = document.querySelector('#navbarToggler')
+    const navbarCollapse = document.querySelector('#navbarCollapse')
 
     // 点击弹出菜单
     navbarToggler?.addEventListener('click', () => {
-      navbarToggler?.classList.toggle('navbarTogglerActive');
-      navbarCollapse?.classList.toggle('hidden');
-    });
+      navbarToggler?.classList.toggle('navbarTogglerActive')
+      navbarCollapse?.classList.toggle('hidden')
+    })
 
     //= ==== close navbar-collapse when a  clicked
     document
       .querySelectorAll('#navbarCollapse ul li:not(.submenu-item) a')
       .forEach(e =>
         e.addEventListener('click', () => {
-          navbarToggler?.classList.remove('navbarTogglerActive');
-          navbarCollapse?.classList.add('hidden');
+          navbarToggler?.classList.remove('navbarTogglerActive')
+          navbarCollapse?.classList.add('hidden')
         })
-      );
+      )
 
     // ===== Sub-menu
-    const submenuItems = document.querySelectorAll('.submenu-item');
+    const submenuItems = document.querySelectorAll('.submenu-item')
     submenuItems.forEach(el => {
       el.querySelector('a')?.addEventListener('click', () => {
-        el.querySelector('.submenu')?.classList.toggle('hidden');
-      });
-    });
-  }, []);
+        el.querySelector('.submenu')?.classList.toggle('hidden')
+      })
+    })
+  }, [])
 
   // 如果 开启自定义菜单，则覆盖Page生成的菜单
   if (siteConfig('CUSTOM_MENU')) {
-    links = customMenu;
+    links = customMenu
   }
+
+  //   if (!links || links.length === 0) {
+  //     return null
+  //   }
 
   return (
     <>
@@ -93,7 +96,7 @@ export const MenuList = props => {
         <nav
           id='navbarCollapse'
           className='absolute right-4 top-full hidden w-full max-w-[250px] rounded-lg bg-white py-5 shadow-lg dark:bg-dark-2 lg:static lg:block lg:w-full lg:max-w-full lg:bg-transparent lg:px-4 lg:py-0 lg:shadow-none dark:lg:bg-transparent xl:px-6'>
-          <ul className='flex justify-center lg:flex'>
+          <ul className='blcok lg:flex'>
             {links?.map((link, index) => (
               <MenuItem key={index} link={link} />
             ))}
@@ -101,5 +104,5 @@ export const MenuList = props => {
         </nav>
       </div>
     </>
-  );
-};
+  )
+}
