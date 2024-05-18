af6fa0609d9b48e125eabecd04b059efe2c92a3c
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-18 19:33:31 +0800

    forms

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -254,7 +254,7 @@ const CONFIG = {
     STARTER_CONTACT_EMAIL_TEXT: 'tiengming@qq.com',
   
     // 嵌入外部表单
-    STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/vqmj87', // 基于NoteForm创建，将留言数据存在Notion中
+    STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/juh6wg', // 基于NoteForm创建，将留言数据存在Notion中
     //   自定义留言表单，以下几个配置暂时废弃
     //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
     //   STARTER_CONTACT_MSG_NAME: '姓名',
