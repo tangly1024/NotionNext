4078a6dd676acec9533fcfb03c118a72260b8933
NanKoYo <92059246+NankoYo@users.noreply.github.com>
2024-05-21 06:42:56 +0800

    Update Footer.js

--- themes/starter/components/Footer.js
+++ themes/starter/components/Footer.js
@@ -152,7 +152,7 @@ export const Footer = props => {
                       rel='nofollow noopner noreferrer'
                       target='_blank'
                       className='px-1 text-gray-1 hover:underline'>
-                      NanKoYo Inc.
+                      NanKoYo,Inc.
                     </a>
                   </p>
                     <p className='text-base text-gray-7'>
