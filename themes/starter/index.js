3334c0f08067562ca5a9bf510270bfbc5f54a186
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-20 13:47:38 +0800

    归档页

--- themes/starter/index.js
+++ themes/starter/index.js
@@ -221,26 +221,25 @@ const LayoutSlug = props => {
  * @returns
  */
 const LayoutArchive = props => {
-  const { archivePosts } = props
-
-  // 归档页顶部显示条，如果是默认归档则不显示。分类详情页显示分类列表，标签详情页显示当前标签
+  const { archivePosts } = props;
 
   return (
-      <div className="p-5 rounded-xl border dark:border-gray-600 max-w-6xl w-full bg-white dark:bg-[#1e1e1e]">
-        {/* 文章分类条 */}
-        <CategoryBar {...props} border={false} />
-        <div className="px-3">
-          {Object.keys(archivePosts).map(archiveTitle => (
-            <BlogPostArchive
-              key={archiveTitle}
-              posts={archivePosts[archiveTitle]}
-              archiveTitle={archiveTitle}
-            />
-          ))}
-        </div>
+    <div className="p-5 rounded-xl border dark:border-gray-600 max-w-6xl w-full bg-white dark:bg-[#1e1e1e] mt-10"> {/* 添加了 mt-10 来提供额外的顶部外边距 */}
+      {/* 文章分类条 */}
+      <CategoryBar {...props} border={false} />
+      <div className="px-3">
+        {Object.keys(archivePosts).map(archiveTitle => (
+          <BlogPostArchive
+            key={archiveTitle}
+            posts={archivePosts[archiveTitle]}
+            archiveTitle={archiveTitle}
+          />
+        ))}
       </div>
-  )
-}
+    </div>
+  );
+};
+
 
 /**
  * 404页面
