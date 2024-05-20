2b5e90abd92cf552c443b97d14413098045cf1be
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-20 10:08:58 +0800

    team.js

--- themes/starter/components/Team.js
+++ themes/starter/components/Team.js
@@ -31,52 +31,41 @@ export const Team = () => {
 
           {/* 团队成员排列矩阵 */}
           <div className='-mx-4 flex flex-wrap justify-center'>
-            {CONFIG.STARTER_TEAM_ITEMS.map((item, index) => {
-              return (
-                <div
-                  key={index}
-                  className='w-full px-4 sm:w-1/2 lg:w-1/4 xl:w-1/4'>
-                  <div className='group mb-8 rounded-xl bg-white px-5 pb-10 pt-12 shadow-testimonial dark:bg-dark dark:shadow-none'>
-                    {/* 头像 */}
-                    <div className='relative z-10 mx-auto mb-5 h-[120px] w-[120px]'>
-                      <img
-                        src={item.STARTER_TEAM_ITEM_AVATAR}
-                        alt='team image'
-                        className='h-[120px] w-[120px] rounded-full'
-                      />
-                      <span className='absolute bottom-0 left-0 -z-10 h-10 w-10 rounded-full bg-secondary opacity-0 transition-all group-hover:opacity-100'></span>
-                      <span className='absolute right-0 top-0 -z-10 opacity-0 transition-all group-hover:opacity-100'>
-                        <SVGAvatarBG />
-                      </span>
-                    </div>
+            {CONFIG.STARTER_TEAM_ITEMS
+              .filter(item => item.STARTER_TEAM_ITEM_AVATAR !== '') // 过滤出存在的成员
+              .slice(0, 4) // 限制最大成员数为4
+              .map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className='w-full px-4 sm:w-1/2 lg:w-1/4 xl:w-1/4'>
+                    <div className='group mb-8 rounded-xl bg-white px-5 pb-10 pt-12 shadow-testimonial dark:bg-dark dark:shadow-none'>
+                      {/* 头像 */}
+                      <div className='relative z-10 mx-auto mb-5 h-[120px] w-[120px]'>
+                        <img
+                          src={item.STARTER_TEAM_ITEM_AVATAR}
+                          alt='team image'
+                          className='h-[120px] w-[120px] rounded-full'
+                        />
+                        <span className='absolute bottom-0 left-0 -z-10 h-10 w-10 rounded-full bg-secondary opacity-0 transition-all group-hover:opacity-100'></span>
+                        <span className='absolute right-0 top-0 -z-10 opacity-0 transition-all group-hover:opacity-100'>
+                          <SVGAvatarBG />
+                        </span>
+                      </div>
 
-                    {/* 文字介绍 */}
-                    <div className='text-center'>
-                      <h4 className='mb-1 text-lg font-semibold text-dark dark:text-white'>
-                        {item.STARTER_TEAM_ITEM_NICKNAME}
-                      </h4>
-
-                      <p className='mb-5 text-sm text-body-color dark:text-dark-6'>
-                        {item.STARTER_TEAM_ITEM_DESCRIPTION}
-                      </p>
-
-                      {/* 社交链接 */}
-                      {/* <div className='flex items-center justify-center gap-5'>
-                        <a className='text-dark-6 hover:text-primary'>
-                          <SVGFacebook className='fill-current' />
-                        </a>
-                        <a className='text-dark-6 hover:text-primary'>
-                          <SVGTwitter className='fill-current' />
-                        </a>
-                        <a className='text-dark-6 hover:text-primary'>
-                          <SVGInstagram className='fill-current' />
-                        </a>
-                      </div> */}
+                      {/* 文字介绍 */}
+                      <div className='text-center'>
+                        <h4 className='mb-1 text-lg font-semibold text-dark dark:text-white'>
+                          {item.STARTER_TEAM_ITEM_NICKNAME}
+                        </h4>
+                        <p className='mb-5 text-sm text-body-color dark:text-dark-6'>
+                          {item.STARTER_TEAM_ITEM_DESCRIPTION}
+                        </p>
+                      </div>
                     </div>
                   </div>
-                </div>
-              )
-            })}
+                );
+              })}
           </div>
         </div>
       </section>
