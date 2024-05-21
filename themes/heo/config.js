b390d73237facf1402083f35bde67500062770ca
T-Rexrush <51684102+T-Rexrush@users.noreply.github.com>
2024-05-21 16:54:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -41,7 +41,7 @@ const CONFIG = {
     '🤖️ 数码科技爱好者',
     '🧱 团队小组发动机'
   ],
-  HEO_INFO_CARD_URL: 'https://github.com/T-Rexrush/NotionNext', // 个人资料底部按钮链接
+  HEO_INFO_CARD_URL: 'https://github.com/T-Rexrush', // 个人资料底部按钮链接
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
