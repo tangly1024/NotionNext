c437e00ad8033acfd72ccec2c17be703e28b626d
xuanmenliuyao <37439881+xuanmenliuyao@users.noreply.github.com>
2024-05-21 13:57:42 +0800

    heo config

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.buxiantang.top' },
-    { title: '我的答疑服务', url: 'https://www.buxiantang.top/' }
+    { title: '我的答疑服务', url: 'https://buxiantang.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
