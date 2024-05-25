8de9ed2d2c8be0d6e129aeac9e446d953929feca
xuanmenliuyao <1601958350@qq.com>
2024-05-25 18:43:52 +0800

    HEO_NOTICE_BAR

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -7,7 +7,7 @@ const CONFIG = {
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.buxiantang.top' },
     { title: '我的答疑服务', url: 'https://buxiantang.top' },
-    { title: '加锁文章说明', url: '/lockdescription' }
+    { title: '加锁文章说明', url: 'https://buxiantang.top/lockdescription' }
   ],
 
   // 英雄区左右侧组件颠倒位置
