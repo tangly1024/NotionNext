e0898c4b65e023bfae55605f728866453c24cabe
kie619 <155668210+kie619@users.noreply.github.com>
2024-05-31 13:25:19 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -16,13 +16,9 @@ const CONFIG = {
   //STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
   //STARTER_HERO_BUTTON_1_URL:
   //  'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
+  STARTER_HERO_BUTTON_2_TEXT: '关注我们', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
-  
-  STARTER_HERO_BUTTON_3_TEXT: '在Github上关注', // 英雄区按钮
-  STARTER_HERO_BUTTON_3_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  STARTER_HERO_BUTTON_3_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+  //STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
  
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
