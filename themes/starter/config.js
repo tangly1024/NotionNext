61a35f049a705636f62f240b28445e7375182e41
kie619 <155668210+kie619@users.noreply.github.com>
2024-05-31 13:04:31 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -13,9 +13,9 @@ const CONFIG = {
   STARTER_HERO_TITLE_1: 'WELCOME MOLA', // 英雄区文字
   STARTER_HERO_TITLE_2: '莫拉工艺品', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
-  STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
+  //STARTER_HERO_BUTTON_1_TEXT: '开始体验', // 英雄区按钮
   //STARTER_HERO_BUTTON_1_URL:
-    'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
+  //  'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
