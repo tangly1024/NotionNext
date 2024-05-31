dbdc08979d51f77d3b767ffdfb7a463b9dc27356
kie619 <155668210+kie619@users.noreply.github.com>
2024-05-31 13:15:04 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -19,7 +19,11 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_TEXT: '在Github上关注', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
-
+  
+  STARTER_HERO_BUTTON_3_TEXT: '在Github上关注', // 英雄区按钮
+  STARTER_HERO_BUTTON_3_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
+  STARTER_HERO_BUTTON_3_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+ 
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
   // 顶部右侧导航暗流
@@ -240,9 +244,9 @@ const CONFIG = {
   STARTER_CONTACT_TITLE: '联系我们',
   STARTER_CONTACT_TEXT: '告诉我们您遇到的问题',
   STARTER_CONTACT_LOCATION_TITLE: '我们的位置',
-  STARTER_CONTACT_LOCATION_TEXT: '中国，福建',
+  STARTER_CONTACT_LOCATION_TEXT: 'CHINA，ZhongShan',
   STARTER_CONTACT_EMAIL_TITLE: '我们如何帮助您？',
-  STARTER_CONTACT_EMAIL_TEXT: 'mail@tangly1024.com',
+  STARTER_CONTACT_EMAIL_TEXT: 'inquiry@molacraft.com',
 
   // 嵌入外部表单
   STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
@@ -290,7 +294,7 @@ const CONFIG = {
     }
   ],
 
-  STARTER_FOOTER_SLOGAN: '我们通过技术为品牌和公司创造数字体验。',
+  STARTER_FOOTER_SLOGAN: '定制专属于您的工艺产品',
 
   // 页脚三列菜单组
   STARTER_FOOTER_LINK_GROUP: [
