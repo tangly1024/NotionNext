1f3de136a069b9b6450ebc0d29b8943a98ec96c0
Alan Lee <1715191173@qq.com>
2024-06-13 17:10:24 +0800

    feat: 增加支持数据库引用外部笔记页面 #2442

--- blog.config.js
+++ blog.config.js
@@ -491,6 +491,7 @@ const BLOG = {
     password: process.env.NEXT_PUBLIC_NOTION_PROPERTY_PASSWORD || 'password',
     type: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE || 'type', // 文章类型，
     type_post: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_POST || 'Post', // 当type文章类型与此值相同时，为博文。
+    type_post_url: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_POST_URL || 'Post_url', // 当type文章类型与此值相同时，为外链博文。
     type_page: process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_PAGE || 'Page', // 当type文章类型与此值相同时，为单页。
     type_notice:
       process.env.NEXT_PUBLIC_NOTION_PROPERTY_TYPE_NOTICE || 'Notice', // 当type文章类型与此值相同时，为公告。
