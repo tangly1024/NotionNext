d990abf702045de05ec7c153405a780e9d49a562
Wenping Zhou <787958336@qq.com>
2024-06-19 09:33:20 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -24,15 +24,15 @@ export function InfoCard(props) {
       <div className='flex justify-between'>
         {/* 问候语 */}
         <GreetingsWords />
-        // <div
-        //   className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-6 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-100 transform transitaion-all duration-200`}>
-        //   <LazyImage
-        //     src={siteInfo?.icon}
-        //     className='rounded-full'
-        //     width={isSlugPage ? 100 : 28}
-        //     alt={siteConfig('AUTHOR')}
-        //   />
-        // </div>
+        <div
+          className={`${isSlugPage ? 'absolute right-0 -mt-8 -mr-6 hover:opacity-0 hover:scale-150 blur' : 'cursor-pointer'} justify-center items-center flex dark:text-gray-100 transform transitaion-all duration-200`}>
+          <LazyImage
+            src={siteInfo?.icon}
+            className='rounded-full'
+            width={isSlugPage ? 100 : 28}
+            alt={siteConfig('AUTHOR')}
+          />
+        </div>
       </div>
 
       <h2 className='text-3xl font-extrabold mt-3'>{siteConfig('TITLE')}</h2>
