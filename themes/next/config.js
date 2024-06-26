331ba1e7eb9eed54b99f44b3a673a12ded70b70f
suki <463882408@qq.com>
2024-06-26 20:42:52 +0800

    恢复侧栏，删除Announcement

--- themes/next/config.js
+++ themes/next/config.js
@@ -11,7 +11,7 @@ const CONFIG = {
   NEXT_POST_HEADER_IMAGE_VISIBLE: false, // 文章详情页是否显示封面图
 
   // 右侧组件
-  NEXT_RIGHT_BAR: false, // 是否显示右侧栏
+  NEXT_RIGHT_BAR: true, // 是否显示右侧栏
   NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
