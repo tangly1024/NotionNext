c1bc21b0cf049ee04839a5386e1a4a1f84db58a7
kforris <58551353+kforris@users.noreply.github.com>
2024-06-29 11:14:52 -0400

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -43,6 +43,14 @@ const CONFIG = {
     '🧱 团队小组发动机'
   ],
   HEO_INFO_CARD_URL: 'https://shredxhub.com', // 个人资料底部按钮链接
+  
+  // 个人资料底部按钮
+  HEO_INFO_CARD_URL1: '/about',
+  HEO_INFO_CARD_ICON1: 'fas fa-user',
+  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_ICON2: 'fab fa-github',
+  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
