567cada8fbd349c81b23a0e423f45582e3873f35
jtysfw <yfhlfp@gmail.com>
2024-06-29 19:20:13 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
-  HEO_SITE_CREATE_TIME: '2024-06-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-06-21' ， // 建站日期，用于计算网站运行的第几天
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的网站'， url: 'https://www.120819.xyz' }，
