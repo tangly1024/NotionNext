1d3d78e3b4496615e18c8490f0920c2d00dcc0b2
jtysfw <yfhlfp@gmail.com>
2024-06-29 18:29:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
    HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: '' },
-    { title: '访问文档中心获取更多帮助', url: '' }
+    { title: '访问博客获取更多帮助', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: '120819.xyz',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '家庭医生服务站',
-  HEO_HERO_TITLE_LINK: 'https://120819.xyz'
+  HEO_HERO_TITLE_LINK: 'https://120819.xyz',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选'， url: '/tag/必看精选' },
