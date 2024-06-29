40e684eec12d18d8e249e06f1dd2d116b00ad81f
jtysfw <yfhlfp@gmail.com>
2024-06-29 18:46:34 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -25,7 +25,7 @@ const CONFIG = {
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' }，
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' }，
-  HEO_HERO_CATEGORY_3: { title: '健康教育', url: '/tag/健康教育' },
+  HEO_HERO_CATEGORY_3: { title: '健康教育', url: '/tag/健康教育' }
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
