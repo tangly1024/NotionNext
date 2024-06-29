8181f2a08026e7fad6d2eb9e7004699b18ad87db
jtysfw <yfhlfp@gmail.com>
2024-06-29 18:59:24 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true ，
 
-  HEO_SITE_CREATE_TIME: '2024-06-21'，// 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-06-21' ，// 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
    HEO_NOTICE_BAR: [
@@ -10,9 +10,9 @@ const CONFIG = {
   ]，
 
   // 英雄区左右侧组件颠倒位置
-  HEO_HERO_REVERSE: false，
+  HEO_HERO_REVERSE: false ，
   // 博客主体区左右侧组件颠倒位置
-  HEO_HERO_BODY_REVERSE: false，
+  HEO_HERO_BODY_REVERSE: false ，
 
   // 英雄区(首页顶部大卡)
     HEO_HERO_TITLE_1: '分享健康'，
@@ -35,87 +35,87 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
+    '🔍 分享与热心帮助'，
+    '🤝 专修交互与设计'，
+    '🏃 脚踏实地行动派'，
+    '🏠 智能家居小能手'，
+    '🤖️ 数码科技爱好者'，
     '🧱 团队小组发动机'
-  ],
+  ]，
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/about',
-  HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://blog.120819.xyz',
-  HEO_INFO_CARD_ICON2: 'fab fa-blog',
-  HEO_INFO_CARD_URL3: 'https://www.120819.xyz',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL1: '/about'，
+  HEO_INFO_CARD_ICON1: 'fas fa-user'，
+  HEO_INFO_CARD_URL2: 'https://blog.120819.xyz'，
+  HEO_INFO_CARD_ICON2: 'fab fa-blog'，
+  HEO_INFO_CARD_URL3: 'https://www.120819.xyz'，
+  HEO_INFO_CARD_TEXT3: '了解更多'，
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_1: 'AfterEffect'，
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp'，
+      color_1: '#989bf8'，
+      title_2: 'Sketch'，
+      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp'，
       color_2: '#ffffff'
-    },
+    }，
     {
-      title_1: 'Docker',
-      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
-      color_1: '#57b6e6',
-      title_2: 'Photoshop',
-      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
+      title_1: 'Docker'，
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp'，
+      color_1: '#57b6e6'，
+      title_2: 'Photoshop'，
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp'，
       color_2: '#4082c3'
-    },
+    }，
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
-      color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_1: 'FinalCutPro'，
+      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp'，
+      color_1: '#ffffff'，
+      title_2: 'Python'，
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp'，
       color_2: '#ffffff'
-    },
+    }，
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
+      title_1: 'Swift'，
+      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp'，
+      color_1: '#eb6840'，
+      title_2: 'Principle'，
+      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp'，
       color_2: '#8f55ba'
-    },
+    }，
     {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
-      color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
+      title_1: 'illustrator'，
+      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp'，
+      color_1: '#f29e39'，
+      title_2: 'CSS3'，
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp'，
       color_2: '#2c51db'
-    },
+    }，
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
-      color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      title_1: 'JS'，
+      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp'，
+      color_1: '#f7cb4f'，
+      title_2: 'HTML'，
+      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp'，
       color_2: '#e9572b'
-    },
+    }，
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      title_1: 'Git'，
+      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp'，
+      color_1: '#df5b40'，
+      title_2: 'Rhino'，
+      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp'，
       color_2: '#1f1f1f'
     }
-  ],
+  ]，
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD: true ， // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD_TITLE_1: '交流频道'，
+  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享'，
+  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群'，
+  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question'，
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
