3657cfa82585f91b1838683c035405518a65a8c3
kforris <58551353+kforris@users.noreply.github.com>
2024-06-30 17:49:09 -0400

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -47,9 +47,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL2: 'https://www.snow-forcast.com',
+  HEO_INFO_CARD_ICON2: 'fas fa-snowflake',
+  HEO_INFO_CARD_URL3: '/about',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
@@ -89,7 +89,7 @@ const CONFIG = {
     {
       title_1: 'Shredhub',
       img_1: '/images/heo/xiahua.png',
-      color_1: '#F84BABff',
+      color_1: '#ffffff',
       title_2: 'Waze',
       img_2: '/images/heo/waze.png',
       color_2: '#2c51db'
