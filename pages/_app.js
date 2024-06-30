b41c258c972df6aa084b3a1e53fb3566cb24848a
Sigma <jeckwell@126.com>
2024-06-30 21:21:31 +0800

    :sparkles: 添加网站数据统计

--- pages/_app.js
+++ pages/_app.js
@@ -2,7 +2,7 @@ import '@/styles/animate.css' // @see https://animate.style/
 import '@/styles/globals.css'
 import '@/styles/nprogress.css'
 import '@/styles/utility-patterns.css'
-
+import {Analytics} from '@vercel/analytics/react'
 // core styles shared by all of react-notion-x (required)
 import 'react-notion-x/src/styles.css'
 import '@/styles/notion.css' //  重写部分样式
@@ -49,6 +49,7 @@ const MyApp = ({ Component, pageProps }) => {
       <GLayout {...pageProps}>
         <GlobalHead {...pageProps}/>
         <Component {...pageProps} />
+        <Analytics />
       </GLayout>
       <ExternalPlugins {...pageProps} />
     </GlobalContextProvider>
