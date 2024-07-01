7242bf9a56780a8ca016928992f487655107d9e5
Wenping Zhou <787958336@qq.com>
2024-07-01 16:47:18 +0800

    Update config.js

--- themes/next/config.js
+++ themes/next/config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
 
-  NEXT_HOME_BANNER: true, // 首页是否显示大图及标语 [true,false]
+  NEXT_HOME_BANNER: false, // 首页是否显示大图及标语 [true,false]
   NEXT_HOME_BANNER_STRINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
 
   NEXT_NAV_TYPE: 'fixed', // ['fixed','autoCollapse','normal'] 分别是固定屏幕顶部并始终显示、固定屏幕顶部且滚动时隐藏，不固定屏幕顶部
 
-  NEXT_POST_LIST_COVER: true, // 文章列表显示封面图
+  NEXT_POST_LIST_COVER: false, // 文章列表显示封面图
   NEXT_POST_LIST_PREVIEW: true, // 显示文章预览
   NEXT_POST_LIST_SUMMARY: true, // 显示用户自定义摘要，有预览时优先只展示预览
 
@@ -16,7 +16,7 @@ const CONFIG = {
   NEXT_RIGHT_LATEST_POSTS: true, // 右侧栏最新文章
   NEXT_RIGHT_CATEGORY_LIST: true, // 右侧边栏文章分类列表
   NEXT_RIGHT_TAG_LIST: true, // 右侧边栏标签分类列表
-  NEXT_RIGHT_AD: true, // 右侧广告
+  NEXT_RIGHT_AD: false, // 右侧广告
 
   // 菜单
   NEXT_MENU_HOME: true, // 显示首页
