1e01503a712d43571b27896ed7af03aabe347f85
suki <463882408@qq.com>
2024-07-02 01:01:01 +0800

    Revert "[add] notion page增加useEffect"
    This reverts commit e6162ef282f1f1384536324c710a3a8f54054dcd.

--- components/NotionPage.js
+++ components/NotionPage.js
@@ -7,37 +7,29 @@ import { isBrowser } from '@/lib/utils'
 import { siteConfig } from '@/lib/config'
 
 // Notion渲染
-const NotionRenderer = dynamic(
-  () =>
-    import('react-notion-x').then(async m => {
-      return m.NotionRenderer
-    }),
-  {
-    ssr: false
-  }
-)
+const NotionRenderer = dynamic(() => import('react-notion-x').then(async (m) => {
+  return m.NotionRenderer
+}), {
+  ssr: false
+})
 
-const Code = dynamic(
-  () =>
-    import('react-notion-x/build/third-party/code').then(async m => {
-      return m.Code
-    }),
-  { ssr: false }
+const Code = dynamic(() =>
+  import('react-notion-x/build/third-party/code').then(async (m) => {
+    return m.Code
+  }), { ssr: false }
 )
 
 // 公式
-const Equation = dynamic(
-  () =>
-    import('@/components/Equation').then(async m => {
-      // 化学方程式
-      await import('@/lib/mhchem')
-      return m.Equation
-    }),
-  { ssr: false }
+const Equation = dynamic(() =>
+  import('@/components/Equation').then(async (m) => {
+    // 化学方程式
+    await import('@/lib/mhchem')
+    return m.Equation
+  }), { ssr: false }
 )
 
 const Pdf = dynamic(
-  () => import('react-notion-x/build/third-party/pdf').then(m => m.Pdf),
+  () => import('react-notion-x/build/third-party/pdf').then((m) => m.Pdf),
   {
     ssr: false
   }
@@ -56,17 +48,12 @@ const TweetEmbed = dynamic(() => import('react-tweet-embed'), {
   ssr: false
 })
 
-const Collection = dynamic(
-  () =>
-    import('react-notion-x/build/third-party/collection').then(
-      m => m.Collection
-    ),
-  { ssr: true }
+const Collection = dynamic(() =>
+  import('react-notion-x/build/third-party/collection').then((m) => m.Collection), { ssr: true }
 )
 
 const Modal = dynamic(
-  () => import('react-notion-x/build/third-party/modal').then(m => m.Modal),
-  { ssr: false }
+  () => import('react-notion-x/build/third-party/modal').then((m) => m.Modal), { ssr: false }
 )
 
 const Tweet = ({ id }) => {
@@ -76,21 +63,13 @@ const Tweet = ({ id }) => {
 const NotionPage = ({ post, className }) => {
   useEffect(() => {
     autoScrollToTarget()
-
-    // 使 <details> 默认展开
-    const detailsElements = document.querySelectorAll('details')
-    detailsElements.forEach(detail => {
-      detail.setAttribute('open', true)
-    })
   }, [])
 
-  const zoom =
-    typeof window !== 'undefined' &&
-    mediumZoom({
-      container: '.notion-viewport',
-      background: 'rgba(0, 0, 0, 0.2)',
-      margin: getMediumZoomMargin()
-    })
+  const zoom = typeof window !== 'undefined' && mediumZoom({
+    container: '.notion-viewport',
+    background: 'rgba(0, 0, 0, 0.2)',
+    margin: getMediumZoomMargin()
+  })
   const zoomRef = useRef(zoom ? zoom.clone() : null)
 
   useEffect(() => {
@@ -98,18 +77,14 @@ const NotionPage = ({ post, className }) => {
     if (siteConfig('POST_DISABLE_GALLERY_CLICK')) {
       setTimeout(() => {
         if (isBrowser) {
-          const imgList = document?.querySelectorAll(
-            '.notion-collection-card-cover img'
-          )
+          const imgList = document?.querySelectorAll('.notion-collection-card-cover img')
           if (imgList && zoomRef.current) {
             for (let i = 0; i < imgList.length; i++) {
-              zoomRef.current.attach(imgList[i])
+              (zoomRef.current).attach(imgList[i])
             }
           }
 
-          const cards = document.getElementsByClassName(
-            'notion-collection-card'
-          )
+          const cards = document.getElementsByClassName('notion-collection-card')
           for (const e of cards) {
             e.removeAttribute('href')
           }
@@ -126,16 +101,10 @@ const NotionPage = ({ post, className }) => {
       const allAnchorTags = document.getElementsByTagName('a') // 或者使用 document.querySelectorAll('a') 获取 NodeList
       for (const anchorTag of allAnchorTags) {
         if (anchorTag?.target === '_blank') {
-          const hrefWithoutQueryHash = anchorTag.href
-            .split('?')[0]
-            .split('#')[0]
-          const hrefWithRelativeHash =
-            currentURL.split('#')[0] + anchorTag.href.split('#')[1]
+          const hrefWithoutQueryHash = anchorTag.href.split('?')[0].split('#')[0]
+          const hrefWithRelativeHash = currentURL.split('#')[0] + anchorTag.href.split('#')[1]
 
-          if (
-            currentURL === hrefWithoutQueryHash ||
-            currentURL === hrefWithRelativeHash
-          ) {
+          if (currentURL === hrefWithoutQueryHash || currentURL === hrefWithRelativeHash) {
             anchorTag.target = '_self'
           }
         }
@@ -147,28 +116,23 @@ const NotionPage = ({ post, className }) => {
     return <>{post?.summary || ''}</>
   }
 
-  return (
-    <div
-      id="notion-article"
-      className={`mx-auto overflow-hidden ${className || ''}`}
-    >
-      <NotionRenderer
-        recordMap={post.blockMap}
-        mapPageUrl={mapPageUrl}
-        mapImageUrl={mapImgUrl}
-        components={{
-          Code,
-          Collection,
-          Equation,
-          Modal,
-          Pdf,
-          Tweet
-        }}
-      />
+  return <div id='notion-article' className={`mx-auto overflow-hidden ${className || ''}`}>
+    <NotionRenderer
+      recordMap={post.blockMap}
+      mapPageUrl={mapPageUrl}
+      mapImageUrl={mapImgUrl}
+      components={{
+        Code,
+        Collection,
+        Equation,
+        Modal,
+        Pdf,
+        Tweet
+      }} />
 
-      <PrismMac />
-    </div>
-  )
+      <PrismMac/>
+
+  </div>
 }
 
 /**
