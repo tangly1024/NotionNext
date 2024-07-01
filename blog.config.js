131dfd8f916a669f3a4c212eafefc28f2005d2d7
suki <463882408@qq.com>
2024-07-01 19:20:19 +0800

    [fix] avatar.svg 修改

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,9 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Dumb fox', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一只傻狐狸🦊', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://notion.sukisq.site', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS:
+    process.env.NEXT_PUBLIC_KEYWORD ||
+    'Notion, 博客, dumb fox, 傻狐狸, 学习笔记', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '463882408@qq.com', // 邮箱地址 例如mail@tangly1024.com
