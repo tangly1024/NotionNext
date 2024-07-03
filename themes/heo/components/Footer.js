587aa2e00b4de58b20885c0fae0896e998195c07
Wenping Zhou <787958336@qq.com>
2024-07-03 10:50:02 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -29,7 +29,7 @@ const Footer = ({ TITLE }) => {
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
         <div id='footer-bottom-left'>
           {/*NotionNext {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}*/}
-          Agercare <i className='fas fa-copyright' />{' '}
+          Senitimes <i className='fas fa-copyright' />{' '}
           {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' />{' '}
           <a
             href={siteConfig('LINK')}
