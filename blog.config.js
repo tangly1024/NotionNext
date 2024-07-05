6d2b9035f24278e5516d4189ac4681ed5514adbe
TeBox <145404809+TeBoxx@users.noreply.github.com>
2024-07-06 03:58:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Tebox', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'TeboxのNotes', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'WEB3打工人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://tebox.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Airdrop, WEB3，空投', // 网站关键词 英文逗号隔开
