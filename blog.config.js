0963495bc6cbde94e146d13c06a3dc2ef0906760
yetaihei8800 <62210070+yetaihei8800@users.noreply.github.com>
2024-07-07 11:55:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -297,7 +297,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_WIDGET_PET_SWITCH_THEME || true, // 点击宠物挂件切换博客主题
 
   // 音乐播放插件
-  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || false, // 是否使用音乐播放插件
+  MUSIC_PLAYER: process.env.NEXT_PUBLIC_MUSIC_PLAYER || true, // 是否使用音乐播放插件
   MUSIC_PLAYER_VISIBLE: process.env.NEXT_PUBLIC_MUSIC_PLAYER_VISIBLE || true, // 是否在左下角显示播放和切换，如果使用播放器，打开自动播放再隐藏，就会以类似背景音乐的方式播放，无法取消和暂停
   MUSIC_PLAYER_AUTO_PLAY:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_AUTO_PLAY || true, // 是否自动播放，不过自动播放时常不生效（移动设备不支持自动播放）
@@ -309,18 +309,32 @@ const BLOG = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      name: '梦中的额吉',
+      artist: '听音踏歌',
+      url: 'http://chuangshicdn.data.mvbox.cn/music/fc/18/12/15/18121514533628265868.mp3',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://data.mvbox.cn/upload/showing/tmp/20/05/11/200511092410732778901.jpg'
     },
     {
-      name: '王都グランセル',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731355.mp3',
+      name: '永远放你在心底',
+      artist: '听音踏歌',
+      url: 'http://chuangshicdn.data.mvbox.cn/music/fc/23/09/22/23092213001535761043.mp3',
       cover:
-        'https://p1.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://data.mvbox.cn/upload/showing/tmp/20/05/11/200511092410732778901.jpg'
+    },
+     {
+      name: '一场秋雨一场凉',
+      artist: '听音踏歌',
+      url: 'http://chuangshicdn.data.mvbox.cn/music/fc/18/12/15/18121514505338897147.mp3',
+      cover:
+        'http://data.mvbox.cn/upload/showing/tmp/20/05/11/200511092410732778901.jpg'
+    },
+   {
+      name: '远方的我在等着你',
+      artist: '听音踏歌',
+      url: 'http://chuangshicdn.data.mvbox.cn/music/fc/17/09/04/17090410514580202187.mp3',
+      cover:
+        'http://data.mvbox.cn/upload/showing/tmp/20/05/11/200511092410732778901.jpg'
     }
   ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
