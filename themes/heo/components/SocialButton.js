5b8496d3931b03780e97ca0f113d29d83e3079f6
jtysfw <yfhlfp@gmail.com>
2024-07-07 14:08:16 +0800

    Update SocialButton.js

--- themes/heo/components/SocialButton.js
+++ themes/heo/components/SocialButton.js
@@ -105,7 +105,7 @@ const SocialButton = () => {
             rel='noreferrer'
             title={'wehchat'}
             href={siteConfig('CONTACT_wehchat')}>
-            <i className='transform hover:scale-125 duration-150 fab weixin dark:hover:text-indigo-400 hover:text-indigo-600' />
+            <i className='transform hover:scale-125 duration-150 fa-brands fa-weixin dark:hover:text-indigo-400 hover:text-indigo-600' />
           </a>
         )}      
       </div>
