3b59d152aba98cd6d3de9924129e78b9634a9af3
今予 <150569554+JYbestow@users.noreply.github.com>
2024-07-09 13:33:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
     'ぶちまけちゃおうか 星に🌠,やけに白いんだ　やたら長いんだ コタエはだいたいカタチばかりの常識だろう,あたしは生涯　あたしであってそれだけだろう'
 
-  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
+  CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '今予', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一只浙江大学机械工程学院的小菜鸡，主修机械工程专业，。', // 作者简介
