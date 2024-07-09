2acfff78deef0de0db137424c76ae9f8ef93835a
今予 <150569554+JYbestow@users.noreply.github.com>
2024-07-09 11:13:11 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -274,7 +274,7 @@ const BLOG = {
   TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '',
 
   // Chatbase
-  CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || 'yxBNOxY0A-RX0zyTHlY5K', // 是否显示chatbase机器人 https://www.chatbase.co/
+  CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || 'https://www.chatbase.co/chatbot-iframe/yxBNOxY0A-RX0zyTHlY5K', // 是否显示chatbase机器人 https://www.chatbase.co/
   // WebwhizAI 机器人 @see https://github.com/webwhiz-ai/webwhiz
   WEB_WHIZ_ENABLED: process.env.NEXT_PUBLIC_WEB_WHIZ_ENABLED || false, // 是否显示
   WEB_WHIZ_BASE_URL:
