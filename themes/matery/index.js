382fe809059526a1a09af02669a0d20a3a4663d4
Layla Joines <80381650+SudieSzoka@users.noreply.github.com>
2024-07-12 06:23:08 +0800

    Update index.js

--- themes/matery/index.js
+++ themes/matery/index.js
@@ -66,10 +66,11 @@ const LayoutBase = props => {
       <BlogListBar {...props} />
     )
   const headerSlot =
-    siteConfig('MATERY_HOME_BANNER_ENABLE', null, CONFIG) &&
-    router.route === '/' ? (
-      <Hero {...props} />
-    ) : post && !fullWidth ? (
+    // siteConfig('MATERY_HOME_BANNER_ENABLE', null, CONFIG) && // 注释掉这一行
+    // router.route === '/' ? ( // 注释掉这一行
+      // <Hero {...props} /> // 注释掉这一行
+    // ) : // 注释掉这一行
+    post && !fullWidth ? (
       <PostHero {...props} />
     ) : null
 
