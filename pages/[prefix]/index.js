426ddb9062bded069cf4e8435877bbc3b844f9a2
zwl <zwlong6@163.com>
2024-07-30 16:30:49 +0800

    Update index.js

--- pages/[prefix]/index.js
+++ pages/[prefix]/index.js
@@ -1,206 +1,187 @@
-import BLOG from '@/blog.config'
-import useNotification from '@/components/Notification'
-import { siteConfig } from '@/lib/config'
-import { getGlobalData, getPost, getPostBlocks } from '@/lib/db/getSiteData'
-import { useGlobal } from '@/lib/global'
-import { getPageTableOfContents } from '@/lib/notion/getPageTableOfContents'
-import { getPasswordQuery } from '@/lib/password'
-import { uploadDataToAlgolia } from '@/lib/plugins/algolia'
-import { checkSlugHasNoSlash, getRecommendPost } from '@/lib/utils/post'
-import { getLayoutByTheme } from '@/themes/theme'
-import md5 from 'js-md5'
-import { useRouter } from 'next/router'
-import { idToUuid } from 'notion-utils'
-import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
+
+import md5 from 'js-md5';
+import { idToUuid } from 'notion-utils';
+
+import BLOG from '@/blog.config';
+import { uploadDataToAlgolia } from '@/lib/algolia';
+import { siteConfig } from '@/lib/config';
+import { getPostBlocks } from '@/lib/notion';
+import { getNotion } from '@/lib/notion/getNotion';
+import { getGlobalData } from '@/lib/notion/getNotionData';
+import { getPageTableOfContents } from '@/lib/notion/getPageTableOfContents';
+import { isBrowser } from '@/lib/utils';
+import { getLayoutByTheme } from '@/themes/theme';
 
 /**
  * 根据notion的slug访问页面
  * 只解析一级目录例如 /about
- * @param {*} props
- * @returns
  */
 const Slug = props => {
-  const { post } = props
-  const router = useRouter()
-  const { locale } = useGlobal()
+  const { post, siteInfo } = props;
+  const router = useRouter();
 
-  // 文章锁🔐
-  const [lock, setLock] = useState(post?.password && post?.password !== '')
-  const { showNotification, Notification } = useNotification()
+  const [lock, setLock] = useState(post?.password && post?.password !== '');
+  const [reloaded, setReloaded] = useState(false);
 
-  /**
-   * 验证文章密码
-   * @param {*} result
-   */
   const validPassword = passInput => {
+    const encrypt = md5(post.slug + passInput);
+    if (passInput && encrypt === post.password) {
+      setLock(false);
+      return true;
+    }
+    return false;
+  };
+
+  // 文章加载
+  useEffect(() => {
     if (!post) {
-      return false
-    }
-    const encrypt = md5(post?.slug + passInput)
-    if (passInput && encrypt === post?.password) {
-      setLock(false)
-      // 输入密码存入localStorage，下次自动提交
-      localStorage.setItem('password_' + router.asPath, passInput)
-      showNotification(locale.COMMON.ARTICLE_UNLOCK_TIPS) // 设置解锁成功提示显示
-      return true
-    }
-    return false
-  }
-
-  // 文章加载
-  useEffect(() => {
-    // 文章加密
-    if (post?.password && post?.password !== '') {
-      setLock(true)
-    } else {
-      setLock(false)
-      if (!lock && post?.blockMap?.block) {
-        post.content = Object.keys(post.blockMap.block).filter(
-          key => post.blockMap.block[key]?.value?.parent_id === post.id
-        )
-        post.toc = getPageTableOfContents(post, post.blockMap)
-      }
-    }
-
-    // 读取上次记录 自动提交密码
-    const passInputs = getPasswordQuery(router.asPath)
-    if (passInputs.length > 0) {
-      for (const passInput of passInputs) {
-        if (validPassword(passInput)) {
-          break // 密码验证成功，停止尝试
+      const timeout = setTimeout(() => {
+        if (isBrowser) {
+          const article = document.getElementById('notion-article');
+          if (!article && !reloaded) {
+            setReloaded(true);
+            console.warn('Try to reload: ', router.asPath);
+            router.replace(router.asPath);
+          } else {
+            router.push('/404')
+          }
         }
-      }
-    }
-  }, [post])
+      }, siteConfig('POST_WAITING_TIME_FOR_404') * 1000);
 
-  // 文章加载
-  useEffect(() => {
-    if (lock) {
-      return
+      return () => clearTimeout(timeout);
     }
-    // 文章解锁后生成目录与内容
-    if (post?.blockMap?.block) {
-      post.content = Object.keys(post.blockMap.block).filter(
-        key => post.blockMap.block[key]?.value?.parent_id === post.id
-      )
-      post.toc = getPageTableOfContents(post, post.blockMap)
-    }
-  }, [router, lock])
 
-  props = { ...props, lock, setLock, validPassword }
+    if (!lock && post?.blockMap?.block) {
+      post.content = Object.keys(post.blockMap.block);
+      post.toc = getPageTableOfContents(post, post.blockMap);
+    }
+  }, [post, router]);
+
+  const meta = {
+    title: post ? `${post?.title} | ${siteConfig('TITLE')}` : `Loading... ｜ ${siteConfig('TITLE')}`,
+    description: post?.summary,
+    type: post?.type,
+    slug: post?.slug,
+    image: post?.pageCoverThumbnail || (siteInfo?.pageCover || BLOG.HOME_BANNER_IMAGE),
+    category: post?.category?.[0],
+    tags: post?.tags
+  };
+
+  props = { ...props, lock, meta, setLock, validPassword };
   // 根据页面路径加载不同Layout文件
-  const Layout = getLayoutByTheme({
-    theme: siteConfig('THEME'),
-    router: useRouter()
-  })
-  return (
-    <>
-      <Layout {...props} />
-      {post?.password && post?.password !== '' && !lock && <Notification />}
-    </>
-  )
-}
+  const Layout = getLayoutByTheme({ theme: siteConfig('THEME'), router: useRouter() });
+  return <Layout {...props} />;
+};
 
 export async function getStaticPaths() {
   if (!BLOG.isProd) {
     return {
       paths: [],
       fallback: true
-    }
+    };
   }
 
-  const from = 'slug-paths'
-  const { allPages } = await getGlobalData({ from })
-  const paths = allPages
-    ?.filter(row => checkSlugHasNoSlash(row))
-    .map(row => ({ params: { prefix: row.slug } }))
+  const from = 'slug-paths';
+  const { allPages } = await getGlobalData({ from });
   return {
-    paths: paths,
+    paths: allPages?.filter(row => row.slug.indexOf('/') < 0 && row.type.indexOf('Menu') < 0).map(row => ({ params: { prefix: row.slug } })),
     fallback: true
-  }
+  };
 }
 
-export async function getStaticProps({ params: { prefix }, locale }) {
-  let fullSlug = prefix
-  const from = `slug-props-${fullSlug}`
-  const props = await getGlobalData({ from, locale })
-  if (siteConfig('PSEUDO_STATIC', false, props.NOTION_CONFIG)) {
+export async function getStaticProps({ params: { prefix } }) {
+  let fullSlug = prefix;
+  if (JSON.parse(BLOG.PSEUDO_STATIC)) {
     if (!fullSlug.endsWith('.html')) {
-      fullSlug += '.html'
+      fullSlug += '.html';
     }
   }
-  
+  const from = `slug-props-${fullSlug}`;
+  const props = await getGlobalData({ from });
   // 在列表内查找文章
-  props.post = props?.allPages?.find(p => {
-    return (
-      p.type.indexOf('Menu') < 0 &&
-      (p.slug === prefix || p.id === idToUuid(prefix))
-    )
-  })
+  props.post = props?.allPages?.find((p) => {
+    return p.slug === fullSlug || p.id === idToUuid(fullSlug);
+  });
 
   // 处理非列表内文章的内信息
   if (!props?.post) {
-    const pageId = prefix
+    const pageId = prefix;
     if (pageId.length >= 32) {
-      const post = await getPost(pageId)
-      props.post = post
+      const post = await getNotion(pageId);
+      props.post = post;
     }
   }
   // 无法获取文章
   if (!props?.post) {
-    props.post = null
-    return {
-      props,
-      revalidate: process.env.EXPORT
-        ? undefined
-        : siteConfig(
-            'NEXT_REVALIDATE_SECOND',
-            BLOG.NEXT_REVALIDATE_SECOND,
-            props.NOTION_CONFIG
-          )
-    }
+    props.post = null;
+    return { props, revalidate: parseInt(BLOG.NEXT_REVALIDATE_SECOND) };
   }
 
   // 文章内容加载
-  if (!props?.post?.blockMap) {
-    props.post.blockMap = await getPostBlocks(props.post.id, from)
+  if (!props?.posts?.blockMap) {
+    props.post.blockMap = await getPostBlocks(props.post.id, from);
   }
 
   // 生成全文索引 && process.env.npm_lifecycle_event === 'build' && JSON.parse(BLOG.ALGOLIA_RECREATE_DATA)
   if (BLOG.ALGOLIA_APP_ID) {
-    uploadDataToAlgolia(props?.post)
+    uploadDataToAlgolia(props?.post);
   }
 
   // 推荐关联文章处理
-  const allPosts = props.allPages?.filter(
-    page => page.type === 'Post' && page.status === 'Published'
-  )
+  const allPosts = props.allPages?.filter(page => page.type === 'Post' && page.status === 'Published');
   if (allPosts && allPosts.length > 0) {
-    const index = allPosts.indexOf(props.post)
-    props.prev = allPosts.slice(index - 1, index)[0] ?? allPosts.slice(-1)[0]
-    props.next = allPosts.slice(index + 1, index + 2)[0] ?? allPosts[0]
-    props.recommendPosts = getRecommendPost(
-      props.post,
-      allPosts,
-      siteConfig('POST_RECOMMEND_COUNT')
-    )
+    const index = allPosts.indexOf(props.post);
+    props.prev = allPosts.slice(index - 1, index)[0] ?? allPosts.slice(-1)[0];
+    props.next = allPosts.slice(index + 1, index + 2)[0] ?? allPosts[0];
+    props.recommendPosts = getRecommendPost(props.post, allPosts, BLOG.POST_RECOMMEND_COUNT);
   } else {
-    props.prev = null
-    props.next = null
-    props.recommendPosts = []
+    props.prev = null;
+    props.next = null;
+    props.recommendPosts = [];
   }
 
-  delete props.allPages
+  delete props.allPages;
   return {
     props,
-    revalidate: process.env.EXPORT
-      ? undefined
-      : siteConfig(
-          'NEXT_REVALIDATE_SECOND',
-          BLOG.NEXT_REVALIDATE_SECOND,
-          props.NOTION_CONFIG
-        )
-  }
+    revalidate: parseInt(BLOG.NEXT_REVALIDATE_SECOND)
+  };
 }
 
-export default Slug
+/**
+ * 获取文章的关联推荐文章列表，目前根据标签关联性筛选
+ * @param post
+ * @param {*} allPosts
+ * @param {*} count
+ * @returns
+ */
+export function getRecommendPost(post, allPosts, count = 6) {
+  let recommendPosts = [];
+  const postIds = [];
+  const currentTags = post?.tags || [];
+  for (let i = 0; i < allPosts.length; i++) {
+    const p = allPosts[i];
+    if (p.id === post.id || p.type.indexOf('Post') < 0) {
+      continue;
+    }
+
+    for (let j = 0; j < currentTags.length; j++) {
+      const t = currentTags[j];
+      if (postIds.indexOf(p.id) > -1) {
+        continue;
+      }
+      if (p.tags && p.tags.indexOf(t) > -1) {
+        recommendPosts.push(p);
+        postIds.push(p.id);
+      }
+    }
+  }
+
+  if (recommendPosts.length > count) {
+    recommendPosts = recommendPosts.slice(0, count);
+  }
+  return recommendPosts;
+}
+
+export default Slug;
