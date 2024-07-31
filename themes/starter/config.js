4531432decf94f5c969c178fe7780f7d08c9d032
香港Henry <168814409+zls0725@users.noreply.github.com>
2024-07-31 21:14:56 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -14,11 +14,10 @@ const CONFIG = {
   STARTER_HERO_TITLE_2: 'Dream, Think, Smile, Design, Together', // 英雄区文字
   // 英雄区两个按钮，如果TEXT留空则隐藏按钮
   STARTER_HERO_BUTTON_1_TEXT: '香港Henry的博客', // 英雄区按钮
-  STARTER_HERO_BUTTON_1_URL:
-    'https://blog.dtsdt.com', // 英雄区按钮
+  STARTER_HERO_BUTTON_1_URL: 'https://blog.dtsdt.com', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: 'YouTube主页', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://www.youtube.com/@hongkonghenry', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
+  STARTER_HERO_BUTTON_2_ICON: 'http://www.w3.org/2000/svg', // 英雄区按钮2的图标，不需要则留空
 
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
 
