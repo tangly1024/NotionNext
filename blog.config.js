84ff57d2d9ded78fdadff54e52efd413382620be
William Cui <cuiweilin31@gmail.com>
2024-08-04 18:45:13 +0800

    Update blog.config.js
    修改博客描述

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'NotionNext', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'William Cui', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'Youtube频道威廉说主理人', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://williamsays.site', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '威廉说,William Cui,williamsays,博客,blog', // 网站关键词 英文逗号隔开
@@ -535,7 +535,7 @@ const BLOG = {
   HOME_BANNER_IMAGE:
     process.env.NEXT_PUBLIC_HOME_BANNER_IMAGE || '/bg_image.jpg', // 首页背景大图, 会被notion中的封面图覆盖，若无封面图则会使用代码中的 /public/bg_image.jpg 文件
   DESCRIPTION:
-    process.env.NEXT_PUBLIC_DESCRIPTION || 'William Cui's Blog', // 站点描述，被notion中的页面描述覆盖
+    process.env.NEXT_PUBLIC_DESCRIPTION || '分享连接你我，科技点亮生活', // 站点描述，被notion中的页面描述覆盖
 
   // 开发相关
   NOTION_ACCESS_TOKEN: process.env.NOTION_ACCESS_TOKEN || '', // Useful if you prefer not to make your database public
