b4d063e873b4b584b98df4c551a947b660dfca86
leaflyingl <leaflyingl@outlook.com>
2024-08-07 04:51:26 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,10 +15,10 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: 'Share Knowledge',
+  HEO_HERO_TITLE_2: 'and Adoration',
   HEO_HERO_TITLE_3: 'GOLDENBUDS.ONLINE',
-  HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_4: 'New Edition',
   HEO_HERO_TITLE_5: 'GoldenBuds, landing',
   HEO_HERO_TITLE_LINK: 'https://www.goldenbuds.online',
 
