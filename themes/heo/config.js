659905e32820f729195b819d4fca69d1c24f92d9
leaflyingl <leaflyingl@outlook.com>
2024-08-07 03:42:35 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -17,7 +17,7 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_3: 'GOLDENBUDS.ONLINE',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
   HEO_HERO_TITLE_LINK: 'https://www.goldenbuds.online',
