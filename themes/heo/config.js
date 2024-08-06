ffd6a9f770ce8f0a2e0c92f725a8130a1ffa7006
leaflyingl <leaflyingl@outlook.com>
2024-08-07 03:35:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -115,7 +115,7 @@ const CONFIG = {
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: '',
+  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
