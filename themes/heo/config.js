2d59ce8d4f5cf06d8c583faf9f4b9f1009cdde37
leaflyingl <leaflyingl@outlook.com>
2024-08-07 03:12:09 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -15,8 +15,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: 'GoldenBuds LED Grow Lights',
-  HEO_HERO_TITLE_2: 'and IT tech Info',
+  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'GOLDENBUDS.ONLINE',
   HEO_HERO_TITLE_4: 'NEW UPDATE',
   HEO_HERO_TITLE_5: 'New web domain launched',
