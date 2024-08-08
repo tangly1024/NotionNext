4a13c7b72e266cce522bd376c6bdcdb6b5c7d81b
leaflyingl <leaflyingl@outlook.com>
2024-08-08 16:37:30 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -456,7 +456,7 @@ const BLOG = {
     process.env.NEXT_PUBLIC_ANALYTICS_ACKEE_DOMAIN_ID || '', // e.g '82e51db6-dec2-423a-b7c9-b4ff7ebb3302'
 
   SEO_GOOGLE_SITE_VERIFICATION:
-    process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || 'pub-5015431604105277', // Remove the value or replace it with your own google site verification code
+    process.env.NEXT_PUBLIC_SEO_GOOGLE_SITE_VERIFICATION || 'pDVyH1KPaLBtO0qUTeuL0nC8uCh-QVqK67lzsQw3Skw', // Remove the value or replace it with your own google site verification code
 
   SEO_BAIDU_SITE_VERIFICATION:
     process.env.NEXT_PUBLIC_SEO_BAIDU_SITE_VERIFICATION || '', // Remove the value or replace it with your own google site verification code
