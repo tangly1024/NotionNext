9a6814f57cd3530f65f2f095e759a79a9aec0713
香港Henry <168814409+zls0725@users.noreply.github.com>
2024-08-08 23:31:58 +0800

    更新 blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -547,7 +547,7 @@ const BLOG = {
     process.env.npm_lifecycle_event === 'export', // 在打包过程中默认开启缓存，开发或运行时开启此功能意义不大。
   isProd: process.env.VERCEL_ENV === 'production' || process.env.EXPORT, // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)  isProd: process.env.VERCEL_ENV === 'production' // distinguish between development and production environment (ref: https://vercel.com/docs/environment-variables#system-environment-variables)
   BUNDLE_ANALYZER: process.env.ANALYZE === 'true' || false, // 是否展示编译依赖内容与大小
-  VERSION: process.env.NEXT_PUBLIC_VERSION // 版本号
+  VERSION: process.env.NEXT_PUBLIC_VERSION // 版本号 4.6.1这个版本开始用的
 }
 
 module.exports = BLOG
