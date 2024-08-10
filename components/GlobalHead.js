447f860bbfa3ddc2078a6a11353f2a232ee1e02a
leaflyingl <leaflyingl@outlook.com>
2024-08-11 04:05:59 +0800

    Update GlobalHead.js

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -118,7 +118,7 @@ const GlobalHead = props => {
           content={SEO_BAIDU_SITE_VERIFICATION}
         />
       )}
-
+<meta name="yandex-verification" content="1705ba7c6061fc61" />
       <meta name='keywords' content={keywords} />
       <meta name='description' content={description} />
       <meta property='og:locale' content={lang} />
