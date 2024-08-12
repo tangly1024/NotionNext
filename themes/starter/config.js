55b0dedd779d472b70bea87f438f09242e42161d
YZ <115330211+U0hZiY@users.noreply.github.com>
2024-08-12 20:17:25 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/U0hZiY/NotionNext', // 英雄区按钮
   STARTER_HERO_BUTTON_2_ICON: '/images/starter/github.svg', // 英雄区按钮2的图标，不需要则留空
 
-  STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
+  STARTER_HERO_PREVIEW_IMAGE: '/images/heo/4_upscayl_4x_ultrasharp.webp', // 产品预览图 ，默认读取public目录下图片
 
   // 顶部右侧导航暗流
   STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
