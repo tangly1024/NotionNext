1e503f7e68302c1fbed728a775b0dcef61ea605c
YZ <115330211+U0hZiY@users.noreply.github.com>
2024-08-12 11:21:23 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,9 +25,9 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'OnlyOnceDesign', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://onlyonce.com', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  BIO: process.env.NEXT_PUBLIC_BIO || '对不起，我们只做意式极简', // 作者简介
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://onlyonce.net', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客，onlyonce,家具定制，东莞家具，佛山家具，佛山家具源头工厂，东莞家具源头工厂，孙耀说家具，卢卡斯家具', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
