0ac667fd4ae82db4649a0ea0158213ca5ae2907e
YZ <115330211+U0hZiY@users.noreply.github.com>
2024-08-12 11:32:48 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -36,8 +36,8 @@ const CONFIG = {
   STARTER_FEATURE_TEXT_2:
     'NotionNext的愿景是帮助您简单、无感知地稳定地搭建自己的网站，放大品牌的价值。 ', // 特性
 
-  STARTER_FEATURE_1_TITLE_1: '免费且开源', // 特性1
-  STARTER_FEATURE_1_TEXT_1: '项目源码在Github上完全开放共享，遵循MIT协议', // 特性1
+  STARTER_FEATURE_1_TITLE_1: '设计模块开源', // 特性1
+  STARTER_FEATURE_1_TEXT_1: 'CAD、效果图及客案面对合作设计师开放', // 特性1
   STARTER_FEATURE_1_BUTTON_TEXT: '了解更多', // 特性1
   STARTER_FEATURE_1_BUTTON_URL: 'https://github.com/tangly1024/NotionNext', // 特性1
 
