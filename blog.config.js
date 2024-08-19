f056792b34a8a2386f2afb2d751c0999909e87c5
xuxiansheng1017 <164535563+xuxiansheng1017@users.noreply.github.com>
2024-08-19 21:58:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -30,15 +30,15 @@ const BLOG = {
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'yipeng, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
-  CONTACT_EMAIL: yipengtongxun@gmai.com || '', // 邮箱地址 例如mail@tangly1024.com
+  CONTACT_EMAIL:  || 'yipengtongxun@gmai.com', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 例如 https://github.com/tangly1024
-  CONTACT_TELEGRAM: https://t.me/yipeng188 || '', // 你的telegram 地址 例如 https://t.me/tangly_1024
-  CONTACT_LINKEDIN: https://t.me/yipeng1888 || '', // 你的telegram 群组
-  CONTACT_INSTAGRAM: https://t.me/yipeng88 || '', // 你的telegram 频道
+  CONTACT_TELEGRAM:  || 'https://t.me/yipeng188', // 你的telegram 地址 例如 https://t.me/tangly_1024
+  CONTACT_LINKEDIN:  || 'https://t.me/yipeng1888', // 你的telegram 群组
+  CONTACT_INSTAGRAM:  || 'https://t.me/yipeng88', // 你的telegram 频道
   CONTACT_BILIBILI: process.env.NEXT_PUBLIC_CONTACT_BILIBILI || '', // B站主页
-  CONTACT_YOUTUBE: http://www.youtube.com/@yipeng888 || '', // Youtube主页
+  CONTACT_YOUTUBE:  || 'http://www.youtube.com/@yipeng888', // Youtube主页
   CONTACT_XIAOHONGSHU: process.env.NEXT_PUBLIC_CONTACT_XIAOHONGSHU || '', // 小红书主页
   CONTACT_ZHISHIXINGQIU: process.env.NEXT_PUBLIC_CONTACT_ZHISHIXINGQIU || '', // 知识星球
   CONTACT_WEHCHAT_PUBLIC: process.env.NEXT_PUBLIC_CONTACT_WEHCHAT_PUBLIC || '', // 微信公众号 格式：https://mp.weixin.qq.com/mp/profile_ext?action=home&__biz=【xxxxxx】==#wechat_redirect
