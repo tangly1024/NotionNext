d53587ba36c44d7376d3c40a22d2885f411ca750
YZ <115330211+U0hZiY@users.noreply.github.com>
2024-08-19 23:14:11 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -234,7 +234,7 @@ const CONFIG = {
   STARTER_CONTACT_EMAIL_TEXT: 'yizhou47@gmail.com',
 
   // 嵌入外部表单
-  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/yfctc7', // 基于NoteForm创建，将留言数据存在Notion中
+  STARTER_CONTACT_MSG_EXTERNAL_URL: 'https://noteforms.com/forms/author-myldry', // 基于NoteForm创建，将留言数据存在Notion中
   //   自定义留言表单，以下几个配置暂时废弃
   //   STARTER_CONTACT_MSG_TITLE: '向我们留言',
   //   STARTER_CONTACT_MSG_NAME: '姓名',
