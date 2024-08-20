8670a174d18fd9628ad9b2e3f3e3e05460b6a7ba
KC_swag <kcswag@foxmail.com>
2024-08-20 21:10:03 +0800

    add inpage, push ad

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,5 +1,6 @@
 // 这里编写自定义js脚本；将被静态引入到页面中 
 
+// banner
 (function(kxsva){
 var d = document,
     s = d.createElement('script'),
@@ -10,5 +11,31 @@ s.async = true;
 s.referrerPolicy = 'no-referrer-when-downgrade';
 l.parentNode.insertBefore(s, l);
 })({})
+
+// inpage
+(function(jexr){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = jexr || {};
+s.src = "\/\/neat-period.com\/bBX.V-sLdhGolN0vYoWRcf\/nevmX9JuqZcUql\/ktPsTeUg1RMdD\/kq4fNCDuYotnNyTDU-wlOaTxg\/0eNbw_";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+
+// push
+(function(oibv){
+var d = document,
+    s = d.createElement('script'),
+    l = d.scripts[d.scripts.length - 1];
+s.settings = oibv || {};
+s.src = "\/\/neat-period.com\/anWx5sw.YuW\/drljQI2v9Mk\/ZGTf9r6PbS2D5olVS\/WWQU9TNfTmUxwIONTAgd1gOIC\/0p1UN\/TTAW5sObDJUJ5-";
+s.async = true;
+s.referrerPolicy = 'no-referrer-when-downgrade';
+l.parentNode.insertBefore(s, l);
+})({})
+
+
 
 
