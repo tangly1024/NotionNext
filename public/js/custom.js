c01e4d387ee121dd89a8bb616596172e33a83adf
KC_swag <kcswag@foxmail.com>
2024-08-20 21:32:02 +0800

    remove banner ad

--- public/js/custom.js
+++ public/js/custom.js
@@ -1,16 +1,5 @@
 // 这里编写自定义js脚本；将被静态引入到页面中 
 
-// banner
-(function(kxsva){
-var d = document,
-    s = d.createElement('script'),
-    l = d.scripts[d.scripts.length - 1];
-s.settings = kxsva || {};
-s.src = "\/\/neat-period.com\/b.XlVesSdIGVlL0fY\/WbdQiZYnWg5juVZ\/XyIw\/ce\/m\/9TulZ\/UAlTkLPYTOUh1SMUDSkj4GMUjgACt\/N-TUU\/waOhT\/gqyDMfQx";
-s.async = true;
-s.referrerPolicy = 'no-referrer-when-downgrade';
-l.parentNode.insertBefore(s, l);
-})({});
 
 // inpage
 (function(jexr){
