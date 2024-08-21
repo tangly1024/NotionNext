58610c8932340d8145270824150e7301258b0afe
KC_swag <kcswag@foxmail.com>
2024-08-21 13:54:58 +0800

    remove ad script

--- components/GlobalHead.js
+++ components/GlobalHead.js
@@ -165,9 +165,6 @@ const GlobalHead = props => {
       {
         <meta name="monetag" content="35a043e073a4954698138102e647cb08" />
       }
-      {
-        <script async="async" data-cfasync="false" src="//thubanoa.com/1?z=7934889"></script>
-      }
     </Head>
   )
 }
