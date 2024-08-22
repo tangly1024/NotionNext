f710cbcb6d0a06445be08b5693ac8446a39aff15
香港Henry <168814409+zls0725@users.noreply.github.com>
2024-08-22 19:24:16 +0800

    Update WordCount.js

--- components/WordCount.js
+++ components/WordCount.js
@@ -1,69 +1,67 @@
-import { useGlobal } from '@/lib/global';
-import { useEffect } from 'react';
+import { useGlobal } from '@/lib/global'
+import { useEffect } from 'react'
 
 /**
  * 字数统计
  * @returns
  */
 export default function WordCount() {
-  const { locale } = useGlobal();
-
+  const { locale } = useGlobal()
   useEffect(() => {
-    countWords();
-  }, []);
+    countWords()
+  })
 
-  return (
-    <span id='wordCountWrapper' className='flex gap-3 font-light hidden'>
-      <span className='flex whitespace-nowrap items-center'>
-        <i className='pl-1 pr-2 fas fa-file-word' />
-        <span id='wordCount'>0</span> 字
-      </span>
-      <span className='flex whitespace-nowrap items-center'>
-        <i className='mr-1 fas fa-clock' />
-        <span id='readTime'>0</span> {locale.COMMON.MINUTE}
-      </span>
+  return <span id='wordCountWrapper' className='flex gap-3 font-light'>
+        <span className='flex whitespace-nowrap items-center'>
+            <i className='pl-1 pr-2 fas fa-file-word' />
+            <span id='wordCount'>0</span>
+        </span>
+        <span className='flex whitespace-nowrap items-center'>
+            <i className='mr-1 fas fa-clock' />
+            <span></span>
+            <span id='readTime'>0</span>&nbsp;{locale.COMMON.MINUTE}
+        </span>
     </span>
-  );
 }
 
 /**
  * 更新字数统计和阅读时间
  */
 function countWords() {
-  const articleText = deleteHtmlTag(document.getElementById('notion-article')?.innerHTML);
-  const wordCount = fnGetCpmisWords(articleText);
-
-  // 阅读速度设置为每分钟400字
-  const readTime = Math.ceil(wordCount / 400);
-
-  // 更新字数和阅读时间显示
-  document.getElementById('wordCount').innerText = wordCount;
-  document.getElementById('readTime').innerText = readTime;
-
-  const wordCountWrapper = document.getElementById('wordCountWrapper');
-  wordCountWrapper.classList.remove('hidden');
+  const articleText = deleteHtmlTag(document.getElementById('notion-article')?.innerHTML)
+  const wordCount = fnGetCpmisWords(articleText)
+  // 阅读速度 300-500每分钟
+  document.getElementById('wordCount').innerHTML = wordCount
+  document.getElementById('readTime').innerHTML = Math.floor(wordCount / 400) + 1
+  const wordCountWrapper = document.getElementById('wordCountWrapper')
+  wordCountWrapper.classList.remove('hidden')
 }
 
 // 去除html标签
 function deleteHtmlTag(str) {
   if (!str) {
-    return '';
+    return ''
   }
-  return str.replace(/<[^>]+>|&[^>]+;/g, '').trim();
+  str = str.replace(/<[^>]+>|&[^>]+;/g, '').trim()// 去掉所有的html标签和&nbsp;之类的特殊符合
+  return str
 }
 
 // 用word方式计算正文字数
 function fnGetCpmisWords(str) {
   if (!str) {
-    return 0;
+    return 0
   }
+  let sLen = 0
+  try {
+    // eslint-disable-next-line no-irregular-whitespace
+    str = str.replace(/(\r\n+|\s+|　+)/g, '龘')
+    // eslint-disable-next-line no-control-regex
+    str = str.replace(/[\x00-\xff]/g, 'm')
+    str = str.replace(/m+/g, '*')
+    str = str.replace(/龘+/g, '')
+    sLen = str.length
+  } catch (e) {
 
-  // 保留你的主要逻辑，只简化了一些步骤
-  str = str.replace(/(\r\n+|\s+|　+)/g, ' ')
-           .replace(/[\x00-\xff]/g, 'm')
-           .replace(/m+/g, '*')
-           .replace(/龘+/g, '')
-           .trim();
-  
-  return str.length;
+  }
+  return sLen
 }
