3ad9a9e625496c3234b6a0df1088305f7f8b4b6e
AlbusSeverusPt <38790470+AlbusSeverusPt@users.noreply.github.com>
2024-08-26 11:48:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -25,8 +25,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '江牧律师', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '知识产权律师
-    软硬件发烧友', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '知识产权律师 软硬件发烧友', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://lawyerjiang.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
