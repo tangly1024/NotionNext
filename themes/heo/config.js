f270f5607f6db26e932986dfa8481167df5544ce
FelixChen <felixchentao@163.com>
2024-08-27 20:07:24 +0800

    update

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -1,12 +1,12 @@
 const CONFIG = {
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2023-02-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.felixchen.top' },
+    { title: '查看卡比的赛博空间', url: 'https://kabi.felixchen.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -16,11 +16,11 @@ const CONFIG = {
 
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_2: '与摄影生活',
+  HEO_HERO_TITLE_3: 'FELIXCHEN.TOP',
+  HEO_HERO_TITLE_4: '随机文档',
+  HEO_HERO_TITLE_5: '访问随机主题带来惊喜 😄',
+  HEO_HERO_TITLE_LINK: 'https://blog.felixchen.top',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
@@ -35,20 +35,20 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
-    '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
-    '🏃 脚踏实地行动派',
-    '🏠 智能家居小能手',
-    '🤖️ 数码科技爱好者',
-    '🧱 团队小组发动机'
+    '☂️ 下雨了知道打伞',
+    '🌙 天黑了知道回家',
+    '📱 会玩智能手机',
+    '🥣 饿了会找碗筷',
+    '😋 吃饱放不下碗',
+    '❤️ 心里永远充满爱'
   ],
 
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://blog.felixchen.top',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://kabi.felixchen.top',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
