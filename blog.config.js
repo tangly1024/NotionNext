00538c9f6a29c0e6c9392b5fb0aba420a0225e2b
yumehikoing <yumehikoing@gmail.com>
2024-08-27 20:24:40 +0900

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是尚雯婕, Hi，我是电子唱作人,Hi，我是一个干饭人,欢迎来到我的粉丝共创社团🎉',
+    'Hi，我是尚雯婕, Hi，我是电子唱作人,Hi，我是一个干饭人,欢迎来到我的粉丝共创工作室🎉',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
