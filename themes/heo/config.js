d82340cdca6028beaa8e5fe1649992685a164d91
uykb <tuzhenqiang@gmail.com>
2024-08-28 11:07:25 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'DIYVV.COM',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: '在Debian上安装Docker',
-  HEO_HERO_TITLE_LINK: '/article/example-11',
+  HEO_HERO_TITLE_LINK: '/article/example-1',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
