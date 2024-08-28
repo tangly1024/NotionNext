d9d2bcd1a8e8a0364c60e58268578e8e5f2b4e54
FelixChen <felixchentao@163.com>
2024-08-28 11:57:27 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Felix Chen', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '「運去金成鐵，時來鐵似金。」', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://meow.felixchen.top', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.felixchen.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客, FelixChen, FelixCTChen, Cat, Kirby, Hexo, Blog', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
