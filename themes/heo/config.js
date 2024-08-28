35cae5eacb6ac2e6155595d7c6fd88d2efd57f3c
FelixChen <felixchentao@163.com>
2024-08-28 11:54:09 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -5,8 +5,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.felixchen.top' },
-    { title: '卡比的赛博空间', url: 'https://kabi.felixchen.top' }
+    { title: '欢迎来到我的博客', url: 'https://www.felixchen.top' },
+    { title: '卡比的赛博空间', url: 'https://www.felixchen.top/kirby' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -20,7 +20,7 @@ const CONFIG = {
   HEO_HERO_TITLE_3: 'FELIXCHEN.TOP',
   HEO_HERO_TITLE_4: '愿你有个好心情',
   HEO_HERO_TITLE_5: '或许, 你也想看看我的猫 😄',
-  HEO_HERO_TITLE_LINK: 'https://kabi.felixchen.top',
+  HEO_HERO_TITLE_LINK: 'https://www.felixchen.top/kirby',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '编程学习', url: '/tag/编程学习' },
@@ -30,7 +30,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  // HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -112,9 +112,9 @@ const CONFIG = {
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_2: '通过邮件联系到我',
+  HEO_SOCIAL_CARD_TITLE_3: '点击联系博主',
+  HEO_SOCIAL_CARD_URL: 'mailto:felixchentao@163.com',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
