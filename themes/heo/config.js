2653959ba01cbecacc8e035734c2c992e9a024cf
hz-lixiuyuan <162880658+hz-lixiuyuan@users.noreply.github.com>
2024-09-02 12:12:31 +0800

    Update config.js
    页面文字修改

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -4,12 +4,12 @@ const CONFIG = {
 
   HEO_HOME_BANNER_ENABLE: true,
 
-  HEO_SITE_CREATE_TIME: '2021-09-21', // 建站日期，用于计算网站运行的第几天
+  HEO_SITE_CREATE_TIME: '2024-09-01', // 建站日期，用于计算网站运行的第几天
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://www.lixiuyuan.pp.ua/' },
+    { title: '我是一个落魄的淘系运营', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,17 +18,17 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '欢迎参观',
+  HEO_HERO_TITLE_2: '我的个人小站',
+  HEO_HERO_TITLE_3: 'LeeXiuYuan',
+  HEO_HERO_TITLE_4: '之乎者也',
+  HEO_HERO_TITLE_5: '半世空转半修缘',
+  HEO_HERO_TITLE_LINK: 'https://www.lixiuyuan.pp.ua/',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '教程分享', url: '/tag/教程分享' },
+  HEO_HERO_CATEGORY_2: { title: '美图鉴赏', url: '/tag/美图鉴赏' },
+  HEO_HERO_CATEGORY_3: { title: '职业杂谈', url: '/tag/职业杂谈' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
