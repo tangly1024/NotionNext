4363ad7baca426ff3de51f21f638cd588f83ef06
yanghe <y279336671@gmail.com>
2024-09-14 11:32:36 +0800

    修改hero

--- themes/hexo/components/Hero.js
+++ themes/hexo/components/Hero.js
@@ -68,26 +68,6 @@ const Hero = props => {
         <div className='font-black text-4xl md:text-5xl shadow-text'>
           {siteInfo?.title || siteConfig('TITLE')}
         </div>
-        {/* 站点欢迎语 */}
-        <div className='mt-2 h-12 items-center text-center font-medium shadow-text text-lg'>
-          <span id='typed' />
-        </div>
-
-        {/* 首页导航大按钮 */}
-        {siteConfig('HEXO_HOME_NAV_BUTTONS', null, CONFIG) && (
-          <NavButtonGroup {...props} />
-        )}
-
-        {/* 滚动按钮 */}
-        <div
-          onClick={scrollToWrapper}
-          className='z-10 cursor-pointer w-full text-center py-4 text-3xl absolute bottom-10 text-white'>
-          <div className='opacity-70 animate-bounce text-xs'>
-            {siteConfig('HEXO_SHOW_START_READING', null, CONFIG) &&
-              locale.COMMON.START_READING}
-          </div>
-          <i className='opacity-70 animate-bounce fas fa-angle-down' />
-        </div>
       </div>
 
       <LazyImage
