604313e2d3f7613d795cddbfb76ff8f34b228d4c
Camp Bill <139830735+soeos@users.noreply.github.com>
2024-09-14 16:13:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -148,9 +148,10 @@ const BLOG = {
     process.env.NEXT_PUBLIC_CUSTOM_RIGHT_CLICK_CONTEXT_MENU_THEME_TAG || true, // 是否显示标签
 
   // 自定义外部脚本，外部样式
-  CUSTOM_EXTERNAL_JS: ["https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js","https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
-  CUSTOM_EXTERNAL_CSS: ["https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
-
+  // CUSTOM_EXTERNAL_JS: ["https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/contrib/auto-render.min.js","https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.js"], // e.g. ['http://xx.com/script.js','http://xx.com/script.js']
+  // CUSTOM_EXTERNAL_CSS: ["https://cdn.jsdelivr.net/npm/katex@0.12.0/dist/katex.min.css"], // e.g. ['http://xx.com/style.css','http://xx.com/style.css']
+ CUSTOM_EXTERNAL_JS: [],
+ CUSTOM_EXTERNAL_CSS: [],
   // 侧栏布局 是否反转(左变右,右变左) 已支持主题: hexo next medium fukasawa example
   LAYOUT_SIDEBAR_REVERSE:
     process.env.NEXT_PUBLIC_LAYOUT_SIDEBAR_REVERSE || false,
