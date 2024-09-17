a8c5814aece69aa9f6ed72d3aa0cfec71b9ba86c
吴雨涵 <106737705+wuyuhanzijin@users.noreply.github.com>
2024-09-17 14:51:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -249,10 +249,10 @@ const BLOG = {
   // AI 文章摘要生成 @see https://docs_s.tianli0.top/
   TianliGPT_CSS:
     process.env.NEXT_PUBLIC_TIANLI_GPT_CSS ||
-    'https://cdn1.tianli0.top/gh/zhheo/Post-Abstract-AI@0.15.2/tianli_gpt.css',
+    'https://www.linyunlink.top/css/tianli_gpt.css',
   TianliGPT_JS:
     process.env.NEXT_PUBLIC_TIANLI_GPT_JS || 'https://www.linyunlink.top/js/summary.js',
-  TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '',
+  TianliGPT_KEY: process.env.NEXT_PUBLIC_TIANLI_GPT_KEY || '1145141919810',
 
   // Chatbase 是否显示chatbase机器人 https://www.chatbase.co/
   CHATBASE_ID: process.env.NEXT_PUBLIC_CHATBASE_ID || null,
