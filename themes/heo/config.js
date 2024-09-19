de600af313b8f1817c4b192df02bd95a5904c9e8
ZhengHaotian <79289323+orcatt@users.noreply.github.com>
2024-09-19 10:58:43 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,7 +8,7 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '我是淏天，欢迎来到我的博客', url: 'https://orcatt.one/about' }
+    { title: '我是淏天，欢迎来到我的博客', url: 'https://orcatt.one/about' },
     { title: '野蛮精神，强健体魄', url: 'https://orcatt.one/about' }
     
   ],
