67e5e2bddd91f35d1673b2c41c83c8dc1c2a2c00
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-22 17:35:28 +0800

    Update style.js

--- themes/heo/style.js
+++ themes/heo/style.js
@@ -8,7 +8,7 @@ const Style = () => {
   return (
     <style jsx global>{`
       body {
-        background-color: #f7f9fe;
+        background-color: ##ec2c64;
         overflow-x: hidden;
       }
 
