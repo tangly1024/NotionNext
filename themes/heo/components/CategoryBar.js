10213393afb6fddc7160b6677abf428caae12b79
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 21:50:15 +0800

    Update CategoryBar.js

--- themes/heo/components/CategoryBar.js
+++ themes/heo/components/CategoryBar.js
@@ -76,7 +76,7 @@ const MenuItem = ({ href, name }) => {
   const selected = category === name
   return (
     <div
-      className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-indigo-600 dark:hover:bg-yellow-600 ${selected ? 'text-white bg-indigo-600 dark:bg-yellow-600' : ''}`}>
+      className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-red-700 dark:hover:bg-yellow-600 ${selected ? 'text-white bg-indigo-600 dark:bg-yellow-600' : ''}`}>
       <Link href={href}>{name}</Link>
     </div>
   )
