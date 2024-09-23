14b90a04978f22d08fdd02c1e6f1f0e505c268d8
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 20:14:17 +0800

    Update Hero.js

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -97,11 +97,11 @@ function Banner(props) {
         id='banner-cover'
         style={{ backdropFilter: 'blur(15px)' }}
         className={
-          'rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#4259efdd] dark:bg-[#dca846] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
+          'rounded-xl overflow-hidden opacity-0 group-hover:opacity-100 duration-300 transition-all bg-[#a61b29] dark:bg-[#dca846] dark:text-white cursor-pointer absolute w-full h-full top-0 flex justify-start items-center'
         }>
         <div className='ml-12 -translate-x-32 group-hover:translate-x-0 duration-300 transition-all ease-in'>
           <div className='text-7xl text-white font-extrabold'>熱推新案</div>
-          <div className='-ml-3 text-gray-300'>
+          <div className='-ml-3 text-red-700'>
             <ArrowSmallRight className={'w-24 h-24 stroke-2'} />
           </div>
         </div>
