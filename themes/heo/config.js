e468295433f5673eecca4b60dc54cc7a3165e0ee
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 20:46:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -44,10 +44,10 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
-  HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
+  HEO_INFO_CARD_URL2: 'https://notion-master.vercel.app/blog',
+  HEO_INFO_CARD_ICON2: 'fa-solid fa-message',
+  HEO_INFO_CARD_URL3: 'https://notion-master.vercel.app/about.html',
+  HEO_INFO_CARD_TEXT3: '認識哖大師',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
