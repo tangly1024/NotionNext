8137d13de814005e7dac72f3b40ed4053f561c1c
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 22:52:42 +0800

    Update CategoryBar.js

--- themes/heo/components/CategoryBar.js
+++ themes/heo/components/CategoryBar.js
@@ -73,7 +73,7 @@ export default function CategoryBar(props) {
 const MenuItem = ({ href, name }) => {
   const router = useRouter()
   const { category } = router.query
-  const selected =主題 === name
+  const selected =category === name
   return (
     <div
       className={`whitespace-nowrap mr-2 duration-200 transition-all font-bold px-2 py-0.5 rounded-md text-gray-900 dark:text-white hover:text-white hover:bg-red-700 dark:hover:bg-yellow-600 ${selected ? 'text-white bg-indigo-600 dark:bg-yellow-600' : ''}`}>
