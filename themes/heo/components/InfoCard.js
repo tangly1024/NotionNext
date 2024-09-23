5e1180178a10919d4a7931424a75dacfb1ca1255
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 18:56:07 +0800

    Update InfoCard.js

--- themes/heo/components/InfoCard.js
+++ themes/heo/components/InfoCard.js
@@ -49,7 +49,7 @@ export function InfoCard(props) {
         <div className='flex space-x-3  hover:text-black dark:hover:text-white'>
           {/* 两个社交按钮 */}
           {url1 && (
-            <div className='w-10 text-center bg-indigo-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
+            <div className='w-10 text-center bg-red-400 p-2 rounded-full  transition-colors duration-200 dark:bg-yellow-500 dark:hover:bg-black hover:bg-white'>
               <Link href={url1}>
                 <i className={icon1} />
               </Link>
