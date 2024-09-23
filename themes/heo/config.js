7c3f66eb8db87f391be8d20db1e9c7808765b332
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 20:26:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -110,10 +110,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '交流頻道',
+  HEO_SOCIAL_CARD_TITLE_2: '一起找好屋',
+  HEO_SOCIAL_CARD_TITLE_3: '點擊加入我的粉專',
+  HEO_SOCIAL_CARD_URL: 'https://www.facebook.com/RealestateMaster2024',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
