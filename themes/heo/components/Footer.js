82a42c7975a8742bd3f2466d96d98811e3a3ed7d
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-23 20:56:08 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -28,7 +28,7 @@ const Footer = ({ title }) => {
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
         <div id='footer-bottom-left'>
-          NotionNext {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
+          核心版本 {siteConfig('VERSION')} <i className='fas fa-copyright' />{' '}
           {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' />{' '}
           <a
             href={siteConfig('LINK')}
