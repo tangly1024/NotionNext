971f856f3364b7b297ac157990ffc9e319e9ea49
laoma2053 <chc123110@gmail.com>
2024-09-24 23:51:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '副业人', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '为副业人而生的全能工具箱', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://fuye.ren', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.fuye.ren', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
