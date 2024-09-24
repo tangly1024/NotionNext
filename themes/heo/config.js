0bdc3497ae8a8ca62fa9a167e9c8e2f958219a98
LazyPigZYH <152492591+Bersulang@users.noreply.github.com>
2024-09-25 00:16:04 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -20,10 +20,10 @@ const CONFIG = {
   // 英雄区(首页顶部大卡)
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  HEO_HERO_TITLE_3: 'lazypigzyh.top',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_5: '更多功能敬请期待',
+  HEO_HERO_TITLE_LINK: 'https://github.com/Bersulang',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
