1821db79a0946d8996186fe78a426196741f04fc
tangly1024.com <tlyong1992@hotmail.com>
2024-09-25 16:04:16 +0800

    screen-width

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,15 +14,15 @@ module.exports = {
     screens: {
       sm: '540px',
       // => @media (min-width: 576px) { ... }
-
       md: '720px',
       // => @media (min-width: 768px) { ... }
-
       lg: '960px',
       // => @media (min-width: 992px) { ... }
-
-      xl: '1140px'
+      xl: '1140px',
       // => @media (min-width: 1200px) { ... }
+      '2xl': '1320px',
+      // => @media (min-width: 1400px) { ... }
+      '3xl': '1920px'
     },
     container: {
       center: true,
