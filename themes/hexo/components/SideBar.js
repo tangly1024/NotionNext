bcf6d39b4dc0f4fd9fea09ef0f005e1e23893493
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-29 02:09:43 +0800

    Update SideBar.js

--- themes/hexo/components/SideBar.js
+++ themes/hexo/components/SideBar.js
@@ -15,28 +15,29 @@ const SideBar = props => {
   const { siteInfo } = props
   const router = useRouter()
   return (
-    &lt;div id='side-bar'&gt;
-      &lt;div className='h-52 w-full flex justify-center'&gt;
-        &lt;div&gt;
-          &lt;div
+    <div id='side-bar'>
+      <div className='h-52 w-full flex justify-center'>
+        <div>
+          <div
             onClick={() => {
               router.push('/')
             }}
-            className='justify-center items-center flex hover:rotate-45 py-6 hover:scale-105 dark:text-gray-100 transform duration-200 cursor-pointer'&gt;
+            className='justify-center items-center flex hover:rotate-45 py-6 hover:scale-105 dark:text-gray-100  transform duration-200 cursor-pointer'>
             {/* 头像 */}
-            &lt;div className="rounded-lg"&gt;
-              &lt;LazyImage
-                src={siteInfo?.icon}
-                className="rounded-lg"
-                width={80}
-                alt={siteConfig('AUTHOR')}
-              /&gt;
-            &lt;/div&gt;
-          &lt;/div&gt;
-        &lt;/div&gt;
-      &lt;/div&gt;
-      {/* 其他代码 ... */}
-    &lt;/div&gt;
+            <LazyImage
+              src={siteInfo?.icon}
+              className='rounded-full'
+              width={80}
+              alt={siteConfig('AUTHOR')}
+            />
+          </div>
+          {/* 总览 */}
+          <MenuGroupCard {...props} />
+        </div>
+      </div>
+      {/* 侧拉抽屉的菜单 */}
+      <MenuListSide {...props} />
+    </div>
   )
 }
 
