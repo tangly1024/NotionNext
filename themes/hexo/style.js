9cf3b918c7cfd92ae688fc7279128e00ad1a9806
sftbob <111048941+sftbob@users.noreply.github.com>
2024-09-28 16:03:07 +0800

    Update style.js

--- themes/hexo/style.js
+++ themes/hexo/style.js
@@ -17,7 +17,7 @@ const Style = () => {
     /*  菜单下划线动画 */
     #theme-hexo .menu-link {
         text-decoration: none;
-        background-image: linear-gradient(#928CEE, #928CEE);
+        background-image: linear-gradient(#928CEE, #FF5809);
         background-repeat: no-repeat;
         background-position: bottom center;
         background-size: 0 2px;
