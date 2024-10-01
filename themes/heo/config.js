5fdee6e671a0d17ec995babd1aae6167d24c767a
ct188579 <149464263+ct188579@users.noreply.github.com>
2024-10-01 14:44:36 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '专注于分享编程知识', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://ctblog.top' },
+    { title: '专注于分享编程知识', url: 'https://ctblog.top' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -33,7 +33,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-    HERO_RECOMMEND_COVER: 'https://p.zhheo.com/vuijfj24090281708594120629.png!cover', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://p.zhheo.com/vuijfj24090281708594120629.png!cover', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
