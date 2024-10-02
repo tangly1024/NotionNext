ec1e734fd24900331835d3c16c9b405eabec8e69
ct188579 <149464263+ct188579@users.noreply.github.com>
2024-10-02 09:46:44 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -114,8 +114,8 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
+  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD_TITLE_1: '联系站长',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
   HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
