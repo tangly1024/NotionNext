00d215de4276fb91aee9d0b394267dfcfbbc4a50
yanghe <y279336671@gmail.com>
2024-10-05 00:36:38 +0800

    no message

--- blog.config.js
+++ blog.config.js
@@ -23,7 +23,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'simo54', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Simo54', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || 'old coder | 代码搬运专家 | Ctrl C+V布道者', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://simo54.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '程序员,blog,博客,自媒体,公众号,独立开发,simo54,Simo54', // 网站关键词 英文逗号隔开
