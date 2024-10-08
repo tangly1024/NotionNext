73aeb75c86c5756e70b467ade93d24e058e4b0f0
ct188579 <149464263+ct188579@users.noreply.github.com>
2024-10-08 20:35:29 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,12 +18,12 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
+  HEO_HERO_TITLE_1: '分享前端',
+  HEO_HERO_TITLE_2: '与IT趣事',
   HEO_HERO_TITLE_3: 'https://ctblog.top',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: '梦想成为优秀的前端全栈工程师',
-  HEO_HERO_TITLE_LINK: 'https://ctblog.top',
+  HEO_HERO_TITLE_5: '你也会开发前端吗',
+  HEO_HERO_TITLE_LINK: 'https://nextjs.org',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
