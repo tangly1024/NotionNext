8d0a3ae6c7c038b8b139ca04f7a4f2d7cf05d703
ct188579 <149464263+ct188579@users.noreply.github.com>
2024-10-08 20:36:13 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '陈涛', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'TAO', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '专注于前端开发', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'http://ctblog.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ctblog, 陈涛', // 网站关键词 英文逗号隔开
