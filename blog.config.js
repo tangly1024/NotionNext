c6598592a7f70c8c45348bc08ee06332eceb3076
M24VC <104404784+M24VC@users.noreply.github.com>
2024-10-10 12:45:35 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -19,14 +19,13 @@ const BLOG = {
 
   // 3.14.1版本后，欢迎语在此配置，英文逗号隔开 ,  即可支持多个欢迎语打字效果。
   GREETING_WORDS:
-    process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '欢迎来到变态集团',
+    process.env.NEXT_PUBLIC_GREETING_WORDS ||'欢迎来到变态集团',
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '变态哥哥', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '变态集团董事长', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://abnormal-group.vercel.app/', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '变态集团', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
