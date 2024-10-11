3b360d0786c4c9c84bdba5edd9529f084ca2f789
tangly1024.com <tlyong1992@hotmail.com>
2024-10-11 17:34:53 +0800

    默认关闭banner

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
-  STARTER_HERO_BANNER_IMAGE: '/images/starter/hero/banner.webp', // hero区下方的全宽图
+  STARTER_HERO_BANNER_IMAGE: '', // hero区下方的全宽图
 
   // 顶部右侧导航暗流
   STARTER_NAV_BUTTON_1_TEXT: 'Sign In',
