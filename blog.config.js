4963ea01d55915deace655c8768c2a04de67c170
momomosm <130282954+momomosm@users.noreply.github.com>
2024-10-18 18:01:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '燕商娇', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '🐹仓鼠一只', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://blog.19520310.xyz', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://mylove.19520310.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '小说, 自留地,桃花源', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
