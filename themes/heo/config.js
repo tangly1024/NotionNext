f34d0436bbfe8c04cce2c92817b8225da72f9f6e
ct188579 <149464263+ct188579@users.noreply.github.com>
2024-10-20 16:44:02 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -33,7 +33,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  HERO_RECOMMEND_COVER: 'https://p.zhheo.com/vuijfj24090281708594120629.png!cover', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'public/images/heo/rightHeroImg.webp', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
