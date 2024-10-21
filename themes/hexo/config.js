32932d4254c8853615eb288efe6636503e0c2af3
zimoxi <65380596+zimoxi@users.noreply.github.com>
2024-10-21 12:27:19 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -2,7 +2,6 @@ const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
-    'Hi，我是一个程序员',
     'Hi，我是一个打工人',
     'Hi，我是一个干饭人',
     '欢迎来到我的博客🎉'
