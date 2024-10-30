1c5b1b7c11aa5858fa60baeec87f0d4c0f9f4e3a
KaterinaJiang <xiaoxuanjiang22@gmail.com>
2024-10-30 10:48:20 +0800

    Update config.js

--- themes/starter/config.js
+++ themes/starter/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
     'https://docs.tangly1024.com/article/vercel-deploy-notion-next', // 英雄区按钮
   STARTER_HERO_BUTTON_2_TEXT: 'Follow us on Facebook', // 英雄区按钮
   STARTER_HERO_BUTTON_2_URL: 'https://github.com/tangly1024/NotionNext', // 英雄区按钮
-  STARTER_HERO_BUTTON_2_ICON: 'https://github.com/KaterinaJiang/facebook-/blob/main/1036479tqmttgmmtnti9rn.png.icon.png?raw=true', // 英雄区按钮2的图标，不需要则留空
+  STARTER_HERO_BUTTON_2_ICON: '<FontAwesomeIcon icon="fa-brands fa-facebook" />', // 英雄区按钮2的图标，不需要则留空
 
   // 英雄区配图，如需隐藏，改为空值即可 ''
   STARTER_HERO_PREVIEW_IMAGE: '/images/starter/hero/hero-image.webp', // 产品预览图 ，默认读取public目录下图片
