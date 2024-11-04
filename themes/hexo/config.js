c94ad34b33d7075009972e680fd053fc933a6481
Jinxia Wang <139682548+jinxiawang01@users.noreply.github.com>
2024-11-04 23:32:32 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -20,7 +20,7 @@ const CONFIG = {
   HEXO_MENU_TAG: true, // 显示标签
   HEXO_MENU_ARCHIVE: true, // 显示归档
   HEXO_MENU_SEARCH: true, // 显示搜索
-  HEXO_MENU_RANDOM: true, // 显示随机跳转按钮
+  HEXO_MENU_RANDOM: false, // 显示随机跳转按钮
 
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
   HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
