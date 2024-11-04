64dd5121512a33b0bef24a606830e7cdd135df83
Jinxia Wang <139682548+jinxiawang01@users.noreply.github.com>
2024-11-04 23:18:44 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -36,8 +36,8 @@ const CONFIG = {
 
   HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
-  HEXO_WIDGET_TO_TOP: true,
-  HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
+  HEXO_WIDGET_TO_TOP: false,
+  HEXO_WIDGET_TO_COMMENT: false, // 跳到评论区
   HEXO_WIDGET_DARK_MODE: false, // 夜间模式
   HEXO_WIDGET_TOC: false // 移动端悬浮目录
 }
