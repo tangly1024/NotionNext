667fe9d302de35529aacf2860693f07e3b2bfebe
Jinxia Wang <139682548+jinxiawang01@users.noreply.github.com>
2024-11-05 17:11:32 +0800

    Update BlogPostCard.js
    card高度改回去了，因为放开之后发现不知道是什么控制的高度了，结构更混乱了。

--- themes/hexo/components/BlogPostCard.js
+++ themes/hexo/components/BlogPostCard.js
@@ -31,7 +31,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
         data-aos-once='false'
         data-aos-anchor-placement='top-bottom'
         id='blog-post-card'
-        className={`group w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
+        className={`group md:h-56 w-full flex justify-between md:flex-row flex-col-reverse ${siteConfig('HEXO_POST_LIST_IMG_CROSSOVER', null, CONFIG) && index % 2 === 1 ? 'md:flex-row-reverse' : ''}
                     overflow-hidden border dark:border-black rounded-xl bg-white dark:bg-hexo-black-gray`}>
         {/* 文字内容 */}
         <BlogPostCardInfo
@@ -51,7 +51,7 @@ const BlogPostCard = ({ index, post, showSummary, siteInfo }) => {
                   priority={index === 1}
                   alt={post?.title}
                   src={post?.pageCoverThumbnail}
-                  className='w-full object-cover object-center group-hover:scale-110 duration-500'
+                  className='h-56 w-full object-cover object-center group-hover:scale-110 duration-500'
                 />
               </>
             </Link>
