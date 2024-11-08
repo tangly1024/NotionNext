2266c0954888248f3f92235cf0fe2ad91e0dd39b
KaterinaJiang <xiaoxuanjiang22@gmail.com>
2024-11-08 15:22:17 +0800

    Update config.js

--- themes/nav/config.js
+++ themes/nav/config.js
@@ -10,7 +10,7 @@ const CONFIG = {
   NAV_MENU_CATEGORY: true, // 显示分类
   NAV_MENU_TAG: true, // 显示标签
   NAV_MENU_ARCHIVE: true, // 显示归档
-  NAV_MENU_SEARCH: true, // 显示搜索
+  NAV_MENU_SEARCH: false, // 显示搜索
 
   // Widget
   NAV_WIDGET_REVOLVER_MAPS:
