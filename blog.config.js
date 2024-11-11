78e47700176a7df562cb41eadafaeb6f396dfca6
crushllys <106291155+Cvsly@users.noreply.github.com>
2024-11-12 01:15:47 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'YongLiu', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '记录生活中的有趣瞬间⚡️', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.ikiss.eu.org', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://ikiss.eu.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
