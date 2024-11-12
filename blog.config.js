e2d30bcf765643c9764e5988e6145357a797633d
Leafbritain <130814915+Leafbritain@users.noreply.github.com>
2024-11-12 17:54:15 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || true, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '莳萝心爱', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '你一定不明白吧', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '我们之于彼此，不过是一种知觉。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://koimoyou.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '爱的狂信徒', // 网站关键词 英文逗号隔开
@@ -256,9 +256,9 @@ const BLOG = {
             'https://i.postimg.cc/SKF4SPn2/IMG-4916-20240201-184413.jpg'
     },
     {
-      name: 'morena',
-      artist: 'maria＆victor',
-      url: 'http://music.163.com/song/media/outer/url?id=1438724778.mp3',
+      name: '文豪とアルケミスト ～Piano ver.～',
+      artist: '坂本英城',
+      url: 'http://music.163.com/song/media/outer/url?id=490637049.mp3',
       cover:
             'https://i.postimg.cc/SKF4SPn2/IMG-4916-20240201-184413.jpg'
     },
