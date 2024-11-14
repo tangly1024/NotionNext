12fd9ed40f4568dc621c6879f811544c0050e618
tangly1024.com <tlyong1992@hotmail.com>
2024-11-14 12:19:23 +0800

    放宽JS检测

--- .eslintrc.js
+++ .eslintrc.js
@@ -58,7 +58,8 @@ module.exports = {
         '@typescript-eslint/explicit-module-boundary-types': 'off',
         '@typescript-eslint/no-explicit-any': 'off',
         '@typescript-eslint/ban-ts-comment': 'off',
-        '@typescript-eslint/no-floating-promises': 'off'
+        '@typescript-eslint/no-floating-promises': 'off',
+        '@typescript-eslint/no-unsafe-return': 'off'
       }
     }
   ],
