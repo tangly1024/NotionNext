ee804dc0d424665a69a864706ec5443f5303fb1f
rinxiaoxiao <33726100+rinxiaoxiao@users.noreply.github.com>
2024-11-17 04:46:13 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -35,7 +35,7 @@ const CONFIG = {
   HEXO_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   HEXO_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
-  HEXO_WIDGET_ANALYTICS: false, // 显示统计卡
+  HEXO_WIDGET_ANALYTICS: true, // 显示统计卡
   HEXO_WIDGET_TO_TOP: true,
   HEXO_WIDGET_TO_COMMENT: true, // 跳到评论区
   HEXO_WIDGET_DARK_MODE: true, // 夜间模式
