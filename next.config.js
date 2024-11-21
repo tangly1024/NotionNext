2e18ed2e0effd0f6fa3161db074ac936cbb15205
yangsong13 <yangsong13@meituan.com>
2024-11-21 17:35:43 +0800

    feat: 优化dockerfile,可使包体积从1.1G缩小至200M

--- next.config.js
+++ next.config.js
@@ -84,7 +84,7 @@ const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true
   },
-  output: process.env.EXPORT ? 'export' : undefined,
+  output: process.env.EXPORT ? 'export' : 'standalone',
   staticPageGenerationTimeout: 120,
   // 多语言， 在export时禁用
   i18n: process.env.EXPORT
