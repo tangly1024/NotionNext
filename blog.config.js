29ea2665a8522f3a9ae7ead3e213a631e1520a52
M24代言人VC <104404784+M24VC@users.noreply.github.com>
2024-11-22 12:00:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -29,10 +29,6 @@ const BLOG = {
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.youtube.com/@M24VC', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '', // 网站关键词 英文逗号隔开
 
-    AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'VC', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.youtube.com/@M24VC', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
