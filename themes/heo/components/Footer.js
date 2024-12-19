d99c703dc272147d539d54ee62dcc5ce068be30a
logtime <66826612+log-time@users.noreply.github.com>
2024-12-19 19:59:07 +0800

    Add files via upload

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,60 +1,55 @@
-import { BeiAnGongAn } from '@/components/BeiAnGongAn'
-import CopyRightDate from '@/components/CopyRightDate'
-import PoweredBy from '@/components/PoweredBy'
-import { siteConfig } from '@/lib/config'
+import React from 'react'
+import BLOG from '@/blog.config'
 import SocialButton from './SocialButton'
-/**
- * 页脚
- * @returns
- */
-const Footer = () => {
+// import DarkModeButton from '@/components/DarkModeButton'
+
+const Footer = ({ title }) => {
+  const d = new Date()
+  const currentYear = d.getFullYear()
+  const copyrightDate = (function () {
+    if (Number.isInteger(BLOG.SINCE) && BLOG.SINCE < currentYear) {
+      return BLOG.SINCE + '-' + currentYear
+    }
+    return currentYear
+  })()
+
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
-      {/* 颜色过度区 */}
-      <div
-        id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
-      />
+        <footer
+            className='relative z-10 flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'
+        >
 
-      {/* 社交按钮 */}
-      <div className='w-full h-24'>
-        <SocialButton />
-      </div>
+            {/* 颜色过度区 */}
+            <div id='color-transition' className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'>
 
-      <br />
+            </div>
 
-      {/* 底部页面信息 */}
-       <div id='footer-bottom' className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#30343f]'>
+            {/* 社交按钮 */}
+            <div className='w-full h-24'>
+                <SocialButton />
+            </div>
 
-       <div id='footer-bottom-left'>
-          NotionNext {BLOG.VERSION} <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' /> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.
-       </div>
+            <br />
 
+            {/* 底部页面信息 */}
+            <div id='footer-bottom' className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#30343f]'>
 
-        <div id='footer-bottom-right'>
-          {siteConfig('BEI_AN') && (
-            <>
-              <i className='fas fa-shield-alt' />{' '}
-              <a href='https://beian.miit.gov.cn/' className='mr-2'>
-                {siteConfig('BEI_AN')}
-              </a>
-            </>
-          )}
-          <BeiAnGongAn />
+                <div id='footer-bottom-left'>
+                   NotionNext {BLOG.VERSION} <i className='fas fa-copyright' /> {`${copyrightDate}`} <i className='mx-1 animate-pulse fas fa-heart' /> <a href={BLOG.LINK} className='underline font-bold  dark:text-gray-300 '>{BLOG.AUTHOR}</a>.
+                </div>
 
-          <span className='hidden busuanzi_container_site_pv'>
-            <i className='fas fa-eye' />
-            <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-          </span>
-          <span className='pl-2 hidden busuanzi_container_site_uv'>
-            <i className='fas fa-users' />{' '}
-            <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-          </span>
+            <div id='footer-bottom-right'>
+                {BLOG.BEI_AN && <><i className='fas fa-shield-alt' /> <a href='https://beian.miit.gov.cn/' className='mr-2'>{BLOG.BEI_AN}</a></>}
 
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
+                <span className='hidden busuanzi_container_site_pv'>
+                    <i className='fas fa-eye' /><span className='px-1 busuanzi_value_site_pv'> </span>  </span>
+                <span className='pl-2 hidden busuanzi_container_site_uv'>
+                    <i className='fas fa-users' /> <span className='px-1 busuanzi_value_site_uv'> </span> </span>
+
+                {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {BLOG.BIO && <>|</>} {BLOG.BIO}</h1> */}
+
+            </div>
         </div>
-      </div>
-    </footer>
+        </footer >
   )
 }
 
