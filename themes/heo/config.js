250c9937ff33e4ff511511696c8b05b88ae0845c
logtime <66826612+log-time@users.noreply.github.com>
2024-12-19 18:06:57 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://blog.logtime.tk' },
+    { title: '', url: '' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -28,8 +28,8 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
+  HEO_HERO_CATEGORY_1: { title: '日常记录', url: '/tag/日常记录' },
+  HEO_HERO_CATEGORY_2: { title: '实用教程', url: '/tag/实用教程' },
   HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
