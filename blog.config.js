30b28b328c2fb9f79cb6e9ae572594d89be9987e
sonetto19 <149054592+sonetto19@users.noreply.github.com>
2024-12-21 00:50:05 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -243,7 +243,7 @@ const BLOG = {
 
   //   ********动态特效相关********
   // 鼠标点击烟花特效
-  FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || false, // 开关
+  FIREWORKS: process.env.NEXT_PUBLIC_FIREWORKS || true, // 开关
   // 烟花色彩，感谢 https://github.com/Vixcity 提交的色彩
   FIREWORKS_COLOR: [
     '255, 20, 97',
