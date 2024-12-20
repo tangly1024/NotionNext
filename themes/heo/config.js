a5b3e59cd24542244c255a32f471a297539ed168
logtime <66826612+log-time@users.noreply.github.com>
2024-12-20 10:06:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.logtime.tk' },
-    { title: '', url: '' }
+    { title: '分享快乐，共同进步', url: 'https://blog.logtime.tk' }
   ],
 
   // 英雄区左右侧组件颠倒位置
