38783f6cc1640d7bd31954e8a4cfbaa394413071
sonetto19 <149054592+sonetto19@users.noreply.github.com>
2024-12-22 00:49:45 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,8 +18,8 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享游戏开发',
-  HEO_HERO_TITLE_2: '与我的杂七杂八',
+  HEO_HERO_TITLE_1: '游戏开发',
+  HEO_HERO_TITLE_2: '与杂七杂八',
   HEO_HERO_TITLE_3: 'NETANIO.COM',
   HEO_HERO_TITLE_4: '新版上线',
   HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
@@ -75,43 +75,43 @@ const CONFIG = {
       color_2: '#9fe404'
     },
     {
-      title_1: 'FinalCutPro',
-      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
+      title_1: 'wuyaowang',
+      img_1: '/images/heo/wuyao.png',
+      color_1: '#f1c232',
+      title_2: 'daboluo',
+      img_2: '/images/heo/da.png',
+      color_2: '#f1c232'
+    },
+    {
+      title_1: 'gta',
+      img_1: '/images/heo/gta.png',
       color_1: '#ffffff',
-      title_2: 'Python',
-      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
+      title_2: 'steam',
+      img_2: '/images/heo/steam.png',
       color_2: '#ffffff'
     },
     {
-      title_1: 'Swift',
-      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
-      color_1: '#eb6840',
-      title_2: 'Principle',
-      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
-      color_2: '#8f55ba'
-    },
-    {
-      title_1: 'illustrator',
-      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
+      title_1: 'pika',
+      img_1: '/images/heo/pika.png',
       color_1: '#f29e39',
-      title_2: 'CSS3',
-      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
-      color_2: '#2c51db'
+      title_2: 'ys',
+      img_2: '/images/heo/ys.png',
+      color_2: '#fff6ec'
     },
     {
-      title_1: 'JS',
-      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
+      title_1: 'mw',
+      img_1: '/images/heo/mw.png',
       color_1: '#f7cb4f',
-      title_2: 'HTML',
-      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
+      title_2: 'cod',
+      img_2: '/images/heo/cod.png',
       color_2: '#e9572b'
     },
     {
-      title_1: 'Git',
-      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
-      color_1: '#df5b40',
-      title_2: 'Rhino',
-      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
+      title_1: 'wf',
+      img_1: '/images/heo/wf.png',
+      color_1: '#0b5394',
+      title_2: 'halo',
+      img_2: '/images/heo/halo.png',
       color_2: '#1f1f1f'
     }
   ],
