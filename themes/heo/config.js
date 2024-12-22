ea5449849e8a42dd0a9c501b3d31fdd8713e8127
sonetto19 <149054592+sonetto19@users.noreply.github.com>
2024-12-22 10:33:33 +0800

    更新 config.js
    修改三个标签归档页

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,10 +28,10 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
-
+  HEO_HERO_CATEGORY_1: { title: '缸中之脑', url: '/tag/缸中之脑' },
+  HEO_HERO_CATEGORY_2: { title: '游戏人生', url: '/tag/游戏人生' },
+  HEO_HERO_CATEGORY_3: { title: '折腾笔记', url: '/tag/折腾笔记' },
+nv
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
@@ -39,7 +39,7 @@ const CONFIG = {
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
-    '你好！我是',
+    '你好矩阵！我是MESSIAH',
     '🔍 分享与热心帮助',
     '🤝 专修交互与设计',
     '🏃 脚踏实地行动派',
