535c9c163155b17d37919977546b7ae9325fb3ec
sonetto19 <149054592+sonetto19@users.noreply.github.com>
2024-12-22 10:42:12 +0800

    更新 config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -31,7 +31,7 @@ const CONFIG = {
   HEO_HERO_CATEGORY_1: { title: '缸中之脑', url: '/tag/缸中之脑' },
   HEO_HERO_CATEGORY_2: { title: '游戏人生', url: '/tag/游戏人生' },
   HEO_HERO_CATEGORY_3: { title: '折腾笔记', url: '/tag/折腾笔记' },
-nv
+
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
