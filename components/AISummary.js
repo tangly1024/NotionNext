2b2d5b2d3399df0465828b2a0e76fce2487a67d1
anime <street-anime-olive@duck.com>
2024-12-23 18:12:12 +0800

    feat(优化原生AI摘要功能显示情况): 没有数据不加载，默认渲染完整摘要，利于SEO
    (cherry picked from commit 1964bc96fdce688129287594103e6555fb42c779)

--- components/AISummary.js
+++ components/AISummary.js
@@ -4,41 +4,43 @@ import { useGlobal } from '@/lib/global'
 
 const AISummary = ({ aiSummary }) => {
   const { locale } = useGlobal()
-  const [summary, setSummary] = useState(locale.AI_SUMMARY.GENERATING + '...')
+  const [summary, setSummary] = useState(aiSummary)
 
   useEffect(() => {
     showAiSummaryAnimation(aiSummary, setSummary)
   }, [])
 
   return (
-    <div className={styles['post-ai']}>
-      <div className={styles['ai-container']}>
-        <div className={styles['ai-header']}>
-          <div className={styles['ai-icon']}>
-            <svg
-              xmlns='http://www.w3.org/2000/svg'
-              viewBox='0 0 24 24'
-              width='24'
-              height='24'>
-              <path
-                fill='#ffffff'
-                d='M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4M12,6A6,6 0 0,1 18,12A6,6 0 0,1 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6M12,8A4,4 0 0,0 8,12A4,4 0 0,0 12,16A4,4 0 0,0 16,12A4,4 0 0,0 12,8Z'
-              />
-            </svg>
+    aiSummary && (
+      <div className={styles['post-ai']}>
+        <div className={styles['ai-container']}>
+          <div className={styles['ai-header']}>
+            <div className={styles['ai-icon']}>
+              <svg
+                xmlns='http://www.w3.org/2000/svg'
+                viewBox='0 0 24 24'
+                width='24'
+                height='24'>
+                <path
+                  fill='#ffffff'
+                  d='M12,2A10,10 0 0,1 22,12A10,10 0 0,1 12,22A10,10 0 0,1 2,12A10,10 0 0,1 12,2M12,4A8,8 0 0,0 4,12A8,8 0 0,0 12,20A8,8 0 0,0 20,12A8,8 0 0,0 12,4M12,6A6,6 0 0,1 18,12A6,6 0 0,1 12,18A6,6 0 0,1 6,12A6,6 0 0,1 12,6M12,8A4,4 0 0,0 8,12A4,4 0 0,0 12,16A4,4 0 0,0 16,12A4,4 0 0,0 12,8Z'
+                />
+              </svg>
+            </div>
+            <div className={styles['ai-title']}>{locale.AI_SUMMARY.NAME}</div>
+            <div className={styles['ai-tag']}>GPT</div>
           </div>
-          <div className={styles['ai-title']}>{locale.AI_SUMMARY.NAME}</div>
-          <div className={styles['ai-tag']}>GPT</div>
-        </div>
-        <div className={styles['ai-content']}>
-          <div className={styles['ai-explanation']}>
-            {summary}
-            {summary !== aiSummary && (
-              <span className={styles['blinking-cursor']}></span>
-            )}
+          <div className={styles['ai-content']}>
+            <div className={styles['ai-explanation']}>
+              {summary}
+              {summary !== aiSummary && (
+                <span className={styles['blinking-cursor']}></span>
+              )}
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    )
   )
 }
 
