1ab87bb26147eeeeb517c7353f7b89acd126eb5c
sonetto19 <149054592+sonetto19@users.noreply.github.com>
2024-12-23 14:42:50 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -119,10 +119,10 @@ const CONFIG = {
   ],
 
   HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
-  HEO_SOCIAL_CARD_TITLE_1: '交流频道',
-  HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
-  HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
-  HEO_SOCIAL_CARD_URL: 'https://docs.tangly1024.com/article/how-to-question',
+  HEO_SOCIAL_CARD_TITLE_1: '还没想好',
+  HEO_SOCIAL_CARD_TITLE_2: '想好以后再添加现在还是进主页',
+  HEO_SOCIAL_CARD_TITLE_3: '点击返回主页',
+  HEO_SOCIAL_CARD_URL: 'https://blog.netanio.com',
 
   // *****  以下配置无效，只是预留开发 ****
   // 菜单配置
