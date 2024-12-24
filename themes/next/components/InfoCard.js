45402ba030903575193a01527832162f939fdf48
xiaolicyq <1625227577@qq.com>
2024-12-24 23:15:55 +0800

    修改头像宽度 不做限制

--- themes/next/components/InfoCard.js
+++ themes/next/components/InfoCard.js
@@ -8,7 +8,7 @@ const InfoCard = (props) => {
   return <>
     <div className='flex flex-col items-center justify-center '>
         <div className='hover:rotate-45 hover:scale-125 transform duration-200 cursor-pointer' onClick={ () => { Router.push('/') }}>
-        <LazyImage src={siteInfo?.icon} className='rounded-full' width={120} alt={siteConfig('AUTHOR')}/>
+        <LazyImage src={siteInfo?.icon} className='rounded-full'  alt={siteConfig('AUTHOR')}/>
         </div>
         <div className='text-2xl font-serif dark:text-white py-2 hover:scale-105 transform duration-200'>{siteConfig('AUTHOR')}</div>
         <div className='font-light dark:text-white py-2 hover:scale-105 transform duration-200 text-center'>{siteConfig('BIO')}</div>
