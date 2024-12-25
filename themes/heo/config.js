490f6a1cc557ec72edec44acd1128a333dcf98de
LazyPig <152492591+Bersulang@users.noreply.github.com>
2024-12-25 17:29:54 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -49,7 +49,7 @@ const CONFIG = {
   ],
 
   // 个人资料底部按钮
-  HEO_INFO_CARD_URL1: '/aboutme',
+  HEO_INFO_CARD_URL1: '/contactme',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/Bersulang',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
