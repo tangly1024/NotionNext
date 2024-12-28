abae8e395c6734d5f341daf7676661d973dfbfa0
Episkey <1986053226mo@gmail.com>
2024-12-28 17:48:08 +0800

    修改文字配置

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://episkey.top' },
+    { title: '访问我的GitHub', url: 'https://github.com/Episkey-G' }
   ],
 
   // 英雄区左右侧组件颠倒位置
@@ -18,19 +18,19 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
-  HEO_HERO_TITLE_2: '与思维认知',
-  HEO_HERO_TITLE_3: 'TANGLY1024.COM',
-  HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
-  HEO_HERO_TITLE_LINK: 'https://tangly1024.com',
+  HEO_HERO_TITLE_1: '享受生活',
+  // HEO_HERO_TITLE_2: '与思维认知',
+  // HEO_HERO_TITLE_3: 'TANGLY1024.COM',
+  // HEO_HERO_TITLE_4: '新版上线',
+  HEO_HERO_TITLE_5: '2024',
+  HEO_HERO_TITLE_LINK: 'https://episkey.top',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: '日常生活', url: '/tag/日常生活' },
+  HEO_HERO_CATEGORY_2: { title: '代码编程', url: '/tag/代码编程' },
+  HEO_HERO_CATEGORY_3: { title: '关于', url: '/tag/关于' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
@@ -40,8 +40,9 @@ const CONFIG = {
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
     '你好！我是',
+    '🧑‍💻 测试开发工程师',
     '🔍 分享与热心帮助',
-    '🤝 专修交互与设计',
+    '📚 持续学习者',
     '🏃 脚踏实地行动派',
     '🏠 智能家居小能手',
     '🤖️ 数码科技爱好者',
@@ -59,11 +60,11 @@ const CONFIG = {
   // 用户技能图标
   HEO_GROUP_ICONS: [
     {
-      title_1: 'AfterEffect',
-      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
-      color_1: '#989bf8',
-      title_2: 'Sketch',
-      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
+      title_1: 'Java',
+      img_1: '/images/heo/java.png',
+      color_1: '#ffffff',
+      title_2: 'SpringBoot',
+      img_2: '/images/heo/springboot.png',
       color_2: '#ffffff'
     },
     {
@@ -116,7 +117,7 @@ const CONFIG = {
     }
   ],
 
-  HEO_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
+  HEO_SOCIAL_CARD: false, // 是否显示右侧，点击加入社群按钮
   HEO_SOCIAL_CARD_TITLE_1: '交流频道',
   HEO_SOCIAL_CARD_TITLE_2: '加入我们的社群讨论分享',
   HEO_SOCIAL_CARD_TITLE_3: '点击加入社群',
