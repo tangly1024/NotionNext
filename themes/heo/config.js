73cf2b9787c2a92f57e0f463976f1be7d58d7146
天天向上 <ltz150@163.com>
2025-01-05 22:56:07 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -22,7 +22,7 @@ const CONFIG = {
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'litianzeng.cn',
   HEO_HERO_TITLE_4: '新版上线',
-  HEO_HERO_TITLE_5: 'NotionNext4.0 轻松定制主题',
+  HEO_HERO_TITLE_5: '最新文章与推荐文章',
   HEO_HERO_TITLE_LINK: 'https://litianzeng.cn',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
