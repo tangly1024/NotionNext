f02bce51f404d43657be8245fca64ecbedc1d8c7
天天向上 <ltz150@163.com>
2025-01-05 22:46:01 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -18,7 +18,7 @@ const CONFIG = {
   HEO_HERO_BODY_REVERSE: false,
 
   // 英雄区(首页顶部大卡)
-  HEO_HERO_TITLE_1: '分享编程',
+  HEO_HERO_TITLE_1: '分享AI使用',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'litianzeng.COM',
   HEO_HERO_TITLE_4: '新版上线',
