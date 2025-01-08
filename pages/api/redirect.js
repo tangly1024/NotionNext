6e47e418aab9ce0ddc1f3632ad28e0bef709eeb3
anime <street-anime-olive@duck.com>
2025-01-08 15:55:47 +0800

    feat(Redirect API设置缓存):

--- pages/api/redirect.js
+++ pages/api/redirect.js
@@ -1,10 +1,14 @@
-import { redisClient } from '@/lib/cache/redis_cache'
+import { redisCacheTime, redisClient } from '@/lib/cache/redis_cache'
 import { redirectCacheKey } from '@/lib/redirect'
 
 export default async function handler(req, res) {
   const { lastPart } = req.body
   try {
     const result = (await redisClient.hget(redirectCacheKey, lastPart)) || null
+    res.setHeader(
+      'Cache-Control',
+      `public, max-age=${redisCacheTime}, stale-while-revalidate=${redisCacheTime / 6}`
+    )
     res.status(200).json({ status: 'success', data: result })
   } catch (error) {
     res.status(400).json({ status: 'error', message: 'failed!', error })
