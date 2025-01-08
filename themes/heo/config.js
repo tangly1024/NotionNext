610e4fb308f5dc19c61dbb263cf34e4fab66ab1b
小车神XCShey <XCShey@outlook.com>
2025-01-08 13:02:51 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '视频翻译合集', url: '/webp' },
+  HEO_HERO_CATEGORY_1: { title: '视频翻译合集', url: '/404' },
   HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '网站聚合页', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_3: { title: '网站聚合页', url: '/webp' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
