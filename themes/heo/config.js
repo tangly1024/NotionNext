e0dfc9cb0f5378c76ec9e78409f1c9b77219638e
LazyPig <152492591+Bersulang@users.noreply.github.com>
2025-01-12 15:56:07 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://www.lazypigzyh.top/' },
-    { title: '访问文档中心获取更多帮助', url: 'https://www.lazypigzyh.top/' }
+    { title: '欢迎来到我的博客', url: '' },
+    { title: '访问文档中心获取更多帮助', url: '[]' }
   ],
 
   // 英雄区左右侧组件颠倒位置
