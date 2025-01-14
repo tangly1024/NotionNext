204fe6bc20b564951eaab79f789712f584de9626
tylin23 <tingyulin.w@gmail.com>
2025-01-15 00:27:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'tylin', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一個雜食人，穿梭於Media/UIUX/PM/AI/ML', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一個多元雜食者，穿梭於Media/UIUX/PM/AI/ML', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'http://tinyurl.com/tylin-portfolio', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, Blog', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
