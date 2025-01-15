8a8091c0f2d57214af2cd897e996560eb7d53ab9
tylin23 <tingyulin.w@gmail.com>
2025-01-15 18:28:33 +0800

    Update BlogPostArchive.js

--- themes/matery/components/BlogPostArchive.js
+++ themes/matery/components/BlogPostArchive.js
@@ -23,14 +23,14 @@ const BlogPostArchive = ({ posts = [], archiveTitle }) => {
             return (
               <li
                 key={post.id}
-                className='border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-indigo-500 dark:hover:border-indigo-300 dark:border-indigo-400 transform duration-500'>
+                className='border-l-2 p-1 text-xs md:text-base items-center  hover:scale-x-105 hover:border-gray-500 dark:hover:border-gray-300 dark:border-gray-400 transform duration-500'>
                 <div id={post?.publishDay}>
                   <span className='text-gray-400'>{post.date?.start_date}</span>{' '}
                   &nbsp;
                   <Link
                     href={post?.href}
                     passHref
-                    className='dark:text-gray-400  dark:hover:text-indigo-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600'>
+                    className='dark:text-gray-400  dark:hover:text-gray-300 overflow-x-hidden hover:underline cursor-pointer text-gray-600'>
                     {post.title}
                   </Link>
                 </div>
