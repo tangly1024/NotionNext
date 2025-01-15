6d37d88b88b1321470ad94ee3d9159ebd5557d2e
tylin23 <tingyulin.w@gmail.com>
2025-01-15 17:31:31 +0800

    Update RightFloatButtons.js
    hide mail

--- themes/matery/components/RightFloatButtons.js
+++ themes/matery/components/RightFloatButtons.js
@@ -12,7 +12,7 @@ export default function RightFloatButtons(props) {
   return <div className="bottom-40 right-2 fixed justify-end space-y-2 z-20">
         <FloatDarkModeButton />
         <JumpToTopButton />
-        <SocialButton />
+        /*<SocialButton /> */
         {/* 可扩展的右下角悬浮 */}
         {floatRightBottom}
     </div>
