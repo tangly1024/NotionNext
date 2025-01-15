4d0a492d8126da60ebe7ea9bb90b3adfeb5a574d
tylin23 <tingyulin.w@gmail.com>
2025-01-15 22:14:20 +0800

    Update font.config.js

--- conf/font.config.js
+++ conf/font.config.js
@@ -6,12 +6,12 @@ module.exports = {
   // START ************网站字体*****************
   // ['font-serif','font-sans'] 两种可选，分别是衬线和无衬线: 参考 https://www.jianshu.com/p/55e410bd2115
   // 后面空格隔开的font-light的字体粗细，留空是默认粗细；参考 https://www.tailwindcss.cn/docs/font-weight
-  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-regular',
+  FONT_STYLE: process.env.NEXT_PUBLIC_FONT_STYLE || 'font-sans font-normal',
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
     //'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@400&display=swap',
-    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+    //'https://fonts.googleapis.com/css?family=Bitter&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+TC:wght@300&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+TC:wght@300&display=swap'
   ],
