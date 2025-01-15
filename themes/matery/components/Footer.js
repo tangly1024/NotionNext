6372ce344f76c8f826c1104171a10537bf777561
tylin23 <tingyulin.w@gmail.com>
2025-01-15 17:52:21 +0800

    Update Footer.js
    change indigo to gray

--- themes/matery/components/Footer.js
+++ themes/matery/components/Footer.js
@@ -15,7 +15,7 @@ const Footer = ({ title }) => {
   })()
 
   return (
-    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-indigo-700 text-gray-300 justify-center text-center m-auto w-full leading-6  dark:text-gray-100 text-sm p-6'>
+    <footer className='relative z-10 dark:bg-black flex-shrink-0 bg-gray-700 text-gray-300 justify-center text-center m-auto w-full leading-6  dark:text-gray-100 text-sm p-6'>
       {/* <DarkModeButton/> */}
       <i className='fas fa-copyright' /> {`${copyrightDate}`}{' '}
       <span>
