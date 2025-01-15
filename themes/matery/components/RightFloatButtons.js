d13ed544700daf5f7ea5b2f5192bdcb62606f362
tylin23 <tingyulin.w@gmail.com>
2025-01-15 17:36:03 +0800

    Update RightFloatButtons.js

--- themes/matery/components/RightFloatButtons.js
+++ themes/matery/components/RightFloatButtons.js
@@ -6,13 +6,14 @@ import SocialButton from './SocialButton'
  * 右下角悬浮按钮
  * @param {*} param0
  * @returns
+ *<SocialButton />
  */
 export default function RightFloatButtons(props) {
   const { floatRightBottom } = props
   return <div className="bottom-40 right-2 fixed justify-end space-y-2 z-20">
         <FloatDarkModeButton />
         <JumpToTopButton />
-        /*<SocialButton /> */
+        
         {/* 可扩展的右下角悬浮 */}
         {floatRightBottom}
     </div>
