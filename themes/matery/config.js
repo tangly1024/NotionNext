84ba953c17fc88d1c8a7c9b9ba54703e4a23474f
tylin23 <tingyulin.w@gmail.com>
2025-01-15 17:12:48 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -26,7 +26,7 @@ const CONFIG = {
   MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
   MATERY_WIDGET_ANALYTICS: false, // 显示统计卡
   MATERY_WIDGET_TO_TOP: true,
-  MATERY_WIDGET_TO_COMMENT: true, // 跳到评论区
+  MATERY_WIDGET_TO_COMMENT: false, // 跳到评论区
   WIDGET_DARK_MODE: true, // 夜间模式
   MATERY_WIDGET_TOC: true // 移动端悬浮目录
 }
