b4d1e38c1369e5919fc58ddf65c7b30b14f6ad78
BarryZed <64117296+Barrryy@users.noreply.github.com>
2025-01-23 15:31:26 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'BarryZed', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '你好，这里是简介', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://space.bilibili.com/334597217', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'www.barryzed.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
