a2ea290d0ac94ab98e5bd05ea5bf8172e52f6cd8
BarryZed <64117296+Barrryy@users.noreply.github.com>
2025-01-24 09:27:22 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,9 +15,9 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'BarryZed', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '你好，这里是简介', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '专注于眼前的事务吧。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'www.barryzed.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '游戏,编程,个人', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    '欢迎来到我的博客🎉',
+    '我希望事情是本来的样子而不是我臆想的样子。,放下助人情节，尊重他人命运。,放下个人素质，享受缺德人生。,童话也是生活的一部分。,你和你喜欢的人一起坐过过山车吗？,欸？这狗怎么上来的？',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
