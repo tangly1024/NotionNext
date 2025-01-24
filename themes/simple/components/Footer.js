67a750a757f3051dbe32c9085b6eb85ae6791c8c
tangly1024.com <tlyong1992@hotmail.com>
2025-01-24 10:11:16 +0800

    simple-主题备案链接

--- themes/simple/components/Footer.js
+++ themes/simple/components/Footer.js
@@ -28,7 +28,7 @@ export default function Footer(props) {
           {/* <a href="#" className="text-black no-underline hover:underline">Privacy Policy</a> */}
           {siteConfig('BEI_AN') && (
             <a
-              href='https://beian.miit.gov.cn/'
+              href={siteConfig('BEI_AN_LINK')}
               className='no-underline hover:underline ml-4'>
               {siteConfig('BEI_AN')}
             </a>
