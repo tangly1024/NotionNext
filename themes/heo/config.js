180ec199616ac8641af75028e6b89422d7666b41
BarryZed <64117296+Barrryy@users.noreply.github.com>
2025-01-29 22:37:54 +0800

    heo的hero修改

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -30,7 +30,7 @@ const CONFIG = {
   HEO_HERO_TITLE_5: 'XXX',
   HEO_HERO_TITLE_LINK: 'https://www.barryzed.top',
   // 英雄区遮罩文字
-  HEO_HERO_COVER_TITLE: '随便逛逛？->',
+  HEO_HERO_COVER_TITLE: '随便逛逛？',
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '教程', url: '/tag/教程' },
@@ -40,7 +40,7 @@ const CONFIG = {
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: 'Latest',
   HEO_HERO_RECOMMEND_POST_SORT_BY_UPDATE_TIME: false, // 推荐文章排序，为`true`时将强制按最后修改时间倒序
-  //   HERO_RECOMMEND_COVER: 'https://cdn.pixabay.com/photo/2015/10/30/20/13/sunrise-1014712_1280.jpg', // 英雄区右侧图片
+  HERO_RECOMMEND_COVER: 'https://pic1.imgdb.cn/item/679a3cfbd0e0a243d4f879f9.webp', // 英雄区右侧图片
 
   // 右侧个人资料卡牌欢迎语，点击可自动切换
   HEO_INFOCARD_GREETINGS: [
@@ -60,7 +60,7 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fa-duotone fa-solid fa-chart-pie',
   HEO_INFO_CARD_URL2: 'https://space.bilibili.com/334597217',
   HEO_INFO_CARD_ICON2: 'fa-brands fa-bilibili',
-  HEO_INFO_CARD_URL3: 'https://space.bilibili.com/334597217',
+  HEO_INFO_CARD_URL3: 'https://www.barryzed.top/article/about',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
