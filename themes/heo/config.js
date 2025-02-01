acf05f3cfdfb636cdc31f5c50f031bbf477f117b
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-02-02 05:02:23 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -53,8 +53,6 @@ const CONFIG = {
   HEO_INFO_CARD_ICON1: 'fas fa-user',
   HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
-  HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
   HEO_GROUP_ICONS: [
