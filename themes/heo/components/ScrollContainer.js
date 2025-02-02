8088b9edb06ea01b94414f21a38f95eb785fba32
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-02-03 07:54:36 +0800

    Update ScrollContainer.js

--- themes/heo/components/ScrollContainer.js
+++ themes/heo/components/ScrollContainer.js
@@ -1,32 +1,36 @@
 // components/ScrollContainer.js
 import React, { useRef, useEffect } from 'react';
 
-const ScrollContainer = ({ children, maxHeight }) => {
-  const scrollRef = useRef(null);
+const ScrollContainer = ({ children, maxHeight, className }) => {
+    const scrollRef = useRef(null);
 
-
-     useEffect(() => {
+    useEffect(() => {
         if (scrollRef.current) {
             const scrollContainer = scrollRef.current;
-               const handleWheel = (e) => {
-                     scrollContainer.scrollTop += e.deltaY;
+
+             const handleWheel = (e) => {
+                 scrollContainer.scrollTop += e.deltaY;
                 };
-           scrollContainer.addEventListener('wheel', handleWheel);
+            scrollContainer.addEventListener('wheel', handleWheel);
 
-               return () => {
-                    if(scrollContainer){
-                      scrollContainer.removeEventListener('wheel', handleWheel);
-                  }
-           };
+             return () => {
+               if(scrollContainer){
+                 scrollContainer.removeEventListener('wheel', handleWheel);
+                }
+             };
         }
-        }, []);
+    }, []);
 
 
-  return (
-      <div className="overflow-y-auto scroll-smooth" style={{maxHeight}} ref={scrollRef}>
-          {children}
-      </div>
-  );
+    return (
+        <div
+            className={`overflow-y-auto scroll-smooth ${className || ''}`}
+            style={{ maxHeight }}
+            ref={scrollRef}
+        >
+            {children}
+        </div>
+    );
 };
 
 export default ScrollContainer;
