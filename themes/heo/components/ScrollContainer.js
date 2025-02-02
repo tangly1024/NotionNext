2286470754cae1a58aee0f746dc87e0b89eee7c1
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2025-02-03 07:46:33 +0800

    Add files via upload

---
+++ themes/heo/components/ScrollContainer.js
@@ -0,0 +1,32 @@
+// components/ScrollContainer.js
+import React, { useRef, useEffect } from 'react';
+
+const ScrollContainer = ({ children, maxHeight }) => {
+  const scrollRef = useRef(null);
+
+
+     useEffect(() => {
+        if (scrollRef.current) {
+            const scrollContainer = scrollRef.current;
+               const handleWheel = (e) => {
+                     scrollContainer.scrollTop += e.deltaY;
+                };
+           scrollContainer.addEventListener('wheel', handleWheel);
+
+               return () => {
+                    if(scrollContainer){
+                      scrollContainer.removeEventListener('wheel', handleWheel);
+                  }
+           };
+        }
+        }, []);
+
+
+  return (
+      <div className="overflow-y-auto scroll-smooth" style={{maxHeight}} ref={scrollRef}>
+          {children}
+      </div>
+  );
+};
+
+export default ScrollContainer;
