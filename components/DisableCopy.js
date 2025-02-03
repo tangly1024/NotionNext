576541dffef4f0f8920c811198e377867af7f369
BarryZed <64117296+Barrryy@users.noreply.github.com>
2025-02-03 21:17:37 +0800

    禁止复制提示框信息修改

--- components/DisableCopy.js
+++ components/DisableCopy.js
@@ -12,7 +12,7 @@ export default function DisableCopy() {
       // 监听复制事件
       document.addEventListener('copy', function (event) {
         event.preventDefault() // 阻止默认复制行为
-        alert('抱歉，本网页内容不可复制捏，想要分享的话请分享网址吧 :)')
+        alert('抱歉，本网页内容不可复制捏，想要分享的话请分享网址吧 :)\n如需复制代码，请使用代码块右上角的复制按钮（若无复制按钮请刷新页面）。')
       })
     }
   }, [])
