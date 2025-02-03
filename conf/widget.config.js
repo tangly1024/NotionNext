c3fff9bab948e7b4f11307b07dc87bc826111589
hht2009 <85049252+hht2009@users.noreply.github.com>
2025-02-03 22:22:48 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -37,11 +37,11 @@ module.exports = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
-      name: '风を共に舞う気持ち',
-      artist: 'Falcom Sound Team jdk',
-      url: 'https://music.163.com/song/media/outer/url?id=731419.mp3',
+      name: 'Rubia',
+      artist: '周深',
+      url: 'https://music.163.com/outchain/player?type=2&id=1815684465',
       cover:
-        'https://p2.music.126.net/kn6ugISTonvqJh3LHLaPtQ==/599233837187278.jpg'
+        'http://p2.music.126.net/499AYZ3epP1T7GJYvnJZZQ==/109951165671947083.jpg?param=130y130'
     },
     {
       name: '王都グランセル',
