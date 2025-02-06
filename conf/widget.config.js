ab43be837f397c95d938141da6602da9fea8452d
hht2009 <85049252+hht2009@users.noreply.github.com>
2025-02-06 15:27:30 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -51,6 +51,14 @@ module.exports = {
         'http://p2.music.126.net/jKutIHhQ2lNADGNm4bePTg==/109951170413164832.jpg?param=130y130'
     }
   ],
+  {
+      name: '我怀念的(Live)',
+      artist: '孙燕姿',
+      url: 'https://music.163.com/song/media/outer/url?id=399354381.mp3',
+      cover:
+        'http://p1.music.126.net/hIOFATG-3vNyYHOj31Tyew==/109951169829283327.jpg?param=130y130'
+    }
+  ],
   MUSIC_PLAYER_METING: process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING || false, // 是否要开启 MetingJS，从平台获取歌单。会覆盖自定义的 MUSIC_PLAYER_AUDIO_LIST，更多配置信息：https://github.com/metowolf/MetingJS
   MUSIC_PLAYER_METING_SERVER:
     process.env.NEXT_PUBLIC_MUSIC_PLAYER_METING_SERVER || 'netease', // 音乐平台，[netease, tencent, kugou, xiami, baidu]
