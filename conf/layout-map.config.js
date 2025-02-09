a2643af89f2f2ca2afc9f290388da6deaa4603d4
HumanMus1c <1714076628@qq.com>
2025-02-09 20:30:46 +0800

    Update layout-map.config.js

--- conf/layout-map.config.js
+++ conf/layout-map.config.js
@@ -1,3 +1,8 @@
+import { SpeedInsights } from "@vercel/speed-insights/next" //2024年2月9日 加入Vercel的Speed Insights，用于优化网站加载速度
+import { Analytics } from "@vercel/analytics/react" //2024年2月9日 加入Vercel的Analytics，用于统计网站访问数据
+
+
+
 /**
  * 路径和组件映射，不同路径分别展示主题的什么组件
  * 可在添加新的路径和对应主题下的布局名称
