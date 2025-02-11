87bffc13c2ed9a9c77cfbe99b17cdf840b33216b
爱良 <130814915+Leafbritain@users.noreply.github.com>
2025-02-12 00:40:01 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -14,8 +14,8 @@ const BLOG = {
   APPEARANCE: process.env.NEXT_PUBLIC_APPEARANCE || 'dark', // ['light', 'dark', 'auto'], // light 日间模式 ， dark夜间模式， auto根据时间和主题自动夜间模式
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '你找到了我', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '而我也找到了你。', // 作者简介
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '我找到了你', // 您的昵称 例如 tangly1024
+  BIO: process.env.NEXT_PUBLIC_BIO || '而你也找到了我。', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://koimoyou.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '爱的狂信徒', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
