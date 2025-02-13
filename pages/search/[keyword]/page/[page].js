7e648454762e9d1a9c0f464658653b659d955754
anime <street-anime-olive@duck.com>
2025-02-13 18:58:35 +0800

    refactor(allPublishedPosts): 不太明确的allPosts重命名为allPublishedPosts

--- pages/search/[keyword]/page/[page].js
+++ pages/search/[keyword]/page/[page].js
@@ -24,10 +24,10 @@ export async function getStaticProps({ params: { keyword, page }, locale }) {
     locale
   })
   const { allPages } = props
-  const allPosts = allPages?.filter(
+  const allPublishedPosts = allPages?.filter(
     page => page.type === 'Post' && page.status === 'Published'
   )
-  props.posts = await filterByMemCache(allPosts, keyword)
+  props.posts = await filterByMemCache(allPublishedPosts, keyword)
   props.postCount = props.posts.length
   const POSTS_PER_PAGE = siteConfig('POSTS_PER_PAGE', 12, props?.NOTION_CONFIG)
   // 处理分页
@@ -103,16 +103,16 @@ const isIterable = obj =>
 
 /**
  * 在内存缓存中进行全文索引
- * @param {*} allPosts
+ * @param {*} allPublishedPosts
  * @param keyword 关键词
  * @returns
  */
-async function filterByMemCache(allPosts, keyword) {
+async function filterByMemCache(allPublishedPosts, keyword) {
   const filterPosts = []
   if (keyword) {
     keyword = keyword.trim()
   }
-  for (const post of allPosts) {
+  for (const post of allPublishedPosts) {
     const cacheKey = 'page_block_' + post.id
     const page = await getDataFromCache(cacheKey, true)
     const tagContent =
