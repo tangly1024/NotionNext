ec17dd5cb1a9a2b15195c6ca5f4e9657887fda0b
dreameutopia <13077995107@163.com>
2025-02-13 16:13:49 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -30,7 +30,7 @@ const Footer = () => {
         id='footer-bottom'
         className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
         <div id='footer-bottom-left' className='text-center lg:text-start'>
-          <PoweredBy />
+        
           <div className='flex gap-x-1'>
             <CopyRightDate />
             <a
