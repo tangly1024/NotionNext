aeec37363d853993e4bc80180b38d06a9002c7dc
BarryZed <64117296+Barrryy@users.noreply.github.com>
2025-02-15 01:12:14 +0800

    hero左侧卡片字体颜色修改

--- themes/heo/components/Hero.js
+++ themes/heo/components/Hero.js
@@ -84,12 +84,12 @@ function Banner(props) {
       <div
         id='banner-title'
         className='z-10 flex flex-col absolute top-10 left-10'>
-        <div className='text-4xl font-bold mb-3 text-orange-500 dark:text-orange-300'>
+        <div className='text-4xl font-bold mb-3 text-orange-300 dark:text-orange-500'>
           {siteConfig('HEO_HERO_TITLE_1', null, CONFIG)}
           <br />
           {siteConfig('HEO_HERO_TITLE_2', null, CONFIG)}
         </div>
-        <div className='text-xs text-gray-600  dark:text-gray-200'>
+        <div className='text-xs font-bold text-gray-600  dark:text-gray-200'>
           {siteConfig('HEO_HERO_TITLE_3', null, CONFIG)}
         </div>
       </div>
