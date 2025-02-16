d5b56c0cd2eb8dd46dc9cdb170f643b69af809d7
breezedeus <breezedeus@163.com>
2025-02-16 17:38:51 +0800

    不展示banner image

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,5 +1,5 @@
 const CONFIG = {
-  HEXO_HOME_BANNER_ENABLE: true,
+  HEXO_HOME_BANNER_ENABLE: false,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
   HEXO_HOME_BANNER_GREETINGS: [
     'Hi，我是一个程序员',
