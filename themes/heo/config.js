4b5b0958d1a42bfe3746b31a100df9bf83fe5ee8
LazyPig <152492591+Bersulang@users.noreply.github.com>
2025-02-17 09:36:30 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -21,8 +21,8 @@ const CONFIG = {
   HEO_HERO_TITLE_1: '分享编程',
   HEO_HERO_TITLE_2: '与思维认知',
   HEO_HERO_TITLE_3: 'lazypigzyh.top',
-  HEO_HERO_TITLE_4: '2025！',
-  HEO_HERO_TITLE_5: '祝愿大家心想事成！',
+  HEO_HERO_TITLE_4: '新版上线！',
+  HEO_HERO_TITLE_5: '更多精彩敬请期待！',
   HEO_HERO_TITLE_LINK: 'https://www.lazypigzyh.top/',
   // 英雄区遮罩文字
   HEO_HERO_COVER_TITLE: '随便逛逛',
