38efba9a34710339cd6daf0506c90af34ee03548
awovkj <147908510+awovkj@users.noreply.github.com>
2025-02-23 14:23:55 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '𝓢𝓪𝓷𝔂𝓮', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://sanye.us.kg', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://sanye.me', // 网站地址
   KEYWORDS: [process.env.NEXT_PUBLIC_KEYWORD || 'Blog', 'ctf', '网络安全'], // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
