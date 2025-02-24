2998ffdd00ac2c9c23802641ccaece7248ab4a4e
lostiv <30612717+lostiv@users.noreply.github.com>
2025-02-24 13:49:08 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024/NotionNext" target="_blank"',
+  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024/NotionNext',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.notion.so/" target="_blank"',
+  HEO_INFO_CARD_URL3: 'https://www.notion.so',
   HEO_INFO_CARD_TEXT3: '编辑blog',
 
   // 用户技能图标
