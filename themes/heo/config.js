bd8a42a455f0ed396a9743cd15922acdcfce95dc
ningwenyan <ningyanke@outlook.com>
2025-02-27 14:28:12 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://blog.tangly1024.com' },
-    { title: '访问文档中心获取更多帮助', url: 'https://docs.tangly1024.com' }
+    { title: '欢迎来到我的博客', url: 'https://note.kwenyan.link' },
+    { title: '访问文档中心获取更多帮助', url: 'https://note.kwenyan.link/help' }
   ],
 
   // 英雄区左右侧组件颠倒位置
