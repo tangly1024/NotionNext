4fe045d1246adf09cef1505547fb683690aa0615
juxiu <iguolintao@163.com>
2025-03-02 11:47:41 +0800

    处理文章内站内外链接&logo加圆角,notion-link样式

--- themes/juxiu/components/Logo.js
+++ themes/juxiu/components/Logo.js
@@ -13,7 +13,7 @@ const Logo = props => {
           width={24}
           height={24}
           alt={siteConfig('AUTHOR')}
-          className='mr-4 hidden md:block'
+          className='mr-4 hidden md:block rounded-md'
         />
         <div id='logo-text' className='group rounded-2xl flex-none relative'>
           <div className='logo group-hover:opacity-0 opacity-100 visible group-hover:invisible text-lg my-auto rounded dark:border-white duration-200'>
