59bd61912c90bb709d55a563e73f797b8ec8a317
ct188579 <149464263+ct188579@users.noreply.github.com>
2025-03-09 14:46:59 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,8 +26,8 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'TAO', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '专注于前端开发', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'http://ctblog.top', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'ctblog, 陈涛', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'http://nextdaddy.cn', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'nextdaddy, 陈涛', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'ct188579@gmail.com', // 邮箱地址 例如mail@tangly1024.com
