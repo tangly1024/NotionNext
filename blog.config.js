d6b7c35f5d07e8ac696c2669ee662c58e761b600
hht2009 <85049252+hht2009@users.noreply.github.com>
2025-03-20 19:40:39 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   
     AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '黄昊霆', // 您的昵称 例如 tangly1024
     BIO: process.env.NEXT_PUBLIC_BIO || '一个普通的干饭人🍚', // 作者简介
-    LINK: process.env.NEXT_PUBLIC_LINK || 'https://hhthht.cn/', // 网站地址
+    LINK: process.env.NEXT_PUBLIC_LINK || 'https://hhtb.cn/', // 网站地址
     KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,黄昊霆,黄昊霆的博客', // 网站关键词 英文逗号隔开
   
     // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
