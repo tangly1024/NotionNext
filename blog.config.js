80dd6150dd75f285d1bff047c0e05551f5e15be5
worldeternitycommunity <157990106+worldeternitycommunity@users.noreply.github.com>
2025-03-19 21:21:52 -0400

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -20,7 +20,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '永恒社区 - World Eternity Community', // 您的昵称
-  BIO: process.env.NEXT_PUBLIC_BIO || '个人OC世界观企划', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '原创OC世界观企划', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || '', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '永恒社区，World Eternity Community', // 网站关键词 英文逗号隔开
 
