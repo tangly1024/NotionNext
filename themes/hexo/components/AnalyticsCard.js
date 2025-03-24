2b20f7a65ffad41ee11f5ec2e31e92ddbd5bed92
M24代言人VC <104404784+M24VC@users.noreply.github.com>
2025-03-24 21:00:52 +0800

    Update AnalyticsCard.js

--- themes/hexo/components/AnalyticsCard.js
+++ themes/hexo/components/AnalyticsCard.js
@@ -4,24 +4,24 @@ export function AnalyticsCard (props) {
   const { postCount } = props
   return <Card>
     <div className='ml-2 mb-3 '>
-      <i className='fas fa-chart-area' /> 统计
+      <i className='fas fa-chart-area' /> 統計
     </div>
     <div className='text-xs  font-light justify-center mx-7'>
       <div className='inline'>
         <div className='flex justify-between'>
-          <div>文章数:</div>
+          <div>文章數:</div>
           <div>{postCount}</div>
         </div>
       </div>
       <div className='hidden busuanzi_container_page_pv ml-2'>
         <div className='flex justify-between'>
-          <div>访问量:</div>
+          <div>訪問量:</div>
           <div className='busuanzi_value_page_pv' />
         </div>
       </div>
       <div className='hidden busuanzi_container_site_uv ml-2'>
         <div className='flex justify-between'>
-          <div>访客数:</div>
+          <div>訪客數:</div>
           <div className='busuanzi_value_site_uv' />
         </div>
       </div>
