22e89a470db0d264afcefd5b41b69151150f1ca5
lostiv <30612717+lostiv@users.noreply.github.com>
2025-03-27 08:13:44 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -60,58 +60,58 @@ const CONFIG = {
   HEO_GROUP_ICONS: [
     {
       title_1: 'AfterEffect',
-      img_1: 'https://vip.123pan.cn/1819096162/yk6baz03t0m000d6xujocx53c5gafsanDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/20239df3f66615b532ce571eac6d14ff21cf072602.webp',
       color_1: '#989bf8',
       title_2: 'Sketch',
-      img_2: 'https://vip.123pan.cn/1819096162/yk6baz03t0l000d6xujntyokoyo10ticDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/2023e0ded7b724a39f12d59c3dc8fbdc7cbe074202.webp',
       color_2: '#ffffff'
     },
     {
       title_1: 'Docker',
-      img_1: 'https://vip.123pan.cn/1819096162/ymjew503t0l000d6xujz7k5uarlq0ezmDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/20231108a540b2862d26f8850172e4ea58ed075102.webp',
       color_1: '#57b6e6',
       title_2: 'Photoshop',
-      img_2: 'https://vip.123pan.cn/1819096162/yk6baz03t0n000d6xujp01viibvwrtymDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/2023e4058a91608ea41751c4f102b131f267075902.webp',
       color_2: '#4082c3'
     },
     {
       title_1: 'FinalCutPro',
-      img_1: 'https://vip.123pan.cn/1819096162/yk6baz03t0l000d6xujntyol6uo20w1jDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/20233e777652412247dd57fd9b48cf997c01070702.webp',
       color_1: '#ffffff',
       title_2: 'Python',
-      img_2: 'https://vip.123pan.cn/1819096162/ymjew503t0m000d6xujzns2aspg83hswDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/20235c0731cd4c0c95fc136a8db961fdf963071502.webp',
       color_2: '#ffffff'
     },
     {
       title_1: 'Swift',
-      img_1: 'https://vip.123pan.cn/1819096162/yk6baz03t0m000d6xujocx545egagy7nDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/202328bbee0b314297917b327df4a704db5c072402.webp',
       color_1: '#eb6840',
       title_2: 'Principle',
-      img_2: 'https://vip.123pan.cn/1819096162/ymjew503t0n000d6xuk03n6ya2vuebmyDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/2023f76570d2770c8e84801f7e107cd911b5073202.webp',
       color_2: '#8f55ba'
     },
     {
       title_1: 'illustrator',
-      img_1: 'https://vip.123pan.cn/1819096162/ymjew503t0l000d6xujz7k5tv1lp0rhkDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/20237359d71b45ab77829cee5972e36f8c30073902.webp',
       color_1: '#f29e39',
       title_2: 'CSS3',
-      img_2: 'https://vip.123pan.cn/1819096162/ymjew503t0m000d6xujzns2abgg825xfDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/20237c548846044a20dad68a13c0f0e1502f074602.webp',
       color_2: '#2c51db'
     },
     {
       title_1: 'JS',
-      img_1: 'https://vip.123pan.cn/1819096162/ymjew503t0n000d6xuk03n6yppvufyz9DIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/2023786e7fc488f453d5fb2be760c96185c0075502.webp',
       color_1: '#f7cb4f',
       title_2: 'HTML',
-      img_2: 'https://vip.123pan.cn/1819096162/yk6baz03t0n000d6xujp01vjbwvwsybaDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/202372b4d760fd8a497d442140c295655426070302.webp',
       color_2: '#e9572b'
     },
     {
       title_1: 'Git',
-      img_1: 'https://vip.123pan.cn/1819096162/ymjew503t0m000d6xujzns29vng81ubiDIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_1: '/images/heo/2023ffa5707c4e25b6beb3e6a3d286ede4c6071102.webp',
       color_1: '#df5b40',
       title_2: 'Rhino',
-      img_2: 'https://vip.123pan.cn/1819096162/ymjew503t0n000d6xuk03n6z5wvugb09DIYPBIa2AqezDpxPDwJOAO==.webp',
+      img_2: '/images/heo/20231ca53fa0b09a3ff1df89acd7515e9516173302.webp',
       color_2: '#1f1f1f'
     }
   ],
