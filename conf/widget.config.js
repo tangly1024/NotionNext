b7a4751c4a863e698eac8542eae2a378bd2a69e8
爱良 <130814915+Leafbritain@users.noreply.github.com>
2025-04-04 10:10:02 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -37,6 +37,12 @@ module.exports = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
+      name: '清醒梦',
+      artist: '在虚无中永存',
+      url: 'http://music.163.com/song/media/outer/url?id=2688242045.mp3',
+      cover: 'https://i.postimg.cc/gcKQkbPT/IMG-8489.png'
+    },
+    {
       name: 'ほのかに暗い日曜日',
       artist: 'Takashi Waraya',
       url: 'https://dlink.host/musics/aHR0cHM6Ly9vbmVkcnYtbXkuc2hhcmVwb2ludC5jb20vOnU6L2cvcGVyc29uYWwvc3Rvcl9vbmVkcnZfb25taWNyb3NvZnRfY29tL0VlWUxXWExGZ1A1Q29sU0h3YW5VbWZzQkhRV2FkU3BqSWR1aTRpV3hzR2kxeXc.mp3',
