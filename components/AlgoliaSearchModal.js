36b08ef9a88fda59af07ca6016866aeb2eb543f3
tylin23 <tingyulin.w@gmail.com>
2025-04-25 04:13:20 +0800

    Update AlgoliaSearchModal.js

--- components/AlgoliaSearchModal.js
+++ components/AlgoliaSearchModal.js
@@ -254,7 +254,7 @@ export default function AlgoliaSearchModal({ cRef }) {
         } flex flex-col justify-between w-full min-h-[10rem] h-full md:h-fit max-w-xl dark:bg-hexo-black-gray dark:border-gray-800 bg-white dark:bg- p-5 rounded-lg z-50 shadow border hover:border-blue-600 duration-300 transition-all `}>
         <div className='flex justify-between items-center'>
           <div className='text-2xl text-blue-600 dark:text-yellow-600 font-bold'>
-            搜索
+            搜尋
           </div>
           <div>
             <i
