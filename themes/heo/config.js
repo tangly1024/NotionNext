856cad866ebbbca3e0c144e810784b2c879f6c72
EternityMar22 <18595122+EternityMar22@users.noreply.github.com>
2025-04-24 14:13:24 +0800

    临时性修补

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -9,7 +9,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.985864.xyz/' },
-    { title: '访问文档中心获取更多帮助', url: 'https://blog.985864.xyz/' }
+    { title: '新建博客正在施工，评论系统即将上线', url: 'https://blog.985864.xyz/' }
   ],
 
   // 英雄区左右侧组件颠倒位置
