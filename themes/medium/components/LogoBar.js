928e4568f0a84a2af24f446e4b0283ec843ab966
kahosense <107840660+kahosense@users.noreply.github.com>
2025-04-28 16:29:35 +0800

    Update LogoBar.js

--- themes/medium/components/LogoBar.js
+++ themes/medium/components/LogoBar.js
@@ -2,10 +2,21 @@ import { siteConfig } from '@/lib/config'
 import Link from 'next/link'
 
 export default function LogoBar(props) {
+  const avatar = siteConfig('AVATAR') // 取出头像URL
+  const title = siteConfig('TITLE')   // 取出标题
+
   return (
-    <div id='top-wrapper' className='w-full flex items-center '>
-      <Link href='/' className='logo text-md md:text-xl dark:text-gray-200'>
-        {siteConfig('TITLE')}
+    <div id='top-wrapper' className='w-full flex items-center space-x-4'>
+      <Link href='/' className='logo flex items-center text-md md:text-xl dark:text-gray-200'>
+        {/* 如果有头像，则显示 */}
+        {avatar && (
+          <img
+            src={avatar}
+            alt='logo'
+            className='w-10 h-10 rounded-full mr-2'
+          />
+        )}
+        <span>{title}</span>
       </Link>
     </div>
   )
