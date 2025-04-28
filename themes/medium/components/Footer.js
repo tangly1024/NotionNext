e5c6ededb88aa10568e9250a5930cc9b90698a64
kahosense <107840660+kahosense@users.noreply.github.com>
2025-04-28 16:14:08 +0800

    Update Footer.js

--- themes/medium/components/Footer.js
+++ themes/medium/components/Footer.js
@@ -41,15 +41,7 @@ const Footer = ({ title }) => {
         </span>
         <br />
         <h1>{title}</h1>
-        <span className='text-xs font-serif'>
-          Powered by
-          <a
-            href='https://github.com/tangly1024/NotionNext'
-            className='underline text-gray-500 dark:text-gray-300'>
-            NotionNext {siteConfig('VERSION')}
-          </a>
-          .
-        </span>
+
       </span>
     </footer>
   )
