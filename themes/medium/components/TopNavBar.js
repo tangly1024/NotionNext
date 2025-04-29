237cef450448c598559c037bc719b3e5c7295694
kahosense <107840660+kahosense@users.noreply.github.com>
2025-04-29 11:25:49 +0800

    Update TopNavBar.js

--- themes/medium/components/TopNavBar.js
+++ themes/medium/components/TopNavBar.js
@@ -69,52 +69,52 @@ const isACategoryPage = router.asPath.startsWith('/category/Neurodivergent') // 
 
  return (
   <>
-    {!isACategoryPage && (
-      <div
-        id='top-nav'
-        className={'sticky top-0 lg:relative w-full z-40 ' + className}>
-        {/* 移动端折叠菜单 */}
-        <Collapse
-          type='vertical'
-          collapseRef={collapseRef}
-          isOpen={isOpen}
-          className='md:hidden'>
-          <div className='bg-white dark:bg-hexo-black-gray pt-1 py-2 lg:hidden '>
-            <MenuBarMobile
-              {...props}
-              onHeightChange={param =>
-                collapseRef.current?.updateCollapseHeight(param)
-              }
-            />
+    <div
+      id='top-nav'
+      className={'sticky top-0 lg:relative w-full z-40 ' + className}>
+      {/* 移动端折叠菜单 */}
+      <Collapse
+        type='vertical'
+        collapseRef={collapseRef}
+        isOpen={isOpen}
+        className='md:hidden'>
+        <div className='bg-white dark:bg-hexo-black-gray pt-1 py-2 lg:hidden '>
+          <MenuBarMobile
+            {...props}
+            onHeightChange={param =>
+              collapseRef.current?.updateCollapseHeight(param)
+            }
+          />
+        </div>
+      </Collapse>
+
+      {/* 导航栏菜单 */}
+      <div className='flex w-full h-20 border-b bg-white dark:bg-hexo-black-gray px-7 items-between'>
+        {/* 左侧图标Logo */}
+        <LogoBar {...props} />
+
+        {/* 折叠按钮、仅移动端显示 */}
+        <div className='mr-1 flex md:hidden justify-end items-center text-sm space-x-4 font-serif dark:text-gray-200'>
+          <div onClick={toggleMenuOpen} className='cursor-pointer'>
+            {isOpen ? (
+              <i className='fas fa-times' />
+            ) : (
+              <i className='fas fa-bars' />
+            )}
           </div>
-        </Collapse>
+        </div>
 
-        {/* 导航栏菜单 */}
-        <div className='flex w-full h-20 border-b bg-white dark:bg-hexo-black-gray px-7 items-between'>
-          {/* 左侧图标Logo */}
-          <LogoBar {...props} />
-
-          {/* 折叠按钮、仅移动端显示 */}
-          <div className='mr-1 flex md:hidden justify-end items-center text-sm space-x-4 font-serif dark:text-gray-200'>
-            <div onClick={toggleMenuOpen} className='cursor-pointer'>
-              {isOpen ? (
-                <i className='fas fa-times' />
-              ) : (
-                <i className='fas fa-bars' />
-              )}
-            </div>
-          </div>
-
-          {/* 桌面端顶部菜单 */}
+        {/* 桌面端顶部菜单 */}
+        {!isACategoryPage && ( // ✅ 只判断右侧菜单，不影响上面LogoBar
           <div className='hidden md:flex'>
             {links &&
               links?.map((link, index) => (
                 <MenuItemDrop key={index} link={link} />
               ))}
           </div>
-        </div>
+        )}
       </div>
-    )}
+    </div>
   </>
 )
 }
