67a66b2d4075e2ac443b798c89688c0553c1453c
kahosense <107840660+kahosense@users.noreply.github.com>
2025-04-29 11:45:47 +0800

    Update CategoryItem.js

--- themes/medium/components/CategoryItem.js
+++ themes/medium/components/CategoryItem.js
@@ -10,7 +10,7 @@ export default function CategoryItem ({ selected, category, categoryCount }) {
         : 'dark:text-green-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-green-600') +
       ' flex text-sm items-center duration-300 cursor-pointer py-1 font-light px-2 whitespace-nowrap'}>
 
-      <div><i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} />{category} {categoryCount && `(${categoryCount})`}
+      <div>{/* <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} /> */}{category} {categoryCount && `(${categoryCount})`}
       </div>
 
     </Link>
