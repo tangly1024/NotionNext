18ed546cc59b756f9e8825b3e80e1a7480b580e8
kahosense <616540728@qq.com>
2025-05-05 16:09:30 +0800

    💬 更新了

--- themes/medium/components/BlogPostBar.js
+++ themes/medium/components/BlogPostBar.js
@@ -11,19 +11,30 @@ export default function BlogPostBar(props) {
 
   if (tag) {
     return (
-      <div className='flex items-center text-xl py-8'>
-        <i className='mr-2 fas fa-tag' />
-        {locale.COMMON.TAGS}:{tag}
-      </div>
+
+      <div className="px-4">
+         
+         <span className="inline-flex items-center border border-gray-200 rounded-md px-3 py-1 bg-white text-gray-400 text-base font-light mb-4">
+         <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-1" viewBox="0 0 24 24" fill="rgba(100,205,138,1)"><path d="M7.78428 14L8.2047 10H4V8H8.41491L8.94043 3H10.9514L10.4259 8H14.4149L14.9404 3H16.9514L16.4259 8H20V10H16.2157L15.7953 14H20V16H15.5851L15.0596 21H13.0486L13.5741 16H9.58509L9.05957 21H7.04855L7.57407 16H4V14H7.78428ZM9.7953 14H13.7843L14.2047 10H10.2157L9.7953 14Z"></path></svg>
+         
+         
+         {/* {locale.COMMON.TAGS}:*/} {tag}
+          </span>
+    </div>
+
     )
   } else if (category) {
     return (
-      <div className='flex items-center text-xl py-8'>
-        <i className='mr-2 fas fa-th' />
-        {locale.COMMON.CATEGORY}:{category}
-      </div>
+      <div className="px-4">
+            <span className="inline-flex items-center border border-gray-200 rounded-md px-3 py-1 bg-white text-gray-400 text-base font-light mb-4">
+              <svg xmlns="http://www.w3.org/2000/svg" className="w-4 h-4 mr-1" viewBox="0 0 24 24" fill="rgba(100,205,138,1)"><path d="M12.9998 3L12.9996 10.267L19.294 6.63397L20.294 8.36602L14.0006 11.999L20.294 15.634L19.294 17.366L12.9996 13.732L12.9998 21H10.9998L10.9996 13.732L4.70557 17.366L3.70557 15.634L9.99857 12L3.70557 8.36602L4.70557 6.63397L10.9996 10.267L10.9998 3H12.9998Z"></path></svg>
+              {/* {locale.COMMON.CATEGORY}: */} {category}
+            </span>
+          </div>
     )
   } else {
     return <></>
   }
 }
+
+
--- themes/medium/components/CategoryItem.js
+++ themes/medium/components/CategoryItem.js
@@ -9,9 +9,8 @@ export default function CategoryItem ({ selected, category, categoryCount }) {
         ? 'hover:text-white dark:hover:text-white bg-green-600 text-white '
         : 'dark:text-green-400 text-gray-500 hover:text-white dark:hover:text-white hover:bg-green-600') +
       ' flex text-sm items-center duration-300 cursor-pointer py-1 font-light px-2 whitespace-nowrap'}>
-
-      <div>{/* <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} /> */}{category} {categoryCount && `(${categoryCount})`}
-      </div>
+      {/* <i className={`mr-2 fas ${selected ? 'fa-folder-open' : 'fa-folder'}`} /> */} <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="10" height="12" fill="rgba(213,221,222,1)"><path d="M12.9998 3L12.9996 10.267L19.294 6.63397L20.294 8.36602L14.0006 11.999L20.294 15.634L19.294 17.366L12.9996 13.732L12.9998 21H10.9998L10.9996 13.732L4.70557 17.366L3.70557 15.634L9.99857 12L3.70557 8.36602L4.70557 6.63397L10.9996 10.267L10.9998 3H12.9998Z"></path></svg>  {category} {categoryCount && `(${categoryCount})`}
+      
 
     </Link>
   );
