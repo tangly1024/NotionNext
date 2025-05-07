12c7f442fed24e27b069fd6dbaf283c8a6085ab1
kahosense <616540728@qq.com>
2025-05-07 19:03:01 +0800

    💬 更新了

--- themes/medium/components/LogoBar.js
+++ themes/medium/components/LogoBar.js
@@ -17,7 +17,7 @@ export default function LogoBar(props) {
     )}
     <div className="flex flex-col">
       <span className="text-xl md:text-3xl text-gray-800 dark:text-gray-200">{title}</span>
-      <span className="text-[11px] text-gray-300 dark:text-gray-400">Louis, quietly ongoing</span>
+      <span className="text-[11px] text-gray-300 dark:text-gray-400">Louis, slice of life</span>
     </div>
   </Link>
 </div>
