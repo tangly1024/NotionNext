bf02e9f31eaa37d10cd9b35d1e5c40056fbcc09c
adamwonghui <120273512+adamwonghui@users.noreply.github.com>
2025-05-17 13:14:46 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -15,7 +15,7 @@ const BLOG = {
   APPEARANCE_DARK_TIME: process.env.NEXT_PUBLIC_APPEARANCE_DARK_TIME || [18, 6], // 夜间模式起至时间，false时关闭根据时间自动切换夜间模式
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'AdamWong', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个热爱技术的IT人🍚', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个虔诚的AGI信仰者🙏', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://greetgic.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
