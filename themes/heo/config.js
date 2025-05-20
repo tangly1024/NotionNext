c8049cf0bc8594a463b00753f5aab62665cbdf90
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-05-20 17:27:47 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -8,8 +8,8 @@ const CONFIG = {
 
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   HEO_NOTICE_BAR: [
-    { title: '欢迎来到我的博客', url: 'https://www.chalice.lol/' },
-    { title: '🐱打赏点击此处喵~', url: 'https://www.chalice.lol/' }
+    { title: '欢迎来到我的博客', url: 'https://www.chalice.lol' },
+    { title: '🐱打赏点击此处喵~', url: 'https://www.chalice.lol' }
   ],
 
   // 英雄区左右侧组件颠倒位置
