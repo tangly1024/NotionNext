46e06b57dffa4c7b4463c042c051a5bf5fc84f18
CC <101262665+Byteinn@users.noreply.github.com>
2025-05-26 21:21:12 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -17,7 +17,7 @@ const BLOG = {
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'CC', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '技术博主🍚', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://juejin.cn/user/1086748304878360', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,cc,曹志', // 网站关键词 英文逗号隔开
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,cc,曹志,javascript,前端,promise,JS,js,ts', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
