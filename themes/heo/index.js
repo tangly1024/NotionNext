626eeeaa963e1fc91420c420456fce9ee53eaf14
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-06-04 14:50:08 +0800

    Update index.js

--- themes/heo/index.js
+++ themes/heo/index.js
@@ -74,9 +74,20 @@ const LayoutBase = props => {
   )
 
   // 右侧栏 用户信息+标签列表
-  const slotRight =
-    router.route === '/404' || fullWidth ? null : <SideRight {...props} />
 
+ const slotRight =
+  router.route === '/404' || fullWidth ? null : (
+    <>
+      <SideRight {...props} />
+      <img 
+        width="400" 
+        height="140" 
+        src="https://card.yuy1n.io/card/76561198314348996/dark,en,badge,group"
+        alt="Steam Profile Card"
+        className="mt-4"
+      />
+    </>
+  )
   const maxWidth = fullWidth ? 'max-w-[96rem] mx-auto' : 'max-w-[86rem]' // 普通最大宽度是86rem和顶部菜单栏对齐，留空则与窗口对齐
 
   const HEO_HERO_BODY_REVERSE = siteConfig(
