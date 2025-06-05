2a770d6fcdbbc9407af00e93ecef012e674e09d4
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-06-05 13:58:01 +0800

    Update SideRight.js

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -57,19 +57,7 @@ export default function SideRight(props) {
           <LatestPostsGroupMini {...props} />
         </div>
 
-        {rightAreaSlot}
-             className={
-          'mt-4 border wow fadeInUp hover:border-indigo-600 dark:hover:border-yellow-600 duration-200 dark:border-gray-700 dark:bg-[#1e1e1e] dark:text-white rounded-xl lg:p-6 p-4 bg-white'
-        }>
-            <img
-            src="https://card.yuy1n.io/card/76561198314348996/dark,en,badge,group"
-            alt="Steam Profile Card"
-            className="w-full rounded-lg"
-            />
-            </div>
-
-        <FaceBookPage />
-       <Live2D />
+    
 
         {/* 标签和成绩 */}
         <Card
