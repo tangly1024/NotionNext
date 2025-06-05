3e3190c8c094803035f850c2c17bf5d75c9c111c
forsakens0ul <52342354+forsakens0ul@users.noreply.github.com>
2025-06-05 11:27:03 +0800

    Update SideRight.js

--- themes/heo/components/SideRight.js
+++ themes/heo/components/SideRight.js
@@ -69,8 +69,7 @@ export default function SideRight(props) {
             </div>
 
         <FaceBookPage />
-        <Live2D />
-               <Live2D />
+       
 
         {/* 标签和成绩 */}
         <Card
