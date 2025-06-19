2ece68711fc5b9fdf96f74822117cdc6d8399b73
rinxiaoxiao <33726100+rinxiaoxiao@users.noreply.github.com>
2025-06-20 07:47:17 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -63,6 +63,7 @@ const BLOG = {
   // 字体CSS 例如 https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
+    'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
     'https://fonts.googleapis.com/css?family=Bitter&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
