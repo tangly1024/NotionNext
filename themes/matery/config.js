9a0000ebd4943f0f5e5fc3131086dcda4c2060b2
TY <tingyulin.w@gmail.com>
2025-07-23 19:23:19 +0800

    Update config.js

--- themes/matery/config.js
+++ themes/matery/config.js
@@ -1,7 +1,12 @@
 const CONFIG = {
   MATERY_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
-  MATERY_HOME_BANNER_GREETINGS: ['Hi，我是一个程序员', 'Hi，我是一个打工人', 'Hi，我是一个干饭人', '欢迎来到我的博客🎉'], // 首页大图标语文字
+  MATERY_HOME_BANNER_GREETINGS: [
+    'Hi，我是一个程序员',
+    'Hi，我是一个打工人',
+    'Hi，我是一个干饭人',
+    '欢迎来到我的博客🎉'
+  ], // 首页大图标语文字
 
   MATERY_HOME_NAV_BUTTONS: true, // 首页是否显示分类大图标按钮
   MATERY_HOME_NAV_BACKGROUND_IMG_FIXED: false, // 首页背景图滚动时是否固定，true 则滚动时图片不懂； false则随鼠标滚动
@@ -21,6 +26,7 @@ const CONFIG = {
 
   MATERY_ARTICLE_ADJACENT: true, // 显示上一篇下一篇文章推荐
   MATERY_ARTICLE_COPYRIGHT: true, // 显示文章版权声明
+  MATERY_ARTICLE_NOT_BY_AI: false, // 显示非AI写作
   MATERY_ARTICLE_RECOMMEND: true, // 文章关联推荐
 
   MATERY_WIDGET_LATEST_POSTS: true, // 显示最新文章卡
