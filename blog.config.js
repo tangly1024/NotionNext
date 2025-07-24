9e1a41f8c130cb2f240cacaf4592d38a7f22aca3
Luoye <arvinwang007@gmail.com>
2025-07-24 16:36:16 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -62,7 +62,7 @@ const BLOG = {
   // 欢迎语打字效果,Hexo,Matery主题支持, 英文逗号隔开多个欢迎语。
   GREETING_WORDS:
     process.env.NEXT_PUBLIC_GREETING_WORDS ||
-    'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
+    '强哥把爬墙的能力开放出来给大家用，因为经常有朋友和前同事找过来咨询怎么看外面世界，问的多了，就想着写一些教程教大家怎么做，于是就有了这个网站🎉🎉',
 
   // uuid重定向至 slug
   UUID_REDIRECT: process.env.UUID_REDIRECT || false
