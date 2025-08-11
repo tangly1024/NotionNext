fd06eb66e6d18b916c64ca38a7fc55ead606afe1
jdsai0 <109773735+jdsai0@users.noreply.github.com>
2025-08-11 08:54:31 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '土豆', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '哦！土豆！', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://kantv.eu.cc', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://next-new-orcin.vercel.app/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '哦!土豆！', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
