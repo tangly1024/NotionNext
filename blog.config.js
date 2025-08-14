5fd76b01520c0fd89c8436f303e1fad6f373ac89
ct188579 <149464263+ct188579@users.noreply.github.com>
2025-08-14 13:34:21 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,10 +24,10 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'TAO', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Chen TAO', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '专注于前端开发', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'http://nextdaddy.cn', // 网站地址
-  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'nextdaddy, 陈涛', // 网站关键词 英文逗号隔开
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://nextdaddy.cn', // 网站地址
+  KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '陈涛,nextdaddy,daddy,blog,programming,博客,陈涛博客,陈涛的博客,编程', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || 'ct188579@gmail.com', // 邮箱地址 例如mail@tangly1024.com
