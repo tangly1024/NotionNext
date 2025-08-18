8df088974022804092d35ea504b658518005973c
ThereChenSnow <127078617+ThereChenSnow@users.noreply.github.com>
2025-08-19 05:25:29 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -65,7 +65,7 @@ const BLOG = {
     'Hi，我是一个程序员, Hi，我是一个打工人,Hi，我是一个干饭人,欢迎来到我的博客🎉',
 
   // uuid重定向至 slug
-  UUID_REDIRECT: process.env.UUID_REDIRECT || false
+  UUID_REDIRECT: process.env.UUID_REDIRECT || true
 }
 
 module.exports = BLOG
