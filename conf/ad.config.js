45d2d6c7d4e8a404c7dfae925cddef14cbcb6c81
kenchikuliu <44759890+kenchikuliu@users.noreply.github.com>
2025-08-18 20:57:44 +0800

    SEO优化: 添加Google Analytics和Google AdSense配置

--- conf/ad.config.js
+++ conf/ad.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   // 谷歌广告
-  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || '', // 谷歌广告ID e.g ca-pub-xxxxxxxxxxxxxxxx
+  ADSENSE_GOOGLE_ID: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_ID || 'ca-pub-xxxxxxxxxxxxxxxx', // 谷歌广告ID，请替换为您的发布商ID
   ADSENSE_GOOGLE_TEST: process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_TEST || false, // 谷歌广告ID测试模式，这种模式获取假的测试广告，用于开发 https://www.tangly1024.com/article/local-dev-google-adsense
   ADSENSE_GOOGLE_SLOT_IN_ARTICLE:
     process.env.NEXT_PUBLIC_ADSENSE_GOOGLE_SLOT_IN_ARTICLE || '3806269138', // Google AdScene>广告>按单元广告>新建文章内嵌广告 粘贴html代码中的data-ad-slot值
