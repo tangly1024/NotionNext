1bab3d10de382ed718b14852754d7418469bc1af
zzx <1181097247@qq.com>
2025-08-19 15:29:59 +0800

    修改github地址

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -51,9 +51,9 @@ const CONFIG = {
   // 个人资料底部按钮
   HEO_INFO_CARD_URL1: '/about',
   HEO_INFO_CARD_ICON1: 'fas fa-user',
-  HEO_INFO_CARD_URL2: 'https://github.com/tangly1024',
+  HEO_INFO_CARD_URL2: 'https://github.com/ashlandy79/AshBlog',
   HEO_INFO_CARD_ICON2: 'fab fa-github',
-  HEO_INFO_CARD_URL3: 'https://www.tangly1024.com',
+  HEO_INFO_CARD_URL3: 'https://github.com/ashlandy79/AshBlog',
   HEO_INFO_CARD_TEXT3: '了解更多',
 
   // 用户技能图标
