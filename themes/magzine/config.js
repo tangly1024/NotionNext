e1025c68592d7e566b2cac2aa91c6de4946f6437
Gzjtxxjd <zjtxxjd@gmail.com>
2025-09-04 21:04:23 +0800

    Update config.js
    去除文章内容

--- themes/magzine/config.js
+++ themes/magzine/config.js
@@ -5,12 +5,12 @@ const CONFIG = {
   MAGZINE_HOME_BUTTON_URL: '/about',
   MAGZINE_HOME_BUTTON_TEXT: '了解更多',
 
-  MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
+  // MAGZINE_HOME_HIDDEN_CATEGORY: '分享杂文', //不希望在首页展示的文章分类，用英文逗号隔开
 
-  MAGZINE_HOME_TITLE: '立即开创您的在线业务。完全免费。',
-  MAGZINE_HOME_DESCRIPTION:
-    '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
-  MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
+  // MAGZINE_HOME_TITLE: '立即开创您的在线业务。完全免费。',
+  // MAGZINE_HOME_DESCRIPTION:
+  //   '借助NotionNext，获得助您开创、经营和扩展业务所需的全部工具和帮助。',
+  // MAGZINE_HOME_TIPS: 'AI时代来临，这是属于超级个体的狂欢盛宴！',
 
   // 首页底部推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   // MAGZINE_RECOMMEND_POST_TAG: '推荐',
@@ -21,13 +21,13 @@ const CONFIG = {
   // Style
   MAGZINE_RIGHT_PANEL_DARK: process.env.NEXT_PUBLIC_MAGZINE_RIGHT_DARK || false, // 右侧面板深色模式
 
-  MAGZINE_POST_LIST_COVER: true, // 文章列表显示图片封面
-  MAGZINE_POST_LIST_PREVIEW: true, // 列表显示文章预览
-  MAGZINE_POST_LIST_CATEGORY: true, // 列表显示文章分类
-  MAGZINE_POST_LIST_TAG: true, // 列表显示文章标签
+  // MAGZINE_POST_LIST_COVER: true, // 文章列表显示图片封面
+  // MAGZINE_POST_LIST_PREVIEW: true, // 列表显示文章预览
+  // MAGZINE_POST_LIST_CATEGORY: true, // 列表显示文章分类
+  // MAGZINE_POST_LIST_TAG: true, // 列表显示文章标签
 
-  MAGZINE_POST_DETAIL_CATEGORY: true, // 文章显示分类
-  MAGZINE_POST_DETAIL_TAG: true, // 文章显示标签
+  // MAGZINE_POST_DETAIL_CATEGORY: true, // 文章显示分类
+  // MAGZINE_POST_DETAIL_TAG: true, // 文章显示标签
 
   // 文章页面联系卡
   // MAGZINE_SOCIAL_CARD: true, // 是否显示右侧，点击加入社群按钮
