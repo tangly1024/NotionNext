4c5fd71d0ce0c522985a6198c4bd54b3af2a5627
hht2009 <85049252+hht2009@users.noreply.github.com>
2025-09-06 18:22:00 +0800

    Update PoweredBy.js

--- components/PoweredBy.js
+++ components/PoweredBy.js
@@ -6,20 +6,14 @@ import { siteConfig } from '@/lib/config'
  */
 export default function PoweredBy(props) {
   return (
-    <div className={`inline font-serif ${props.className || ''}`}>
-      <span className='mr-1 text-yellow-300 text-xs'>Powered by</span>
+    <div className={`inline text-sm font-serif ${props.className || ''}`}>
+      <span className='mr-1'>Powered by</span>
       <a
         href='https://github.com/tangly1024/NotionNext'
-        className='justify-start text-yellow-300 text-xs hover:underline'
-      >
+        className='underline justify-start'>
         NotionNext {siteConfig('VERSION')}
       </a>
       .
-      {/* 新增 IPv6 支持提示，完全匹配hhtb.cn样式 */}
-      <span className="ml-4 text-yellow-300 text-xs">
-        本网站已支持IPv6访问！
-      </span>
     </div>
   )
 }
-    
