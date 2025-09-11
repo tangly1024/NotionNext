85ab2c9ac0f4b8ac57f2112e1f3e4630b8aefecb
zzx <1181097247@qq.com>
2025-09-11 17:00:39 +0800

    英雄区显示三个置顶分类修改

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -29,8 +29,8 @@ const CONFIG = {
 
   // 英雄区显示三个置顶分类
   HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/推荐' },
-  HEO_HERO_CATEGORY_2: { title: '技术分享', url: '/tag/技术分享' },
-  HEO_HERO_CATEGORY_3: { title: '靠谱教程', url: '/tag/靠谱教程' },
+  HEO_HERO_CATEGORY_2: { title: '技术分享', url: '/category/技术分享' },
+  HEO_HERO_CATEGORY_3: { title: '靠谱教程', url: '/category/靠谱教程' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '推荐',
