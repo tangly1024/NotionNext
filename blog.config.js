78d95b4ca3b325aaae6a0983bbeadca1400f54e9
suola <1411619413@qq.com>
2025-09-18 18:02:50 +0800

    修改了网站地址

--- blog.config.js
+++ blog.config.js
@@ -16,7 +16,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Suola', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '荔枝成为计算机领域大佬', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://suolaWithLaffy.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://suolawithlaffy.com', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || '野生的博客', // 网站关键词 英文逗号隔开
   BLOG_FAVICON: process.env.NEXT_PUBLIC_FAVICON || '/favicon.ico', // blog favicon 配置, 默认使用 /public/favicon.ico，支持在线图片，如 https://img.imesong.com/favicon.png
   BEI_AN: process.env.NEXT_PUBLIC_BEI_AN || '', // 备案号 闽ICP备XXXXXX
