36b7c6c086d248d54efbab8fdf9538015a2d232c
suola <1411619413@qq.com>
2025-09-19 23:13:13 +0800

    修改了指定分类，待填充

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -28,9 +28,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '学习日记', url: '/tag/dailylearingdiary' },
-  HEO_HERO_CATEGORY_2: { title: '', url: '/tag/2' },
-  HEO_HERO_CATEGORY_3: { title: '', url: '/tag/3' },
+  HEO_HERO_CATEGORY_1: { title: '', url: '' },
+  HEO_HERO_CATEGORY_2: { title: '', url: '' },
+  HEO_HERO_CATEGORY_3: { title: '', url: '' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
