ce085bc7f5c1f39f6581c5244c44a7df8e6ec227
leolin <6leolin6@gmail.com>
2025-09-22 22:31:43 +0800

    update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,3 +1,7 @@
+/*
+ * @Author: linlei
+ * @Date: 2025-09-22 22:24:37
+ */
 // 注: process.env.XX是Vercel的环境变量，配置方式见：https://docs.tangly1024.com/article/how-to-config-notion-next#c4768010ae7d44609b744e79e2f9959a
 
 const BLOG = {
