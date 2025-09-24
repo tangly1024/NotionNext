3480119132bc9bb4f374eccb88fe2a6fa6633d01
Eric JI <8057221@qq.com>
2025-09-24 16:30:51 +0800

    Update comment.config.js

--- conf/comment.config.js
+++ conf/comment.config.js
@@ -16,11 +16,13 @@ module.exports = {
     'https://cdnjs.cloudflare.com/ajax/libs/artalk/2.5.5/Artalk.css', // ArtalkServert css cdn
 
 // twikoo
-COMMENT_TWIKOO_ENV_ID: process.env.NEXT_PUBLIC_COMMENT_ENV_ID || 'cloud1-9gt33iged738e5f4', // TWIKOO后端地址 腾讯云环境填envId；Vercel环境填域名
-COMMENT_TWIKOO_COUNT_ENABLE: process.env.NEXT_PUBLIC_COMMENT_TWIKOO_COUNT_ENABLE || false, // 博客列表是否显示评论数
-COMMENT_TWIKOO_REGION: process.env.NEXT_PUBLIC_TWIKOO_REGION || 'ap-shanghai',
-COMMENT_TWIKOO_CDN_URL: process.env.NEXT_PUBLIC_COMMENT_TWIKOO_CDN_URL || 'https://cdn.jsdelivr.net/npm/twikoo@1.6.44/dist/twikoo.all.min.js', // twikoo客户端cdn
-
+  COMMENT_TWIKOO_ENV_ID: process.env.NEXT_PUBLIC_COMMENT_ENV_ID || '', // TWIKOO后端地址 腾讯云环境填envId；Vercel环境填域名，教程：https://tangly1024.com/article/notionnext-twikoo
+  COMMENT_TWIKOO_COUNT_ENABLE:
+    process.env.NEXT_PUBLIC_COMMENT_TWIKOO_COUNT_ENABLE || false, // 博客列表是否显示评论数
+  COMMENT_TWIKOO_CDN_URL:
+    process.env.NEXT_PUBLIC_COMMENT_TWIKOO_CDN_URL ||
+    'https://s4.zstatic.net/npm/twikoo@1.6.44/dist/twikoo.min.js', // twikoo客户端cdn
+  
   // utterance
   COMMENT_UTTERRANCES_REPO:
     process.env.NEXT_PUBLIC_COMMENT_UTTERRANCES_REPO || '', // 你的代码仓库名， 例如我是 'tangly1024/NotionNext'； 更多文档参考 https://utteranc.es/
