500d8521ae356fb7f4ea84266dde6196e1221461
stone3 <54737414+Linlei-dev@users.noreply.github.com>
2025-09-25 16:04:20 +0800

    Update Coze.js
    add auth

--- components/Coze.js
+++ components/Coze.js
@@ -24,6 +24,9 @@ export default function Coze() {
         },
         componentProps: {
           title: title
+        } ,
+        auth: {
+          type: 'token'
         }
       })
       console.log('coze', cozeClient)
