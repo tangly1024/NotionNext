e04ce7e67a201afff4d13b942ec018174ee4fd16
typegod <42210735+typegod@users.noreply.github.com>
2025-09-25 18:42:37 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -26,7 +26,7 @@ const BLOG = {
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Kingsley', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '半导体器件工程师', // 作者简介
-  LINK: process.env.NEXT_PUBLIC_LINK || 'https://tangly1024.com', // 网站地址
+  LINK: process.env.NEXT_PUBLIC_LINK || 'https://garlan.top', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
