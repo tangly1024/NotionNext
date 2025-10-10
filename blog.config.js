d7692b36d88d7b808e9d5a25d1416ae95f78c266
xiaoliyue <147219463+xiaoliyue@users.noreply.github.com>
2025-10-10 18:27:12 +0800

    Update blog.config.js
    update font-awesome cdn

--- blog.config.js
+++ blog.config.js
@@ -88,7 +88,7 @@ const BLOG = {
     '"Apple Color Emoji"',
     'SimSun',
   ],
-  FONT_AWESOME: process.env.NEXT_PUBLIC_FONT_AWESOME_PATH || 'https://cdn.bootcss.com/font-awesome/6.4.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
+  FONT_AWESOME: process.env.NEXT_PUBLIC_FONT_AWESOME_PATH || 'https://cdn.bootcdn.net/ajax/libs/font-awesome/6.4.0/css/all.min.css', // font-awesome 字体图标地址; 可选 /css/all.min.css ， https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-M/font-awesome/6.0.0/css/all.min.css
 
   // END ************网站字体*****************
 
