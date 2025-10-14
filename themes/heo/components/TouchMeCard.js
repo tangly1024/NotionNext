395a94d0b4259817c77314e1b3d032f2ac9f1783
kenchikuliu <44759890+kenchikuliu@users.noreply.github.com>
2025-10-14 16:29:01 +0800

    Add multi-language support to TouchMeCard component
    - Add useGlobal hook to get current locale
    - Add translations for zh-CN, en, and en-US
    - Replace hardcoded Chinese text with locale-specific translations
    - Contact Channel / Communication Channel now displays in appropriate language

--- themes/heo/components/TouchMeCard.js
+++ themes/heo/components/TouchMeCard.js
@@ -1,6 +1,7 @@
 import FlipCard from '@/components/FlipCard'
 import { siteConfig } from '@/lib/config'
 import SmartLink from '@/components/SmartLink'
+import { useGlobal } from '@/lib/global'
 import CONFIG from '../config'
 
 /**
@@ -8,9 +9,33 @@ import CONFIG from '../config'
  * @returns
  */
 export default function TouchMeCard() {
+  const { locale } = useGlobal()
+
   if (!JSON.parse(siteConfig('HEO_SOCIAL_CARD', null, CONFIG))) {
     return <></>
   }
+
+  // 多语言文本配置
+  const translations = {
+    'zh-CN': {
+      title1: '交流频道',
+      title2: '添加微信，加入微信群讨论',
+      title3: '点击扫码添加微信'
+    },
+    'en': {
+      title1: 'Contact Channel',
+      title2: 'Add WeChat to join group discussion',
+      title3: 'Click to scan QR code'
+    },
+    'en-US': {
+      title1: 'Contact Channel',
+      title2: 'Add WeChat to join group discussion',
+      title3: 'Click to scan QR code'
+    }
+  }
+
+  const currentLang = translations[locale] || translations['zh-CN']
+
   return (
     <div className={'relative h-28 text-white flex flex-col'}>
       <FlipCard
@@ -18,10 +43,10 @@ export default function TouchMeCard() {
         frontContent={
           <div className='h-full'>
             <h2 className='font-[1000] text-3xl'>
-              {siteConfig('HEO_SOCIAL_CARD_TITLE_1', null, CONFIG)}
+              {currentLang.title1}
             </h2>
             <h3 className='pt-2'>
-              {siteConfig('HEO_SOCIAL_CARD_TITLE_2', null, CONFIG)}
+              {currentLang.title2}
             </h3>
             <div
               className='absolute left-0 top-0 w-full h-full'
@@ -34,7 +59,7 @@ export default function TouchMeCard() {
         backContent={
           <SmartLink href={siteConfig('HEO_SOCIAL_CARD_URL', null, CONFIG)}>
             <div className='font-[1000] text-xl h-full'>
-              {siteConfig('HEO_SOCIAL_CARD_TITLE_3', null, CONFIG)}
+              {currentLang.title3}
             </div>
           </SmartLink>
         }
