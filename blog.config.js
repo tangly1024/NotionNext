5663ef1b3449c4366902e40a91359ed26ef0bf26
VinkoYang <125409865+VinkoYang@users.noreply.github.com>
2025-12-03 22:24:22 +0000

    changes theme

--- blog.config.js
+++ blog.config.js
@@ -5,7 +5,7 @@ const BLOG = {
   // Important page_id！！！Duplicate Template from  https://tanghh.notion.site/02ab3b8678004aa69e9e415905ef32a5
   NOTION_PAGE_ID:
     process.env.NOTION_PAGE_ID ||
-    '02ab3b8678004aa69e9e415905ef32a5,en:7c1d570661754c8fbc568e00a01fd70e',
+    '2aa107292510819f9a99cc8aee54b999,en:7c1d570661754c8fbc568e00a01fd70e', //02ab3b8678004aa69e9e415905ef32a5
   THEME: process.env.NEXT_PUBLIC_THEME || 'aurora', // 当前主题，在themes文件夹下可找到所有支持的主题；主题名称就是文件夹名，例如 example,fukasawa,gitbook,heo,hexo,landing,matery,medium,next,nobelium,plog,simple
   LANG: process.env.NEXT_PUBLIC_LANG || 'en-US', // e.g 'zh-CN','en-US'  see /lib/lang.js for more.
   SINCE: process.env.NEXT_PUBLIC_SINCE || 2025, // e.g if leave this empty, current year will be used.
