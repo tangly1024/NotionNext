c7dc49697e293ba76ca20b925aa6479182016253
Ricky Wang <91827325+luotianyi233@users.noreply.github.com>
2026-01-24 18:20:42 -0500

    Comment out footer components
    Comment out the CopyRightDate and PoweredBy components in the footer.

--- themes/proxio/components/Footer.js
+++ themes/proxio/components/Footer.js
@@ -71,8 +71,8 @@ export const Footer = ({ title }) => {
         {/* 页脚 */}
         <div className='dark:text-gray-200 py-4 flex flex-col lg:flex-row  justify-between items-center border-t border-gray-600'>
           <div className='flex gap-x-2 flex-wrap justify-between items-center'>
-          <CopyRightDate />
-          <PoweredBy />
+          </*CopyRightDate /*/>
+          </*PoweredBy /*/>
           </div>
 
           <DarkModeButton className='dark:text-white' />
