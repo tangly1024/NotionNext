868863141d9a8a5ee31e6762ec029d2373c498a6
lyizhuo <lyizhuooo@gmail.com>
2026-02-18 22:01:47 +0800

    优化nobelium主题

--- themes/nobelium/components/Catalog.js
+++ themes/nobelium/components/Catalog.js
@@ -60,8 +60,8 @@ const Catalog = ({ toc }) => {
   }
 
   return (
-    <div className='hidden lg:block absolute right-0 top-0 -mr-96 h-full'>
-      <div className='px-3 sticky top-32'>
+    <div className='hidden lg:block absolute right-0 top-0 -mr-64 h-full'>
+      <div className='pl-3 pr-28 sticky top-32'>
         <div
           className='pl-10 mt-32 overflow-y-auto max-w-96  max-h-96 overscroll-none scroll-hidden'
           ref={tRef}>
