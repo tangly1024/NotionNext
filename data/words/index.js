00dfdf50252f26ba2375b7d4e9ba3419db34a5dc
Hurt6465-ai <hurt6465@gmail.com>
2026-03-02 22:27:22 +0630

    Update index.js

--- data/words/index.js
+++ data/words/index.js
@@ -1,5 +1,12 @@
 export const wordDataMap = {
-  health: () => import('./health').then((m) => m.default),
-  travel: () => import('./travel').then((m) => m.default),
-  hsk: () => import('./hsk').then((m) => m.default),
+  // health
+  'health/hospital': () => import('./health/hospital').then((m) => m.default),
+  'health/treatment': () => import('./health/treatment').then((m) => m.default),
+
+  // travel
+  'travel/airport': () => import('./travel/airport').then((m) => m.default),
+  'travel/hotel': () => import('./travel/hotel').then((m) => m.default),
+
+  // hsk
+  'hsk/hsk1': () => import('./hsk/hsk1').then((m) => m.default),
 };
