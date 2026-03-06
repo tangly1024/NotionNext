154482100063cdc1d78227a5cb9b2a000eb28fac
Hurt6465-ai <hurt6465@gmail.com>
2026-03-06 18:32:50 +0630

    Create index.js

---
+++ data/oral/index.js
@@ -0,0 +1,12 @@
+export const oralDataMap = {
+  'daily/greeting': () => import('./daily/greeting.json').then((m) => m.default),
+  'daily/thanks': () => import('./daily/thanks.json').then((m) => m.default),
+  'daily/selfintro': () => import('./daily/selfintro.json').then((m) => m.default),
+
+  'travel/airport': () => import('./travel/airport.json').then((m) => m.default),
+  'travel/hotel': () => import('./travel/hotel.json').then((m) => m.default),
+  'travel/direction': () => import('./travel/direction.json').then((m) => m.default),
+
+  'medical/registration': () => import('./medical/registration.json').then((m) => m.default),
+  'medical/symptoms': () => import('./medical/symptoms.json').then((m) => m.default),
+};
