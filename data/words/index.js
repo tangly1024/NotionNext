d5ab7510e041da3efb3506f1cbeb7cdf34c41e1e
Hurt6465-ai <hurt6465@gmail.com>
2026-03-08 06:46:40 +0630

    Update index.js

--- data/words/index.js
+++ data/words/index.js
@@ -1,4 +1,6 @@
 export const wordDataMap = {
+ // health
+  'jcjj/rcdc': () => import('./jcjj/rcdc.json').then((m) => m.default),
   // hsk
   'hsk/hsk1': () => import('./hsk/hsk1.json').then((m) => m.default),
 };
