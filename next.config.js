3f974dc1db0cc3648fbde35b5f4b575195e1534a
Hurt6465-ai <hurt6465@gmail.com>
2026-03-14 11:43:21 +0630

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -112,7 +112,7 @@ const nextConfig = {
     : {
         defaultLocale: BLOG.LANG,
         // 支持的所有多语言,按需填写即可
-        locales: locales
+        locales: locales,
         localeDetection: false
       },
   images: {
