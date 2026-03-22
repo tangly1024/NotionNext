1bf641a78ffd3228c7ac4e9ac31cd6b0f9fd77e1
你好呀 <431761794@qq.com>
2026-03-22 20:31:08 +0800

    test

--- themes/heo/components/MenuItemDrop.js
+++ themes/heo/components/MenuItemDrop.js
@@ -16,8 +16,9 @@ export const MenuItemDrop = ({ link }) => {
 
   return (
     <div
-      onMouseOver={() => changeShow(true)}
-      onMouseOut={() => changeShow(false)}>
+      className="relative"
+      onMouseEnter={() => changeShow(true)}
+      onMouseLeave={() => changeShow(false)}>
       {/* 不含子菜单 */}
       {!hasSubMenu && (
         <>
@@ -27,7 +28,7 @@ export const MenuItemDrop = ({ link }) => {
               href={link?.href}
               target="_blank"
               rel="noopener noreferrer"
-              className="rounded-full flex justify-center items-center px-3 py-1 no-underline tracking-widest hover:bg-gradient-to-r from-cyan-400/20 to-blue-500/20 dark:hover:from-transparent dark:hover:to-transparent dark:hover:bg-purple-600/25 hover:shadow-lg transform"
+              className="rounded-full flex justify-center items-center px-3 py-1 no-underline tracking-widest hover:bg-gradient-to-r from-cyan-400/20 to-blue-500/20 dark:hover:from-transparent dark:hover:to-transparent dark:hover:bg-purple-900 hover:shadow-lg transform"
             >
               {link?.icon && <i className={link?.icon} />} {link?.name}
             </a>
@@ -35,7 +36,7 @@ export const MenuItemDrop = ({ link }) => {
             // 内部链接 - 使用 Next.js 的 Link 组件
             <SmartLink
               href={link?.href}
-              className="rounded-full flex justify-center items-center px-3 py-1 no-underline tracking-widest hover:bg-gradient-to-r from-cyan-400/20 to-blue-500/20 dark:hover:from-transparent dark:hover:to-transparent dark:hover:bg-purple-600/25 hover:shadow-lg transform"
+              className="rounded-full flex justify-center items-center px-3 py-1 no-underline tracking-widest hover:bg-gradient-to-r from-cyan-400/20 to-blue-500/20 dark:hover:from-transparent dark:hover:to-transparent dark:hover:bg-purple-900 hover:shadow-lg transform"
             >
               {link?.icon && <i className={link?.icon} />} {link?.name}
             </SmartLink>
@@ -45,7 +46,7 @@ export const MenuItemDrop = ({ link }) => {
       {/* 含子菜单的按钮 */}
       {hasSubMenu && (
         <>
-          <div className="cursor-pointer rounded-full flex justify-center items-center px-3 py-1 no-underline tracking-widest relative hover:bg-gradient-to-r from-cyan-400/20 to-blue-500/20 dark:hover:from-transparent dark:hover:to-transparent dark:hover:bg-purple-600/25 hover:shadow-lg transform">
+          <div className="cursor-pointer rounded-full flex justify-center items-center px-3 py-1 no-underline tracking-widest relative hover:bg-gradient-to-r from-cyan-400/20 to-blue-500/20 dark:hover:from-transparent dark:hover:to-transparent dark:hover:bg-purple-900 hover:shadow-lg transform">
             {link?.icon && <i className={link?.icon} />} {link?.name}
             {/* 主菜单下方的安全区域 */}
             {show && (
@@ -57,8 +58,7 @@ export const MenuItemDrop = ({ link }) => {
       {/* 子菜单 */}
       {hasSubMenu && (
         <ul
-          style={{ backdropFilter: 'blur(3px)' }}
-          className={`${show ? 'opacity-100 top-14 pointer-events-auto' : 'opacity-0 top-20 pointer-events-none'} shadow-md overflow-hidden rounded-3xl bg-white/70 backdrop-blur-xl ring-1 ring-white/60 ring-inset dark:bg-purple-600/20 transition-all duration-300 ease-in-out z-20 absolute`}>
+          className={`${show ? 'opacity-100 top-14 pointer-events-auto' : 'opacity-0 top-20 pointer-events-none'} shadow-md overflow-hidden rounded-3xl bg-white/95 ring-1 ring-black/5 ring-inset dark:bg-purple-900/95 dark:ring-purple-300/40 transition-all duration-300 ease-in-out z-20 absolute`}>
           {link.subMenus.map((sLink, index) => {
             return (
               <li
