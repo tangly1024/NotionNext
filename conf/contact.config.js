9f20543c32b2ce91d51f02ea45aa47c4d3b48068
Jasnode <112321869+Jasnode@users.noreply.github.com>
2026-03-26 17:16:59 +0800

    fix:修复邮箱解密失败错误
    Update contact email to encoded format

--- conf/contact.config.js
+++ conf/contact.config.js
@@ -8,7 +8,7 @@ module.exports = {
       btoa(
         unescape(encodeURIComponent(process.env.NEXT_PUBLIC_CONTACT_EMAIL))
       )) ||
-    '431761794@qq.com', // 邮箱地址 例如mail@tangly1024.com
+    'NDMxNzYxNzk0QHFxLmNvbQ==', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || '', // 你的微博个人主页
   CONTACT_TWITTER: process.env.NEXT_PUBLIC_CONTACT_TWITTER || '', // 你的twitter个人主页
   CONTACT_GITHUB: process.env.NEXT_PUBLIC_CONTACT_GITHUB || '', // 你的github个人主页 
