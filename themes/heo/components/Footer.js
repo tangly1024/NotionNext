9a908957adf8daae996284a6a7b45d3bc14fe436
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2026-04-13 07:51:22 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -3,6 +3,7 @@ import CopyRightDate from '@/components/CopyRightDate'
 import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
 import SocialButton from './SocialButton'
+
 /**
  * 页脚
  * @returns
@@ -11,59 +12,77 @@ const Footer = () => {
   const BEI_AN = siteConfig('BEI_AN')
   const BEI_AN_LINK = siteConfig('BEI_AN_LINK')
   const BIO = siteConfig('BIO')
+  const AUTHOR = siteConfig('AUTHOR')
+
   return (
-    <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
-      {/* 颜色过度区 */}
-      <div
-        id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
-      />
+    <footer className='relative flex-shrink-0 w-full bg-white dark:bg-[#1a191d]'>
+      {/* 顶部颜色过渡区：更加柔和的渐变 */}
+      <div className='h-24 w-full bg-gradient-to-b from-[#f7f9fe] to-transparent dark:from-[#151419] dark:to-transparent' />
 
-      {/* 社交按钮 */}
-      <div className='w-full h-24'>
-        <SocialButton />
-      </div>
-
-      <br />
-
-      {/* 底部页面信息 */}
-      <div
-        id='footer-bottom'
-        className='w-full h-20 flex flex-col p-3 lg:flex-row justify-between px-6 items-center bg-[#f1f3f7] dark:bg-[#21232A] border-t dark:border-t-[#3D3D3F]'>
-        <div id='footer-bottom-left' className='text-center lg:text-start'>
-          <PoweredBy />
-          <div className='flex gap-x-1'>
-            <CopyRightDate />
-            <a
-              href={'/about'}
-              className='underline font-semibold dark:text-gray-300 '>
-              {siteConfig('AUTHOR')}
-            </a>
-            {BIO && <span className='mx-1'> | {BIO}</span>}
-          </div>
+      <div className='max-w-6xl mx-auto px-6 pb-10 flex flex-col items-center'>
+        {/* 社交按钮区 */}
+        <div className='mb-8'>
+          <SocialButton />
         </div>
 
-        <div id='footer-bottom-right'>
-          {BEI_AN && (
-            <>
-              <i className='fas fa-shield-alt' />{' '}
-              <a href={BEI_AN_LINK} className='mr-2'>
-                {siteConfig('BEI_AN')}
+        {/* 分割线 */}
+        <div className='w-full border-t border-gray-100 dark:border-gray-800/50 mb-8' />
+
+        {/* 底部页面信息区 */}
+        <div className='w-full flex flex-col lg:flex-row justify-between items-center gap-6 text-sm text-gray-500 dark:text-gray-400'>
+          
+          {/* 左侧：版权与作者信息 */}
+          <div className='flex flex-col items-center lg:items-start gap-2'>
+            <div className='flex flex-wrap items-center justify-center lg:justify-start gap-1'>
+              <CopyRightDate />
+              <span className='mx-1'>©</span>
+              <a
+                href='/about'
+                className='font-medium text-gray-700 dark:text-gray-300 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-300'
+              >
+                {AUTHOR}
               </a>
-            </>
-          )}
-          <BeiAnGongAn />
+              {BIO && (
+                <>
+                  <span className='mx-1 text-gray-300 dark:text-gray-700'>|</span>
+                  <span>{BIO}</span>
+                </>
+              )}
+            </div>
+            <PoweredBy />
+          </div>
 
-          <span className='hidden busuanzi_container_site_pv'>
-            <i className='fas fa-eye' />
-            <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
-          </span>
-          <span className='pl-2 hidden busuanzi_container_site_uv'>
-            <i className='fas fa-users' />{' '}
-            <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
-          </span>
+          {/* 右侧：备案与统计信息 */}
+          <div className='flex flex-col items-center lg:items-end gap-2'>
+            {/* 备案信息 */}
+            <div className='flex flex-wrap items-center justify-center gap-3'>
+              {BEI_AN && (
+                <a
+                  href={BEI_AN_LINK || 'https://beian.miit.gov.cn/'}
+                  target='_blank'
+                  rel='noreferrer'
+                  className='flex items-center gap-1 hover:text-blue-500 dark:hover:text-blue-400 transition-colors duration-300'
+                >
+                  <i className='fas fa-shield-alt text-xs opacity-70' />
+                  <span>{BEI_AN}</span>
+                </a>
+              )}
+              <BeiAnGongAn />
+            </div>
 
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
+            {/* 访问量统计 (Busuanzi) */}
+            <div className='flex items-center gap-4 text-xs opacity-80'>
+              <span className='hidden busuanzi_container_site_pv flex items-center gap-1.5'>
+                <i className='fas fa-eye' />
+                <span className='busuanzi_value_site_pv'>--</span>
+              </span>
+              <span className='hidden busuanzi_container_site_uv flex items-center gap-1.5'>
+                <i className='fas fa-users' />
+                <span className='busuanzi_value_site_uv'>--</span>
+              </span>
+            </div>
+          </div>
+          
         </div>
       </div>
     </footer>
