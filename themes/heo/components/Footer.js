753e925d60afc038f85076405c034956ca0402cd
dxs1256 <23136671+dxs1256@users.noreply.github.com>
2026-04-13 09:46:15 +0800

    Update Footer.js

--- themes/heo/components/Footer.js
+++ themes/heo/components/Footer.js
@@ -1,30 +1,20 @@
-import { BeiAnGongAn } from '@/components/BeiAnGongAn'
 import CopyRightDate from '@/components/CopyRightDate'
 import PoweredBy from '@/components/PoweredBy'
 import { siteConfig } from '@/lib/config'
-import SocialButton from './SocialButton'
+
 /**
  * 页脚
  * @returns
  */
 const Footer = () => {
-  const BEI_AN = siteConfig('BEI_AN')
-  const BEI_AN_LINK = siteConfig('BEI_AN_LINK')
   const BIO = siteConfig('BIO')
   return (
     <footer className='relative flex-shrink-0 bg-white dark:bg-[#1a191d] justify-center text-center m-auto w-full leading-6  text-gray-600 dark:text-gray-100 text-sm'>
       {/* 颜色过度区 */}
       <div
         id='color-transition'
-        className='h-32 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
+        className='h-16 bg-gradient-to-b from-[#f7f9fe] to-white  dark:bg-[#1a191d] dark:from-inherit dark:to-inherit'
       />
-
-      {/* 社交按钮 */}
-      <div className='w-full h-24'>
-        <SocialButton />
-      </div>
-
-      <br />
 
       {/* 底部页面信息 */}
       <div
@@ -44,16 +34,7 @@ const Footer = () => {
         </div>
 
         <div id='footer-bottom-right'>
-          {BEI_AN && (
-            <>
-              <i className='fas fa-shield-alt' />{' '}
-              <a href={BEI_AN_LINK} className='mr-2'>
-                {siteConfig('BEI_AN')}
-              </a>
-            </>
-          )}
-          <BeiAnGongAn />
-
+          {/* 不蒜子访问统计 */}
           <span className='hidden busuanzi_container_site_pv'>
             <i className='fas fa-eye' />
             <span className='px-1 busuanzi_value_site_pv'> </span>{' '}
@@ -62,8 +43,6 @@ const Footer = () => {
             <i className='fas fa-users' />{' '}
             <span className='px-1 busuanzi_value_site_uv'> </span>{' '}
           </span>
-
-          {/* <h1 className='text-xs pt-4 text-light-400 dark:text-gray-400'>{title} {siteConfig('BIO') && <>|</>} {siteConfig('BIO')}</h1> */}
         </div>
       </div>
     </footer>
