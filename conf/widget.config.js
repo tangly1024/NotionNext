4c2a0ac6d932345ae9ad803f798f65b41f9431fa
爱良 <130814915+Leafbritain@users.noreply.github.com>
2026-04-16 12:03:34 +0800

    Update widget.config.js

--- conf/widget.config.js
+++ conf/widget.config.js
@@ -37,6 +37,12 @@ module.exports = {
   MUSIC_PLAYER_AUDIO_LIST: [
     // 示例音乐列表。除了以下配置外，还可配置歌词，具体配置项看此文档 https://aplayer.js.org/#/zh-Hans/
     {
+      name: '永恒天堂',
+      artist: '在虚无中永存',
+      url: 'http://music.163.com/song/media/outer/url?id=3368805321.mp3',
+      cover: 'https://i.postimg.cc/gcKQkbPT/IMG-8489.png'
+    },
+    {
       name: '春之飞行',
       artist: 'Terry Devine-King',
       url: 'http://music.163.com/song/media/outer/url?id=2120637597.mp3',
