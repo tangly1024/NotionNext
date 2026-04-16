d7fba5b56907def8359968cdba94ca223afbf98f
dxs1256 <dxs1256@users.noreply.github.com>
2026-04-16 10:16:20 +0000

    feat: 移除文章 URL 前缀 /article/
    - POST_URL_PREFIX 默认值改为空字符串
    - 文章 URL 将从 /article/xxx 变为 /xxx

--- conf/post.config.js
+++ conf/post.config.js
@@ -3,7 +3,7 @@
  */
 module.exports = {
   // 文章URL前缀
-  POST_URL_PREFIX: process.env.NEXT_PUBLIC_POST_URL_PREFIX ?? 'article',
+  POST_URL_PREFIX: process.env.NEXT_PUBLIC_POST_URL_PREFIX ?? '',
   // POST类型文章的默认路径前缀，例如默认POST类型的路径是  /article/[slug]
   // 如果此项配置为 '' 空， 则文章将没有前缀路径
   // 支援類似 WP 可自訂文章連結格式的功能：https://wordpress.org/documentation/article/customize-permalinks/，目前只先實作 %year%/%month%/%day%
