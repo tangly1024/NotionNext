42d36943428ffaca85eb6e8116e533e70e0b600d
ProcessMonitor <getperfectjob@yeah.net>
2026-04-21 15:56:15 +0800

    Update config.js

--- themes/proxio/config.js
+++ themes/proxio/config.js
@@ -3,7 +3,7 @@
  */
 const CONFIG = {
   PROXIO_WELCOME_COVER_ENABLE: true, //是否显示页面进入的欢迎文字
-  PROXIO_WELCOME_TEXT: '欢迎来到此网站，点击任意位置进入', // 欢迎文字，留空则不启用
+  PROXIO_WELCOME_TEXT: 'Wincy 的个人展，点击拉开帷幕', // 欢迎文字，留空则不启用
 
   // 英雄区块导航
   PROXIO_HERO_ENABLE: true, // 开启英雄区
