dfda4458a5819d6f3489815f0ce3622dc840b93b
tlyong1992 <573865178@qq.com>
2026-05-01 18:37:50 +0800

    refactor(config): move collapse threshold into code config
    Centralize CODE_COLLAPSE_MIN_LINES in conf/code.config.js so code-related settings stay together and easier to maintain.

--- blog.config.js
+++ blog.config.js
@@ -24,9 +24,6 @@ const BLOG = {
   BEI_AN_LINK: process.env.NEXT_PUBLIC_BEI_AN_LINK || 'https://beian.miit.gov.cn/', // 备案查询链接，如果用了萌备等备案请在这里填写
   BEI_AN_GONGAN: process.env.NEXT_PUBLIC_BEI_AN_GONGAN || '', // 公安备案号，例如 '浙公网安备3xxxxxxxx8号'
 
-  // 只有超过该行数的代码块才显示折叠条，默认 20
-  CODE_COLLAPSE_MIN_LINES: 20,
-  
   // RSS订阅
   ENABLE_RSS: process.env.NEXT_PUBLIC_ENABLE_RSS || true, // 是否开启RSS订阅功能
 
