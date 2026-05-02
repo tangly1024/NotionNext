57bcf73a950800e69c8c7a47d40ef1793168576c
qixing-jk <street-anime-olive@duck.com>
2026-05-02 23:57:10 +0800

    feat: Notion 文章自定义版权声明（多主题）
    * feat: support custom copyright content from Notion
    
    - Add post prop to ArticleCopyright and PostCopyright components
    - Update copyright display to use post.copyright or fallback to default notice
    - Modify ArticleDetail to pass props to ArticleCopyright component
    - Implement changes across multiple theme components (next, hexo, matery, heo)
    
    closes #2428
    
    * refactor: remove unused ArticleCopyright component in commerce themes
    
    ---------
    
    Co-authored-by: tangly1024 <mail@tangly1024.com>

--- themes/hexo/components/ArticleCopyright.js
+++ themes/hexo/components/ArticleCopyright.js
@@ -6,7 +6,7 @@ import CONFIG from '../config'
 import { siteConfig } from '@/lib/config'
 import NotByAI from '@/components/NotByAI'
 
-export default function ArticleCopyright() {
+export default function ArticleCopyright({ post }) {
   const router = useRouter()
   const [path, setPath] = useState(siteConfig('LINK') + router.asPath)
   useEffect(() => {
@@ -38,7 +38,7 @@ export default function ArticleCopyright() {
         </li>
         <li>
           <strong className='mr-2'>{locale.COMMON.COPYRIGHT}:</strong>
-          {locale.COMMON.COPYRIGHT_NOTICE}
+          {post.copyright || locale.COMMON.COPYRIGHT_NOTICE}
         </li>
         {siteConfig('HEXO_ARTICLE_NOT_BY_AI', false, CONFIG) && (
           <li>
